7c265f162817aa89e7f100e86d76d19fb41c5ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:05 +0000

    Add package awagpt/0.0.111/awagpt-0.0.111.tar.gz

---
+++ packages/awagpt/awagpt-0.0.111.tar.gz/awagpt-0.0.111/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="awagpt",
+    version="0.0.111",
+    description="AwaGPT is a powerful Python library designed by Awarri Technologies for developers to interact with advanced localalized language and audio models that have been trained with local Nigerian data.",
+    packages=find_packages(),  # Automatically discover the `awagpt` package
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Awarri/awagpt",
+    author="Awarri",
+    author_email="moses@awarri.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["requests >= 2.32.3"],
+    python_requires=">=3.8.10",
+)
