25d32d7fffcac5d74676a047f7b6a46fa5962f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:42 +0000

    Add package dekvenv/0.1.43/dekvenv-0.1.43-py3-none-any.whl

---
+++ packages/dekvenv/dekvenv-0.1.43-py3-none-any.whl/dekvenv/command.py
@@ -0,0 +1,10 @@
+from .main.command import app
+from .pdm import command as pdm_command
+from .pipconf import command as pipconf_command
+from .pypirc import command as pypirc_command
+from .script import command as script_command
+
+app.add_typer(pdm_command.app, name='pdm')
+app.add_typer(pipconf_command.app, name='pipconf')
+app.add_typer(pypirc_command.app, name='pypirc')
+app.add_typer(script_command.app, name='script')
