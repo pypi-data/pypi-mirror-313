6183ccfcbaff79c2a50d9b70aaa6b51eb734b8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:29 +0000

    Add package udata/10.0.5.dev32777/udata-10.0.5.dev32777-py2.py3-none-any.whl

---
+++ packages/udata/udata-10.0.5.dev32777-py2.py3-none-any.whl/udata/core/organization/metrics.py
@@ -0,0 +1,28 @@
+from udata.core.owned import Owned
+from udata.models import Dataset, Organization, Reuse
+
+
+@Dataset.on_create.connect
+@Dataset.on_update.connect
+@Dataset.on_delete.connect
+def update_datasets_metrics(document, **kwargs):
+    if document.organization:
+        document.organization.count_datasets()
+
+
+@Reuse.on_create.connect
+@Reuse.on_update.connect
+@Reuse.on_delete.connect
+def update_reuses_metrics(document, **kwargs):
+    if document.organization:
+        document.organization.count_reuses()
+
+
+@Owned.on_owner_change.connect
+def update_org_metrics(document, previous):
+    if not isinstance(previous, Organization):
+        return
+    if isinstance(document, Dataset):
+        previous.count_datasets()
+    elif isinstance(document, Reuse):
+        previous.count_reuses()
