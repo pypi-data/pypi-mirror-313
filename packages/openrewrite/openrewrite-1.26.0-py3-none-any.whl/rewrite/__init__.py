d8910d63772f5716e514d60c6d6e741d705864a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:02 +0000

    Add package openrewrite/1.26.0/openrewrite-1.26.0-py3-none-any.whl

---
+++ packages/openrewrite/openrewrite-1.26.0-py3-none-any.whl/rewrite/__init__.py
@@ -0,0 +1,45 @@
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+# helps pytest to rewrite the assert statements in test.py
+import pytest
+pytest.register_assert_rewrite("rewrite.test")
+
+from .execution import ExecutionContext, DelegatingExecutionContext, InMemoryExecutionContext, Recipe, RecipeRunException
+from .markers import *
+from .tree import Checksum, FileAttributes, SourceFile, Tree, PrintOutputCapture, PrinterFactory
+from .utils import random_id
+from .visitor import Cursor, TreeVisitor
+from .parser import *
+from .result import *
+from .style import *
+
+__all__ = [
+    'Checksum',
+    'FileAttributes',
+    'SourceFile',
+    'Tree',
+    'PrintOutputCapture',
+    'PrinterFactory',
+    'random_id',
+    'Cursor',
+    'TreeVisitor',
+    'ExecutionContext',
+    'DelegatingExecutionContext',
+    'InMemoryExecutionContext',
+    'Marker',
+    'Markers',
+    'Recipe',
+    'RecipeRunException',
+    'Parser',
+    'ParserInput',
+    'ParseError',
+    'ParseErrorVisitor',
+    'ParserBuilder',
+    'ParseExceptionResult',
+    'Result',
+    'SourceFile',
+
+    # Style
+    'Style',
+    'NamedStyles',
+]
