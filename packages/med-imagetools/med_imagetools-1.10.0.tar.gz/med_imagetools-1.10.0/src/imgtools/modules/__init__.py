dde6958311d0eabc78870090d7b73dbc00adab99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:31 +0000

    Add package med-imagetools/1.10.0/med_imagetools-1.10.0.tar.gz

---
+++ packages/med-imagetools/med_imagetools-1.10.0.tar.gz/med_imagetools-1.10.0/src/imgtools/modules/__init__.py
@@ -0,0 +1,7 @@
+from .segmentation import *
+from .structureset import *
+from .pet import *
+from .dose import *
+from .datagraph import *
+from .sparsemask import *
+from .scan import *
---
+++ packages/med-imagetools/med_imagetools-1.10.0.tar.gz/med_imagetools-1.10.0/src/imgtools/modules/scan.py
@@ -0,0 +1,10 @@
+import SimpleITK as sitk
+from typing import Dict, TypeVar
+
+T = TypeVar('T')
+
+
+class Scan:
+    def __init__(self, image: sitk.Image, metadata: Dict[str, T]):
+        self.image = image
+        self.metadata = metadata
