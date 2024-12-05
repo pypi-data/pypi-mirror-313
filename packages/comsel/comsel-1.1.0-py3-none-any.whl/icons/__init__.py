c0df72298228ef88eb8ff7c3f2ddbcf6e82e2199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:43 +0000

    Add package comsel/1.1.0/comsel-1.1.0-py3-none-any.whl

---
+++ packages/comsel/comsel-1.1.0-py3-none-any.whl/icons/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+# Получаем путь к текущей папке icons
+ICON_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Указываем путь к конкретным файлам
+ICON_PATH = os.path.join(ICON_DIR, "icon.ico")
+
+# Если требуется, можно добавить больше иконок
