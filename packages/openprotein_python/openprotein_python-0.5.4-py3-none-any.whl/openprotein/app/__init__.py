23d8da6c91b4f98ab2c4addd037502ad56a63d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:53 +0000

    Add package openprotein_python/0.5.4/openprotein_python-0.5.4-py3-none-any.whl

---
+++ packages/openprotein_python/openprotein_python-0.5.4-py3-none-any.whl/openprotein/app/__init__.py
@@ -0,0 +1,20 @@
+"""
+OpenProtein APIs.
+
+A collection of the available APIs available on the pythonic interface.
+isort:skip_file
+"""
+
+from .services import (
+    JobsAPI,
+    AssayDataAPI,
+    TrainingAPI,
+    DesignAPI,
+    AlignAPI,
+    EmbeddingsAPI,
+    FoldAPI,
+    SVDAPI,
+    UMAPAPI,
+    PredictorAPI,
+    DesignerAPI,
+)
