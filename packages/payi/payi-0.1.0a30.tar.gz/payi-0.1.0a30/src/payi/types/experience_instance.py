fe335c6d6932e8b70875ea915e37f9afde75d44f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:49 +0000

    Add package payi/0.1.0a30/payi-0.1.0a30.tar.gz

---
+++ packages/payi/payi-0.1.0a30.tar.gz/payi-0.1.0a30/src/payi/types/experience_instance.py
@@ -0,0 +1,12 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+
+from .._models import BaseModel
+
+__all__ = ["ExperienceInstance"]
+
+
+class ExperienceInstance(BaseModel):
+    experience_id: str
+
+    request_id: str
