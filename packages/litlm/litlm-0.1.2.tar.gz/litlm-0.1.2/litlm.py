8a0231f0e90569d7d1f706321dc54d53c2d016d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:42 +0000

    Add package litlm/0.1.2/litlm-0.1.2.tar.gz

---
+++ packages/litlm/litlm-0.1.2.tar.gz/litlm-0.1.2/litlm.py
@@ -0,0 +1,28 @@
+import time
+import requests
+from litellm import completion
+
+OPENROUTER_MODELS = requests.get("https://openrouter.ai/api/v1/models").json()
+OPENROUTER_MODELS = list(pd.DataFrame(OPENROUTER_MODELS['data']).id)
+
+def complete(prompt,m,max_tokens=64, silent_exception=False, free_sleep=3, **kwargs):
+    m=sorted([x for x in OPENROUTER_MODELS if m in x], key=len)[0]
+    provider= f"openrouter/{m}" if "ollama" not in m else m
+    if ":free" in m:
+        time.sleep(free_sleep)
+    try:
+        response = completion(
+            provider,
+            [{ "role": "user", "content": prompt,
+             }],
+            max_tokens=max_tokens, **kwargs
+        )
+        return response.choices[0].message.content.strip()
+    except Exception as e:
+        if not silent_exception:
+            print("❗",e)
+        return ""
+
+def complete_m(m,**kwargs):
+    return lambda x:complete(x,m,**kwargs)
+
---
+++ packages/litlm/litlm-0.1.2.tar.gz/litlm-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="litlm",
+    version="0.1.2",
+    description="Minimalist litellm wrapper for simpler requests and better openrouter support",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Damien Sileo",
+    url="https://github.com/sileod/litlm",
+    py_modules=["litlm"],
+    install_requires=[
+        "litellm",
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
