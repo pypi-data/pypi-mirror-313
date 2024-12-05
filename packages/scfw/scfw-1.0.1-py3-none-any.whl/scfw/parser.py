e5040cf44270f85831923e2a009cd6a3ac3120fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:02 +0000

    Add package scfw/1.0.1/scfw-1.0.1-py3-none-any.whl

---
+++ packages/scfw/scfw-1.0.1-py3-none-any.whl/scfw/parser.py
@@ -0,0 +1,29 @@
+"""
+A drop-in replacement for `argparse.ArgumentParser`.
+"""
+
+import argparse
+
+
+class ArgumentError(Exception):
+    """
+    An exception for `ArgumentParser` to raise.
+    """
+    pass
+
+
+class ArgumentParser(argparse.ArgumentParser):
+    """
+    A drop-in replacement for `argparse.ArgumentParser` with a patched
+    implementation of the latter's `exit_on_error` behavior.
+
+    See https://github.com/python/cpython/issues/103498 for more info.
+    """
+    def error(self, message):
+        """
+        Handle a parsing error.
+
+        Args:
+            message: The error message.
+        """
+        raise ArgumentError(message)
