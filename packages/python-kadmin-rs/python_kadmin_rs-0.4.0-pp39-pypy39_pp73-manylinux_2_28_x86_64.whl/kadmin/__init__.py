380bfe9eaea3ff52d1cbf0f0173b9b5a6cd9fcc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:52 +0000

    Add package python-kadmin-rs/0.4.0/python_kadmin_rs-0.4.0-pp39-pypy39_pp73-manylinux_2_28_x86_64.whl

---
+++ packages/python-kadmin-rs/python_kadmin_rs-0.4.0-pp39-pypy39_pp73-manylinux_2_28_x86_64.whl/kadmin/__init__.py
@@ -0,0 +1,21 @@
+from kadmin._lib import (
+    DbArgs,
+    KAdmin,
+    Params,
+    Policy,
+    Principal,
+    TlData,
+    TlDataEntry,
+    __version__,
+)
+
+__all__ = (
+    "DbArgs",
+    "KAdmin",
+    "Params",
+    "Policy",
+    "Principal",
+    "TlData",
+    "TlDataEntry",
+    "__version__",
+)
