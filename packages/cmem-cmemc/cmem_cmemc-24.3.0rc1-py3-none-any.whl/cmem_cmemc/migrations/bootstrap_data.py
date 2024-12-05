f3ce6d54b6e1e9daa4624f5af165f778fd8d8631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:50 +0000

    Add package cmem-cmemc/24.3.0rc1/cmem_cmemc-24.3.0rc1-py3-none-any.whl

---
+++ packages/cmem-cmemc/cmem_cmemc-24.3.0rc1-py3-none-any.whl/cmem_cmemc/migrations/bootstrap_data.py
@@ -0,0 +1,30 @@
+"""Bootstrap Data migration recipe"""
+
+from typing import ClassVar
+
+from cmem.cmempy.dp.admin import import_bootstrap_data
+from cmem.cmempy.health import get_complete_status_info
+
+from cmem_cmemc.migrations.abc import MigrationRecipe, components
+
+
+class MigrateBootstrapData(MigrationRecipe):
+    """Update Bootstrap Data to current version"""
+
+    id = "bootstrap-data"
+    description = "Migrate bootstrap system data (same as admin store bootstrap --import)"
+    component: components = "explore"
+    first_version = "20.12"
+    tags: ClassVar[list[str]] = ["system", "shapes"]
+
+    def is_applicable(self) -> bool:
+        """Test if the recipe can be applied."""
+        status_info = get_complete_status_info()
+        return status_info["shapes"]["version"] not in (
+            status_info["explore"]["version"],
+            "UNKNOWN",
+        )
+
+    def apply(self) -> None:
+        """Apply the recipe to the current version."""
+        import_bootstrap_data()
