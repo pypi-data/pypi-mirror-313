0458d21c6ce86758c9710162bbddb7da926a6f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:04:09 +0000

    Add package appfl/1.1.0/appfl-1.1.0-py3-none-any.whl

---
+++ packages/appfl/appfl-1.1.0-py3-none-any.whl/appfl/comm/globus_compute/__init__.py
@@ -0,0 +1,3 @@
+from .globus_compute_server_communicator import GlobusComputeServerCommunicator
+from .globus_compute_client_communicator import globus_compute_client_entry_point
+from ..globus_compute_legacy import *
