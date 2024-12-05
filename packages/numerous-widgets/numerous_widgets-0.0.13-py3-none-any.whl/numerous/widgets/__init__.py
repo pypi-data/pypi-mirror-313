579bd6b2c5daad2d38a5e0cccf62e9d2e72f3b48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:54 +0000

    Add package numerous-widgets/0.0.13/numerous_widgets-0.0.13-py3-none-any.whl

---
+++ packages/numerous-widgets/numerous_widgets-0.0.13-py3-none-any.whl/numerous/widgets/__init__.py
@@ -0,0 +1,29 @@
+import importlib.metadata
+#from .project_widget import ProjectsMenuWidget
+#from .scenario_input_widget import ScenarioInputWidget
+try:
+    __version__ = importlib.metadata.version("widget")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
+
+from .base.button import Button
+from .base.drop_down import DropDown
+from .base.number import Number
+from .base.tabs import Tabs, render_tab_content
+from .base.checkbox import CheckBox
+from .base.map_selector import MapSelector
+from .base.card import card
+from .base.container import container
+from .base.progress_bar import ProgressBar
+from .base.markdown_drawer import MarkdownDrawer
+from .base.task import Task
+from .base.timer import Timer
+
+from .task.process_task import process_task_control, ProcessTask, SubprocessTask
+
+try:
+    import numerous
+    from .numerous.project import ProjectsMenu
+except ImportError:
+    pass
+
