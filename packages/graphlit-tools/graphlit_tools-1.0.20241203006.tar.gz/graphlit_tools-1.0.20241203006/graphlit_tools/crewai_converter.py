d75f63155d67dbc611ffd40e4b05bb4cc1755d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:33 +0000

    Add package graphlit-tools/1.0.20241203006/graphlit_tools-1.0.20241203006.tar.gz

---
+++ packages/graphlit-tools/graphlit_tools-1.0.20241203006.tar.gz/graphlit_tools-1.0.20241203006/graphlit_tools/crewai_converter.py
@@ -0,0 +1,46 @@
+from typing import Any, cast
+
+from crewai_tools.tools.base_tool import BaseTool as CrewAIBaseTool
+
+from .base_tool import BaseTool
+
+class CrewAIConverter(CrewAIBaseTool):
+    """Tool to convert Graphlit tools into CrewAI tools."""
+
+    graphlit_tool: Any
+
+    def _run(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Any:
+        tool = cast(BaseTool, self.graphlit_tool)
+
+        return tool.run(self, args, kwargs)
+
+    async def _arun(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Any:
+        tool = cast(BaseTool, self.graphlit_tool)
+
+        return await tool.arun(self, args, kwargs)
+
+    @classmethod
+    def from_tool(cls, tool: Any, **kwargs: Any) -> "BaseTool":
+        if not isinstance(tool, BaseTool):
+            raise ValueError(f"Expected a Graphlit tool, got {type(tool)}")
+
+        tool = cast(BaseTool, tool)
+
+        if tool.args_schema is None:
+            raise ValueError("Invalid arguments JSON schema.")
+
+        return cls(
+            name=tool.name,
+            description=tool.description,
+            args_schema=tool.args_schema,
+            graphlit_tool=tool,
+            **kwargs,
+        )
