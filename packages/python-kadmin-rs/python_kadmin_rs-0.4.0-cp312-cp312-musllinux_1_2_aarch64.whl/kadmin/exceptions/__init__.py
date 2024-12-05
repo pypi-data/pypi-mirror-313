3a568e6eaa9e81b31f34ffc5a53904926fe31bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:48 +0000

    Add package python-kadmin-rs/0.4.0/python_kadmin_rs-0.4.0-cp312-cp312-musllinux_1_2_aarch64.whl

---
+++ packages/python-kadmin-rs/python_kadmin_rs-0.4.0-cp312-cp312-musllinux_1_2_aarch64.whl/kadmin/exceptions/__init__.py
@@ -0,0 +1,29 @@
+from kadmin._lib import exceptions
+
+PyKAdminException = exceptions.PyKAdminException
+KAdminException = exceptions.KAdminException
+KerberosException = exceptions.KerberosException
+NullPointerDereference = exceptions.NullPointerDereference
+CStringConversion = exceptions.CStringConversion
+CStringImportFromVec = exceptions.CStringImportFromVec
+StringConversion = exceptions.StringConversion
+ThreadSendError = exceptions.ThreadSendError
+ThreadRecvError = exceptions.ThreadRecvError
+TimestampConversion = exceptions.TimestampConversion
+DateTimeConversion = exceptions.DateTimeConversion
+DurationConversion = exceptions.DurationConversion
+
+__all__ = (
+    "PyKAdminException",
+    "KAdminException",
+    "KerberosException",
+    "NullPointerDereference",
+    "CStringConversion",
+    "CStringImportFromVec",
+    "StringConversion",
+    "ThreadSendError",
+    "ThreadRecvError",
+    "TimestampConversion",
+    "DateTimeConversion",
+    "DurationConversion",
+)
