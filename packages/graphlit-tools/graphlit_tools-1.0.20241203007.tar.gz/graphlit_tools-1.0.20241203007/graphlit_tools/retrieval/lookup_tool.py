c53ca546036c46e7cdbf97c2bcbb3a7951a5a9e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:53 +0000

    Add package graphlit-tools/1.0.20241203007/graphlit_tools-1.0.20241203007.tar.gz

---
+++ packages/graphlit-tools/graphlit_tools-1.0.20241203007.tar.gz/graphlit_tools-1.0.20241203007/graphlit_tools/retrieval/lookup_tool.py
@@ -0,0 +1,71 @@
+import asyncio
+import logging
+from typing import Optional, Type
+
+from graphlit import Graphlit
+from graphlit_api import exceptions
+from pydantic import BaseModel, Field
+
+from ..base_tool import BaseTool
+from ..exceptions import ToolException
+from . import helpers
+
+logger = logging.getLogger(__name__)
+
+class LookupInput(BaseModel):
+    content_id: str = Field(description="ID of content which has been ingested into knowledge base")
+
+class LookupTool(BaseTool):
+    name: str = "Graphlit content lookup tool"
+    description: str = """Retrieves content by ID from knowledge base.
+    Returns extracted Markdown text and metadata from content."""
+    args_schema: Type[BaseModel] = LookupInput
+
+    graphlit: Graphlit = Field(None, exclude=True)
+
+    model_config = {
+        "arbitrary_types_allowed": True
+    }
+
+    def __init__(self, graphlit: Optional[Graphlit] = None, **kwargs):
+        """
+        Initializes the LookupTool.
+
+        Args:
+            graphlit (Optional[Graphlit]): An optional Graphlit instance to interact with the Graphlit API.
+                If not provided, a new Graphlit instance will be created.
+            **kwargs: Additional keyword arguments for the BaseTool superclass.
+        """
+        super().__init__(**kwargs)
+        self.graphlit = graphlit or Graphlit()
+
+    async def _arun(self, content_id: str) -> Optional[str]:
+        try:
+            response = await self.graphlit.client.get_content(
+                id=content_id
+            )
+
+            if response.content is None:
+                return None
+
+            print(f'LookupTool: Retrieved content by ID [{content_id}].')
+
+            results = helpers.format_content(response.content)
+
+            text = "\n".join(results)
+
+            return text
+        except exceptions.GraphQLClientError as e:
+            logger.error(str(e))
+            raise ToolException(str(e)) from e
+
+    def _run(self, content_id: str) -> Optional[str]:
+        try:
+            loop = asyncio.get_event_loop()
+            if loop.is_running():
+                future = asyncio.ensure_future(self._arun(content_id))
+                return loop.run_until_complete(future)
+            else:
+                return loop.run_until_complete(self._arun(content_id))
+        except RuntimeError:
+            return asyncio.run(self._arun(content_id))
