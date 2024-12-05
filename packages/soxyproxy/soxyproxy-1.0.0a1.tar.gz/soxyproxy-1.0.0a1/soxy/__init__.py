6ff43417f971123a6976c0510b689c033e1d3b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:10 +0000

    Add package soxyproxy/1.0.0a1/soxyproxy-1.0.0a1.tar.gz

---
+++ packages/soxyproxy/soxyproxy-1.0.0a1.tar.gz/soxyproxy-1.0.0a1/soxy/__init__.py
@@ -0,0 +1,41 @@
+from soxy._config import Config
+from soxy._errors import (
+    AuthorizationError,
+    PackageError,
+    ProtocolError,
+    RejectError,
+    ResolveDomainError,
+)
+from soxy._proxy import Proxy
+from soxy._ruleset import Rule, Ruleset
+from soxy._socks import Socks4, Socks5
+from soxy._tcp import TcpTransport
+from soxy._types import (
+    Address,
+    Connection,
+    Resolver,
+)
+
+__title__ = 'winregistry'
+__version__ = '1.0.0a1'
+__url__ = 'https://github.com/shpaker/soxyproxy'
+__author__ = 'Aleksandr Shpak'
+__author_email__ = 'shpaker@gmail.com'
+__license__ = 'MIT'
+__all__ = [
+    'Address',
+    'AuthorizationError',
+    'Config',
+    'Connection',
+    'PackageError',
+    'ProtocolError',
+    'Proxy',
+    'RejectError',
+    'ResolveDomainError',
+    'Resolver',
+    'Rule',
+    'Ruleset',
+    'Socks4',
+    'Socks5',
+    'TcpTransport',
+]
