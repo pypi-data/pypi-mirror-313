8d2162b3540c7cb1297c311ca0bde95379d72f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:08 +0000

    Add package zomboid-rcon/1.1.0/zomboid_rcon-1.1.0-py3-none-any.whl

---
+++ packages/zomboid-rcon/zomboid_rcon-1.1.0-py3-none-any.whl/zomboid_rcon/__init__.py
@@ -0,0 +1,11 @@
+"""
+Zomboid RCON: https://github.com/jmwhitworth/zomboid_rcon
+    :copyright: (c) 2025 by JW: https://jackwhitworth.com
+    :license: GPL-3.0, see LICENSE for more details.
+"""
+
+
+from .zomboid_rcon  import ZomboidRCON
+
+__title__ = 'zomboid_rcon'
+__version__ = '1.1.0'
