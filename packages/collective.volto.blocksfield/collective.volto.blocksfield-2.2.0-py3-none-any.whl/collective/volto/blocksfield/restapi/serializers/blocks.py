8d222d8aeb7f269b402d3faf9d402f7eb575ea9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:01 +0000

    Add package collective.volto.blocksfield/2.2.0/collective.volto.blocksfield-2.2.0-py3-none-any.whl

---
+++ packages/collective.volto.blocksfield/collective.volto.blocksfield-2.2.0-py3-none-any.whl/collective/volto/blocksfield/restapi/serializers/blocks.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from plone.dexterity.interfaces import IDexterityContent
+from plone.restapi.interfaces import IBlockFieldSerializationTransformer
+from plone.restapi.serializer.blocks import ResolveUIDSerializerBase
+from plone.restapi.serializer.blocks import SlateBlockSerializerBase
+from plone.restapi.serializer.blocks import SlateTableBlockSerializerBase
+from plone.restapi.serializer.blocks import TextBlockSerializerBase
+from zope.component import adapter
+from zope.interface import implementer
+from zope.publisher.interfaces.browser import IBrowserRequest
+
+
+@implementer(IBlockFieldSerializationTransformer)
+@adapter(IDexterityContent, IBrowserRequest)
+class ResolveUIDSerializer(ResolveUIDSerializerBase):
+    """Base Serializer registered for all content-types"""
+
+
+@implementer(IBlockFieldSerializationTransformer)
+@adapter(IDexterityContent, IBrowserRequest)
+class TextBlockSerializer(TextBlockSerializerBase):
+    """TextBlock Serializer for all content-types"""
+
+
+@implementer(IBlockFieldSerializationTransformer)
+@adapter(IDexterityContent, IBrowserRequest)
+class SlateBlockSerializer(SlateBlockSerializerBase):
+    """TextBlock Serializer for all content-types"""
+
+
+@implementer(IBlockFieldSerializationTransformer)
+@adapter(IDexterityContent, IBrowserRequest)
+class SlateTableBlockSerializer(SlateTableBlockSerializerBase):
+    """TextBlock Serializer for all content-types"""
