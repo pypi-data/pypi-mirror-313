e7ec7319f3e2a551934445317fe2de00ce890212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:14 +0000

    Add package blue-yonder/0.0.7/blue_yonder-0.0.7.tar.gz

---
+++ packages/blue-yonder/blue_yonder-0.0.7.tar.gz/blue_yonder-0.0.7/src/blue_yonder/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Python
+
+"""Copyright (c) Alexander Fedotov.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+"""
+from .client import Client
+
+Butterfly = Client  # playful
+Bird = Client       # aliases
+
+__all__ = [
+    'Client',
+    'Butterfly',
+    'Bird'
+]
