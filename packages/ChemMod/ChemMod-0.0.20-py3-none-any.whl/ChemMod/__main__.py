b94841d48d0b05a58bb2e6cee49290fcad38b978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:11 +0000

    Add package ChemMod/0.0.20/ChemMod-0.0.20-py3-none-any.whl

---
+++ packages/ChemMod/ChemMod-0.0.20-py3-none-any.whl/ChemMod/__main__.py
@@ -0,0 +1,17 @@
+
+# print('''
+#         Hey there, I am Chem.Mod. Your Python Package for Chemistry.
+#         Beneath you see a list of content of the package.
+#         For any additional information please type in .help()
+#         or check out our circle community "!INSERT LINK!"
+        
+#         List of content:
+#         \n\tequation
+#         \n\t\t
+#         \n\tplot
+#         \n\t\t  
+#         ''')
+
+
+import numpy as np
+import matplotlib.pyplot as plt
---
+++ packages/ChemMod/ChemMod-0.0.20-py3-none-any.whl/ChemMod/experiment/__init__.py
@@ -0,0 +1,13 @@
+
+# from ChemMod.equation import equation
+# from ChemMod.plot import plot
+# from ChemMod import ChemMod
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+__all__ = [s for s in dir() if not s.startswith("_")]
+
+from ChemMod.experiment import __all__
+# from ChemMod.experiment._experiment import pxrd
+
