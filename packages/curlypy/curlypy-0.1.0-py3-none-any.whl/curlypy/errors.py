606e9b040d255a91b45c9e8f99b7cc6dd1a87704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:43 +0000

    Add package curlypy/0.1.0/curlypy-0.1.0-py3-none-any.whl

---
+++ packages/curlypy/curlypy-0.1.0-py3-none-any.whl/curlypy/errors.py
@@ -0,0 +1,13 @@
+
+
+class CurlyPyTranslatorError(Exception):
+    """
+    Base class for CurlyPy translator errors.
+    """
+    pass
+
+class CurlyPySyntaxError(CurlyPyTranslatorError):
+    """
+    Raised when unmatched brackets are found in the CurlyPy code.
+    """
+    pass
