b4944e137f0c2e9ffde7bea297d5a3f2ebad2102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:54 +0000

    Add package thestage/0.5.31/thestage-0.5.31.tar.gz

---
+++ packages/thestage/thestage-0.5.31.tar.gz/thestage-0.5.31/thestage/services/clients/thestage_api/dtos/logging_controller/log_polling_request.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+from pydantic import Field, ConfigDict, BaseModel
+
+
+class LogPollingRequest(BaseModel):
+    model_config = ConfigDict(use_enum_values=True)
+
+    taskId: int = Field(None, alias='taskId')
+    lastLogId: Optional[str] = Field(None, alias='lastLogId')
+    dockerContainerId: Optional[str] = Field(None, alias='dockerContainerId')
+    lastLogTimestamp: Optional[str] = Field(None, alias='lastLogTimestamp')
