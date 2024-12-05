e4e501386481ec1cef2e83c2a38e458b53d840cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:16 +0000

    Add package hatch-polylith-bricks/1.3.0/hatch_polylith_bricks-1.3.0.tar.gz

---
+++ packages/hatch-polylith-bricks/hatch_polylith_bricks-1.3.0.tar.gz/hatch_polylith_bricks-1.3.0/hatch_polylith_bricks/polylith/hatch/core.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from typing import Union
+from hatch_polylith_bricks.polylith import toml
+
+def get_work_dir(config: dict) -> Path:
+    work_dir = config.get('work-dir', '.polylith_tmp')
+    return Path(work_dir)
+
+def get_top_namespace(pyproject: dict, config: dict) -> Union[str, None]:
+    top_ns = toml.get_custom_top_namespace_from_polylith_section(pyproject)
+    return top_ns or config.get('top-namespace')
