4a9b4181b37d8bd371c92e3e607cc611059edc69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:33 +0000

    Add package dbt-analyticdb/0.0.2/dbt_analyticdb-0.0.2.tar.gz

---
+++ packages/dbt-analyticdb/dbt_analyticdb-0.0.2.tar.gz/dbt_analyticdb-0.0.2/setup.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+import os
+import sys
+import re
+
+# require python 3.7 or newer
+if sys.version_info < (3, 7):
+    print("Error: dbt does not support this version of Python.")
+    print("Please upgrade to Python 3.7 or higher.")
+    sys.exit(1)
+
+
+# require version of setuptools that supports find_namespace_packages
+from setuptools import setup
+
+try:
+    from setuptools import find_namespace_packages
+except ImportError:
+    # the user has a downlevel version of setuptools.
+    print("Error: dbt requires setuptools v40.1.0 or higher.")
+    print('Please upgrade setuptools with "pip install --upgrade setuptools" ' "and try again")
+    sys.exit(1)
+
+
+# pull long description from README
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+
+# get this package's version from dbt/adapters/<name>/__version__.py
+def _get_plugin_version_dict():
+    _version_path = os.path.join(this_directory, "dbt", "adapters", "analyticdb", "__version__.py")
+    _semver = r"""(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"""
+    _pre = r"""((?P<prekind>a|b|rc)(?P<pre>\d+))?"""
+    _nightly = r"""(\.(?P<nightly>[a-z0-9]+)?)?"""
+    _version_pattern = rf"""version\s*=\s*["']{_semver}{_pre}{_nightly}["']"""
+    with open(_version_path) as f:
+        match = re.search(_version_pattern, f.read().strip())
+        if match is None:
+            raise ValueError(f"invalid version at {_version_path}")
+        return match.groupdict()
+
+
+# require a compatible minor version (~=), prerelease if this is a prerelease
+def _get_dbt_core_version():
+    parts = _get_plugin_version_dict()
+    minor = "{major}.{minor}.0".format(**parts)
+    pre = parts["prekind"] + "1" if parts["prekind"] else ""
+    return f"{minor}{pre}"
+
+
+package_name = "dbt-analyticdb"
+package_version = "0.0.2"
+dbt_core_version = _get_dbt_core_version()
+description = """The analyticdb adapter plugin for dbt"""
+
+setup(
+    name=package_name,
+    version=package_version,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Doug Beatty",
+    author_email="doug.beatty@gmail.com",
+    url="https://github.com/dbeatty10/dbt-mysql",
+    packages=find_namespace_packages(include=["dbt", "dbt.*"]),
+    include_package_data=True,
+    install_requires=[
+        "dbt-core~={}".format(dbt_core_version),
+        "mysql-connector-python>=8.0.0",
+    ],
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.7",
+)
