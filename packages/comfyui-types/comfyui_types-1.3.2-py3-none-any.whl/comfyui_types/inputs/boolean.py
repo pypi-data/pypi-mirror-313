a04b2a065eb541e2a6eaf02e2040ff441cbb83c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:11 +0000

    Add package comfyui-types/1.3.2/comfyui_types-1.3.2-py3-none-any.whl

---
+++ packages/comfyui-types/comfyui_types-1.3.2-py3-none-any.whl/comfyui_types/inputs/boolean.py
@@ -0,0 +1,43 @@
+"""ComfyUI boolean input."""
+
+from .base import InputBase, InputTypeReturnType
+
+
+class BooleanInput(InputBase):
+    """ComfyUI boolean input."""
+
+    default: bool = False
+    lazy: bool = True
+    label_on: str = 'On'
+    label_off: str = 'Off'
+
+    def __init__(
+        self,
+        *,
+        required: bool = True,
+        hidden: bool | None = None,
+        default: bool = False,
+        label_on: str = 'true',
+        label_off: str = 'false',
+        display_name: str | None = None,
+    ) -> None:
+        """Initialize BooleanInput."""
+        super().__init__(
+            required=required, hidden=hidden, display_name=display_name
+        )
+
+        self.default = default
+        self.label_on = label_on
+        self.label_off = label_off
+
+    def get_input_type(self) -> InputTypeReturnType:
+        """Return input type."""
+        return (
+            'BOOLEAN',
+            {
+                'default': self.default,
+                'lazy': self.lazy,
+                'label_on': self.label_on,
+                'label_off': self.label_off,
+            },
+        )
