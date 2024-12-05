d49d458f56bfeac606c954e2b8f70d5cf8a08348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:48 +0000

    Add package pysequoia/0.1.25/pysequoia-0.1.25.tar.gz

---
+++ packages/pysequoia/pysequoia-0.1.25.tar.gz/pysequoia-0.1.25/NEXT.md
@@ -0,0 +1,12 @@
+# Next version changes
+## This file contains changes that will be included in the next version that is released
+v0.1.25
+
+New:
+
+Changed:
+
+Removed:
+  - `Card` - OpenPGP Card features removed as they are complex to build for multiple architectures and `openpgp-card-sequoia` is deprecated.
+
+### git tag --edit -s -F NEXT.md v...
---
+++ packages/pysequoia/pysequoia-0.1.25.tar.gz/pysequoia-0.1.25/src/sign.rs
@@ -0,0 +1,26 @@
+use std::borrow::Cow;
+use std::io::Write;
+
+use openpgp::serialize::stream::Armorer;
+use openpgp::serialize::stream::{LiteralWriter, Message};
+use pyo3::prelude::*;
+use sequoia_openpgp as openpgp;
+
+use crate::signer::PySigner;
+
+#[pyfunction]
+pub fn sign(signer: PySigner, bytes: &[u8]) -> PyResult<Cow<'static, [u8]>> {
+    use openpgp::serialize::stream::Signer;
+
+    let mut sink = vec![];
+    {
+        let message = Message::new(&mut sink);
+        let message = Armorer::new(message).build()?;
+        let message = Signer::new(message, signer).build()?;
+        let mut message = LiteralWriter::new(message).build()?;
+        message.write_all(bytes)?;
+        message.finalize()?;
+    }
+
+    Ok(sink.into())
+}
