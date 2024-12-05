32e1a2919321d031e696b556cf6524ee172fe917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:04:02 +0000

    Add package spicy-vki/1.1.0/spicy_vki-1.1.0.tar.gz

---
+++ packages/spicy-vki/spicy_vki-1.1.0.tar.gz/spicy_vki-1.1.0/spicy_vki/__init__.py
@@ -0,0 +1,7 @@
+from spicy_vki.spicy import Spicy
+
+from .utils._basis_collocation import *
+from .utils._basis_Harmonic import *
+from .utils._basis_RBF import *
+from .utils._extnumpy import *
+from .utils._input_checks import *
