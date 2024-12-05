d9d7d07807d3466116062c906a65c0b440d58ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:58 +0000

    Add package helper-decorators/0.1.0/helper_decorators-0.1.0.tar.gz

---
+++ packages/helper-decorators/helper_decorators-0.1.0.tar.gz/helper_decorators-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="helper_decorators",
+    version="0.1.0",
+    author="Kavinda Kobbekaduwe",
+    author_email="kavindakobbekaduwe@gmail.com",  # Replace with your email
+    description="Python decorator functions for regular use.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kavinda-kobbekaduwe/helper_decorators",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
