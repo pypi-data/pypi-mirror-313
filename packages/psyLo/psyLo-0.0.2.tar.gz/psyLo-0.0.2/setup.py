778d4603daa6005abd060bbe090ffe03d708e80e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:09 +0000

    Add package psyLo/0.0.2/psyLo-0.0.2.tar.gz

---
+++ packages/psyLo/psyLo-0.0.2.tar.gz/psyLo-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as file_readme:
+    description = file_readme.read()
+
+setup(
+    name='psyLo',
+    version='0.0.2',
+    packages=find_packages(),
+    install_requires=[],
+    #entry_points={
+        #"console_scripts": [
+        #    "command_name = psyLo:finction_name",
+        #    "testLib = psyLo:testLib"
+        #],
+    #},
+
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
