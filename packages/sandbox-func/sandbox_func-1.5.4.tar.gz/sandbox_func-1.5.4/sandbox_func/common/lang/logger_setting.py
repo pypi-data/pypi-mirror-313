83162b1b18d5798baa685d03ce2ebbbfcaf25f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:09 +0000

    Add package sandbox-func/1.5.4/sandbox_func-1.5.4.tar.gz

---
+++ packages/sandbox-func/sandbox_func-1.5.4.tar.gz/sandbox_func-1.5.4/sandbox_func/common/lang/logger_setting.py
@@ -0,0 +1,24 @@
+import logging
+
+
+def init():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s.%(msecs)03d [%(levelname)s] %(name)s:%(filename)s:%(lineno)s - %(message)s",
+        datefmt='%Y-%m-%dT%H:%M:%S',
+    )
+
+
+def set_logger_formatter(request_id: str):
+    """
+    日志输出增加request_id
+    :param request_id: 请求id
+    :return:
+    """
+    logger = logging.getLogger()
+    handlers = logger.handlers
+    formatter = logging.Formatter(
+        f"%(asctime)s.%(msecs)03d [%(levelname)s] <{request_id}> [][]%(name)s:%(filename)s:%(lineno)s - %(message)s",
+        datefmt='%Y-%m-%dT%H:%M:%S')
+    for handler in handlers:
+        handler.setFormatter(formatter)
