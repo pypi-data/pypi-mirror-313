6fa2b942c66050d3a358c5549ce0b32a5d8a15cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:50 +0000

    Add package xray-combined-api/1.0.1/xray_combined_api-1.0.1.tar.gz

---
+++ packages/xray-combined-api/xray_combined_api-1.0.1.tar.gz/xray_combined_api-1.0.1/openapi_client/api/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.dataset_api import DatasetApi
+from openapi_client.api.external_apps_api import ExternalAppsApi
+from openapi_client.api.import_api import ImportApi
+from openapi_client.api.pre_conditions_api import PreConditionsApi
+from openapi_client.api.project_api import ProjectApi
+from openapi_client.api.requirement_status_api import RequirementStatusApi
+from openapi_client.api.test_execution_tests_api import TestExecutionTestsApi
+from openapi_client.api.test_plan_test_executions_api import TestPlanTestExecutionsApi
+from openapi_client.api.test_plan_tests_api import TestPlanTestsApi
+from openapi_client.api.test_repository_api import TestRepositoryApi
+from openapi_client.api.test_run_api import TestRunApi
+from openapi_client.api.test_run_status_api import TestRunStatusApi
+from openapi_client.api.test_sets_api import TestSetsApi
+from openapi_client.api.test_step_api import TestStepApi
+from openapi_client.api.tests_api import TestsApi
+from openapi_client.api.jira_functions import JiraFunctions
