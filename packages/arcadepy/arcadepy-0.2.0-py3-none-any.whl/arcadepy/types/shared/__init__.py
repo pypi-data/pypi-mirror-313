5e4391e1ee68d629bf7946e84201efe657b06767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:53 +0000

    Add package arcadepy/0.2.0/arcadepy-0.2.0-py3-none-any.whl

---
+++ packages/arcadepy/arcadepy-0.2.0-py3-none-any.whl/arcadepy/types/shared/__init__.py
@@ -0,0 +1,5 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .error import Error as Error
+from .tool_definition import ToolDefinition as ToolDefinition
+from .authorization_response import AuthorizationResponse as AuthorizationResponse
