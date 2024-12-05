3f8aaa5c596bda5a108de345ac89c4f5a10e91fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:41 +0000

    Add package django-cloudinary-helper/0.2.7/django_cloudinary_helper-0.2.7.tar.gz

---
+++ packages/django-cloudinary-helper/django_cloudinary_helper-0.2.7.tar.gz/django_cloudinary_helper-0.2.7/cloudinary_helper/apps.py
@@ -0,0 +1,17 @@
+from django.apps import AppConfig
+from django.conf import settings
+from .utils import setup_cloudinary 
+
+
+class CloudinaryHelperConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cloudinary_helper"
+
+    def ready(self):
+        if not settings.DEBUG:
+            # Automatically configure Cloudinary in production
+            setup_cloudinary(
+                settings.CLOUDINARY_STORAGE['CLOUD_NAME'],
+                settings.CLOUDINARY_STORAGE['API_KEY'],
+                settings.CLOUDINARY_STORAGE['API_SECRET']
+            )
