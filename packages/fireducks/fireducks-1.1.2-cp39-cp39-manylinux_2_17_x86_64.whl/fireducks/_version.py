fe0427cf104df35a49546fdcd44d1f9987e9ba75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:24 +0000

    Add package fireducks/1.1.2/fireducks-1.1.2-cp39-cp39-manylinux_2_17_x86_64.whl

---
+++ packages/fireducks/fireducks-1.1.2-cp39-cp39-manylinux_2_17_x86_64.whl/fireducks/_version.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2023 NEC Corporation. All Rights Reserved.
+
+# _version.py is generated from this file by bazel's genrule with
+# `--workspace_status_command=utils/fireducks-version.sh` bazel command-line
+# option
+#
+# Reference:
+# - https://bazel.build/docs/user-manual?hl=ja#workspace-status
+# - https://github.com/bazelbuild/bazel/issues/4942
+
+
+def get_version():
+    return "1.1.2"
+
+
+def get_git_version():
+    return "569778a231e0e987a054ed4b145fa9a23e365dd4"
+
+
+def get_dfkl_version():
+    return "1.1.2"
