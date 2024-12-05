d00444921775bf592d60e8cc68aeceddb03fc372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:13 +0000

    Add package hatch-polylith-bricks/1.3.0/hatch_polylith_bricks-1.3.0-py3-none-any.whl

---
+++ packages/hatch-polylith-bricks/hatch_polylith_bricks-1.3.0-py3-none-any.whl/hatch_polylith_bricks/polylith/hatch/hooks/bricks.py
@@ -0,0 +1,38 @@
+import shutil
+from pathlib import Path
+from typing import Any, Dict
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+from hatch_polylith_bricks.polylith import parsing, repo, toml
+from hatch_polylith_bricks.polylith.hatch import core
+
+class PolylithBricksHook(BuildHookInterface):
+    PLUGIN_NAME = 'polylith-bricks'
+
+    def initialize(self, version: str, build_data: Dict[str, Any]) -> None:
+        include_key = 'force_include_editable' if version == 'editable' else 'force_include'
+        root = self.root
+        pyproject = Path(f'{root}/{repo.default_toml}')
+        data = toml.read_toml_document(pyproject)
+        bricks = toml.get_project_packages_from_polylith_section(data)
+        found_bricks = {k: v for (k, v) in bricks.items() if Path(f'{root}/{k}').exists()}
+        if not bricks or not found_bricks:
+            return
+        top_ns = core.get_top_namespace(data, self.config)
+        work_dir = core.get_work_dir(self.config)
+        if not top_ns:
+            build_data[include_key] = bricks
+            return
+        ns = parsing.parse_brick_namespace_from_path(bricks)
+        for (source, brick) in bricks.items():
+            path = parsing.copy_brick(source, brick, work_dir)
+            rewritten_bricks = parsing.rewrite_modules(path, ns, top_ns)
+            for item in rewritten_bricks:
+                print(f'Updated {item} with new top namespace for local imports.')
+        key = work_dir.as_posix()
+        build_data[include_key][key] = top_ns
+
+    def finalize(self, *args, **kwargs) -> None:
+        work_dir = core.get_work_dir(self.config)
+        if not work_dir.exists() or not work_dir.is_dir():
+            return
+        shutil.rmtree(work_dir.as_posix())
