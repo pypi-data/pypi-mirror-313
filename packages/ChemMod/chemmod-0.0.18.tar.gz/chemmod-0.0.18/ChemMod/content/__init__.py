0bc218d23c6de2c494a6b0c255adfc5bfa4a8ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:00 +0000

    Add package ChemMod/0.0.18/chemmod-0.0.18.tar.gz

---
+++ packages/ChemMod/chemmod-0.0.18.tar.gz/chemmod-0.0.18/ChemMod/content/__init__.py
@@ -0,0 +1,11 @@
+
+# from ChemMod.equation import equation
+# from ChemMod.plot import plot
+# from ChemMod import ChemMod
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+__all__ = [s for s in dir() if not s.startswith("_")]
+
+from ChemMod.content import __all__
---
+++ packages/ChemMod/chemmod-0.0.18.tar.gz/chemmod-0.0.18/ChemMod/plot/_theme_data.py
@@ -0,0 +1,36 @@
+theme_data = {
+  # [0 border_color, 1 border_width, 2 text_color, 3 data_color, 4 marker_color, 5 markeredge_color, 6 label_color, 7 outside_face_color, 8 indside_face_color, 9 grid_color]
+    
+    "radar": ['mediumspringgreen', 1, 'orange','mediumspringgreen','Oranges','orange','orange','black','black','gray']
+    ,
+    "cyan": ['teal', 1, 'teal', 'teal', 'viridis', 'teal', 'teal', 'paleturquoise', 'lightcyan', 'paleturquoise']
+    ,
+    "knight": ['black', 1, 'red', 'black', 'viridis', 'black', 'black', 'darkgray', 'white', 'gray']
+    ,
+    "nibble": ['mediumspringgreen', 1, 'deeppink', 'mediumspringgreen', 'RdPu', 'deeppink', 'deeppink', 'cornsilk', 'antiquewhite', 'gray']
+    ,
+    "grayscale": ['black', 1, 'black', 'black', 'Greys', 'black', 'black', 'aliceblue', 'Gainsboro', 'gray']
+    ,
+    "depressed": ['white', 1, 'white', 'black', 'binary', 'black', 'black', 'black', 'white', 'gray']
+    ,
+    "ultra_depressed": ['black', 1, 'black', 'black', 'binary', 'black', 'black', 'black', 'black', 'black']
+    ,
+    "color_maniac": ['DarkRed', 1, 'lime', 'yellow', 'hsv', 'yellow', 'navy', 'magenta', 'cyan', 'mediumblue']
+    ,
+    "red": ['darkred', 1, 'darkred', 'darkred', 'Reds', 'darkred', 'darkred', 'lightcoral', 'lavenderblush', 'lightcoral']
+    ,
+    "blue": ['navy', 1, 'navy', 'navy', 'Blues', 'navy', 'navy', 'skyblue', 'lightblue', 'skyblue']
+    ,
+    "yellow": ['darkkhaki', 1, 'darkkhaki', 'darkkhaki', 'YlOrBr', 'darkkhaki', 'darkkhaki', 'yellow', 'lightyellow', 'palegoldenrod']
+    ,
+    "green": ['darkgreen', 1, 'darkgreen', 'darkgreen', 'Greens', 'darkgreen', 'darkgreen', 'lightgreen', 'honeydew', 'lightgreen']
+    ,
+    "orange": ['orangered', 1, 'orangered', 'orangered', 'Oranges', 'orangered', 'orangered', 'orange', 'moccasin', 'orange']
+    ,
+    "purple": ['indigo', 1, 'indigo', 'indigo', 'Purples', 'indigo', 'indigo', 'mediumpurple', 'lavender', 'mediumpurple']
+    ,
+     "pink": ['darkred', 1, 'darkred', 'darkred', 'RdPu', 'darkred', 'darkred', 'deeppink', 'lavenderblush', 'palevioletred']
+    ,
+    "brown": ['maroon', 1, 'maroon', 'maroon', 'copper', 'maroon', 'maroon', 'sandybrown', 'cornsilk', 'chocolate']
+    ,
+    }
