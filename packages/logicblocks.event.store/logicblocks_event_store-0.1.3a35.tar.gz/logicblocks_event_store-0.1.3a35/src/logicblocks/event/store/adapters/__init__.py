7d4921ca0246222a38e7e9e4c841e29f204ae539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:57 +0000

    Add package logicblocks.event.store/0.1.3a35/logicblocks_event_store-0.1.3a35.tar.gz

---
+++ packages/logicblocks.event.store/logicblocks_event_store-0.1.3a35.tar.gz/logicblocks_event_store-0.1.3a35/src/logicblocks/event/store/adapters/__init__.py
@@ -0,0 +1,3 @@
+from .base import StorageAdapter as StorageAdapter
+from .in_memory import InMemoryStorageAdapter as InMemoryStorageAdapter
+from .postgres import PostgresStorageAdapter as PostgresStorageAdapter
