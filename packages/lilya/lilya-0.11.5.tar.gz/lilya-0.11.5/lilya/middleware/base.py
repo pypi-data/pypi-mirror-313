985bddf72d052c3e54821cd3dd7f2589cabd55b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:43 +0000

    Add package lilya/0.11.5/lilya-0.11.5.tar.gz

---
+++ packages/lilya/lilya-0.11.5.tar.gz/lilya-0.11.5/lilya/middleware/base.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+import sys
+from collections.abc import Callable, Iterator
+from typing import Any
+
+from lilya.types import ASGIApp
+
+if sys.version_info >= (3, 10):  # pragma: no cover
+    from typing import ParamSpec
+else:  # pragma: no cover
+    from typing_extensions import ParamSpec
+
+
+P = ParamSpec("P")
+
+
+class DefineMiddleware:
+    """
+    Wrapper that create the middleware classes.
+    """
+
+    __slots__ = ("app", "args", "kwargs", "middleware")
+
+    def __init__(self, cls: Callable[..., ASGIApp], *args: P.args, **kwargs: P.kwargs) -> None:
+        self.middleware = cls
+        self.args = args
+        self.kwargs = kwargs
+
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> Any:
+        return self.middleware(*args, **kwargs)
+
+    def __iter__(self) -> Iterator[Any]:
+        return iter((self.middleware, self.args, self.kwargs))
+
+    def __repr__(self) -> str:
+        args_repr = ", ".join(
+            [self.middleware.__name__]
+            + [f"{value!r}" for value in self.args]
+            + [f"{key}={value!r}" for key, value in self.kwargs.items()]
+        )
+        return f"{self.__class__.__name__}({args_repr})"
+
+
+Middleware = DefineMiddleware
