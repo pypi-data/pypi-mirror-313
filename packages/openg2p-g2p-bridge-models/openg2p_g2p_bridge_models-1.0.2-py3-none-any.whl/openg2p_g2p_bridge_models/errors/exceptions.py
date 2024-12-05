7a3a5dbae519de025671147013b065557b5ead50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:29 +0000

    Add package openg2p-g2p-bridge-models/1.0.2/openg2p_g2p_bridge_models-1.0.2-py3-none-any.whl

---
+++ packages/openg2p-g2p-bridge-models/openg2p_g2p_bridge_models-1.0.2-py3-none-any.whl/openg2p_g2p_bridge_models/errors/exceptions.py
@@ -0,0 +1,42 @@
+from typing import List, Optional
+
+from ..schemas import DisbursementPayload
+from .codes import G2PBridgeErrorCodes
+
+
+class DisbursementEnvelopeException(Exception):
+    def __init__(self, code: G2PBridgeErrorCodes, message: Optional[str] = None):
+        self.code: G2PBridgeErrorCodes = code
+        self.message: Optional[str] = message
+        super().__init__(self.message)
+
+
+class DisbursementException(Exception):
+    def __init__(
+        self,
+        code: G2PBridgeErrorCodes,
+        disbursement_payloads: List[DisbursementPayload],
+        message: Optional[str] = None,
+    ):
+        self.code: G2PBridgeErrorCodes = code
+        self.message: Optional[str] = message
+        self.disbursement_payloads: List[DisbursementPayload] = disbursement_payloads
+        super().__init__(code, self.message)
+
+
+class AccountStatementException(Exception):
+    def __init__(self, code: G2PBridgeErrorCodes, message: Optional[str] = None):
+        self.code: G2PBridgeErrorCodes = code
+        self.message: Optional[str] = message
+        super().__init__(self.message)
+
+
+class DisbursementStatusException(Exception):
+    def __init__(
+        self,
+        code: G2PBridgeErrorCodes,
+        message: Optional[str] = None,
+    ):
+        self.code: G2PBridgeErrorCodes = code
+        self.message: Optional[str] = message
+        super().__init__(code, self.message)
