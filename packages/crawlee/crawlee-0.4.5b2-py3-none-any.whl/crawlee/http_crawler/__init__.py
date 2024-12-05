d323fb097d5843c4d50fbe9dd06bea79d011c127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:24 +0000

    Add package crawlee/0.4.5b2/crawlee-0.4.5b2-py3-none-any.whl

---
+++ packages/crawlee/crawlee-0.4.5b2-py3-none-any.whl/crawlee/http_crawler/__init__.py
@@ -0,0 +1,4 @@
+from ._http_crawler import HttpCrawler
+from ._http_crawling_context import HttpCrawlingContext, HttpCrawlingResult
+
+__all__ = ['HttpCrawler', 'HttpCrawlingContext', 'HttpCrawlingResult']
