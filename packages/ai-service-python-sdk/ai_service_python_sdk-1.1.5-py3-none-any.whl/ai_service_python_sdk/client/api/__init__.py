2987e5ed6e5c76682c1e170cc6a736c6798edb13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:55 +0000

    Add package ai-service-python-sdk/1.1.5/ai_service_python_sdk-1.1.5-py3-none-any.whl

---
+++ packages/ai-service-python-sdk/ai_service_python_sdk-1.1.5-py3-none-any.whl/ai_service_python_sdk/client/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ai_service_python_sdk.client.api.ai_service_image_api import AiServiceImageApi
+from ai_service_python_sdk.client.api.ai_service_job_api import AiServiceJobApi
+from ai_service_python_sdk.client.api.ai_service_text_api import AiServiceTextApi
+from ai_service_python_sdk.client.api.ai_service_video_api import AiServiceVideoApi
