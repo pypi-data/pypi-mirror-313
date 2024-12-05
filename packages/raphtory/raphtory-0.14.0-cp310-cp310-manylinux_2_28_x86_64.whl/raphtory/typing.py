15b51755200c11fce8b0a09822abc1ca668a0bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:27 +0000

    Add package raphtory/0.14.0/raphtory-0.14.0-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/raphtory/raphtory-0.14.0-cp310-cp310-manylinux_2_28_x86_64.whl/raphtory/typing.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from typing import Union, Literal, Mapping
+
+PropValue = Union[
+    bool,
+    int,
+    float,
+    datetime,
+    str,
+    "Graph",
+    "PersistentGraph",
+    "Document",
+    list["Prop"],
+    dict[str, "Prop"],
+]
+
+GID = Union[int, str]
+
+PropInput = Mapping[str, PropValue]
+
+Direction = Literal["in", "out", "both"]
+
+InputNode = Union[int, str, "Node"]
+
+TimeInput = Union[int, str, float, datetime]
