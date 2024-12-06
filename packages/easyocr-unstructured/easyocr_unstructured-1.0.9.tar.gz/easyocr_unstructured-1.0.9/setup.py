8e3dda16d51073ffdd8726494ccc4bada96d6931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:42 +0000

    Add package easyocr-unstructured/1.0.9/easyocr_unstructured-1.0.9.tar.gz

---
+++ packages/easyocr-unstructured/easyocr_unstructured-1.0.9.tar.gz/easyocr_unstructured-1.0.9/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='easyocr_unstructured',
+    version='1.0.9',
+    author='Kevin Fink',
+    author_email='kevin@shorecode.org',
+    description='Parse unstructured text from PDFs',
+    long_description=open('./readme.MD').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/shorecodeorg/easyocr-unstructured',  # Replace with your project URL
+    packages=find_packages(),
+    #package_dir={'': 'src'},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Change if using a different license
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',  # Specify your Python version requirement
+)
