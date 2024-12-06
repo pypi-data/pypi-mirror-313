bc6a2a247f46ebdfa52a896dd9c42f74cee65de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:33:10 +0000

    Add package wave-venture/0.0.48/wave_venture-0.0.48-py3-none-any.whl

---
+++ packages/wave-venture/wave_venture-0.0.48-py3-none-any.whl/wave_venture/__init__.py
@@ -0,0 +1,5 @@
+from wave_venture import interface
+from wave_venture.interface import new, load, clone, import_, add, update, remove, upload, run, resolve
+from wave_venture.plotting import plot
+
+__all__ = ["interface", "new", "load", "clone", "import_", "add", "update", "remove", "upload", "run", "resolve", "plot"]
---
+++ packages/wave-venture/wave_venture-0.0.48-py3-none-any.whl/wave_venture/config.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+from wave_venture import utils
+
+
+url = os.environ.get("URL", "http://127.0.0.1:8000/api" if sys.platform.startswith("win") else "http://0.0.0.0:8000/api")
+plotter = os.environ.get("PLOTTER", utils.find_gui_plotter())
+auth_token = os.environ.get("AUTH_TOKEN", utils.find_auth_token())
+
+websocket_max_size = os.environ.get("WEBSOCKET_MAX_SIZE", None)
+websocket_max_queue = os.environ.get("WEBSOCKET_MAX_QUEUE", None)
+websocket_read_limit = os.environ.get("WEBSOCKET_READ_LIMIT", 2**32)
+websocket_write_limit = os.environ.get("WEBSOCKET_WRITE_LIMIT", 2**32)
+websocket_close_timeout = os.environ.get("WEBSOCKET_CLOSE_TIMEOUT", 600000)
+websocket_ping_interval = os.environ.get("WEBSOCKET_PING_INTERVAL", 20)
+websocket_ping_timeout = os.environ.get("WEBSOCKET_PING_TIMEOUT", 600000)
