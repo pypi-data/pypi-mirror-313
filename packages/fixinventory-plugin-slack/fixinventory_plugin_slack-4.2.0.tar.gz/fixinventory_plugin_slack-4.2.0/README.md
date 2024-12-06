487ec232df4036bdf644f0e99419b5c6e3890d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:52 +0000

    Add package fixinventory-plugin-slack/4.2.0/fixinventory_plugin_slack-4.2.0.tar.gz

---
+++ packages/fixinventory-plugin-slack/fixinventory_plugin_slack-4.2.0.tar.gz/fixinventory_plugin_slack-4.2.0/README.md
@@ -0,0 +1,5 @@
+# fix-plugin-slack
+Slack collector for Fix
+
+## License
+See [LICENSE](../../LICENSE) for details.
---
+++ packages/fixinventory-plugin-slack/fixinventory_plugin_slack-4.2.0.tar.gz/fixinventory_plugin_slack-4.2.0/test/test_config.py
@@ -0,0 +1,12 @@
+from fixlib.config import Config
+from fix_plugin_slack import SlackBotPlugin, SlackCollectorPlugin
+
+
+def test_config():
+    config = Config("dummy", "dummy")
+    SlackCollectorPlugin.add_config(config)
+    SlackBotPlugin.add_config(config)
+    Config.init_default_config()
+    assert Config.slack.bot_token is None
+    assert Config.slack.include_archived is False
+    assert Config.slack.do_not_verify_ssl is False
