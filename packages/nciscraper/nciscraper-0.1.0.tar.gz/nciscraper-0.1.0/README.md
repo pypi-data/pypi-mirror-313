bc123b11768aa369992d89f253a904a2bbc420ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:54 +0000

    Add package nciscraper/0.1.0/nciscraper-0.1.0.tar.gz

---
+++ packages/nciscraper/nciscraper-0.1.0.tar.gz/nciscraper-0.1.0/README.md
@@ -0,0 +1,26 @@
+# NCIScraper
+
+A Python package to fetch web content using different advance methods.
+
+## Installation
+
+```bash
+python -m pip install ncirequest
+```
+
+### Step 5: Create a `LICENSE` File
+
+Choose a license for your code (e.g., MIT) and add it to a file named `LICENSE` in the root of your repository. Here's an example of an MIT License:
+
+```plaintext
+MIT License
+
+Copyright (c) 2024 Your Name
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+...
---
+++ packages/nciscraper/nciscraper-0.1.0.tar.gz/nciscraper-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nciscraper",  # Name of your package
+    version="0.1.0",  # Version number
+    author="Tux MacGiver",  # Your name or the name of your organization
+    author_email="tuxmacg1v991@gmail.com",  # Your email address
+    description="A Python package to fetch web content using different advance methods.",
+    long_description=open('README.md').read(),  # Read long description from README.md
+    long_description_content_type="text/markdown",  # Use markdown for README
+    url="https://github.com/Tux-MacG1v/nciscraper",  # URL of your GitHub repo
+    packages=find_packages(where='src'),  # Use the src directory for your packages
+    package_dir={'': 'src'},  # Tell setuptools where your packages are located
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Adjust if using another license
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        "requests",
+        "cloudscraper",
+        "selenium",
+        "seleniumbase",
+        "playwright",
+        "beautifulsoup4",
+        "fake_useragent",
+        "webdriver-manager",
+        "bs4",
+        "tenacity",
+    ],
+    python_requires='>=3.6',  # Minimum Python version required
+)
