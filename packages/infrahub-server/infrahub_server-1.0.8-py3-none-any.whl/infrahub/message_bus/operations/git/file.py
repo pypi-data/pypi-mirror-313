427976c19a1e7c082aaefdae5108022d77348e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:10:23 +0000

    Add package infrahub-server/1.0.8/infrahub_server-1.0.8-py3-none-any.whl

---
+++ packages/infrahub-server/infrahub_server-1.0.8-py3-none-any.whl/infrahub/message_bus/operations/git/file.py
@@ -0,0 +1,27 @@
+from prefect import flow
+
+from infrahub.git.repository import get_initialized_repo
+from infrahub.log import get_logger
+from infrahub.message_bus import messages
+from infrahub.message_bus.messages.git_file_get import GitFileGetResponse, GitFileGetResponseData
+from infrahub.services import InfrahubServices
+
+log = get_logger()
+
+
+@flow(name="git-repository-get-file")
+async def get(message: messages.GitFileGet, service: InfrahubServices) -> None:
+    log.info("Collecting file from repository", repository=message.repository_name, file=message.file)
+
+    repo = await get_initialized_repo(
+        repository_id=message.repository_id,
+        name=message.repository_name,
+        service=service,
+        repository_kind=message.repository_kind,
+    )
+
+    content = await repo.get_file(commit=message.commit, location=message.file)
+
+    if message.reply_requested:
+        response = GitFileGetResponse(data=GitFileGetResponseData(content=content))
+        await service.reply(message=response, initiator=message)
