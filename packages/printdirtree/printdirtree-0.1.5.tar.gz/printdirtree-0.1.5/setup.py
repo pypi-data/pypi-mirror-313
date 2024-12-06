09591396aed7a75ce58d6fb14c4d052a222b3156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:32 +0000

    Add package printdirtree/0.1.5/printdirtree-0.1.5.tar.gz

---
+++ packages/printdirtree/printdirtree-0.1.5.tar.gz/printdirtree-0.1.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='printdirtree',
+    version='0.1.5',
+    packages=find_packages(),
+    url="https://github.com/timf34/PrintDirTree",
+    entry_points={
+        'console_scripts': [
+            'printdirtree=printdirtree.__main__:main',
+        ],
+    },
+    author='Tim Farrelly',
+    author_email='timf34@gmail.com',
+    description='A CLI utility to print the directory tree structure with customizable exclusions.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'pyperclip',
+    ],
+    python_requires='>=3.6',
+)
