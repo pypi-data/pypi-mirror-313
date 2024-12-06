73c97f6b7cfa3f0c45a6a9cb73c0f32770edbc7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:34 +0000

    Add package env-tools/2.4.0/env_tools-2.4.0.tar.gz

---
+++ packages/env-tools/env_tools-2.4.0.tar.gz/env_tools-2.4.0/setup.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+from setuptools import setup
+
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+setup(
+    name="env-tools",
+    author="Beau Gunderson",
+    author_email="beau@beaugunderson.com",
+    url="https://github.com/beaugunderson/python-env-tools",
+    description="Tools for using .env files in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=".env env heroku procfile foreman",
+    version="2.4.0",
+    license="MIT",
+    py_modules=["env_tools"],
+    install_requires=[
+        "six>=1.2.0",
+        "tini>=4.0.0",
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
