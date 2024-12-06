425a8441a42e477df26c98c261510e0777651f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:03:54 +0000

    Add package pytsa-ais/2.3.14/pytsa_ais-2.3.14-cp312-cp312-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/pytsa-ais/pytsa_ais-2.3.14-cp312-cp312-manylinux_2_17_s390x.manylinux2014_s390x.whl/pytsa/decoder/filedescriptor.py
@@ -0,0 +1,63 @@
+"""
+File descriptors for the AIS data files.
+
+This is mostly user-defined and depends on the
+data source. The default values are for data set
+acquired from the European Maritime Safety Agency (EMSA).
+
+If you are using a different data set, please
+change the values accordingly.
+"""
+from enum import Enum
+
+class BaseColumns(Enum):
+    """
+    Column names that must be present in both
+    the dynamic and static source files,
+    otherwise the decoder will raise an error.
+    """
+    __order__ = (
+        "TIMESTAMP MESSAGE_ID "
+        "RAW_MESSAGE RAW_MESSAGE1 "
+        "RAW_MESSAGE2"
+    )
+    TIMESTAMP: str = "timestamp"
+    MESSAGE_ID: str = "message_id"
+    RAW_MESSAGE: str = "raw_message"
+    RAW_MESSAGE1: str = "raw_message1"
+    RAW_MESSAGE2: str = "raw_message2"
+
+class Msg12318Columns(Enum):
+    """
+    Data columns for message 1,2,3 and 18
+    source files.
+    """
+    __order__ = (
+        "MMSI LAT LON SPEED COURSE "
+        "TURN SECOND"
+    )
+    MMSI: str = "MMSI"
+    LAT: str = "lat"
+    LON: str = "lon"
+    SPEED: str = "speed"
+    COURSE: str = "course"
+    TURN: str = "turn"
+    SECOND: str = "second"
+
+class Msg5Columns(Enum):
+    """
+    Data columns for message 5
+    source files. 
+    """
+    __order__ = (
+        "MMSI SHIPTYPE SHIPNAME CALLSIGN "
+        "TO_BOW TO_STERN TO_PORT TO_STARBOARD"
+    )
+    MMSI: str = "MMSI"
+    SHIPTYPE: str = "ship_type"
+    SHIPNAME: str = "shipname"
+    CALLSIGN: str = "callsign"
+    TO_BOW: str = "to_bow"
+    TO_STERN: str = "to_stern"
+    TO_PORT: str = "to_port"
+    TO_STARBOARD: str = "to_starboard"
