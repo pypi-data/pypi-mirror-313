45dd3bfef8728d6f752fc84d862f45ec6d708d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:16:59 +0000

    Add package Nash-Geo-Scanner-V1/1.1.0/nash_geo_scanner_v1-1.1.0.tar.gz

---
+++ packages/Nash-Geo-Scanner-V1/nash_geo_scanner_v1-1.1.0.tar.gz/nash_geo_scanner_v1-1.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Nash-Geo-Scanner_V1",  # Unique package name on PyPI
+    version="1.1.0",  # Initial version
+    author="Mohammed Althaf",
+    author_email="althafnash14@gmail.com",
+    description="Geo locator OFFICAL version",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Althafnash/Nash-NetGeoScanner-.git",
+    packages=find_packages(),  # Automatically find and include packages
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",  # Minimum Python version
+    install_requires=[],  # List of dependencies
+)
