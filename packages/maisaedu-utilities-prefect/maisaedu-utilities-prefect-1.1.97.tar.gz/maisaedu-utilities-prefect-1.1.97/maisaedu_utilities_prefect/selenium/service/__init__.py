cb4ed1bc7ed998b40bb77899bc2a99406993ff77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:26 +0000

    Add package maisaedu-utilities-prefect/1.1.97/maisaedu-utilities-prefect-1.1.97.tar.gz

---
+++ packages/maisaedu-utilities-prefect/maisaedu-utilities-prefect-1.1.97.tar.gz/maisaedu-utilities-prefect-1.1.97/maisaedu_utilities_prefect/selenium/service/__init__.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from typing import Optional, Any
+
+from selenium import webdriver 
+
+from maisaedu_utilities_prefect.constants.selenium import (
+    SELENIUM_HUB_URL
+)
+
+class ScraperService(BaseModel):
+    options: Any
+    driver: Any
+    
+    def __init__(self, options: Optional[webdriver.ChromeOptions] = None, url = SELENIUM_HUB_URL):
+        if options is None:
+            options = webdriver.ChromeOptions()
+            options.add_argument("profile-directory=MaisAEduPartialSales") 
+            options.add_argument("user-data-dir=/home/seluser/.cache/selenium/chrome")
+            options.add_argument("start-maximized")
+
+        driver = webdriver.Remote(
+            command_executor=url,
+            options=options
+        )
+
+        super().__init__(options=options, driver=driver)
+
+    def quit(self):
+        self.driver.quit()
