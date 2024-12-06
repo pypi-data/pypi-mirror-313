b505165baf5dcee8206780ff236e3301ab808d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:32 +0000

    Add package lagoon/48/lagoon-48-py3-none-any.whl

---
+++ packages/lagoon/lagoon-48-py3-none-any.whl/lagoon/__init__.py
@@ -0,0 +1,4 @@
+'Text mode instances of Program for every executable, with dash translated to underscore e.g. `from lagoon import pkg_config` for `pkg-config`.'
+from .program import scan
+
+scan(__name__)
