26e4d09d9802d129dbc559f767dc51255a7884ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:39 +0000

    Add package unitcellsdf/0.0.2/unitcellsdf-0.0.2.tar.gz

---
+++ packages/unitcellsdf/unitcellsdf-0.0.2.tar.gz/unitcellsdf-0.0.2/sdf/__init__.py
@@ -0,0 +1,10 @@
+from . import d2, d3, ease
+from .core import generate, sample_slice, save, show_slice
+from .d2 import *
+from .d3 import *
+from .mesh import Mesh
+from .stl import write_binary_stl
+from .text import image, measure_image, measure_text, text
+from .util import *
+
+__version__ = version = "0.0.2"
