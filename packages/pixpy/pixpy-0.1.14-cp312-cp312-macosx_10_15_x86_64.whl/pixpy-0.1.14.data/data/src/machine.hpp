712a4985f1d1be64cdadcb728a6940df6f26c2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:54 +0000

    Add package pixpy/0.1.14/pixpy-0.1.14-cp312-cp312-macosx_10_15_x86_64.whl

---
+++ packages/pixpy/pixpy-0.1.14-cp312-cp312-macosx_10_15_x86_64.whl/pixpy-0.1.14.data/data/src/machine.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "system.hpp"
+#include "context.hpp"
+
+#include <functional>
+#include <memory>
+#include <vector>
+
+struct Machine
+{
+    std::shared_ptr<System> sys{};
+    std::shared_ptr<Screen> screen{};
+    std::shared_ptr<pix::Context> context{};
+    uint32_t frame_counter = 0;
+    static Machine& get_instance();
+};
