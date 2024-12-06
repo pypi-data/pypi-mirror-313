022c815051f4d97fe4869b39680bc63b00cd6e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:10:54 +0000

    Add package fsorter/2.4.1/fsorter-2.4.1.tar.gz

---
+++ packages/fsorter/fsorter-2.4.1.tar.gz/fsorter-2.4.1/README.md
@@ -0,0 +1,67 @@
+## Table of contents
+* [General Info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [How to use Fsorter](#how-to-use-Fsorter)
+* [Example](#Example)
+* [Example 2](#example-2)
+* [Note](#Note)
+
+## General Info
+This program checks the numeric values ​​at the end of the  file names in the folder and sorts the file names from the smallest number to the larger number.
+	
+## Technologies
+Project is created with:
+* Python 3.8
+	
+## Setup
+To run this project, install it locally using pip:
+
+```
+$ pip install fsorter
+
+```
+
+## How to use Fsorter
+
+```
+from fsorter import fsorter
+
+test_list = fsorter.filesort('file path',['extensions'])
+ 
+```
+
+## Example
+
+```
+from fsorter import fsorter
+
+test_list = fsorter.filesort('C://Windows//Users//test_user//Desktop//test_folder',['jpg','png','tiff','pdf','pptx','xlsx'])
+ 
+```
+
+## Example 2
+
+You can make an example like below to see how it sorts files in console output.
+
+```
+
+from fsorter import fsorter
+
+dir = 'test'
+test = fsorter.fileSort(dir)
+
+for k in test:
+    print(k)
+ 
+```
+
+## Note
+
+If the file extensions that need to be sorted into the Fsorter program are not entered as parameters, by default they are ['.jpg', '.png', '.jpeg', '.tif', '.tiff', '.nii', '.JPG', '. JPEG', '.TIFF', '.TIF', '.NII'] extensions sort.
+ 
+
+
+
+
+
---
+++ packages/fsorter/fsorter-2.4.1.tar.gz/fsorter-2.4.1/setup.py
@@ -0,0 +1,49 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+""" 
+
+setuptools.setup(
+    name="fsorter",
+    author="Kadir Barut",
+    version='2.5.0',
+    author_email="kadirrbrtt@gmail.com",
+    description="This program checks the numeric values ​​at the end of the  file names in the folder and sorts the file names from the smallest number to the larger number.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kadirrbrtt/fsorter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6',
+    install_requires=[]
+)
+"""
+
+
+
+setuptools.setup(
+    name="fsorter",
+    version="2.4.1",
+    description="File sorting program according to the numbers in the file name.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kadirrbrtt/fsorter",
+    author="Kadir Barut",
+    author_email="kadirrbrtt@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    
+    
+)
