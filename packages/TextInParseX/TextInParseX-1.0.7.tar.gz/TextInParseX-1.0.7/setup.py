f46f5201da2da8fd471600f7102e5027e29b73fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:11 +0000

    Add package TextInParseX/1.0.7/TextInParseX-1.0.7.tar.gz

---
+++ packages/TextInParseX/TextInParseX-1.0.7.tar.gz/TextInParseX-1.0.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TextInParseX',
+    version='1.0.7',
+    author='bin_zhang',
+    description='A text-in parser engine for pdf to markdown processing',
+    long_description=open('../README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/intsig-textin/parsex-sdk',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python-headless',
+        'numpy',
+        'requests',
+        'openpyxl'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
