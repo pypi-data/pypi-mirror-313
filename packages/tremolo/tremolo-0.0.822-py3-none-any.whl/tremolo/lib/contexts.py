41ce917bd96336b267d6b00e4990d4b2e1ae417f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:33 +0000

    Add package tremolo/0.0.822/tremolo-0.0.822-py3-none-any.whl

---
+++ packages/tremolo/tremolo-0.0.822-py3-none-any.whl/tremolo/lib/contexts.py
@@ -0,0 +1,85 @@
+# Copyright (c) 2023 nggit
+
+
+class Context:
+    def __init__(self):
+        self.__dict__ = {}
+
+    def __repr__(self):
+        return self.__dict__.__repr__()
+
+    def get(self, key, default=None):
+        return self.__dict__.get(key, default)
+
+    def update(self, *args, **kwargs):
+        return self.__dict__.update(*args, **kwargs)
+
+    def __setitem__(self, *args):
+        self.__setattr__(*args)
+
+    def __getitem__(self, name):
+        return self.__dict__[name]
+
+    def __contains__(self, name):
+        return self.__dict__.__contains__(name)
+
+
+class WorkerContext(Context):
+    def __init__(self):
+        self.__dict__ = {
+            'info': {},
+            'options': {},
+            'queues': {},
+            'tasks': set()
+        }
+
+    @property
+    def info(self):
+        return self.__dict__['info']
+
+    @property
+    def options(self):
+        return self.__dict__['options']
+
+    @property
+    def queues(self):
+        return self.__dict__['queues']
+
+    @property
+    def tasks(self):
+        return self.__dict__['tasks']
+
+
+class ConnectionContext(Context):
+    def __init__(self):
+        self.__dict__ = {
+            'transport': None,
+            'socket': None,
+            'tasks': set()
+        }
+
+    @property
+    def transport(self):
+        return self.__dict__['transport']
+
+    @property
+    def socket(self):
+        if not self.__dict__['socket'] and self.transport is not None:
+            self.__dict__['socket'] = self.transport.get_extra_info('socket')
+
+        return self.__dict__['socket']
+
+    @property
+    def tasks(self):
+        return self.__dict__['tasks']
+
+
+class RequestContext(Context):
+    def __init__(self):
+        self.__dict__ = {
+            'options': {}
+        }
+
+    @property
+    def options(self):
+        return self.__dict__['options']
