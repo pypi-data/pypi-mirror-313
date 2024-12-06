ca4be7cee2abb8d9a2c48ea604238a9051ff25e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:11 +0000

    Add package ultimate-rvc/0.1.25/ultimate_rvc-0.1.25.tar.gz

---
+++ packages/ultimate-rvc/ultimate_rvc-0.1.25.tar.gz/ultimate_rvc-0.1.25/src/ultimate_rvc/core/generate/__init__.py
@@ -0,0 +1,12 @@
+"""
+Package which defines modules that facilitate RVC based audio
+generation.
+"""
+
+from __future__ import annotations
+
+import static_ffmpeg
+import static_sox
+
+static_ffmpeg.add_paths()
+static_sox.add_paths()
