b77ea2d422552d961379364e3379c6e32e69b4be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:37 +0000

    Add package opentea/3.8.0/opentea-3.8.0.tar.gz

---
+++ packages/opentea/opentea-3.8.0.tar.gz/opentea-3.8.0/src/opentea/gui_forms/_exceptions.py
@@ -0,0 +1,9 @@
+
+class SetException(Exception):
+    """Define an exception on the widget setters."""
+    pass
+
+
+class GetException(Exception):
+    """Define an exception on the widget getters."""
+    pass
