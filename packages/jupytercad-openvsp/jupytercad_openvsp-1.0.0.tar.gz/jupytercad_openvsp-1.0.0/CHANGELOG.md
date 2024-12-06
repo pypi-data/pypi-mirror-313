4ada9121ed1250600c012da071b11bad0963a13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:26 +0000

    Add package jupytercad-openvsp/1.0.0/jupytercad_openvsp-1.0.0.tar.gz

---
+++ packages/jupytercad-openvsp/jupytercad_openvsp-1.0.0.tar.gz/jupytercad_openvsp-1.0.0/CHANGELOG.md
@@ -0,0 +1,50 @@
+# Changelog
+
+<!-- <START NEW CHANGELOG ENTRY> -->
+
+## 1.0.0
+
+([Full Changelog](https://github.com/jupytercad/JupyterCAD-OpenVSP/compare/v0.1.3...921ad84370e9022d18d99731d71e0962f90aebb4))
+
+### Enhancements made
+
+- Bump JupyterCAD 3.0.0 [#10](https://github.com/jupytercad/JupyterCAD-OpenVSP/pull/10) ([@martinRenou](https://github.com/martinRenou))
+
+### Bugs fixed
+
+- Releaser: Fix package url [#7](https://github.com/jupytercad/JupyterCAD-OpenVSP/pull/7) ([@martinRenou](https://github.com/martinRenou))
+
+### Maintenance and upkeep improvements
+
+- Bump JupyterCAD to `3.0.0a6` [#8](https://github.com/jupytercad/JupyterCAD-OpenVSP/pull/8) ([@arjxn-py](https://github.com/arjxn-py))
+
+### Contributors to this release
+
+([GitHub contributors page for this release](https://github.com/jupytercad/JupyterCAD-OpenVSP/graphs/contributors?from=2024-05-13&to=2024-12-05&type=c))
+
+[@arjxn-py](https://github.com/search?q=repo%3Ajupytercad%2FJupyterCAD-OpenVSP+involves%3Aarjxn-py+updated%3A2024-05-13..2024-12-05&type=Issues) | [@martinRenou](https://github.com/search?q=repo%3Ajupytercad%2FJupyterCAD-OpenVSP+involves%3AmartinRenou+updated%3A2024-05-13..2024-12-05&type=Issues) | [@pre-commit-ci](https://github.com/search?q=repo%3Ajupytercad%2FJupyterCAD-OpenVSP+involves%3Apre-commit-ci+updated%3A2024-05-13..2024-12-05&type=Issues) | [@trungleduc](https://github.com/search?q=repo%3Ajupytercad%2FJupyterCAD-OpenVSP+involves%3Atrungleduc+updated%3A2024-05-13..2024-12-05&type=Issues)
+
+<!-- <END NEW CHANGELOG ENTRY> -->
+
+## 0.1.3
+
+No merged PRs
+
+## 0.1.2
+
+No merged PRs
+
+## 0.1.1
+
+([Full Changelog](https://github.com/jupytercad/jupytercad-openvsp/compare/3d315ea515f0683be532a0953bc9ff009d01e05f...34b3dfe67933c28f479d71d39b64cc96de640ae9))
+
+### Enhancements made
+
+- Add UI tests [#2](https://github.com/jupytercad/jupytercad-openvsp/pull/2) ([@trungleduc](https://github.com/trungleduc))
+- Add JCAD export endpoint [#1](https://github.com/jupytercad/jupytercad-openvsp/pull/1) ([@trungleduc](https://github.com/trungleduc))
+
+### Contributors to this release
+
+([GitHub contributors page for this release](https://github.com/jupytercad/jupytercad-openvsp/graphs/contributors?from=2024-04-29&to=2024-05-10&type=c))
+
+[@trungleduc](https://github.com/search?q=repo%3Ajupytercad%2Fjupytercad-openvsp+involves%3Atrungleduc+updated%3A2024-04-29..2024-05-10&type=Issues)
---
+++ packages/jupytercad-openvsp/jupytercad_openvsp-1.0.0.tar.gz/jupytercad_openvsp-1.0.0/src/index.ts
@@ -0,0 +1,94 @@
+import {
+  ICollaborativeDrive,
+  SharedDocumentFactory
+} from '@jupyter/collaborative-drive';
+import {
+  IJCadWorkerRegistry,
+  IJCadWorkerRegistryToken,
+  IJupyterCadDocTracker,
+  IJupyterCadWidget,
+  IJCadExternalCommandRegistry,
+  IJCadExternalCommandRegistryToken
+} from '@jupytercad/schema';
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import { IThemeManager, WidgetTracker } from '@jupyterlab/apputils';
+import { JupyterCadWidgetFactory } from '@jupytercad/jupytercad-core';
+import { JupyterCadVspModelFactory } from './modelfactory';
+import { JupyterCadOVSPDoc } from './model';
+
+const FACTORY = 'JupyterCAD VSP3 Viewer';
+
+/**
+ * Initialization data for the jupytercad_openvsp extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupytercad_openvsp:plugin',
+  description: 'A JupyterCAD extension for OpenVSP',
+  autoStart: true,
+  requires: [
+    IJupyterCadDocTracker,
+    IThemeManager,
+    IJCadWorkerRegistryToken,
+    IJCadExternalCommandRegistryToken,
+    ICollaborativeDrive
+  ],
+  activate: (
+    app: JupyterFrontEnd,
+    tracker: WidgetTracker<IJupyterCadWidget>,
+    themeManager: IThemeManager,
+    workerRegistry: IJCadWorkerRegistry,
+    externalCommandRegistry: IJCadExternalCommandRegistry,
+    drive: ICollaborativeDrive | null
+  ) => {
+    const widgetFactory = new JupyterCadWidgetFactory({
+      name: FACTORY,
+      modelName: 'jupytercad-vspmodel',
+      fileTypes: ['vsp'],
+      defaultFor: ['vsp'],
+      tracker,
+      commands: app.commands,
+      workerRegistry,
+      externalCommandRegistry
+    });
+    app.docRegistry.addWidgetFactory(widgetFactory);
+
+    const modelFactory = new JupyterCadVspModelFactory();
+    app.docRegistry.addModelFactory(modelFactory);
+    // register the filetype
+    app.docRegistry.addFileType({
+      name: 'vsp',
+      displayName: 'VSP',
+      mimeTypes: ['text/json'],
+      extensions: ['.vsp3', '.VSP3'],
+      fileFormat: 'text',
+      contentType: 'vsp'
+    });
+
+    const vspSharedModelFactory: SharedDocumentFactory = () => {
+      return new JupyterCadOVSPDoc();
+    };
+    if (drive) {
+      drive.sharedModelFactory.registerDocumentFactory(
+        'vsp',
+        vspSharedModelFactory
+      );
+    }
+
+    widgetFactory.widgetCreated.connect((sender, widget) => {
+      widget.context.pathChanged.connect(() => {
+        tracker.save(widget);
+      });
+      themeManager.themeChanged.connect((_, changes) =>
+        widget.context.model.themeChanged.emit(changes)
+      );
+      tracker.add(widget);
+      app.shell.activateById('jupytercad::leftControlPanel');
+    });
+    console.log('jupytercad_openvsp:plugin is activated!');
+  }
+};
+
+export default plugin;
