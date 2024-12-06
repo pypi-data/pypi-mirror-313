015d336a09f2b873e62aeef9f125a79e5696ba68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:04:34 +0000

    Add package python-office/1.0.0/python_office-1.0.0-py3-none-any.whl

---
+++ packages/python-office/python_office-1.0.0-py3-none-any.whl/office/__init__.py
@@ -0,0 +1,13 @@
+from office.api import email
+from office.api import excel
+from office.api import file
+from office.api import finance
+from office.api import image
+from office.api import pdf
+from office.api import ppt
+from office.api import tools
+from office.api import video
+from office.api import wechat
+from office.api import word
+# 以下是beta版本
+from office.api.testApi import ruiming
