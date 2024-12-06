b7b5256f63acf8914b0756f8dfa873c3cb99e737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:21 +0000

    Add package crypto-connector/0.4.0/crypto_connector-0.4.0.tar.gz

---
+++ packages/crypto-connector/crypto_connector-0.4.0.tar.gz/crypto_connector-0.4.0/tests/account/test_account.py
@@ -0,0 +1,48 @@
+import pytest
+
+from crypto_connector.base.exchange import Exchange
+from crypto_connector.base.schemas import Balance
+
+
+@pytest.mark.parametrize(
+    "exchange",
+    argvalues=[
+        ("Binance"),
+        ("HTX"),
+    ],
+    indirect=["exchange"],
+)
+def test_get_api_key_info(exchange: Exchange) -> None:
+    info = exchange.get_api_key_info()
+    assert info["timestamp"] > 0
+
+
+@pytest.mark.parametrize(
+    "exchange",
+    argvalues=[
+        ("Binance"),
+        ("HTX"),
+    ],
+    indirect=["exchange"],
+)
+def test_get_balance(exchange: Exchange) -> None:
+    balance = exchange.get_balance()
+    assert Balance.model_validate(balance)
+    assert balance["equity"] >= 0
+
+
+@pytest.mark.parametrize(
+    "exchange",
+    argvalues=[
+        ("Binance"),
+        ("HTX"),
+    ],
+    indirect=["exchange"],
+)
+def test_get_transfer_history(exchange: Exchange) -> None:
+    transfers = exchange.get_transfer_history()
+    assert isinstance(transfers, list)
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
