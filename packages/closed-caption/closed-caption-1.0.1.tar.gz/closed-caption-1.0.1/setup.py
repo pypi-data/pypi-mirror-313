6d44df19d657586eb828c5dd7df5fc9f6f3b3e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:39 +0000

    Add package closed-caption/1.0.1/closed-caption-1.0.1.tar.gz

---
+++ packages/closed-caption/closed-caption-1.0.1.tar.gz/closed-caption-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="closed-caption",
+    version="1.0.1",
+    description="Tạo phụ đề từ video sử dụng OpenAI Whisper.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Phạm Ngọc Hải",
+    author_email="ngochai285nd@gmail.com",
+    url="https://github.com/haiphamcoder/closed-caption",
+    packages=find_packages(),
+    install_requires=[
+        "pysrt>=1.1.2",
+        "openai-whisper>=0.1.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "closed_caption=closed_caption.cli:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
