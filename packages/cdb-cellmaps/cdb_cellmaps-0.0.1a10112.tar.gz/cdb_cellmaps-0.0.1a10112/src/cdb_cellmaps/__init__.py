bfbff9ed5b39b721ebaab5def64d6dfc770263ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:23 +0000

    Add package cdb-cellmaps/0.0.1a10112/cdb_cellmaps-0.0.1a10112.tar.gz

---
+++ packages/cdb-cellmaps/cdb_cellmaps-0.0.1a10112.tar.gz/cdb_cellmaps-0.0.1a10112/src/cdb_cellmaps/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+# Need to figure out how to populate these things automatically
+__version__ = "0.0.1a0010112"
+__detailed_version__ = "0.0.1+ds.0.0.1.0.1.12" #type: ignore
+__library_name__ = 'cdb_cellmaps' #type: ignore
