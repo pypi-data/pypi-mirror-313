b37d79c2e8c8f75465d044a3c49fdbefadbe6d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:04 +0000

    Add package BrazilFiscalReport/0.5.9/BrazilFiscalReport-0.5.9-py3-none-any.whl

---
+++ packages/BrazilFiscalReport/BrazilFiscalReport-0.5.9-py3-none-any.whl/brazilfiscalreport/danfe/__init__.py
@@ -0,0 +1,21 @@
+from .config import (
+    DanfeConfig,
+    DecimalConfig,
+    FontType,
+    InvoiceDisplay,
+    Margins,
+    ReceiptPosition,
+    TaxConfiguration,
+)
+from .danfe import Danfe
+
+__all__ = [
+    "Danfe",
+    "DanfeConfig",
+    "DecimalConfig",
+    "FontType",
+    "InvoiceDisplay",
+    "Margins",
+    "ReceiptPosition",
+    "TaxConfiguration",
+]
