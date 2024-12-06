43e675deedfedad219146722ce3d8a7adbecd9f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:18:04 +0000

    Add package senaps_sensor/2.16.0/senaps_sensor-2.16.0-py2.py3-none-any.whl

---
+++ packages/senaps_sensor/senaps_sensor-2.16.0-py2.py3-none-any.whl/senaps_sensor/__init__.py
@@ -0,0 +1,21 @@
+# Senaps data platform v2 API client library.
+#
+# Originally created by
+# Copyright 2016 Ionata Digital
+#
+# Now maintained as part of Senaps platform by
+# Copyright 2021 CSIRO
+#
+# See LICENSE for details.
+
+"""
+Senaps data platform sensor API client library
+"""
+from __future__ import absolute_import, unicode_literals, print_function
+from .api import API
+
+__version__ = '2.16.0'
+__author__ = 'CSIRO Data61'
+__license__ = 'MIT'
+
+api = API()
