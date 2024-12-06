3c357a1d4592aae0952064af3cda316e7c632e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:33 +0000

    Add package vend-sentry-google-chat/10.0.1/vend-sentry-google-chat-10.0.1.tar.gz

---
+++ packages/vend-sentry-google-chat/vend-sentry-google-chat-10.0.1.tar.gz/vend-sentry-google-chat-10.0.1/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""
+vend-sentry-google-chat
+==============
+
+A `Sentry <https://getsentry.com>`_ plugin which posts notifications
+to `Google Chat <https://gsuite.google.com/products/chat/>`_.
+
+:license: MIT, see LICENSE for more details.
+"""
+
+from setuptools import setup, find_packages
+from sentry_googlechat import __version__
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='vend-sentry-google-chat',
+    version=__version__,
+    author='Jonhnny Weslley',
+    author_email='jw@jonhnnyweslley.net',
+    url='https://github.com/jweslley/sentry-googlechat',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    description='A Sentry plugin which posts notifications to Google Chat (https://gsuite.google.com/products/chat/).',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'sentry.apps': [
+            'sentry_googlechat = sentry_googlechat',
+        ],
+        'sentry.plugins': [
+            'sentry_googlechat = sentry_googlechat.plugin:GoogleChatPlugin',
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        'Topic :: System :: Monitoring'
+    ],
+)
