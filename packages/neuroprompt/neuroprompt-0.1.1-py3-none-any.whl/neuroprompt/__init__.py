af1234cfd56b8afd68fe3f2eb15f54a3e2912e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:56 +0000

    Add package neuroprompt/0.1.1/neuroprompt-0.1.1-py3-none-any.whl

---
+++ packages/neuroprompt/neuroprompt-0.1.1-py3-none-any.whl/neuroprompt/__init__.py
@@ -0,0 +1,5 @@
+from .compressor import NeuroPromptCompress
+from .compressor import NeuroPromptCompressWithEval
+
+__version__ = "0.1.1"
+__all__ = ["NeuroPromptCompress", "NeuroPromptCompressWithEval"]
