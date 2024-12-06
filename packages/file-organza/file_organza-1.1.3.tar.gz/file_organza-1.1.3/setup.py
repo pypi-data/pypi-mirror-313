d9117cb4ebb1230e878e00991a2a98c092aa58ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:58 +0000

    Add package file-organza/1.1.3/file_organza-1.1.3.tar.gz

---
+++ packages/file-organza/file_organza-1.1.3.tar.gz/file_organza-1.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='file_organza',
+    version='1.1.3',
+    description='A Python package to organize files by type or date',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/asimsolehria/file_organza',
+    packages=find_packages(),
+    install_requires=[
+        'pytest>=6.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'organize-files=file_organza.cli:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
