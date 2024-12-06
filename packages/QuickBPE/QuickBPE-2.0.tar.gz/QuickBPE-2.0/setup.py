48f68cac92b82b35142bff69f6b9455b0461bc65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:30 +0000

    Add package QuickBPE/2.0/QuickBPE-2.0.tar.gz

---
+++ packages/QuickBPE/QuickBPE-2.0.tar.gz/QuickBPE-2.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='QuickBPE',         
+    version='2.0',           
+    license='MIT',           
+    description='A fast BPE implementation in C',
+    author='Johannes Voderholzer',
+    author_email='invenis2@gmail.com',
+    url='https://github.com/JohannesVod/QuickBPE',
+    download_url='https://github.com/JohannesVod/QuickBPE/archive/refs/tags/v2.0.tar.gz',
+    keywords=['BPE', 'LLM', 'tokenization'], 
+    install_requires=[
+        'numpy',
+        'tiktoken',
+        'regex',
+    ],
+    packages=find_packages(),  # Automatically include subpackages
+    include_package_data=True, # Include non-Python files specified in MANIFEST.in
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
