bc335e3649893ba067b8e22191caa03065e1fa77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:05:08 +0000

    Add package django-esewa/0.1.2/django_esewa-0.1.2.tar.gz

---
+++ packages/django-esewa/django_esewa-0.1.2.tar.gz/django_esewa-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-esewa",
+    version="0.1.2",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    description="A Django utility for eSewa signature generation.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Nischal Lamichhane",
+    author_email="nischallc56@gmail.com",
+    url="https://github.com/hehenischal/django-esewa",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.6",
+)
