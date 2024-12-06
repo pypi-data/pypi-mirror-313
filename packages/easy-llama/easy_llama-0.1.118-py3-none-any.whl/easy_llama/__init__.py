5277de8681797b4e97e2e88461d41eaece6debe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:33 +0000

    Add package easy-llama/0.1.118/easy_llama-0.1.118-py3-none-any.whl

---
+++ packages/easy-llama/easy_llama-0.1.118-py3-none-any.whl/easy_llama/__init__.py
@@ -0,0 +1,21 @@
+# __init__.py
+# https://github.com/ddh0/easy-llama/
+from ._version import __version__, __llama_cpp_version__
+
+"""
+Text generation in Python, as easy as possible
+
+For documentation, see:
+https://github.com/ddh0/easy-llama/blob/main/DOCS.md
+"""
+
+from . import samplers
+from . import formats
+from . import thread
+from . import webui
+from . import utils
+from . import model
+
+from .model  import Model
+from .thread import Thread
+from .webui import WebUI
