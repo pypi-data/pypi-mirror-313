d1beca1640dd9dac71a67eed15fc313dcb143ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:45 +0000

    Add package tja/0.3.1/tja-0.3.1-cp38-abi3-manylinux_2_5_i686.manylinux1_i686.whl

---
+++ packages/tja/tja-0.3.1-cp38-abi3-manylinux_2_5_i686.manylinux1_i686.whl/tja/__init__.pyi
@@ -0,0 +1,31 @@
+from typing import Dict, List, Optional
+
+class PyNote:
+    note_type: str
+    timestamp: float
+    scroll: float
+    delay: float
+    bpm: float
+    gogo: bool
+
+class PySegment:
+    measure_num: int
+    measure_den: int
+    barline: bool
+    branch: Optional[str]
+    branch_condition: Optional[str]
+    notes: List[PyNote]
+
+class PyChart:
+    player: int
+    course: Optional[str]
+    level: Optional[int]
+    balloons: List[int]
+    headers: Dict[str, str]
+    segments: List[PySegment]
+
+class PyParsedTJA:
+    metadata: Dict[str, str]
+    charts: List[PyChart]
+
+def parse_tja(content: str) -> PyParsedTJA: ... 
