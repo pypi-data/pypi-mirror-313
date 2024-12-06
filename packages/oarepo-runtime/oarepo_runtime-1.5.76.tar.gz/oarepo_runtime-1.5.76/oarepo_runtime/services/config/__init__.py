413b994a69ab70fff876703491ab0085fcfce6a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:02 +0000

    Add package oarepo-runtime/1.5.76/oarepo_runtime-1.5.76.tar.gz

---
+++ packages/oarepo-runtime/oarepo_runtime-1.5.76.tar.gz/oarepo_runtime-1.5.76/oarepo_runtime/services/config/__init__.py
@@ -0,0 +1,29 @@
+from .link_conditions import (
+    has_draft,
+    has_permission,
+    has_permission_file_service,
+    has_published_record,
+    is_draft_record,
+    is_published_record,
+)
+from .permissions_presets import (
+    AuthenticatedPermissionPolicy,
+    EveryonePermissionPolicy,
+    OaiHarvesterPermissionPolicy,
+    ReadOnlyPermissionPolicy,
+)
+from .service import PermissionsPresetsConfigMixin
+
+__all__ = (
+    "PermissionsPresetsConfigMixin",
+    "OaiHarvesterPermissionPolicy",
+    "ReadOnlyPermissionPolicy",
+    "EveryonePermissionPolicy",
+    "AuthenticatedPermissionPolicy",
+    "is_published_record",
+    "is_draft_record",
+    "has_draft",
+    "has_permission",
+    "has_permission_file_service",
+    "has_published_record",
+)
