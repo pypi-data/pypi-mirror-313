6cdcb74011f85ff264915f1a2a7e88dee8f0c94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:08 +0000

    Add package mlrun/1.8.0rc6/mlrun-1.8.0rc6-py3-none-any.whl

---
+++ packages/mlrun/mlrun-1.8.0rc6-py3-none-any.whl/mlrun/model_monitoring/applications/base.py
@@ -0,0 +1,167 @@
+# Copyright 2023 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from abc import ABC, abstractmethod
+from typing import Any, Optional, Union, cast
+
+import mlrun
+import mlrun.model_monitoring.applications.context as mm_context
+import mlrun.model_monitoring.applications.results as mm_results
+from mlrun.serving.utils import MonitoringApplicationToDict
+
+
+class ModelMonitoringApplicationBase(MonitoringApplicationToDict, ABC):
+    """
+    The base class for a model monitoring application.
+    Inherit from this class to create a custom model monitoring application.
+
+    For example, :code:`MyApp` below is a simplistic custom application::
+
+        class MyApp(ModelMonitoringApplicationBase):
+            def do_tracking(
+                self,
+                monitoring_context: mm_context.MonitoringApplicationContext,
+            ) -> ModelMonitoringApplicationResult:
+                monitoring_context.log_artifact(
+                    TableArtifact(
+                        "sample_df_stats", df=self.dict_to_histogram(sample_df_stats)
+                    )
+                )
+                return ModelMonitoringApplicationResult(
+                    name="data_drift_test",
+                    value=0.5,
+                    kind=mm_constant.ResultKindApp.data_drift,
+                    status=mm_constant.ResultStatusApp.detected,
+                )
+    """
+
+    kind = "monitoring_application"
+
+    def do(
+        self, monitoring_context: mm_context.MonitoringApplicationContext
+    ) -> tuple[
+        list[
+            Union[
+                mm_results.ModelMonitoringApplicationResult,
+                mm_results.ModelMonitoringApplicationMetric,
+            ]
+        ],
+        mm_context.MonitoringApplicationContext,
+    ]:
+        """
+        Process the monitoring event and return application results & metrics.
+        Note: this method is internal and should not be called directly or overridden.
+
+        :param monitoring_context:   (MonitoringApplicationContext) The monitoring application context.
+        :returns:                    A tuple of:
+                                        [0] = list of application results that can be either from type
+                                        `ModelMonitoringApplicationResult`
+                                        or from type `ModelMonitoringApplicationResult`.
+                                        [1] = the original application event, wrapped in `MonitoringApplicationContext`
+                                         object
+        """
+        results = self.do_tracking(monitoring_context=monitoring_context)
+        if isinstance(results, dict):
+            results = [
+                mm_results.ModelMonitoringApplicationMetric(name=key, value=value)
+                for key, value in results.items()
+            ]
+        results = results if isinstance(results, list) else [results]
+        return results, monitoring_context
+
+    def _handler(self, context: "mlrun.MLClientCtx"):
+        """
+        A custom handler that wraps the application's logic implemented in
+        :py:meth:`~mlrun.model_monitoring.applications.ModelMonitoringApplicationBase.do_tracking`
+        for an MLRun job.
+        This method should not be called directly.
+        """
+        monitoring_context = mm_context.MonitoringApplicationContext(
+            event={},
+            application_name=self.__class__.__name__,
+            logger=context.logger,
+            artifacts_logger=context,
+        )
+        result = self.do_tracking(monitoring_context)
+        return result
+
+    @classmethod
+    def evaluate(
+        cls,
+        func_path: Optional[str] = None,
+        func_name: Optional[str] = None,
+        tag: Optional[str] = None,
+        run_local: bool = True,
+    ) -> "mlrun.RunObject":
+        """
+        Call this function to run the application's
+        :py:meth:`~mlrun.model_monitoring.applications.ModelMonitoringApplicationBase.do_tracking`
+        model monitoring logic as a :py:class:`~mlrun.runtimes.KubejobRuntime`, which is an MLRun function.
+
+        :param func_path: The path to the function. If not passed, the current notebook is used.
+        :param func_name: The name of the function. If not passed, the class name is used.
+        :param tag:       An optional tag for the function.
+        :param run_local: Whether to run the function locally or remotely.
+
+        :returns: The output of the
+                  :py:meth:`~mlrun.model_monitoring.applications.ModelMonitoringApplicationBase.do_tracking`
+                  method wrapped in a :py:class:`~mlrun.model.RunObject`.
+        """
+        if not run_local:
+            raise NotImplementedError  # ML-8360
+
+        project = cast("mlrun.MlrunProject", mlrun.get_current_project())
+        class_name = cls.__name__
+        name = func_name if func_name is not None else class_name
+        handler = f"{class_name}::{cls._handler.__name__}"
+
+        job = cast(
+            mlrun.runtimes.KubejobRuntime,
+            project.set_function(
+                func=func_path,
+                name=name,
+                kind=mlrun.runtimes.KubejobRuntime.kind,
+                handler=handler,
+                tag=tag,
+            ),
+        )
+        run_result = job.run(local=run_local)
+        return run_result
+
+    @abstractmethod
+    def do_tracking(
+        self,
+        monitoring_context: mm_context.MonitoringApplicationContext,
+    ) -> Union[
+        mm_results.ModelMonitoringApplicationResult,
+        list[
+            Union[
+                mm_results.ModelMonitoringApplicationResult,
+                mm_results.ModelMonitoringApplicationMetric,
+            ]
+        ],
+        dict[str, Any],
+    ]:
+        """
+        Implement this method with your custom monitoring logic.
+
+        :param monitoring_context:      (MonitoringApplicationContext) The monitoring context to process.
+
+        :returns:                       (ModelMonitoringApplicationResult) or
+                                        (list[Union[ModelMonitoringApplicationResult,
+                                        ModelMonitoringApplicationMetric]])
+                                        or dict that contains the application metrics only (in this case the name of
+                                        each metric name is the key and the metric value is the corresponding value).
+        """
+        raise NotImplementedError
