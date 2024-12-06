4714305f9cbe8560266106f7b34af9c4c91aa143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:23 +0000

    Add package spacecases-common/0.7.0/spacecases_common-0.7.0-py3-none-any.whl

---
+++ packages/spacecases-common/spacecases_common-0.7.0-py3-none-any.whl/spacecases_common/__init__.py
@@ -0,0 +1,114 @@
+import re
+import string
+from dataclasses import dataclass
+from typing import Optional
+from enum import IntEnum
+from abc import ABC, abstractmethod
+
+__all__ = [
+    "Rarity",
+    "ItemMetadatum",
+    "SkinMetadatum",
+    "StickerMetadatum",
+    "Container",
+    "remove_skin_name_formatting",
+]
+
+
+class Rarity(IntEnum):
+    Common = 0
+    Uncommon = 1
+    Rare = 2
+    Mythical = 3
+    Legendary = 4
+    Ancient = 5
+    Contraband = 6
+
+    def get_name_for_skin(self) -> str:
+        """
+        Get the rarity string if the item is a skin
+        """
+        return [
+            "Consumer Grade",
+            "Industrial Grade",
+            "Mil-Spec",
+            "Restricted",
+            "Classified",
+            "Covert",
+            "Contraband",
+        ][self.value]
+
+    def get_name_for_regular_item(self) -> str:
+        """
+        Get the rarity string if the item is a regular item
+        """
+        return [
+            "Base Grade",
+            "Industrial Grade",
+            "High Grade",
+            "Remarkable",
+            "Exotic",
+            "Extraordinary",
+            "Contraband",
+        ][self.value]
+
+
+@dataclass
+class ItemMetadatum(ABC):
+    """
+    Base class for different types of ItemMetadatum. Every single item contains atleast these fields and implements these methods.
+    """
+
+    formatted_name: str
+    rarity: Rarity
+    price: int
+    image_url: str
+
+    @abstractmethod
+    def get_rarity_string(self) -> str:
+        """
+        Obtain the corresponding rarity string on the type of the item
+        """
+
+
+@dataclass
+class SkinMetadatum(ItemMetadatum):
+    """
+    An ItemMetadatum that also includes an optional description and a float range.
+    """
+
+    description: Optional[str]
+    min_float: float
+    max_float: float
+
+    def get_rarity_string(self) -> str:
+        return self.rarity.get_name_for_skin()
+
+
+@dataclass
+class StickerItemMetadatum(ItemMetadatum):
+    """A sticker is just a default item"""
+
+    def get_rarity_string(self) -> str:
+        return self.rarity.get_name_for_regular_item()
+
+
+@dataclass
+class Container:
+    formatted_name: str
+    image_url: str
+    price: int
+    contains: dict[Rarity, list[str]]
+
+
+_SPECIAL_CHARS_REGEX = re.compile(r"[™★♥\s]")
+
+
+def remove_skin_name_formatting(skin_name: str) -> str:
+    """
+    Removes formatting from skin names:
+    - Converts to lowercase
+    - Removes punctuation, whitespace and special characters
+    """
+    skin_name = _SPECIAL_CHARS_REGEX.sub("", skin_name.lower())
+    return skin_name.translate(str.maketrans("", "", string.punctuation))
