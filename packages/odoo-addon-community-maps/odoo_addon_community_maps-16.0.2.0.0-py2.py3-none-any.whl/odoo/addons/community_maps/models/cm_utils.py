9ebafa96d5cd30a6b11b8368cf901349054a5465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:28 +0000

    Add package odoo-addon-community-maps/16.0.2.0.0/odoo_addon_community_maps-16.0.2.0.0-py2.py3-none-any.whl

---
+++ packages/odoo-addon-community-maps/odoo_addon_community_maps-16.0.2.0.0-py2.py3-none-any.whl/odoo/addons/community_maps/models/cm_utils.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+class CmUtils(object):
+    @staticmethod
+    def get_system_crowdfunding_types_selection():
+        return [
+            ("none", "None"),
+            ("invoicing_amount", "Invoicing goal"),
+            ("submission_amount", "Submissions goal"),
+        ]
+
+    @staticmethod
+    def get_create_existing_model(model_env, query, creation_data=False):
+        existing_model = model_env.search(query)
+        create_model = True
+        if existing_model:
+            model = existing_model
+            create_model = False
+        if create_model and creation_data:
+            model = model_env.create(creation_data)
+        return model
