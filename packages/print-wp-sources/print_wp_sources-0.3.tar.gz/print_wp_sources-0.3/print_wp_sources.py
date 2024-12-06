535fefaa49821de4e51f166887ca9cfbc70b84f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:05:18 +0000

    Add package print-wp-sources/0.3/print_wp_sources-0.3.tar.gz

---
+++ packages/print-wp-sources/print_wp_sources-0.3.tar.gz/print_wp_sources-0.3/print_wp_sources.py
@@ -0,0 +1,21 @@
+import sys
+from urllib.parse import unquote
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
+
+def main():
+    if len(sys.argv) != 2:
+        exit("Usage: dl_wp_pdf ARTICLE_NAME")
+    article = sys.argv[1]
+
+    url = f"https://en.wikipedia.org/wiki/{article}"
+    text = urlopen(url).read()
+    soup = BeautifulSoup(text, "html.parser")
+    for link in soup.find_all("a", attrs={"class": "external text"}):
+        print(unquote(link.get("href")))
+
+
+if __name__ == "__main__":
+    main()
