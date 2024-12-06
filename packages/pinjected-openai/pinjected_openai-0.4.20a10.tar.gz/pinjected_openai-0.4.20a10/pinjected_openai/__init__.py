2719bbcc748e96c1b5ddb4c9c0718020dcb48348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:41 +0000

    Add package pinjected-openai/0.4.20a10/pinjected_openai-0.4.20a10.tar.gz

---
+++ packages/pinjected-openai/pinjected_openai-0.4.20a10.tar.gz/pinjected_openai-0.4.20a10/pinjected_openai/__init__.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+import loguru
+from pinjected import *
+import pinjected
+
+from pinjected_openai.clients import async_openai_client, openai_api_key
+default_design = design(
+    cache_root_path=Path("~/.cache/pinjected_openai").expanduser(),
+) + providers(
+    logger=lambda: loguru.logger,
+    async_openai_client=async_openai_client,
+    #openai_api_key=openai_api_key
+)
+__meta_design__ = instances(
+    #default_design_path="pinjected_openai.default_design"
+    overrides=default_design
+)
+
+
