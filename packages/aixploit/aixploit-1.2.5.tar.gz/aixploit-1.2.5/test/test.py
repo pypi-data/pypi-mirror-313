64f6403e6f579cc86c0a84022d930407322e3d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:12:25 +0000

    Add package aixploit/1.2.5/aixploit-1.2.5.tar.gz

---
+++ packages/aixploit/aixploit-1.2.5.tar.gz/aixploit-1.2.5/test/test.py
@@ -0,0 +1,59 @@
+import os
+from datetime import datetime
+from aixploit.plugins import PromptInjection, Privacy, Integrity, Availability, Abuse
+from aixploit.core import run
+
+
+target = ["Openai", "", "gpt-3.5-turbo"]
+attackers = [
+    PromptInjection("quick"),
+    Privacy("quick"),
+    Integrity("quick"),
+    Availability("quick"),
+    Abuse("quick"),
+    # PromptInjection("full")
+]
+
+start_time = datetime.now()
+print("Redteaming exercise started at : ", start_time.strftime("%H:%M:%S"))
+
+(
+    conversation,
+    attack_prompts,
+    success_rates_percentage,
+    total_tokens,
+    total_cost,
+) = run(attackers, target, os.getenv("OPENAI_KEY"))
+
+for idx, attacker in enumerate(attackers):  # {{ edit_1 }}
+    try:
+        print("Attacker: ", attacker.__class__.__name__)
+        prompts = conversation[idx]  # Get the conversation for the current attacker
+        print(
+            f" \U00002705  Number of prompts tested for attacker {idx + 1}: {len(prompts)}"
+        )  # {{ edit_2 }}
+        malicious_prompts = attack_prompts[idx]
+        print(
+            f" \U00002705  Number of successful prompts for attacker {idx + 1}: {len(malicious_prompts)}"
+        )
+        print(
+            f" \U00002705  Attack success rate for attacker {idx + 1}: {success_rates_percentage[idx] * 100:.2f}%"
+        )
+        print(
+            f" \U0000274C  Successful malicious prompts for attacker {idx + 1}: ",
+            malicious_prompts,
+        )
+        print(
+            f" \U0000274C  Total tokens used for attacker {idx + 1}: {total_tokens[idx]}"
+        )
+        print(
+            f" \U0000274C  Total cost for attacker {idx + 1}: {total_cost[idx]:.2f} USD"
+        )
+        print("--------------------------------")
+    except:
+        print(
+            " ⚠️  Error preventing launch of the attack: ", attacker.__class__.__name__
+        )
+
+print("Redteaming exercise ended at : ", datetime.now().strftime("%H:%M:%S"))
+print("Total time taken: ", datetime.now() - start_time)
