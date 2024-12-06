c8b9490a6026561ece84c7870b4d7d94d80cfad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:49 +0000

    Add package avro-py/2024.12.5/avro_py-2024.12.5.tar.gz

---
+++ packages/avro-py/avro_py-2024.12.5.tar.gz/avro_py-2024.12.5/src/avro/__init__.py
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: MIT
+
+
+"""
+A modern Pythonic implementation of Avro Phonetic.
+
+Please refer to the GitHub repository for examples on getting started:
+    https://github.com/hitblast/avro.py
+
+Licensed under the terms of the MIT License.
+"""
+
+# Import local modules.
+from .main import *
+
+# Version information.
+__version_info__ = (2024, 10, 30)
+__version__ = ".".join(map(str, __version_info__))
