05b17ccb90406c34533ac21c6aa128a3049b16e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:05:07 +0000

    Add package shap-explainer/0.1.6/shap_explainer-0.1.6.tar.gz

---
+++ packages/shap-explainer/shap_explainer-0.1.6.tar.gz/shap_explainer-0.1.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.6'
+DESCRIPTION = 'SHAP explainer with ChatGPT API'
+
+# Setting up
+setup(
+    name="shap_explainer",
+    version=VERSION,
+    author="raytanghw (Ray Tang)",
+    author_email="<raytanghw@gmail.com>",
+    description=DESCRIPTION,
+    packages=['shap_explainer'],
+    install_requires=[
+        'shap',
+        'langchain'
+    ],
+    keywords=['python', 'shap', 'explainer', 'llm', 'chatgpt', 'xai'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
