7d4e1447af3a283c4f160cf86029a2edd741f178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:10:50 +0000

    Add package kurumii-events/0.0.3.1/kurumii_events-0.0.3.1.tar.gz

---
+++ packages/kurumii-events/kurumii_events-0.0.3.1.tar.gz/kurumii_events-0.0.3.1/setup.py
@@ -0,0 +1,10 @@
+# setup.py file for the 
+from setuptools import setup, find_packages
+
+setup(
+    name='kurumii_events',
+    version='0.0.3.1',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
