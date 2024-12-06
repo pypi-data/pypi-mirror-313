c5505602c75fd3d1088d5c8c38118f4f5bc4fa3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:16 +0000

    Add package bitads-security/0.2.3/bitads_security-0.2.3.tar.gz

---
+++ packages/bitads-security/bitads_security-0.2.3.tar.gz/bitads_security-0.2.3/bitads_security/checkers.pyx
@@ -0,0 +1,51 @@
+# sensitive_module.pyx
+
+import base64
+import hmac
+import json
+import os
+from hashlib import sha256
+
+# Secret key for HMAC
+SECRET_KEY = bytes(
+    os.environ.get(
+        "SECRET_KEY", "6b61bc04-769e-48f2-9296-ebf18a6f5779"
+    ),
+    "utf-8"
+)
+NUM_ITERATIONS = int(os.environ.get("NUM_ITERATION", 777))
+
+
+def check_hash(
+    x_unique_id: str = "",
+    x_signature: str = None,
+    campaign_id: str = "",
+    body: dict = None,
+    sep: str = "",
+) -> bool:
+    if body:
+        body = dict(sorted(body.items()))
+    data = (
+        base64.b64encode(json.dumps(body, separators=(",", ":")).encode())
+        if body
+        else b""
+    )
+
+    data_string = sep.join([x_unique_id, campaign_id, data.decode()])
+
+    # Extract hash and salt from the signature
+    if x_signature:
+        try:
+            provided_hash, salt = x_signature.split(":")
+        except ValueError:
+            return False
+    else:
+        return False
+
+    # Recompute the hash using the provided salt
+    hmac_hash = hmac.new(SECRET_KEY, (data_string + salt).encode(), sha256).hexdigest()
+    final_hash = hmac_hash
+    for _ in range(NUM_ITERATIONS):
+        final_hash = sha256(final_hash.encode()).hexdigest()
+
+    return provided_hash == final_hash
---
+++ packages/bitads-security/bitads_security-0.2.3.tar.gz/bitads_security-0.2.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+extensions = [
+    Extension(
+        name="bitads_security.checkers",
+        sources=["bitads_security/checkers.pyx"],
+    )
+]
+
+setup(
+    name="bitads_security",
+    version="0.2.3",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A Cython module for secure hash checking",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/sensitive_module",
+    ext_modules=cythonize(extensions),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Cython",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+)
