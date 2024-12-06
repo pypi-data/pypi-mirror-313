52f22dce80d66f6719bb21ce8362e9ee2e9f2a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:51 +0000

    Add package nomad-media-cli/0.0.1a12/nomad_media_cli-0.0.1a12-py3-none-any.whl

---
+++ packages/nomad-media-cli/nomad_media_cli-0.0.1a12-py3-none-any.whl/nomad_media_cli/commands/init.py
@@ -0,0 +1,31 @@
+import click
+import json
+import os
+
+@click.command()
+@click.option("--service-api-url", required=True, help="API URL for the service")
+@click.option("--api-type", default="admin", type=click.Choice(['admin', 'portal']), help="API type (i.e. admin, portal )")
+@click.option("--debug-mode", default="false", type=click.Choice(['true', 'false']), help="Enable debug mode")
+@click.option("--singleton", default="true", type=click.Choice(['true', 'false']), help="Enable singleton mode")
+@click.pass_context
+def init(ctx, service_api_url, api_type, debug_mode, singleton):
+    """Initialize the SDK and save configuration"""
+    
+    config_path = ctx.obj["config_path"]
+    config_dir = os.path.dirname(config_path)
+    os.makedirs(config_dir, exist_ok=True)
+    
+    config = {
+        "serviceApiUrl": service_api_url,
+        "apiType": api_type,
+        "debugMode": False,
+        "singleton": singleton == "true",
+        "disableLogging": True
+    }
+    
+    try:
+        with open(config_path, "w") as file:
+            json.dump(config, file, indent=4)
+    
+    except Exception as e:
+        click.echo({ "error": f"Error saving configuration: {e}" })
