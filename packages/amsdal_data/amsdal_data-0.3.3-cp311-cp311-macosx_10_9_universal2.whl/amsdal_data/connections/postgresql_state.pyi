d6c18b20fd45d96cdeac5afda65caa3925bbf4aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:03 +0000

    Add package amsdal_data/0.3.3/amsdal_data-0.3.3-cp311-cp311-macosx_10_9_universal2.whl

---
+++ packages/amsdal_data/amsdal_data-0.3.3-cp311-cp311-macosx_10_9_universal2.whl/amsdal_data/connections/postgresql_state.pyi
@@ -0,0 +1,23 @@
+import amsdal_glue as glue
+from _typeshed import Incomplete
+from typing import Any
+
+class PostgresStateConnection(glue.PostgresConnection):
+    def _default_envs(self) -> dict[str, Any]: ...
+    _connection: Incomplete
+    def connect(self, dsn: str = '', schema: str | None = None, timezone: str = 'UTC', *, autocommit: bool = True, **kwargs: Any) -> None:
+        """
+        Establishes a connection to the PostgreSQL database.
+
+        Args:
+            dsn (str): The Data Source Name for the connection.
+            schema (str | None): The default schema to be used for the connection. If None,
+                                 the default schema usually is 'public'.
+            timezone (str): The timezone to be used for the connection.
+            autocommit (bool): Whether to enable autocommit mode.
+            **kwargs: Additional connection parameters.
+
+        Raises:
+            ConnectionError: If the connection is already established.
+            ImportError: If the 'psycopg' package is not installed.
+        """
