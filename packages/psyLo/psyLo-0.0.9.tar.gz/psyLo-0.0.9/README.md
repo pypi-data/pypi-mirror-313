04f230ecf1338cc468f9180cd73f056cf1e168b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:41 +0000

    Add package psyLo/0.0.9/psyLo-0.0.9.tar.gz

---
+++ packages/psyLo/psyLo-0.0.9.tar.gz/psyLo-0.0.9/README.md
@@ -0,0 +1,2 @@
+# test
+ - this is a test to see whether the setup is fetching the md file properly
---
+++ packages/psyLo/psyLo-0.0.9.tar.gz/psyLo-0.0.9/psyLo/__init__.py
@@ -0,0 +1,48 @@
+# to allow users to do things like - from psyLo import testLib
+from .main import (
+    testLib,
+    printModules
+)
+
+
+# nlp
+from .nlp import (
+    rmExcessWhitespaces,
+    rmHTML,
+    rmURL,
+    rmSpecialChar,
+    rmPunctuation,
+    rmNumbers,
+    tokenize,
+    rmStopWords,
+    wordNetLemmatize,
+    porterStemmer,
+
+    sentimentLogReg,
+    sentimentRandomForest,
+
+    textGeneration,
+    seq2seq,
+
+    help,
+    example,
+
+    sourceTextPreprocessing,
+    sourceSentimentAnalysis,
+    sourceTextGeneration,
+    sourceSeq2seq,
+    sourceImports
+)
+
+# Deep Learning
+
+from .dl import (
+    sourceANN,
+    sourceCNN,
+    sourceRNN,
+    sourceLSTM,
+    sourceTransLearn,
+    sourceRBM,
+    sourceImports,
+    help
+)
