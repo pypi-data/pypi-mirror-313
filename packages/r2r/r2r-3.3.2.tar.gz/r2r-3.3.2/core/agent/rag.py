bf24bc5f3be4bcd36829fccd2b8d1b8e551c46e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:49 +0000

    Add package r2r/3.3.2/r2r-3.3.2.tar.gz

---
+++ packages/r2r/r2r-3.3.2.tar.gz/r2r-3.3.2/core/agent/rag.py
@@ -0,0 +1,118 @@
+from typing import Union
+
+from core.agent import R2RAgent, R2RStreamingAgent
+from core.base import (
+    format_search_results_for_llm,
+    format_search_results_for_stream,
+)
+from core.base.abstractions import (
+    AggregateSearchResult,
+    GraphSearchSettings,
+    SearchSettings,
+)
+from core.base.agent import AgentConfig, Tool
+from core.base.providers import CompletionProvider
+from core.base.utils import to_async_generator
+from core.pipelines import SearchPipeline
+from core.providers import (
+    LiteLLMCompletionProvider,
+    OpenAICompletionProvider,
+    PostgresDBProvider,
+)
+
+
+class RAGAgentMixin:
+    def __init__(self, search_pipeline: SearchPipeline, *args, **kwargs):
+        self.search_pipeline = search_pipeline
+        super().__init__(*args, **kwargs)
+
+    def _register_tools(self):
+        if not self.config.tool_names:
+            return
+        for tool_name in self.config.tool_names:
+            if tool_name == "search":
+                self._tools.append(self.search_tool())
+            else:
+                raise ValueError(f"Unsupported tool name: {tool_name}")
+
+    def search_tool(self) -> Tool:
+        return Tool(
+            name="search",
+            description="Search for information using the R2R framework",
+            results_function=self.search,
+            llm_format_function=RAGAgentMixin.format_search_results_for_llm,
+            stream_function=RAGAgentMixin.format_search_results_for_stream,
+            parameters={
+                "type": "object",
+                "properties": {
+                    "query": {
+                        "type": "string",
+                        "description": "The query to search the local vector database with.",
+                    },
+                },
+                "required": ["query"],
+            },
+        )
+
+    async def search(
+        self,
+        query: str,
+        search_settings: SearchSettings,
+        *args,
+        **kwargs,
+    ) -> list[AggregateSearchResult]:
+        response = await self.search_pipeline.run(
+            to_async_generator([query]),
+            state=None,
+            search_settings=search_settings,
+        )
+        return response
+
+    @staticmethod
+    def format_search_results_for_stream(
+        results: AggregateSearchResult,
+    ) -> str:
+        return format_search_results_for_stream(results)
+
+    @staticmethod
+    def format_search_results_for_llm(
+        results: AggregateSearchResult,
+    ) -> str:
+        return format_search_results_for_llm(results)
+
+
+class R2RRAGAgent(RAGAgentMixin, R2RAgent):
+    def __init__(
+        self,
+        database_provider: PostgresDBProvider,
+        llm_provider: Union[
+            LiteLLMCompletionProvider, OpenAICompletionProvider
+        ],
+        search_pipeline: SearchPipeline,
+        config: AgentConfig,
+    ):
+        super().__init__(
+            database_provider=database_provider,
+            search_pipeline=search_pipeline,
+            llm_provider=llm_provider,
+            config=config,
+        )
+
+
+class R2RStreamingRAGAgent(RAGAgentMixin, R2RStreamingAgent):
+    def __init__(
+        self,
+        database_provider: PostgresDBProvider,
+        llm_provider: Union[
+            LiteLLMCompletionProvider, OpenAICompletionProvider
+        ],
+        search_pipeline: SearchPipeline,
+        config: AgentConfig,
+    ):
+        config.stream = True
+        super().__init__(
+            database_provider=database_provider,
+            search_pipeline=search_pipeline,
+            llm_provider=llm_provider,
+            config=config,
+        )
