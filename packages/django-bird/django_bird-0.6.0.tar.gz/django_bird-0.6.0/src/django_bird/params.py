9b1aff57ac2b0dc8b3207dd9406ea9f1ab4629ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:23 +0000

    Add package django-bird/0.6.0/django_bird-0.6.0.tar.gz

---
+++ packages/django-bird/django_bird-0.6.0.tar.gz/django_bird-0.6.0/src/django_bird/params.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from dataclasses import field
+
+from django import template
+from django.template.base import NodeList
+from django.template.context import Context
+from django.utils.safestring import SafeString
+from django.utils.safestring import mark_safe
+
+from ._typing import TagBits
+
+
+@dataclass
+class Param:
+    name: str
+    value: str | bool | None
+
+    def render(self, context: Context) -> str:
+        if self.value is None or (
+            isinstance(self.value, str) and self.value == "False"
+        ):
+            return ""
+        if (isinstance(self.value, bool) and self.value) or (
+            isinstance(self.value, str) and self.value == "True"
+        ):
+            return self.name
+        try:
+            value = template.Variable(str(self.value)).resolve(context)
+        except template.VariableDoesNotExist:
+            value = self.value
+        return f'{self.name}="{value}"'
+
+    @classmethod
+    def from_bit(cls, bit: str):
+        value: str | bool
+        if "=" in bit:
+            name, value = bit.split("=", 1)
+            value = value.strip("'\"")
+        else:
+            name, value = bit, True
+        return cls(name, value)
+
+
+@dataclass
+class Params:
+    attrs: list[Param] = field(default_factory=list)
+    props: list[Param] = field(default_factory=list)
+
+    def render_props(self, nodelist: NodeList | None, context: Context):
+        from django_bird.templatetags.tags.prop import PropNode
+
+        if nodelist is None:
+            return
+
+        attrs_to_remove = set()
+
+        for node in nodelist:
+            if not isinstance(node, PropNode):
+                continue
+
+            value: str | bool | None = node.default
+
+            for idx, attr in enumerate(self.attrs):
+                if node.name == attr.name:
+                    if attr.value:
+                        value = attr.value
+                    attrs_to_remove.add(idx)
+
+            self.props.append(Param(name=node.name, value=value))
+
+        for idx in sorted(attrs_to_remove, reverse=True):
+            self.attrs.pop(idx)
+
+        return {prop.name: prop.value for prop in self.props}
+
+    def render_attrs(self, context: Context) -> SafeString:
+        rendered = " ".join(attr.render(context) for attr in self.attrs)
+        return mark_safe(rendered)
+
+    @classmethod
+    def from_bits(cls, bits: TagBits):
+        params: list[Param] = []
+        for bit in bits:
+            param = Param.from_bit(bit)
+            params.append(param)
+        return cls(attrs=params)
---
+++ packages/django-bird/django_bird-0.6.0.tar.gz/django_bird-0.6.0/tests/test_params.py
@@ -0,0 +1,102 @@
+from __future__ import annotations
+
+import pytest
+
+from django_bird.params import Param
+from django_bird.params import Params
+from django_bird.templatetags.tags.prop import PropNode
+
+
+class TestParam:
+    @pytest.mark.parametrize(
+        "param,context,expected",
+        [
+            (Param(name="class", value="btn"), {}, 'class="btn"'),
+            (Param(name="class", value="btn"), {"btn": "blue"}, 'class="blue"'),
+            (Param(name="disabled", value=True), {}, "disabled"),
+            (Param(name="disabled", value=None), {}, ""),
+        ],
+    )
+    def test_render(self, param, context, expected):
+        assert param.render(context) == expected
+
+    @pytest.mark.parametrize(
+        "bit,expected",
+        [
+            ("class='btn'", Param(name="class", value="btn")),
+            ('class="btn"', Param(name="class", value="btn")),
+            ("disabled", Param(name="disabled", value=True)),
+        ],
+    )
+    def test_from_bit(self, bit, expected):
+        assert Param.from_bit(bit) == expected
+
+
+class TestParams:
+    @pytest.mark.parametrize(
+        "params,nodelist,context,expected_props,expected_attrs",
+        [
+            (
+                Params(attrs=[Param(name="class", value=None)]),
+                [PropNode(name="class", default="btn", attrs=[])],
+                {},
+                {"class": "btn"},
+                [],
+            ),
+            (
+                Params(attrs=[Param(name="class", value="btn")]),
+                [PropNode(name="class", default=None, attrs=[])],
+                {},
+                {"class": "btn"},
+                [],
+            ),
+            (
+                Params(attrs=[Param(name="class", value="btn")]),
+                None,
+                {},
+                None,
+                [Param(name="class", value="btn")],
+            ),
+        ],
+    )
+    def test_render_props(
+        self, params, nodelist, context, expected_props, expected_attrs
+    ):
+        assert params.render_props(nodelist, context) == expected_props
+        assert params.attrs == expected_attrs
+
+    @pytest.mark.parametrize(
+        "params,context,expected",
+        [
+            (Params(attrs=[Param(name="class", value="btn")]), {}, 'class="btn"'),
+            (
+                Params(attrs=[Param(name="class", value="btn")]),
+                {"btn": "blue"},
+                'class="blue"',
+            ),
+            (Params(attrs=[Param(name="disabled", value=True)]), {}, "disabled"),
+            (
+                Params(
+                    attrs=[
+                        Param(name="class", value="btn"),
+                        Param(name="disabled", value=True),
+                    ]
+                ),
+                {},
+                'class="btn" disabled',
+            ),
+        ],
+    )
+    def test_render_attrs(self, params, context, expected):
+        assert params.render_attrs(context) == expected
+
+    @pytest.mark.parametrize(
+        "bits,expected",
+        [
+            (["class='btn'"], Params(attrs=[Param(name="class", value="btn")])),
+            (['class="btn"'], Params(attrs=[Param(name="class", value="btn")])),
+            (["disabled"], Params(attrs=[Param(name="disabled", value=True)])),
+        ],
+    )
+    def test_from_bits(self, bits, expected):
+        assert Params.from_bits(bits) == expected
