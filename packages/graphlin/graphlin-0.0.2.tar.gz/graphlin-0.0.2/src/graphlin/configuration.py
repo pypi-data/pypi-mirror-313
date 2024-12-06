20222523c1e0e709fa6effb37ebe4ef9afa1ce78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:16 +0000

    Add package graphlin/0.0.2/graphlin-0.0.2.tar.gz

---
+++ packages/graphlin/graphlin-0.0.2.tar.gz/graphlin-0.0.2/src/graphlin/configuration.py
@@ -0,0 +1,15 @@
+import os
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    LOG_LEVEL: str = "INFO"
+    MULTIPLE_LABELS: bool = False
+
+    model_config = {
+        "env_file": os.environ.get("GRAPHLIN_CONFIG", ".env"),
+    }
+
+
+settings = Settings()
