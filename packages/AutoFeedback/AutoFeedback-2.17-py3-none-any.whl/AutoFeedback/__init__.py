e14741df954ac35eedbc174d61accdbef6772268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:38 +0000

    Add package AutoFeedback/2.17/AutoFeedback-2.17-py3-none-any.whl

---
+++ packages/AutoFeedback/AutoFeedback-2.17-py3-none-any.whl/AutoFeedback/__init__.py
@@ -0,0 +1,12 @@
+from . import _version
+__version__ = _version.get_versions()['version']
+
+try:
+    from AutoFeedback.plotchecks import check_plot
+except ModuleNotFoundError:
+    pass
+
+from AutoFeedback.varchecks import check_vars
+from AutoFeedback.funcchecks import check_func
+from AutoFeedback.plotclass import line
+from AutoFeedback.randomclass import randomvar
