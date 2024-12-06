1045632c3019348aad4e1bf6b2a6e644e8989031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:11:04 +0000

    Add package bizon/0.0.13/bizon-0.0.13.tar.gz

---
+++ packages/bizon/bizon-0.0.13.tar.gz/bizon-0.0.13/bizon/sources/dummy/tests/dummy_pipeline.py
@@ -0,0 +1,33 @@
+from yaml import safe_load
+
+from bizon.engine.engine import RunnerFactory
+
+config_yaml = """
+source:
+  source_name: dummy
+  stream_name: creatures
+  authentication:
+    type: api_key
+    params:
+      token: dummy_key
+
+destination:
+  name: logger
+  config:
+    dummy: dummy
+
+engine:
+  backend:
+    type: postgres
+    config:
+      database: bizon
+      schema: public
+      syncCursorInDBEvery: 2
+      host: localhost
+      port: 5432
+      username: postgres
+      password: bizon
+"""
+config = safe_load(config_yaml)
+runner = RunnerFactory.create_from_config_dict(config=config)
+runner.run()
