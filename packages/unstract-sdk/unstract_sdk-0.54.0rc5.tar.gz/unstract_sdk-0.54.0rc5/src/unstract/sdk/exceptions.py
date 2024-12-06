0993984ca5631dc67bdc7b53443fc6b253d19131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:49 +0000

    Add package unstract-sdk/0.54.0rc5/unstract_sdk-0.54.0rc5.tar.gz

---
+++ packages/unstract-sdk/unstract_sdk-0.54.0rc5.tar.gz/unstract_sdk-0.54.0rc5/src/unstract/sdk/exceptions.py
@@ -0,0 +1,56 @@
+class SdkError(Exception):
+    DEFAULT_MESSAGE = "Something went wrong"
+
+    def __init__(self, message: str = DEFAULT_MESSAGE):
+        super().__init__(message)
+        # Make it user friendly wherever possible
+        self.message = message
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class IndexingError(SdkError):
+    def __init__(self, message: str = ""):
+        if "404" in message:
+            message = "Index not found. Please check vector DB settings."
+        super().__init__(message)
+
+
+class LLMError(SdkError):
+    DEFAULT_MESSAGE = "Error ocurred related to LLM"
+
+
+class EmbeddingError(SdkError):
+    DEFAULT_MESSAGE = "Error ocurred related to embedding"
+
+
+class VectorDBError(SdkError):
+    DEFAULT_MESSAGE = "Error ocurred related to vector DB"
+
+
+class X2TextError(SdkError):
+    DEFAULT_MESSAGE = "Error ocurred related to text extractor"
+
+
+class OCRError(SdkError):
+    DEFAULT_MESSAGE = "Error ocurred related to OCR"
+
+
+class RateLimitError(SdkError):
+    DEFAULT_MESSAGE = "Running into rate limit errors, please try again later"
+
+
+class FileStorageError(SdkError):
+    DEFAULT_MESSAGE = (
+        "Error while connecting with the storage. "
+        "Please check the configuration credentials"
+    )
+
+
+class FileOperationError(SdkError):
+    DEFAULT_MESSAGE = (
+        "Error while performing operation on the file. "
+        "Please check specific storage error for "
+        "further information"
+    )
