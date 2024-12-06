9d2d8e940b481bbe3797040945923e36cdde415e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:25 +0000

    Add package agi-med-common/1.2.0/agi_med_common-1.2.0-py3-none-any.whl

---
+++ packages/agi-med-common/agi_med_common-1.2.0-py3-none-any.whl/agi_med_common/models/chat_item/replica_item.py
@@ -0,0 +1,10 @@
+from pydantic import Field
+
+from .. import _Base
+
+
+class ReplicaItem(_Base):
+    body: str = Field(alias="Body")
+    role: bool = Field(alias="Role", description="True = ai, False = client")
+    date_time: str = Field(alias="DateTime")
+    previous_score: float | None = Field(None, alias="PreviousScore")
