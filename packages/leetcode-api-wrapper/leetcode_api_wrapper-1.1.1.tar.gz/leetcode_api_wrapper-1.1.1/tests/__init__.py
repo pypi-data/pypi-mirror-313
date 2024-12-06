f7bdc5dba0b9ac83ccdc7c719ce6a1802703cc28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:10:36 +0000

    Add package leetcode-api-wrapper/1.1.1/leetcode_api_wrapper-1.1.1.tar.gz

---
+++ packages/leetcode-api-wrapper/leetcode_api_wrapper-1.1.1.tar.gz/leetcode_api_wrapper-1.1.1/tests/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py for tests
+
+"""
+Test suite for the LeetcodeWrapper Python library.
+
+This package contains unit tests for the functionality of the LeetcodeWrapper.
+"""
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
