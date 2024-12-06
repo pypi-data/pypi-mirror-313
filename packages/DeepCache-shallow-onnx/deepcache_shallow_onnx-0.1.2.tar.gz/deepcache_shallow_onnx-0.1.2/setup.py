242ef6c8bc5b478c80a49816e007aa11fe46769e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:29 +0000

    Add package DeepCache-shallow-onnx/0.1.2/deepcache_shallow_onnx-0.1.2.tar.gz

---
+++ packages/DeepCache-shallow-onnx/deepcache_shallow_onnx-0.1.2.tar.gz/deepcache_shallow_onnx-0.1.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DeepCache-shallow-onnx",
+    version="v0.1.2",
+    author="Xinyin Ma",
+    author_email="maxinyin@u.nus.edu",
+    description="DeepCache-shallow-onnx: Accelerating Diffusion Models for Free",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/horseee/DeepCache-shallow-onnx",
+    packages=setuptools.find_packages(exclude=["DeepCache-shallow-onnx.ddpm"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['torch', 'diffusers==0.24.0', 'transformers'],
+    python_requires='>=3.6',
+)
