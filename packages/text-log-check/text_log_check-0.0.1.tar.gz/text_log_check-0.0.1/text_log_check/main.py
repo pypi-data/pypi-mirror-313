e30f87022c9c9f72ee711700d8724830b940ea50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:49 +0000

    Add package text-log-check/0.0.1/text_log_check-0.0.1.tar.gz

---
+++ packages/text-log-check/text_log_check-0.0.1.tar.gz/text_log_check-0.0.1/text_log_check/main.py
@@ -0,0 +1,12 @@
+"""
+Main module
+"""
+
+
+def info():
+    """
+    TextLogCheck info
+    :return: TextLogCheck usage info
+    """
+    result = 'Visit: https://github.com/LibSecSource/text-log-check and enjoy yourself'
+    return result
