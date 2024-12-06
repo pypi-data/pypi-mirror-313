c4b5fe210d4cb6c620f2ba96ddce28f6cad826fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:07 +0000

    Add package ntnx-security-py-client/4.0.1b1/ntnx_security_py_client-4.0.1b1.tar.gz

---
+++ packages/ntnx-security-py-client/ntnx_security_py_client-4.0.1b1.tar.gz/ntnx_security_py_client-4.0.1b1/ntnx_security_py_client/api/__init__.py
@@ -0,0 +1,8 @@
+
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ntnx_security_py_client.api.approval_policies_api import ApprovalPoliciesApi
+from ntnx_security_py_client.api.sti_gs_api import STIGsApi
