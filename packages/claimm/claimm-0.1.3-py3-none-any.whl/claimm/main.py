a5e514ec79333597174c20b19ba3ca1830864af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:05:00 +0000

    Add package claimm/0.1.3/claimm-0.1.3-py3-none-any.whl

---
+++ packages/claimm/claimm-0.1.3-py3-none-any.whl/claimm/main.py
@@ -0,0 +1,12 @@
+def main() -> str:
+    """
+    Entry point for the CLI app.
+    Returns:
+        str: A confirmation message that the app is initialized.
+    """
+    print("Claimm initialized.")
+    return "App initialized"
+
+
+if __name__ == "__main__":
+    main()
