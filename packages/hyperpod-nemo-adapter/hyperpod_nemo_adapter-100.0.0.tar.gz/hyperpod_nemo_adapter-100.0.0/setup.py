95652e4701a63ddc49b5e7a37c61d48aa9631729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:19 +0000

    Add package hyperpod-nemo-adapter/100.0.0/hyperpod_nemo_adapter-100.0.0.tar.gz

---
+++ packages/hyperpod-nemo-adapter/hyperpod_nemo_adapter-100.0.0.tar.gz/hyperpod_nemo_adapter-100.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="hyperpod-nemo-adapter",
+    version="100.0.0",
+    author="sharmajiii",
+    author_email="sharmaji@ict.com",
+    description="sdfsdf sdafaf safafa sfsdf",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
