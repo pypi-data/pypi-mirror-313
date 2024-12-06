d51616ba37e7218e79ad5e1a1a7784567fc33e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:04:46 +0000

    Add package torchio/0.20.3/torchio-0.20.3-py3-none-any.whl

---
+++ packages/torchio/torchio-0.20.3-py3-none-any.whl/torchio/datasets/mni/mni.py
@@ -0,0 +1,16 @@
+from ...data.subject import _RawSubjectCopySubject
+from ...utils import get_torchio_cache_dir
+
+
+class SubjectMNI(_RawSubjectCopySubject):
+    """Atlases from the Montreal Neurological Institute (MNI).
+
+    See `the website <https://nist.mni.mcgill.ca/?page_id=714>`_ for more
+    information.
+    """
+
+    url_base = 'http://packages.bic.mni.mcgill.ca/mni-models/'
+
+    @property
+    def download_root(self):
+        return get_torchio_cache_dir() / self.name
