62b4ecbcccb1c2cad9c7bac7c70979ea26acb2a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:46 +0000

    Add package youqu-playbook/2024.12.6/youqu_playbook-2024.12.6.tar.gz

---
+++ packages/youqu-playbook/youqu_playbook-2024.12.6.tar.gz/youqu_playbook-2024.12.6/playbook/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class _Config:
+    PASSWORD = os.getenv("YOUQU_PASSWORD") or "1"
+    PLAYBOOK_JSON_PATH = "./playbook.json"
+    PYPI_MIRROR = "https://pypi.tuna.tsinghua.edu.cn/simple"
+    WORKDIR = os.path.abspath(".")
+
+
+config = _Config()
