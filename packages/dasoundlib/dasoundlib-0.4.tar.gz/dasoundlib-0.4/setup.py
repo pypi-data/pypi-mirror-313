1de5d2c66c46d9481791d35a1ec1db973346ca5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:13 +0000

    Add package dasoundlib/0.4/dasoundlib-0.4.tar.gz

---
+++ packages/dasoundlib/dasoundlib-0.4.tar.gz/dasoundlib-0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+# Read the contents of README.md
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='dasoundlib',
+    version='0.4',
+    author='ForNoOne401',
+    author_email='fornoone401401@gmail.com',
+    description='Generate and play sine wave sounds',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ForNoOne401/Soundlib',
+    packages=find_packages(include=['Soundlib', 'Soundlib.*']),
+    package_data={'dasoundlib': ['_soundlib.pyd']},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: Microsoft :: Windows',
+    ],
+    python_requires='>=3.6',
+)
