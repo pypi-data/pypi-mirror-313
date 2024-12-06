f0e8213377b07ca2644a7cea0ea46de8afa520dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:27 +0000

    Add package gtdev/1.6.2/gtdev-1.6.2.tar.gz

---
+++ packages/gtdev/gtdev-1.6.2.tar.gz/gtdev-1.6.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gtdev",
+    version="1.6.2",
+    author="Spectre Lee",
+    author_email="lxwk1spectre@foxmail.com",
+    description="高效碳基测试运算模块",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    url="https://gitee.com/lxwk1spectre/gdevsample",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
