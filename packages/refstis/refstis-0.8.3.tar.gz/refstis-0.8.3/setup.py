bf9ae01b790b54981ac69ca3ed9e9c230d97b38e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:10 +0000

    Add package refstis/0.8.3/refstis-0.8.3.tar.gz

---
+++ packages/refstis/refstis-0.8.3.tar.gz/refstis-0.8.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+import glob
+
+setup(
+    name = 'refstis',
+    version = '0.8.3',
+    description = 'Pipeline to create STIS CCD superdarks and superbiases',
+    author = 'Allyssa Riley, Sean Lockwood, Justin Ely',
+    url = 'https://refstis.readthedocs.io',
+    packages = find_packages(),
+    keywords = ['astronomy'],
+    classifiers = ['Programming Language :: Python :: 3',
+                   'Development Status :: 4 - Beta',
+                   'Intended Audience :: Science/Research',
+                   'Topic :: Scientific/Engineering :: Astronomy',
+                   'Topic :: Scientific/Engineering :: Physics',
+                   'Topic :: Software Development :: Libraries :: Python Modules'],
+    scripts = glob.glob('scripts/*'),
+    install_requires = ['pyyaml',
+                        'numpy',
+                        'astropy>=3.1',
+                        'stistools'],
+    )
