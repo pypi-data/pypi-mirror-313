2fda01d5a8668d91fd5d330babc0b331fb7c31fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:03 +0000

    Add package hello-pckg/0.1/hello_pckg-0.1.tar.gz

---
+++ packages/hello-pckg/hello_pckg-0.1.tar.gz/hello_pckg-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="hello_pckg",
+    version="0.1",
+    packages=["hello_pckg"],
+    description="Hello ouou!",
+    author="Omar",
+    author_email="omarfessy@gmail.com",
+    install_requires=[],
+)
