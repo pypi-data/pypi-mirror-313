a3ec69067a8523d3c224349789369411efe8f76f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:27 +0000

    Add package build-a-brain/0.0.2/build_a_brain-0.0.2.tar.gz

---
+++ packages/build-a-brain/build_a_brain-0.0.2.tar.gz/build_a_brain-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from os import path
+
+working_directory = path.abspath(path.dirname(__file__))
+with open(path.join(working_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = "build_a_brain",
+    version = "0.0.1",
+    author = "Noah Stanis",
+    author_email = 'stani123@uw.edu',
+    description = 'Builds a 5 layer LIF network',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    packages = find_packages(),
+    install_requires = []
+)
