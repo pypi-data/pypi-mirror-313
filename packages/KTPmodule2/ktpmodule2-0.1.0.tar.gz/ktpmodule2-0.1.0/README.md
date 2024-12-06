a1573bb10b1f723140f883f30c326ffa1dc7ebc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:13 +0000

    Add package KTPmodule2/0.1.0/ktpmodule2-0.1.0.tar.gz

---
+++ packages/KTPmodule2/ktpmodule2-0.1.0.tar.gz/ktpmodule2-0.1.0/README.md
@@ -0,0 +1,16 @@
+# My Package
+
+My Package is a Python library for [describe purpose briefly].
+
+## Features
+
+- Easy to use
+- Lightweight
+- Extensible
+
+## Installation
+
+Install the package via pip:
+
+```bash
+pip install my_package
---
+++ packages/KTPmodule2/ktpmodule2-0.1.0.tar.gz/ktpmodule2-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='KTPmodule2',  # Tên package của bạn
+    version='0.1.0',  # Phiên bản của package
+    description='A brief description of your package',  # Mô tả ngắn gọn
+    long_description=open('README.md').read(),  # Đọc mô tả dài từ file README
+    long_description_content_type='text/markdown',  # Định dạng markdown cho README
+    author='Your Name',  # Tên tác giả
+    author_email='your.email@example.com',  # Email tác giả
+    url='https://github.com/yourusername/my_package',  # Địa chỉ repository
+    packages=find_packages(),  # Tự động tìm các package trong thư mục my_package
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Phiên bản Python tối thiểu
+)
