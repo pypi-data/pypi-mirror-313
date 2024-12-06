c7214997624cc5056a7b6bcf26731814dd98a15d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:04 +0000

    Add package no-rizz/1.0.1/no_rizz-1.0.1.tar.gz

---
+++ packages/no-rizz/no_rizz-1.0.1.tar.gz/no_rizz-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="no_rizz",
+    version="1.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "google-auth-oauthlib>=1.2.1",
+        "google-api-python-client>=2.154.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
