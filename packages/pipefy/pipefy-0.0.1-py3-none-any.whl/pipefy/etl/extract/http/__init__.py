f0322e5e1126a8912d7c41e4c00aad224902a1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:05:11 +0000

    Add package pipefy/0.0.1/pipefy-0.0.1-py3-none-any.whl

---
+++ packages/pipefy/pipefy-0.0.1-py3-none-any.whl/pipefy/etl/extract/http/__init__.py
@@ -0,0 +1,9 @@
+from pipefy.etl.extract.http.http_extractor import (
+    HttpDataExtractProcessor,
+    HttpxStreamDownloadProcessor,
+)
+
+__all__ = (
+    "HttpDataExtractProcessor",
+    "HttpxStreamDownloadProcessor",
+)
---
+++ packages/pipefy/pipefy-0.0.1-py3-none-any.whl/pipefy/processors/file_system/__init__.py
@@ -0,0 +1,13 @@
+from pipefy.processors.file_system.base import (
+    AbstractFileSystemManager,
+    FileEncodingEnum,
+    FileModeEnum,
+    FileSystemManager,
+)
+
+__all__ = (
+    "AbstractFileSystemManager",
+    "FileSystemManager",
+    "FileModeEnum",
+    "FileEncodingEnum",
+)
