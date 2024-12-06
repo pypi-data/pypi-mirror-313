3637264ea2d3428a6bd9a26ba97dc615ef0167dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:18:06 +0000

    Add package pybooster/0.0.3/pybooster-0.0.3.tar.gz

---
+++ packages/pybooster/pybooster-0.0.3.tar.gz/pybooster-0.0.3/src/pybooster/types.py
@@ -0,0 +1,40 @@
+from collections.abc import AsyncIterator
+from collections.abc import Callable
+from collections.abc import Iterator
+from collections.abc import Mapping
+from collections.abc import Sequence
+from contextlib import AbstractAsyncContextManager
+from contextlib import AbstractContextManager
+from typing import ParamSpec
+from typing import TypeVar
+
+P = ParamSpec("P")
+R = TypeVar("R")
+Y = TypeVar("Y")
+S = TypeVar("S")
+
+IteratorCallable = Callable[P, Iterator[R]]
+"""A callable that returns an iterator."""
+AsyncIteratorCallable = Callable[P, AsyncIterator[R]]
+"""A callable that returns an async iterator."""
+ContextManagerCallable = Callable[P, AbstractContextManager[R]]
+"""A callable that returns a context manager."""
+AsyncContextManagerCallable = Callable[P, AbstractAsyncContextManager[R]]
+"""A callable that returns an async context manager."""
+AnyContextManagerCallable = Callable[P, AbstractContextManager[R] | AbstractAsyncContextManager[R]]
+"""A callable that returns any kind of context manager."""
+
+HintSeq = Sequence[type]
+"""A sequence of types."""
+HintMap = Mapping[str, type]
+"""A mapping of parameter or attribute names to their type."""
+HintDict = dict[str, type]
+"""A dictionary of parameter or attribute names to their type."""
+
+
+class InjectionError(RuntimeError):
+    """An error raised when an injection fails."""
+
+
+class SolutionError(RuntimeError):
+    """An error raised when a solution fails."""
