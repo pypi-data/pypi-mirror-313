b80690b269c5dc3e6807bb189d933411db36881a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:31:39 +0000

    Add package spider-py3/0.1.0.20241130/spider_py3-0.1.0.20241130-py3-none-any.whl

---
+++ packages/spider-py3/spider_py3-0.1.0.20241130-py3-none-any.whl/spiderpy3/objects/__init__.py
@@ -0,0 +1,5 @@
+from . import object, handler
+
+__all__ = [
+    "object", "handler"
+]
---
+++ packages/spider-py3/spider_py3-0.1.0.20241130-py3-none-any.whl/spiderpy3/utils/__init__.py
@@ -0,0 +1,6 @@
+from . import execute, algo, compress, cookies, file, headers, image, logger, number, url
+
+__all__ = [
+    "execute",
+    "algo", "compress", "cookies", "file", "headers", "image", "logger", "number", "url"
+]
