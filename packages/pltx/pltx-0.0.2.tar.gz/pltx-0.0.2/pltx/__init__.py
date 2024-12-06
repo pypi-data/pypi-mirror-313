d1ed7f7a61209df3e99047ce3cd0b979e5a12529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:25 +0000

    Add package pltx/0.0.2/pltx-0.0.2.tar.gz

---
+++ packages/pltx/pltx-0.0.2.tar.gz/pltx-0.0.2/pltx/__init__.py
@@ -0,0 +1,13 @@
+from ._tools import (
+    plot_confusion_matrix,
+    plot_confusion_matrix2,
+    plot_roc,
+    plot_multi_class_roc,
+)
+
+__all__ = [
+    "plot_confusion_matrix",
+    "plot_confusion_matrix2",
+    "plot_roc",
+    "plot_multi_class_roc",
+]
