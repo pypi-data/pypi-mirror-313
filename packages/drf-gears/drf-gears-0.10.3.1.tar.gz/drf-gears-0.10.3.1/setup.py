e79f15d95fb534ffe3561f28e34afdf27d7cce65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:21 +0000

    Add package drf-gears/0.10.3.1/drf-gears-0.10.3.1.tar.gz

---
+++ packages/drf-gears/drf-gears-0.10.3.1.tar.gz/drf-gears-0.10.3.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="drf-gears",
+    version="0.10.3.1",
+    author="Alexander Yudkin",
+    author_email="san4ezy@gmail.com",
+    description="Some gears collection for getting life a little bit better.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/san4ezy/drf-gears",
+    packages=setuptools.find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+    ],
+)
