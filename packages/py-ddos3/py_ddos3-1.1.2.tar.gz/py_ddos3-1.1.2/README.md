48d37582d5b6e3b6bd4ad8d16093bdbaa2849323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:05:45 +0000

    Add package py-ddos3/1.1.2/py_ddos3-1.1.2.tar.gz

---
+++ packages/py-ddos3/py_ddos3-1.1.2.tar.gz/py_ddos3-1.1.2/README.md
@@ -0,0 +1,16 @@
+# PyDDOS3
+
+A simple DDoS testing library in Python. It provides various attack methods such as UDP flood, SYN flood, and Botnet simulation for testing and simulation purposes.
+
+## Features
+- **UDP Flood**: Launch a UDP flood attack to test the resilience of a server.
+- **SYN Flood**: Simulate a SYN flood attack.
+- **Botnet**: Simulate a botnet attack by using multiple threads.
+
+## Installation
+
+You can install `pyddos3` via `pip` from PyPI:
+
+```bash
+pip install py-ddos3
+```
---
+++ packages/py-ddos3/py_ddos3-1.1.2.tar.gz/py_ddos3-1.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py-ddos3",
+    version="1.1.2",
+    author="MohamedLunar",
+    author_email="contact.mohamedlunardev@gmail.com",
+    description="🔗 Apckage Guide On https://github.com/mohamedlunar/py-ddos3",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mohamedlunar/py-ddos3",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "pyweb-server=pyweb:main",
+        ]
+    },
+)
