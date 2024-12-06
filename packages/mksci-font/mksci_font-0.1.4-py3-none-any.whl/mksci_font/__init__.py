72638a3f606cc12f830d07d01e5c2ce7ee2a1afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:31 +0000

    Add package mksci-font/0.1.4/mksci_font-0.1.4-py3-none-any.whl

---
+++ packages/mksci-font/mksci_font-0.1.4-py3-none-any.whl/mksci_font/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python 3.11.0
+# -*-coding:utf-8 -*-
+# @Author  : Shuang (Twist) Song
+# @Contact   : SongshGeo@gmail.com
+# GitHub   : https://github.com/SongshGeo
+# Website: https://cv.songshgeo.com/
+
+from .font_wrapper import config_font, mksci_font, show, update_font
+
+__all__ = ["mksci_font", "show", "config_font", "update_font"]
