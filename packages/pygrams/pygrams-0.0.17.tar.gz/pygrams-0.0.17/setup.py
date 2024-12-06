d0cedf0db47bcaa9572072fde74f62f40bf4526b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:40 +0000

    Add package pygrams/0.0.17/pygrams-0.0.17.tar.gz

---
+++ packages/pygrams/pygrams-0.0.17.tar.gz/pygrams-0.0.17/setup.py
@@ -0,0 +1,34 @@
+# from distutils.core import setup
+import pathlib
+from setuptools import setup, find_packages
+
+here = pathlib.Path(__file__).parent
+
+readme = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+  name = 'pygrams',
+  # packages = ['pygrams'],
+  packages=find_packages(),  # Automatically finds 'src' directory
+  version = '0.0.17',
+  license='MIT',
+  description = 'Get python programs handy as a function',
+  long_description=readme,
+  long_description_content_type="text/markdown",
+  author = 'Yaseen',
+  author_email = 'fantasticyaseenshariff@gmail.com',
+  url = 'https://pygrams.netlify.app',
+  download_url = 'https://github.com/Yaseen549/pygrams/archive/refs/tags/v0.0.17.tar.gz',
+  keywords = ['PYTHONPROGRAMS', 'PROGRAMS', 'FUNCTIONS', 'PYGRAMS'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
