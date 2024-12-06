5bc1a86d9027b7cfd997210d1b6b752df0aed6e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:14 +0000

    Add package PXL/0.0.10/pxl-0.0.10.tar.gz

---
+++ packages/PXL/pxl-0.0.10.tar.gz/pxl-0.0.10/pxl/tests/test_timeseries.py
@@ -0,0 +1,195 @@
+from __future__ import division, print_function
+
+import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from numpy.testing import assert_almost_equal
+from uncertainties import unumpy
+
+from .. import timeseries
+from ..timeseries import *
+
+
+def test_autocorrelation():
+    t = np.linspace(0, 1, num=1000)
+    fs = 1.0 / (t[1] - t[0])
+    y = np.sin(2 * np.pi * t)  # + 0.5*np.sin(5*t)
+    y = np.random.rand(len(t))
+    y2 = np.random.rand(len(t))
+    tau, rho = autocorr_coeff(y, t, -0.5, 0.5)
+    print(
+        "Mean autocorrelation coefficient for random time series =", rho.mean()
+    )
+    assert np.abs(rho.max() - 1.0) < 1e-12
+
+
+def test_spectrum_band_averaging():
+    """Test band averaging with `psd` function."""
+    from scipy.signal import firwin, lfilter
+
+    n_band_average = 20
+    n = 10000
+    ts = np.random.randn(n)
+    t = np.linspace(0.0, 1.0, num=n)
+    N = 5
+    Fc = 50
+    Fs = 1500
+    h = firwin(numtaps=N, cutoff=40, fs=Fs)
+    ts = lfilter(h, 1.0, ts)  # 'x' is the time-series data you are filtering
+    f, spec = timeseries.psd(t, ts, n_band_average=1)
+    f2, spec2 = timeseries.psd(t, ts, n_band_average=n_band_average)
+    plt.plot(f, spec, label="Raw")
+    plt.plot(
+        f2, spec2, label="Band-averaged over {} bands".format(n_band_average)
+    )
+    plt.xlabel("$f$")
+    plt.ylabel("$S$")
+    plt.legend()
+    plt.show()
+
+
+def test_average_over_area():
+    x = np.array([-6.0, -2.0, -1.0, 0.0, 1.0, 2.0, 6.0])
+    y = np.array([-6.0, -2.0, -1.0, 0.0, 1.0, 2.0, 6.0])
+    q = np.ones((x.size, y.size))
+    ave = average_over_area(q, x, y)
+    print("Computed average is", ave)
+    assert np.abs(ave - 1) < 1e-12
+    # Now test something a bit harder
+    y = np.array([0.0, 1.0])
+    q = np.array(
+        [
+            [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+            [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+        ]
+    )
+    ave = average_over_area(q, x, y)
+    print("Computed average is", ave)
+    assert np.abs(ave - 0.33333333333333) < 1e-12
+
+
+def test_average_over_area_uncertainties():
+    """Test `average_over_area` with uncertainties."""
+    x = np.array([-6.0, -2.0, -1.0, 0.0, 1.0, 2.0, 6.0])
+    y = np.array([0.0, 1.0])
+    q = np.array(
+        [
+            [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+            [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+        ]
+    )
+    # Convert arrays to have uncertainties
+    x = unumpy.uarray(x, 1e-3)
+    y = unumpy.uarray(y, 0.6e-2)
+    q = unumpy.uarray(q, 1e-2)
+    ave = average_over_area(q, x, y)
+    print("Computed average is", ave)
+    assert np.abs(ave - 0.33333333333333) < 1e-12
+
+
+def test_integral_scale(plot=False):
+    t = np.linspace(0, 1, num=1000)
+    u = 0.1 * np.random.randn(len(t))
+    u += 0.1 * np.sin(2 * np.pi * t)
+    print(integral_scale(u, t, tau2=1.0))
+    if plot:
+        tau, rho = autocorr_coeff(u, t, 0.0, 0.5)
+        plt.figure()
+        plt.plot(tau, rho)
+        plt.xlabel(r"$\tau$")
+        plt.ylabel(r"$\rho$")
+
+
+def test_runningstd():
+    t = np.linspace(0, 10, 10000)
+    a = np.sin(2 * np.pi * t)
+    a += np.random.randn(len(t))
+    t_std, a_std = runningstd(t, a, 1000)
+    print("Mean of running standard deviation:", a_std.mean())
+    print("Standard deviation of test time series:", a.std())
+    assert np.abs(a_std.mean() - a.std()) < 0.05
+    plt.figure()
+    plt.plot(t_std, a_std)
+    plt.ylim((-4, 4))
+    plt.figure()
+    plt.plot(t, a)
+    plt.show()
+
+
+def test_combine_std():
+    """Test `timeseries.combine_std`."""
+    n = int(1e5)
+    ts = np.random.normal(size=n)
+    std_tot = ts.std()
+    ts1 = ts[: n // 2]
+    ts2 = ts[n // 2 :]
+    n1 = len(ts1)
+    mean1 = ts1.mean()
+    std1 = ts1.std()
+    n2 = len(ts2)
+    mean2 = ts2.mean()
+    std2 = ts2.std()
+    assert_almost_equal(ts.mean(), np.mean((mean1, mean2)))
+    assert n1 + n2 == n
+    n = np.array((n1, n2))
+    mean = np.array((mean1, mean2))
+    std = np.array((std1, std2))
+    assert_almost_equal(std_tot, combine_std(n, mean, std), decimal=2)
+
+
+def test_find_amp_phase(plot=False):
+    # Create a time series with a known amplitude and phase
+    amp = 2.4
+    phase = np.deg2rad(121)
+    min_phase = np.deg2rad(10)
+    mean = 0.5
+    npeaks = 3
+    places = 2  # Decimal places the values should match
+    noise_std = 0.01
+    # Angle will start at zero first
+    angle = np.linspace(0, 4 * np.pi, num=300)
+    angle_deg = np.rad2deg(angle)
+    data = amp * np.cos(npeaks * (angle - phase)) + mean
+    noise = np.random.normal(0, noise_std, len(data))
+    data += noise
+    amp_fit, phase_fit = find_amp_phase(
+        angle, data, npeaks, min_phase=min_phase
+    )
+    if not plot:
+        assert_almost_equal(amp, amp_fit, decimal=places)
+        assert_almost_equal(phase, phase_fit, decimal=places)
+    # Now check that we can fit a time series that doesn't start at zero
+    data_old = data.copy()
+    angle_old = angle.copy()
+    angle_deg_old = angle_deg.copy()
+    angle_deg += 247.05
+    angle = np.deg2rad(angle_deg)
+    data = amp * np.cos(npeaks * (angle - phase)) + mean + noise
+    if plot:
+        import matplotlib.pyplot as plt
+
+        plt.plot(angle_deg_old, data_old, marker="o")
+        plt.plot(
+            angle_deg_old,
+            amp_fit * np.cos(npeaks * (angle_old - phase_fit)) + mean,
+            linewidth=4,
+            alpha=0.6,
+            color="red",
+        )
+        plt.plot(angle_deg, data, marker="^", color="g")
+    amp_fit, phase_fit = find_amp_phase(
+        angle, data, npeaks, min_phase=min_phase
+    )
+    if not plot:
+        assert_almost_equal(amp, amp_fit, decimal=places)
+        assert_almost_equal(phase, phase_fit, decimal=places)
+    else:
+        plt.plot(
+            angle_deg,
+            amp_fit * np.cos(npeaks * (angle - phase_fit)) + mean,
+            linewidth=4,
+            alpha=0.6,
+            color="purple",
+        )
+        plt.show()
---
+++ packages/PXL/pxl-0.0.10.tar.gz/pxl-0.0.10/pxl/timeseries.py
@@ -0,0 +1,304 @@
+"""A collection of useful time series analysis functions."""
+
+from __future__ import division, print_function
+
+import numpy as np
+import pandas as _pd
+import scipy.stats
+from scipy.optimize import curve_fit
+from scipy.signal import lfilter
+
+from .io import *
+
+
+def sigmafilter(data, sigmas, passes):
+    """Remove datapoints outside of a specified standard deviation range."""
+    for n in range(passes):
+        meandata = np.mean(data[~np.isnan(data)])
+        sigma = np.std(data[~np.isnan(data)])
+        data[data > meandata + sigmas * sigma] = np.nan
+        data[data < meandata - sigmas * sigma] = np.nan
+    return data
+
+
+def psd(t, data, window=None, n_band_average=1):
+    """Compute one-sided power spectral density, subtracting mean automatically.
+
+    Parameters
+    ----------
+    t : Time array
+    data : Time series data
+    window : {None, "Hanning"}
+    n_band_average : Number of samples over which to band average
+
+    Returns
+    -------
+    f : Frequency array
+    psd : Spectral density array
+    """
+    dt = t[1] - t[0]
+    N = len(data)
+    data = data - np.mean(data)
+    if window == "Hanning":
+        data = data * np.hanning(N)
+    f = np.fft.fftfreq(N, dt)
+    y = np.fft.fft(data)
+    f = f[0 : N // 2]
+    psd = (2 * dt / N) * abs(y) ** 2
+    psd = np.real(psd[0 : N // 2])
+    if n_band_average > 1:
+        f_raw, s_raw = f * 1, psd * 1
+        f = np.zeros(len(f_raw) // n_band_average)
+        psd = np.zeros(len(f_raw) // n_band_average)
+        for n in range(len(f_raw) // n_band_average):
+            f[n] = np.mean(
+                f_raw[n * n_band_average : (n + 1) * n_band_average]
+            )
+            psd[n] = np.mean(
+                s_raw[n * n_band_average : (n + 1) * n_band_average]
+            )
+    return f, psd
+
+
+def runningstd(t, data, width):
+    """Compute the running standard deviation of a time series.
+
+    Returns `t_new`, `std_r`.
+    """
+    ne = len(t) - width
+    t_new = np.zeros(ne)
+    std_r = np.zeros(ne)
+    for i in range(ne):
+        t_new[i] = np.mean(t[i : i + width + 1])
+        std_r[i] = np.nanstd(data[i : i + width + 1])
+    return t_new, std_r
+
+
+def smooth(data, fw):
+    """Smooth data with a moving average."""
+    if fw == 0:
+        fdata = data
+    else:
+        fdata = lfilter(np.ones(fw) / fw, 1, data)
+    return fdata
+
+
+def calcstats(data, t1, t2, sr):
+    """Calculate the mean and standard deviation of some array between
+    t1 and t2 provided the sample rate sr.
+    """
+    dataseg = data[sr * t1 : sr * t2]
+    meandata = np.mean(dataseg[~np.isnan(dataseg)])
+    stddata = np.std(dataseg[~np.isnan(dataseg)])
+    return meandata, stddata
+
+
+def average_over_area(q, x, y):
+    """Averages a quantity `q` over a rectangular area given a 2D array and
+    the x and y vectors for sample locations, using the trapezoidal rule"""
+    area = (np.max(x) - np.min(x)) * (np.max(y) - np.min(y))
+    integral = np.trapz(np.trapz(q, y, axis=0), x)
+    return integral / area
+
+
+def build_plane_arrays(x, y, qlist):
+    """Build a 2-D array out of data taken in the same plane, for contour
+    plotting.
+    """
+    if type(qlist) is not list:
+        return_list = False
+        qlist = [qlist]
+    else:
+        return_list = True
+    xv = x[np.where(y == y[0])[0]]
+    yv = y[np.where(x == x[0])[0]]
+    qlistp = []
+    for n in range(len(qlist)):
+        qlistp.append(np.zeros((len(yv), len(xv))))
+    for j in range(len(qlist)):
+        for n in range(len(yv)):
+            i = np.where(y == yv[n])[0]
+            qlistp[j][n, :] = qlist[j][i]
+    if not return_list:
+        qlistp = qlistp[0]
+    return xv, yv, qlistp
+
+
+def corr_coeff(x1, x2, t, tau1, tau2):
+    """Compute lagged correlation coefficient for two time series."""
+    dt = t[1] - t[0]
+    tau = np.arange(tau1, tau2 + dt, dt)
+    rho = np.zeros(len(tau))
+    for n in range(len(tau)):
+        i = np.abs(int(tau[n] / dt))
+        if tau[n] >= 0:  # Positive lag, push x2 forward in time
+            seg2 = x2[0 : -1 - i]
+            seg1 = x1[i:-1]
+        elif tau[n] < 0:  # Negative lag, push x2 back in time
+            seg1 = x1[0 : -i - 1]
+            seg2 = x2[i:-1]
+        seg1 = seg1 - seg1.mean()
+        seg2 = seg2 - seg2.mean()
+        rho[n] = np.mean(seg1 * seg2) / seg1.std() / seg2.std()
+    return tau, rho
+
+
+def autocorr_coeff(x, t, tau1, tau2):
+    """Calculate the autocorrelation coefficient."""
+    return corr_coeff(x, x, t, tau1, tau2)
+
+
+def integral_scale(u, t, tau1=0.0, tau2=1.0):
+    """Calculate the integral scale of a time series by integrating up to
+    the first zero crossing.
+    """
+    tau, rho = autocorr_coeff(u, t, tau1, tau2)
+    zero_cross_ind = np.where(np.diff(np.sign(rho)))[0][0]
+    int_scale = np.trapz(rho[:zero_cross_ind], tau[:zero_cross_ind])
+    return int_scale
+
+
+def combine_std(n, mean, std):
+    """Compute combined standard deviation for subsets.
+
+    See https://stats.stackexchange.com/questions/43159/\
+    how-to-calculate-pooled-variance-of-two-groups-given-known-group-variances-\
+    mean for derivation.
+
+    Parameters
+    ----------
+    n : numpy array of sample sizes
+    mean : numpy array of sample means
+    std : numpy array of sample standard deviations
+    """
+    # Calculate weighted mean
+    mean_tot = np.sum(n * mean) / np.sum(n)
+    var_tot = np.sum(n * (std**2 + mean**2)) / np.sum(n) - mean_tot**2
+    return np.sqrt(var_tot)
+
+
+def calc_multi_exp_unc(sys_unc, n, mean, std, dof, confidence=0.95):
+    """Calculate expanded uncertainty using values from multiple runs.
+
+    Note that this function assumes the statistic is a mean value, therefore
+    the combined standard deviation is divided by `sqrt(N)`.
+
+    Parameters
+    ----------
+    sys_unc : numpy array of systematic uncertainties
+    n : numpy array of numbers of samples per set
+    std : numpy array of sample standard deviations
+    dof : numpy array of degrees of freedom
+    confidence : Confidence interval for t-statistic
+    """
+    sys_unc = sys_unc.mean()
+    std_combined = combine_std(n, mean, std)
+    std_combined /= np.sqrt(n.sum())
+    std_unc_combined = np.sqrt(std_combined**2 + sys_unc**2)
+    dof = dof.sum()
+    t_combined = scipy.stats.t.interval(confidence, df=dof)[-1]
+    exp_unc_combined = t_combined * std_unc_combined
+    return exp_unc_combined
+
+
+def student_t(degrees_of_freedom, confidence=0.95):
+    """Return Student-t statistic for given DOF and confidence interval."""
+    return scipy.stats.t.interval(confidence, df=degrees_of_freedom)[-1]
+
+
+def calc_uncertainty(quantity, sys_unc, mean=True):
+    """Calculate the combined standard uncertainty of a quantity."""
+    n = len(quantity)
+    std = np.nanstd(quantity)
+    if mean:
+        std /= np.sqrt(n)
+    return np.sqrt(std**2 + sys_unc**2)
+
+
+def calc_exp_uncertainty(
+    n, std, combined_unc, sys_unc, rel_unc=0.25, confidence=0.95, mean=True
+):
+    """Calculate expanded uncertainty.
+
+    Parameters
+    ----------
+    n : Number of independent samples
+    std : Sample standard deviation
+    sys_unc : Systematic uncertainty (b in Coleman and Steele)
+    rel_unc : Relative uncertainty of each systematic error source (guess 0.25)
+    confidence : Confidence interval (0, 1)
+    mean : bool whether or not the quantity is a mean value
+
+    Returns
+    -------
+    exp_unc : Expanded uncertainty
+    nu_x : Degrees of freedom
+    """
+    s_x = std
+    if mean:
+        s_x /= np.sqrt(n)
+    nu_s_x = n - 1
+    b = sys_unc
+    nu_b = 0.5 * rel_unc ** (-2)
+    nu_x = ((s_x**2 + b**2) ** 2) / (s_x**4 / nu_s_x + b**4 / nu_b)
+    t = scipy.stats.t.interval(confidence, df=nu_x)[-1]
+    exp_unc = t * combined_unc
+    return exp_unc, nu_x
+
+
+def find_amp_phase(angle, data, npeaks=3, min_amp=None, min_phase=None):
+    """Estimate amplitude and phase of an approximately sinusoidal quantity
+    using `scipy.optimize.curve_fit`.
+
+    Phase is defined as the angle at which the cosine curve fit reaches its
+    first peak. It is assumed that phase is positive. For example:
+
+        data_fit = amp*np.cos(npeaks*(angle - phase)) + mean_data
+
+    Parameters
+    ----------
+    angle : numpy array
+        Time series of angle values in radians
+    data : numpy array
+        Time series of data to be fit
+    npeaks : int
+        Number of peaks per revolution, or normalized frequency
+    min_phase : float
+        Minimum phase to allow for guess to least squares fit
+
+    Returns
+    -------
+    amp : float
+        Amplitude of regressed cosine
+    phase : float
+        Angle of the first peak in radians
+    """
+    # First subtract the mean of the data
+    data = data - data.mean()
+    # Make some guesses for parameters from a subset of data starting at an
+    # integer multiple of periods
+    if angle[0] != 0.0:
+        angle1 = angle[0] + (
+            2 * np.pi / npeaks - (2 * np.pi / npeaks) % angle[0]
+        )
+    else:
+        angle1 = angle[0]
+    angle1 += min_phase
+    angle2 = angle1 + 2 * np.pi / npeaks
+    ind = np.logical_and(angle >= angle1, angle <= angle2)
+    angle_sub = angle[ind]
+    data_sub = data[ind]
+    amp_guess = (data_sub.max() - data_sub.min()) / 2
+    phase_guess = angle[np.where(data_sub == data_sub.max())[0][0]] % (
+        np.pi * 2 / npeaks
+    )
+
+    # Define the function we will try to fit to
+    def func(angle, amp, phase, mean):
+        return amp * np.cos(npeaks * (angle - phase)) + mean
+
+    # Calculate fit
+    p0 = amp_guess, phase_guess, 0.0
+    popt, pcov = curve_fit(func, angle, data, p0=p0)
+    amp, phase, mean = popt
+    return amp, phase
