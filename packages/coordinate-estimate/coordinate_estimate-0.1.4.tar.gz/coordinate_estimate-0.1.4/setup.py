308ea8e98b99a28f8baf7339b399995994388357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:13 +0000

    Add package coordinate-estimate/0.1.4/coordinate_estimate-0.1.4.tar.gz

---
+++ packages/coordinate-estimate/coordinate_estimate-0.1.4.tar.gz/coordinate_estimate-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="coordinate-estimate",
+    version="0.1.4",
+    description="Drone YOLO client for object detection and server communication.",
+    author="ittipat jitrada",
+    author_email="ittipatjitrada@gmail.com",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    package_data={
+        'object_position_calculator': ['best.pt'],
+    },
+    install_requires=[
+        "python-socketio",
+        "opencv-python",
+        "pillow",
+        "ultralytics",
+        "numpy"
+    ],
+    entry_points={
+        "console_scripts": [
+            "drone-yolo-client=drone_yolo_client.client:main",
+        ],
+    },
+)
