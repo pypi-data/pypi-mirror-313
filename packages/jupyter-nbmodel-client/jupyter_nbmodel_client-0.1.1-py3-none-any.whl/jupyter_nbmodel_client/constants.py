8f6725625bb4569b39eed372633dff4a8718990c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:45 +0000

    Add package jupyter-nbmodel-client/0.1.1/jupyter_nbmodel_client-0.1.1-py3-none-any.whl

---
+++ packages/jupyter-nbmodel-client/jupyter_nbmodel_client-0.1.1-py3-none-any.whl/jupyter_nbmodel_client/constants.py
@@ -0,0 +1,7 @@
+import os
+import re
+
+HTTP_PROTOCOL_REGEXP = re.compile(r"^http")
+"""http protocol regular expression."""
+REQUEST_TIMEOUT = float(os.getenv("REQUEST_TIMEOUT", 10))
+"""Default request timeout in seconds"""
---
+++ packages/jupyter-nbmodel-client/jupyter_nbmodel_client-0.1.1-py3-none-any.whl/jupyter_nbmodel_client/tests/test_client.py
@@ -0,0 +1,8 @@
+from jupyter_nbmodel_client import NbModelClient
+
+
+# async def test_one(jp_serverapp, rtc_create_notebook):
+#     path, content = await rtc_create_notebook("test.ipynb")
+
+#     async with NbModelClient(jp_serverapp.connection_url, path, jp_serverapp.identity_provider.token):
+#         assert False
