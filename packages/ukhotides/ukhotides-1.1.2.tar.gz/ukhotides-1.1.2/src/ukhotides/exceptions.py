0ef416d3f896b27788db4993286eee13dac1de04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:11:04 +0000

    Add package ukhotides/1.1.2/ukhotides-1.1.2.tar.gz

---
+++ packages/ukhotides/ukhotides-1.1.2.tar.gz/ukhotides-1.1.2/src/ukhotides/exceptions.py
@@ -0,0 +1,32 @@
+"""Exceptions"""
+
+# pylint: disable=missing-class-docstring
+
+class ApiError(Exception):
+    def __init__(self, status):
+        super().__init__(status)
+        self.status = status
+
+
+class InvalidApiKeyError(Exception):
+    def __init__(self, status):
+        super().__init__(status)
+        self.status = status
+
+
+class ApiQuotaExceededError(Exception):
+    def __init__(self, status):
+        super().__init__(status)
+        self.status = status
+
+
+class TooManyRequestsError(Exception):
+    def __init__(self, status):
+        super().__init__(status)
+        self.status = status
+
+
+class StationNotFoundError(Exception):
+    def __init__(self, status):
+        super().__init__(status)
+        self.status = status
