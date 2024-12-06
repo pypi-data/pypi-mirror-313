4ae111551185765c0db5ab20577e21d7e3c51172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:15 +0000

    Add package freaky/1.0.0a9/freaky-1.0.0a9.tar.gz

---
+++ packages/freaky/freaky-1.0.0a9.tar.gz/freaky-1.0.0a9/src/freaky/__init__.py
@@ -0,0 +1,8 @@
+from .encode import encode
+from .encode import encode_cli as _encode_cli
+from .decode import decode
+from .decode import decode_cli as _decode_cli
+
+from .entropy import average_entropy
+
+from .compare import imgdiff
