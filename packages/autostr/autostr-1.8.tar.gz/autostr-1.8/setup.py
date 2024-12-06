61992b88fbfbc68e511df189598b79cfc6717260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:22 +0000

    Add package autostr/1.8/autostr-1.8.tar.gz

---
+++ packages/autostr/autostr-1.8.tar.gz/autostr-1.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+"""
+:author: Andrey Kuchebo
+:license: MIT
+"""
+
+version = 1.8
+
+def write_readme():
+	with open('README.md', 'r', encoding='utf-8') as f:
+		return f.read()
+
+setup(
+	name='autostr',
+	version=version,
+	author='Andrey Kuchebo',
+	author_email='kucheboandrey@gmail.com',
+	package=find_packages(),
+	long_description=write_readme(),
+	long_description_content_type='text/markdown',
+	url='https://github.com/number3141/autostr',
+)
