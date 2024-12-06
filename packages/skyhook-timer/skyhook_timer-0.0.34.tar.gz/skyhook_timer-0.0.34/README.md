0efde2aea68fc293d1d2e96e2cfdcab57e715120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:14 +0000

    Add package skyhook-timer/0.0.34/skyhook_timer-0.0.34.tar.gz

---
+++ packages/skyhook-timer/skyhook_timer-0.0.34.tar.gz/skyhook_timer-0.0.34/README.md
@@ -0,0 +1,35 @@
+# skyhook-app
+Alliance Auth plugin for tracking Skyhook timers
+
+# Install
+https://pypi.org/project/skyhook-timer/
+
+# CHANGELOG
+
+### 0.0.22
+- Working html rendering
+
+### 0.0.8
+- Redo most of everything
+
+### 0.0.7
+- Update html to use old names
+
+### 0.0.6
+- Remove ready def
+
+### 0.0.5
+- Remove plugins.py
+
+### 0.0.4
+- Remove references to allianceauth.plugins
+
+### 0.0.3
+- Add app.py for nav bar
+
+### 0.0.2
+- Update so members can view timers
+
+### 0.0.1
+- initial test release
+- admins can view and edit skyhooks
---
+++ packages/skyhook-timer/skyhook_timer-0.0.34.tar.gz/skyhook_timer-0.0.34/skyhook_timer/auth_hooks.py
@@ -0,0 +1,35 @@
+from django.utils.translation import gettext_lazy as _
+
+from allianceauth import hooks
+from allianceauth.services.hooks import MenuItemHook, UrlHook
+
+from . import urls
+
+
+class SkyhookTimerMenuItem(MenuItemHook):
+    """This class ensures only authorized users will see the menu entry"""
+
+    def __init__(self):
+        # setup menu entry for sidebar
+        MenuItemHook.__init__(
+            self,
+            _("skyhook_timer"),
+            "fas fa-clock",
+            "skyhook_timer:view_timers",
+            navactive=["skyhook_timer:"],
+        )
+
+    def render(self, request):
+        if request.user.has_perm('skyhook_timer.view_skyhooktimer'):
+            return MenuItemHook.render(self, request)
+        return ''
+
+
+@hooks.register("menu_item_hook")
+def register_menu():
+    return SkyhookTimerMenuItem()
+
+
+@hooks.register("url_hook")
+def register_urls():
+    return UrlHook(urls, "skyhook_timer", r"^view_timers/")
