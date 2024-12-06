cd6414e07e15e89d7812763bec9b44f1ab81d0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:23 +0000

    Add package coralME/1.1.10/coralME-1.1.10-py3-none-any.whl

---
+++ packages/coralME/coralME-1.1.10-py3-none-any.whl/coralme/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+__version__ = "1.0"
+
+import coralme.builder
+import coralme.core
+import coralme.io
+
+import sys
+if sys.platform in ['win32']:
+	pass
+else:
+	import coralme.solver.solver
+
+import coralme.util
+
+from . import _version
+__version__ = _version.get_versions()['version']
