626f5b16611784f76d66616b40e92d0c0cc32135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:15 +0000

    Add package haiway/0.5.3/haiway-0.5.3.tar.gz

---
+++ packages/haiway/haiway-0.5.3.tar.gz/haiway-0.5.3/src/haiway/state/__init__.py
@@ -0,0 +1,8 @@
+from haiway.state.attributes import AttributeAnnotation, attribute_annotations
+from haiway.state.structure import State
+
+__all__ = [
+    "attribute_annotations",
+    "AttributeAnnotation",
+    "State",
+]
