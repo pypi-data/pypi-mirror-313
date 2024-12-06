61ddb1b7c49c78dda1d5cf04006f3bb0a5931ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:11:20 +0000

    Add package bifabrik/0.11.0/bifabrik-0.11.0.tar.gz

---
+++ packages/bifabrik/bifabrik-0.11.0.tar.gz/bifabrik-0.11.0/src/bifabrik/cfg/specific/TableConfiguration.py
@@ -0,0 +1,230 @@
+from bifabrik.cfg.engine.Configuration import Configuration
+from bifabrik.cfg.engine.Configuration import CfgProperty
+
+class TableConfiguration(Configuration):
+    """Configuration related to saving data to delta tables in a Fabric lakehouse or warehouse
+    """
+    def __init__(self):
+        super().__init__()
+        self.__watermarkColumn = None
+        self.__increment = None
+        self.__identityColumnPattern = None
+        self.__insertDateColumn = None
+        self.__mergeKeyColumns = []
+        self.__snapshotKeyColumns = []
+        self.__invalidCharactersInColumnNamesReplacement = '_'
+        self.__canAddNewColumns = True
+        self.__addNARecord = False
+        self.__addBadValueRecord = False
+        self.__largeTableMethodEnabled = True
+        self.__largeTableMethodSourceThresholdGB: float = 0.2
+        self.__largeTableMethodDestinationThresholdGB: float = 20
+        self.__rowStartColumn = 'RowStart'
+        self.__rowEndColumn = 'RowEnd'
+        self.__currentRowColumn = 'CurrentRow'
+        self.__scd2SoftDelete = False
+        self.__scd2ExcludeColumns = []
+
+    @CfgProperty
+    def watermarkColumn(self) -> str:
+        """Which column to use as the watermark when filtering records for incremental load.
+        If the target table already exists and the watermark column is set, only records where the watermark is greater than the MAX from the table will be inserted.
+        default None
+        """
+        return self.__watermarkColumn
+    @watermarkColumn.setter(key='watermarkColumn')
+    def watermarkColumn(self, val):
+        self.__watermarkColumn = val
+
+    @CfgProperty
+    def increment(self) -> str:
+        """Increment method
+        None or "overwrite" : full load
+        "append"            : add incoming rows to the table
+        "merge"             : update / insert (SCD 1) after matching based on the mergeKeyColumns setting
+        "scd2"              : perform SCD2 matching rows based on the mergeKeyColumns setting, using RowStartColumn, RowEndColumn and CurrentRowColumn column names 
+        "snapshot"          : based on the snapshotKeyColumns setting (typically a date), inseert the new records and remove any old records that refer to a newly loaded snapshot key
+        default None
+        """
+        return self.__increment
+    @increment.setter(key='increment')
+    def increment(self, val):
+        if val is not None:
+            val = val.lower()
+        self.__increment = val
+
+    @CfgProperty
+    def mergeKeyColumns(self) -> str:
+        """Array of column names used as the business key columns when the 'merge' increment method is used
+        default []
+        """
+        return self.__mergeKeyColumns
+    @mergeKeyColumns.setter(key='mergeKeyColumns')
+    def mergeKeyColumns(self, val):
+        t = str(type(val))
+        if t == "<class 'str'>":
+            self.__mergeKeyColumns = val.split(',')
+        else:
+            self.__mergeKeyColumns = val
+
+    @CfgProperty
+    def snapshotKeyColumns(self) -> str:
+        """Array of column names used as the snapshot key columns when the 'snapshot' increment method is used
+        default []
+        """
+        return self.__snapshotKeyColumns
+    @snapshotKeyColumns.setter(key='snapshotKeyColumns')
+    def snapshotKeyColumns(self, val):
+        self.__snapshotKeyColumns = val
+
+    @CfgProperty
+    def identityColumnPattern(self) -> str:
+        """The name pattern of the identity column to be added to the table. The values are 1, 2, 3..., each new record gets a new number.
+        If configured, the column will be placed at the beginning of the table.
+        
+        Supported placeholders:
+        {tablename} : the delta table name
+        {lakehousename} : the lakehouse name
+
+        example: '{tablename}_ID'
+
+        default None
+        """
+        return self.__identityColumnPattern
+    @identityColumnPattern.setter(key='identityColumnPattern')
+    def identityColumnPattern(self, val):
+        self.__identityColumnPattern = val
+
+    @CfgProperty
+    def insertDateColumn(self) -> str:
+        """The name of the timestamp column to be added to each record, reflecting the time when the record was inserted.
+        default None
+        """
+        return self.__insertDateColumn
+    @insertDateColumn.setter(key='insertDateColumn')
+    def insertDateColumn(self, val):
+        self.__insertDateColumn = val
+
+    @CfgProperty
+    def invalidCharactersInColumnNamesReplacement(self) -> str:
+        """The characters ' ,;{}()\n\t=' are invalid in delta tables. This sets the replacement character. You can use '' to just remove the invalid characters.
+        default '_'
+        """
+        return self.__invalidCharactersInColumnNamesReplacement
+    @invalidCharactersInColumnNamesReplacement.setter(key='invalidCharactersInColumnNamesReplacement')
+    def invalidCharactersInColumnNamesReplacement(self, val):
+        self.__invalidCharactersInColumnNamesReplacement = val
+    
+    @CfgProperty
+    def canAddNewColumns(self) -> bool:
+        """If the table already exists and the input dataset has extra columns, add the columns to the table with empty values before loading the new input.
+        default True
+        """
+        return self.__canAddNewColumns
+    @canAddNewColumns.setter(key='canAddNewColumns')
+    def canAddNewColumns(self, val):
+        self.__canAddNewColumns = val
+    
+    @CfgProperty
+    def addNARecord(self) -> bool:
+        """Add a dimension "N/A" record with identity -1, N/A for strings, 0 for numbers. identityColumnPattern needs to be configured when this is enabled.
+        default False
+        """
+        return self.__addNARecord
+    @addNARecord.setter(key='addNARecord')
+    def addNARecord(self, val):
+        self.__addNARecord = val
+
+    @CfgProperty
+    def addBadValueRecord(self) -> bool:
+        """Add a dimension "Bad Value" record with identity 0, Bad Value for strings, 0 for numbers. identityColumnPattern needs to be configured when this is enabled.
+        default False
+        """
+        return self.__addBadValueRecord
+    @addBadValueRecord.setter(key='addBadValueRecord')
+    def addBadValueRecord(self, val):
+        self.__addBadValueRecord = val
+
+    @CfgProperty
+    def largeTableMethodEnabled(self) -> bool:
+        """For large tables in merge mode, first load the changed records to a separate temp table and perform the merge there before appending back to the original table
+        default True
+        """
+        return self.__largeTableMethodEnabled
+    @largeTableMethodEnabled.setter(key='largeTableMethodEnabled')
+    def largeTableMethodEnabled(self, val):
+        self.__largeTableMethodEnabled = val
+
+    @CfgProperty
+    def largeTableMethodSourceThresholdGB(self) -> float:
+        """For large tables in merge mode, first load the changed records to a separate temp table and perform the merge there before appending back to the original table - threshold for the size of the source data in GB
+        default 0.2
+        """
+        return self.__largeTableMethodSourceThresholdGB
+    @largeTableMethodSourceThresholdGB.setter(key='largeTableMethodSourceThresholdGB')
+    def largeTableMethodSourceThresholdGB(self, val):
+        self.__largeTableMethodSourceThresholdGB = val
+    
+    @CfgProperty
+    def largeTableMethodDestinationThresholdGB(self) -> float:
+        """For large tables in merge mode, first load the changed records to a separate temp table and perform the merge there before appending back to the original table - threshold for the size of the destination table in GB
+        default 20
+        """
+        return self.__largeTableMethodDestinationThresholdGB
+    @largeTableMethodDestinationThresholdGB.setter(key='largeTableMethodDestinationThresholdGB')
+    def largeTableMethodDestinationThresholdGB(self, val):
+        self.__largeTableMethodDestinationThresholdGB = val
+
+    @CfgProperty
+    def rowStartColumn(self) -> str:
+        """Only for SCD2 increment - the row start timestamp column name
+        default RowStart
+        """
+        return self.__rowStartColumn
+    @rowStartColumn.setter(key='rowStartColumn')
+    def rowStartColumn(self, val):
+        self.__rowStartColumn = val
+    
+    @CfgProperty
+    def rowEndColumn(self) -> str:
+        """Only for SCD2 increment - the row end timestamp column name
+        default RowEnd
+        """
+        return self.__rowEndColumn
+    @rowEndColumn.setter(key='rowEndColumn')
+    def rowEndColumn(self, val):
+        self.__rowEndColumn = val
+    
+    @CfgProperty
+    def currentRowColumn(self) -> str:
+        """Only for SCD2 increment - the current row (Ture / False) column name
+        default CurrentRow
+        """
+        return self.__currentRowColumn
+    @currentRowColumn.setter(key='currentRowColumn')
+    def currentRowColumn(self, val):
+        self.__currentRowColumn = val
+    
+    @CfgProperty
+    def scd2SoftDelete(self) -> bool:
+        """For SCD 2 increment, set the RowEnd timestamp for rows not found in source (for full load only)
+        default False
+        """
+        return self.__scd2SoftDelete
+    @scd2SoftDelete.setter(key='scd2SoftDelete')
+    def scd2SoftDelete(self, val):
+        self.__scd2SoftDelete = val
+
+    @CfgProperty
+    def scd2ExcludeColumns(self) -> bool:
+        """For SCD 2 increment - a list of untracked non-key columns - these will not trigger a new row version if their value changes
+        default []
+        """
+        return self.__scd2ExcludeColumns
+    @scd2ExcludeColumns.setter(key='scd2ExcludeColumns')
+    def scd2ExcludeColumns(self, val):
+        t = str(type(val))
+        if t == "<class 'str'>":
+            self.__scd2ExcludeColumns = val.split(',')
+        else:
+            self.__scd2ExcludeColumns = val
