99123f27f9e313dd9c80608bc0d2a00362997d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:51 +0000

    Add package skyhook-timer/0.0.36/skyhook_timer-0.0.36-py3-none-any.whl

---
+++ packages/skyhook-timer/skyhook_timer-0.0.36-py3-none-any.whl/skyhook_timer/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+
+from .models import SkyhookTimer
+import logging
+logger = logging.getLogger(__name__)
+# from django.contrib.auth.decorators import state_required
+
+@permission_required("skyhook_timer.view_skyhooktimer")
+def skyhook_timer_view(request):
+    # Get all timers for the member to view
+    timers = SkyhookTimer.objects.all()
+    sorted_timers = sorted(
+        timers,
+        key=lambda t: (t.time_remaining is None, t.time_remaining)
+    )
+    logger.info("Rendering the view_timers template")
+    # Render the view, allow "Members" to see but not interact with the data
+    return render(request, 'skyhook_timer/view_timers.html', {'timers': sorted_timers})
