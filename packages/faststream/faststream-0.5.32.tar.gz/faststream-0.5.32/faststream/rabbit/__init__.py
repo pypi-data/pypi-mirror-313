8435936da513897319880b29d84d92c566197b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:30 +0000

    Add package faststream/0.5.32/faststream-0.5.32.tar.gz

---
+++ packages/faststream/faststream-0.5.32.tar.gz/faststream-0.5.32/faststream/rabbit/__init__.py
@@ -0,0 +1,28 @@
+from faststream.rabbit.annotations import RabbitMessage
+from faststream.rabbit.broker import RabbitBroker
+from faststream.rabbit.response import RabbitResponse
+from faststream.rabbit.router import RabbitPublisher, RabbitRoute, RabbitRouter
+from faststream.rabbit.schemas import (
+    ExchangeType,
+    RabbitExchange,
+    RabbitQueue,
+    ReplyConfig,
+)
+from faststream.rabbit.testing import TestRabbitBroker
+from faststream.testing.app import TestApp
+
+__all__ = (
+    "ExchangeType",
+    "RabbitBroker",
+    "RabbitExchange",
+    # Annotations
+    "RabbitMessage",
+    "RabbitPublisher",
+    "RabbitQueue",
+    "RabbitResponse",
+    "RabbitRoute",
+    "RabbitRouter",
+    "ReplyConfig",
+    "TestApp",
+    "TestRabbitBroker",
+)
