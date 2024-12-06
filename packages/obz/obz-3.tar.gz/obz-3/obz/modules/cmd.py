14b1bd073a8e27b64ca48b67dbca66a5589e557a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:05:35 +0000

    Add package obz/3/obz-3.tar.gz

---
+++ packages/obz/obz-3.tar.gz/obz-3/obz/modules/cmd.py
@@ -0,0 +1,15 @@
+# This file is placed in the Public Domain.
+# pylint: disable=C,W0105,E0402
+
+
+"commands"
+
+
+from obx import keys
+
+
+from ..runtime import Commands
+
+
+def cmd(event):
+    event.reply(",".join(sorted(keys(Commands.cmds))))
