90a8db353c0d0231ae846b07cdc28937b9ce5564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:53 +0000

    Add package nonebot-plugin-arcaea-sticker/0.1.4/nonebot_plugin_arcaea_sticker-0.1.4.tar.gz

---
+++ packages/nonebot-plugin-arcaea-sticker/nonebot_plugin_arcaea_sticker-0.1.4.tar.gz/nonebot_plugin_arcaea_sticker-0.1.4/nonebot-plugin-arcaea-sticker/__init__.py
@@ -0,0 +1,19 @@
+from nonebot.plugin import PluginMetadata
+
+from .config import Config, plugin_config
+from .matcher import *
+
+__plugin_meta__ = PluginMetadata(
+    name="Arcaea表情包生成器",
+    description="生成Arcaea风格的表情包",
+    usage="""
+    基础指令：
+    - arc <角色> <文字> - 生成表情包
+    - arc -h - 显示帮助
+    - arc - 进入交互模式
+    """,
+    type="application",
+    homepage="https://github.com/JQ-28/nonebot-plugin-arcaea-sticker",
+    config=Config,
+    supported_adapters={"~onebot.v11"},
+)
