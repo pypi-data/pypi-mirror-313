353cf1e42da2484fdb3af1c7853d56e095a7732c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:11 +0000

    Add package FastMarkerDetector/0.0.5/FastMarkerDetector-0.0.5-py3-none-any.whl

---
+++ packages/FastMarkerDetector/FastMarkerDetector-0.0.5-py3-none-any.whl/FastMarkerDetector/__init__.py
@@ -0,0 +1,14 @@
+
+from .FastMarkerDetector import (
+    Compare_A_B_variables_of_each_gene_in_each_cluster_v3,
+    avg_jcd_score,
+    dotplot,
+    finder_marker
+)
+
+__all__ = [
+    Compare_A_B_variables_of_each_gene_in_each_cluster_v3,
+    avg_jcd_score,
+    dotplot,
+    finder_marker
+]
