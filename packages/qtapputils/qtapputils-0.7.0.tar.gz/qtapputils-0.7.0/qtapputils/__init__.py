4ec82837da01f2b4118e6402656f767f471e814f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:11 +0000

    Add package qtapputils/0.7.0/qtapputils-0.7.0.tar.gz

---
+++ packages/qtapputils/qtapputils-0.7.0.tar.gz/qtapputils-0.7.0/qtapputils/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © QtAppUtils Project Contributors
+# https://github.com/jnsebgosselin/apputils
+#
+# This file is part of QtAppUtils.
+# Licensed under the terms of the MIT License.
+# -----------------------------------------------------------------------------
+
+version_info = (0, 7, 0)
+__version__ = '.'.join(map(str, version_info))
+__date__ = '03/12/2024'
+__project_url__ = "https://github.com/jnsebgosselin/qtapputils"
