e13070625ac59892a6d496d0c2d64a5c48e149c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:33:13 +0000

    Add package FlowerPower/0.8.3.2/flowerpower-0.8.3.2.tar.gz

---
+++ packages/FlowerPower/flowerpower-0.8.3.2.tar.gz/flowerpower-0.8.3.2/src/flowerpower/cfg/pipeline/run.py
@@ -0,0 +1,16 @@
+from munch import Munch, munchify
+from pydantic import Field
+
+from ..base import BaseConfig
+
+
+class PipelineRunConfig(BaseConfig):
+    final_vars: list[str] = Field(default_factory=list)
+    inputs: dict | Munch = Field(default_factory=dict)
+    executor: str | None = None
+    with_tracker: bool = False
+    with_opentelemetry: bool = False
+
+    def model_post_init(self, __context):
+        if isinstance(self.inputs, dict):
+            self.inputs = munchify(self.inputs)
