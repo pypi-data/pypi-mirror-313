ccde1f86c337dc4561b6e5ffbb1c0b114999b243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:09 +0000

    Add package polartoolkit/0.7.0/polartoolkit-0.7.0-py3-none-any.whl

---
+++ packages/polartoolkit/polartoolkit-0.7.0-py3-none-any.whl/polartoolkit/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2024 The Polartoolkit Developers.
+# Distributed under the terms of the MIT License.
+# SPDX-License-Identifier: MIT
+#
+# This code is part of the package:
+# PolarToolkit (https://github.com/mdtanker/polartoolkit)
+#
+# Copyright (c) 2022 The Polartoolkit Developers.
+# Distributed under the terms of the MIT License.
+# SPDX-License-Identifier: MIT
+#
+# This code is part of the package:
+# PolarToolkit (https://github.com/mdtanker/polartoolkit)
+#
+"""Helpful tools for polar researchers"""
+
+from __future__ import annotations
+
+__version__ = "0.7.0"
