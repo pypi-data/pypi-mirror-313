24738385084184465c831e0c78ef5184bd2f0c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:23 +0000

    Add package Pyrostates/1.0.0/pyrostates-1.0.0.tar.gz

---
+++ packages/Pyrostates/pyrostates-1.0.0.tar.gz/pyrostates-1.0.0/pyrostates/global_state_machine.py
@@ -0,0 +1,28 @@
+from pyrogram.filters import Filter
+
+from .state_machine import StateMachine
+
+global_state_machine = StateMachine()
+
+
+def get_state(user, state_machine=global_state_machine):
+    return state_machine[user]
+
+
+def set_state(user, state, state_machine=global_state_machine):
+    state_machine[user] = state
+
+
+def del_state(user, state_machine=global_state_machine):
+    del state_machine[user]
+
+
+class AtState(Filter):
+    def __init__(self, state, state_machine=global_state_machine):
+        super().__init__()
+        self.state = state
+        self.state_machine = state_machine
+
+    async def __call__(self, client, update) -> bool:
+        at_state = self.state_machine.at(self.state)
+        return at_state(client, update)
