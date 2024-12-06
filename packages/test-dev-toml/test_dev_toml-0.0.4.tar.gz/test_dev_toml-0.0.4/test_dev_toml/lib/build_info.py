0647e2b904574b5723bb3b3aa2aef95245449952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:31:37 +0000

    Add package test-dev-toml/0.0.4/test_dev_toml-0.0.4.tar.gz

---
+++ packages/test-dev-toml/test_dev_toml-0.0.4.tar.gz/test_dev_toml-0.0.4/test_dev_toml/lib/build_info.py
@@ -0,0 +1,15 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.10.15'
+    os_name = 'posix:ubuntu'
+    version = '0.0.4'
+    git_sha = 'ec12bc8f1198c76f76a9906bc49129ff13aec1f5'
+    git_branch = 'master'
+    git_uncommitted = [
+        'M  do_cpip_update',
+        'MM do_publish',
+        ' M test_dev_toml/lib/constants_version.py',
+        ' M tools/pyproject_template.toml',
+        'M  tools/requirements.txt',
+        ' m tools/xplat_utils',
+        ' M xplat.cfg',
+    ]
