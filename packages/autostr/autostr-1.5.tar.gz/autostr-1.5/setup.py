c1b66e01166adbcc77243fc422ef5a37220ddf44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:29 +0000

    Add package autostr/1.5/autostr-1.5.tar.gz

---
+++ packages/autostr/autostr-1.5.tar.gz/autostr-1.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+"""
+:author: Andrey Kuchebo
+:license: MIT
+"""
+
+version = 1.5
+
+setup(
+	name='autostr',
+	version=version,
+	author='Andrey Kuchebo',
+	author_email='kucheboandrey@gmail.com',
+	package=['autostr']
+)
