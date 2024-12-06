1c6467f56f9059d27ab7c73ccbf2781dcf283986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:42 +0000

    Add package comsel/1.0.0/comsel-1.0.0.tar.gz

---
+++ packages/comsel/comsel-1.0.0.tar.gz/comsel-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='comsel',  # Имя вашей библиотеки
+    version='1.0.0',  # Версия
+    description='A Python library for selecting COM port and baudrate',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Molisc',
+    author_email='ivanrudko445@gmail.com',
+    url='https://github.com/Molisc/comsel',  # URL вашего репозитория
+    packages=find_packages(),  # Поиск всех пакетов в директории comsel
+    install_requires=[  # Зависимости, которые будут установлены
+        'pyserial',
+        'dearpygui',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Минимальная поддерживаемая версия Python
+)
