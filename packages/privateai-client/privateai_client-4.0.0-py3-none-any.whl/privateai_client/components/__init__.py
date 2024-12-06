b72f14d951559a0a47eaed57b78efe4ce5f60919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:30 +0000

    Add package privateai-client/4.0.0/privateai_client-4.0.0-py3-none-any.whl

---
+++ packages/privateai-client/privateai_client-4.0.0-py3-none-any.whl/privateai_client/components/__init__.py
@@ -0,0 +1,14 @@
+from .pai_requests import PAIGetRequests, PAIPostRequests
+from .pai_responses import (
+    BleepResponse,
+    DiagnosticResponse,
+    FilesBase64Response,
+    FilesUriResponse,
+    MetricsResponse,
+    ReidentifyTextResponse,
+    TextResponse,
+    VersionResponse,
+    NerTextResponse,
+)
+from .pai_uris import PAIURIs
+from .request_objects import *
