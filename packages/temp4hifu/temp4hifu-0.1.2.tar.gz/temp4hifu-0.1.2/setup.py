cba4b0ed79eda3d6f3981f9d17cbdf5502e6ce99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:57 +0000

    Add package temp4hifu/0.1.2/temp4hifu-0.1.2.tar.gz

---
+++ packages/temp4hifu/temp4hifu-0.1.2.tar.gz/temp4hifu-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name = 'temp4hifu',
+    version = '0.1.2',
+    description = 'A Python package designed to estimate the temperature increase due to high intensity focused ultrasound (HIFU) excitation.',
+    url='https://github.com/C2H5OH-Consumer/temp4hifu',
+    author = 'Gerald Lee',
+    author_email = 'gerald13@uw.edu',
+    license='MIT',
+    packages = ['temp4hifu'],
+    zip_safe=False,
+)
