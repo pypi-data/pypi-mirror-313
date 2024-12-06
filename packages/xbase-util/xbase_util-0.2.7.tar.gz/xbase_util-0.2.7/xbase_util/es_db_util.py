0aa9a9f1513d982ad7f74b87f0653f5ef1654927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:59 +0000

    Add package xbase-util/0.2.7/xbase_util-0.2.7.tar.gz

---
+++ packages/xbase-util/xbase_util-0.2.7.tar.gz/xbase_util-0.2.7/xbase_util/es_db_util.py
@@ -0,0 +1,32 @@
+import os.path
+
+
+class EsDb:
+    def __init__(self, req):
+        self.req = req
+        self.internals = {}
+        print("初始化:Elasticsearch DB")
+
+    def get_file_by_file_id(self, node, num, prefix=None):
+        key = f'{node}!{num}'
+        if key in self.internals:
+            return self.internals[key]
+        res = self.req.search_file(f"{node}-{num}")
+        try:
+            hits = res.json()['hits']['hits']
+            if len(hits) > 0:
+                self.internals[key] = hits[0]['_source']
+                file = hits[0]['_source']
+                if prefix is None:
+                    return file
+                prefix_res = prefix
+                if not prefix.endswith('/'):
+                    prefix_res = f"{prefix}/"
+                origin_path = file['name']
+                basename = os.path.basename(origin_path)
+                result_path = f"{prefix_res}{basename}"
+                file['name'] = result_path
+                return file
+        except Exception as e:
+            print(f"获取es文件失败：{res.text}获取es文件失败")
+        return None
