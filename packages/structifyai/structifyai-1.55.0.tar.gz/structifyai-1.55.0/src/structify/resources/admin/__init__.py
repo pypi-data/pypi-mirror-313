2f4d19dd01de78f3e934374a0411f100a2a02330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:27 +0000

    Add package structifyai/1.55.0/structifyai-1.55.0.tar.gz

---
+++ packages/structifyai/structifyai-1.55.0.tar.gz/structifyai-1.55.0/src/structify/resources/admin/__init__.py
@@ -0,0 +1,75 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .admin import (
+    AdminResource,
+    AsyncAdminResource,
+    AdminResourceWithRawResponse,
+    AsyncAdminResourceWithRawResponse,
+    AdminResourceWithStreamingResponse,
+    AsyncAdminResourceWithStreamingResponse,
+)
+from .users import (
+    UsersResource,
+    AsyncUsersResource,
+    UsersResourceWithRawResponse,
+    AsyncUsersResourceWithRawResponse,
+    UsersResourceWithStreamingResponse,
+    AsyncUsersResourceWithStreamingResponse,
+)
+from .entity import (
+    EntityResource,
+    AsyncEntityResource,
+    EntityResourceWithRawResponse,
+    AsyncEntityResourceWithRawResponse,
+    EntityResourceWithStreamingResponse,
+    AsyncEntityResourceWithStreamingResponse,
+)
+from .human_llm import (
+    HumanLlmResource,
+    AsyncHumanLlmResource,
+    HumanLlmResourceWithRawResponse,
+    AsyncHumanLlmResourceWithRawResponse,
+    HumanLlmResourceWithStreamingResponse,
+    AsyncHumanLlmResourceWithStreamingResponse,
+)
+from .training_datasets import (
+    TrainingDatasetsResource,
+    AsyncTrainingDatasetsResource,
+    TrainingDatasetsResourceWithRawResponse,
+    AsyncTrainingDatasetsResourceWithRawResponse,
+    TrainingDatasetsResourceWithStreamingResponse,
+    AsyncTrainingDatasetsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "EntityResource",
+    "AsyncEntityResource",
+    "EntityResourceWithRawResponse",
+    "AsyncEntityResourceWithRawResponse",
+    "EntityResourceWithStreamingResponse",
+    "AsyncEntityResourceWithStreamingResponse",
+    "HumanLlmResource",
+    "AsyncHumanLlmResource",
+    "HumanLlmResourceWithRawResponse",
+    "AsyncHumanLlmResourceWithRawResponse",
+    "HumanLlmResourceWithStreamingResponse",
+    "AsyncHumanLlmResourceWithStreamingResponse",
+    "UsersResource",
+    "AsyncUsersResource",
+    "UsersResourceWithRawResponse",
+    "AsyncUsersResourceWithRawResponse",
+    "UsersResourceWithStreamingResponse",
+    "AsyncUsersResourceWithStreamingResponse",
+    "TrainingDatasetsResource",
+    "AsyncTrainingDatasetsResource",
+    "TrainingDatasetsResourceWithRawResponse",
+    "AsyncTrainingDatasetsResourceWithRawResponse",
+    "TrainingDatasetsResourceWithStreamingResponse",
+    "AsyncTrainingDatasetsResourceWithStreamingResponse",
+    "AdminResource",
+    "AsyncAdminResource",
+    "AdminResourceWithRawResponse",
+    "AsyncAdminResourceWithRawResponse",
+    "AdminResourceWithStreamingResponse",
+    "AsyncAdminResourceWithStreamingResponse",
+]
