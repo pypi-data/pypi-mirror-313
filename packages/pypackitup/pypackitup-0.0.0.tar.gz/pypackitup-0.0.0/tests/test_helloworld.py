b6dbf2ef734d0fca050c4e1c44c540d841319c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:32 +0000

    Add package pypackitup/0.0.0/pypackitup-0.0.0.tar.gz

---
+++ packages/pypackitup/pypackitup-0.0.0.tar.gz/pypackitup-0.0.0/tests/test_helloworld.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+def test_example_method_default(python_package_template):
+    result = python_package_template.example_method()
+
+    assert result == "Hello, World!"
+
+
+def test_example_method_custom(python_package_template):
+    result = python_package_template.example_method("Everyone")
+
+    assert result == "Hello, Everyone!"
+
+
+def test_example_method_none(python_package_template):
+    with pytest.raises(ValueError, match="Invalid argument provided for example_parameter."):
+        python_package_template.example_method("")
