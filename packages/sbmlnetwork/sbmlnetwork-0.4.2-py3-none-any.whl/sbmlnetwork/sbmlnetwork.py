cc648fcf8b0f5cf479c77320be294a81bd1217f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:19 +0000

    Add package sbmlnetwork/0.4.2/sbmlnetwork-0.4.2-py3-none-any.whl

---
+++ packages/sbmlnetwork/sbmlnetwork-0.4.2-py3-none-any.whl/sbmlnetwork/sbmlnetwork.py
@@ -0,0 +1,40 @@
+import libsbmlnetwork
+import networkinfotranslator
+from IPython.display import display
+
+
+class SBMLNetwork(libsbmlnetwork.LibSBMLNetwork):
+
+    def __init__(self, sbml=""):
+        super().__init__(sbml)
+
+    def draw(self, file_name=""):
+        """
+        Draws the network of the SBML model. Saves the figure to the file_directory if specified, otherwise displays the figure.
+
+        :param file_directory:
+        :param file_name:
+        """
+        if file_name:
+            networkinfotranslator.import_sbml_export_figure(self, file_name,
+                                                            self.display_compartments_text_label,
+                                                            self.display_species_text_label,
+                                                            self.display_reactions_text_label)
+        else:
+            display(networkinfotranslator.import_sbml_export_pil_image(self,
+                                                                       self.display_compartments_text_label,
+                                                                       self.display_species_text_label,
+                                                                       self.display_reactions_text_label))
+
+
+def load(sbml):
+    """
+    Loads the SBML model.
+
+    :param sbml: The SBML model.
+    :return: SBMLNetwork
+    """
+    return SBMLNetwork(sbml)
+
+
+
