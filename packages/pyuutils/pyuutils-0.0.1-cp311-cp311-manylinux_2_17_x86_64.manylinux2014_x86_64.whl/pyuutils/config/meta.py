186f72017fdb63b6f6f9833fd61090542264d593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:05 +0000

    Add package pyuutils/0.0.1/pyuutils-0.0.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyuutils/pyuutils-0.0.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyuutils/config/meta.py
@@ -0,0 +1,19 @@
+"""
+Overview:
+    Meta information for pyuutils package.
+"""
+
+#: Title of this project (should be `pyuutils`).
+__TITLE__ = "pyuutils"
+
+#: Version of this project.
+__VERSION__ = "0.0.1"
+
+#: Short description of the project, will be included in ``setup.py``.
+__DESCRIPTION__ = 'Python wrapper for Uppaal UUtils.'
+
+#: Author of this project.
+__AUTHOR__ = "HansBug"
+
+#: Email of the authors'.
+__AUTHOR_EMAIL__ = "hansbug@buaa.edu.cn"
