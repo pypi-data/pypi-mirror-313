a6cb94a710b6a2c609515a65577df7ead1708cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:49 +0000

    Add package mypy-boto3-bedrock/1.35.74/mypy_boto3_bedrock-1.35.74-py3-none-any.whl

---
+++ packages/mypy-boto3-bedrock/mypy_boto3_bedrock-1.35.74-py3-none-any.whl/mypy_boto3_bedrock/__init__.py
@@ -0,0 +1,71 @@
+"""
+Main interface for bedrock service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_bedrock import (
+        BedrockClient,
+        Client,
+        ListCustomModelsPaginator,
+        ListEvaluationJobsPaginator,
+        ListGuardrailsPaginator,
+        ListImportedModelsPaginator,
+        ListInferenceProfilesPaginator,
+        ListModelCopyJobsPaginator,
+        ListModelCustomizationJobsPaginator,
+        ListModelImportJobsPaginator,
+        ListModelInvocationJobsPaginator,
+        ListProvisionedModelThroughputsPaginator,
+    )
+
+    session = Session()
+    client: BedrockClient = session.client("bedrock")
+
+    list_custom_models_paginator: ListCustomModelsPaginator = client.get_paginator("list_custom_models")
+    list_evaluation_jobs_paginator: ListEvaluationJobsPaginator = client.get_paginator("list_evaluation_jobs")
+    list_guardrails_paginator: ListGuardrailsPaginator = client.get_paginator("list_guardrails")
+    list_imported_models_paginator: ListImportedModelsPaginator = client.get_paginator("list_imported_models")
+    list_inference_profiles_paginator: ListInferenceProfilesPaginator = client.get_paginator("list_inference_profiles")
+    list_model_copy_jobs_paginator: ListModelCopyJobsPaginator = client.get_paginator("list_model_copy_jobs")
+    list_model_customization_jobs_paginator: ListModelCustomizationJobsPaginator = client.get_paginator("list_model_customization_jobs")
+    list_model_import_jobs_paginator: ListModelImportJobsPaginator = client.get_paginator("list_model_import_jobs")
+    list_model_invocation_jobs_paginator: ListModelInvocationJobsPaginator = client.get_paginator("list_model_invocation_jobs")
+    list_provisioned_model_throughputs_paginator: ListProvisionedModelThroughputsPaginator = client.get_paginator("list_provisioned_model_throughputs")
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+from .client import BedrockClient
+from .paginator import (
+    ListCustomModelsPaginator,
+    ListEvaluationJobsPaginator,
+    ListGuardrailsPaginator,
+    ListImportedModelsPaginator,
+    ListInferenceProfilesPaginator,
+    ListModelCopyJobsPaginator,
+    ListModelCustomizationJobsPaginator,
+    ListModelImportJobsPaginator,
+    ListModelInvocationJobsPaginator,
+    ListProvisionedModelThroughputsPaginator,
+)
+
+Client = BedrockClient
+
+
+__all__ = (
+    "BedrockClient",
+    "Client",
+    "ListCustomModelsPaginator",
+    "ListEvaluationJobsPaginator",
+    "ListGuardrailsPaginator",
+    "ListImportedModelsPaginator",
+    "ListInferenceProfilesPaginator",
+    "ListModelCopyJobsPaginator",
+    "ListModelCustomizationJobsPaginator",
+    "ListModelImportJobsPaginator",
+    "ListModelInvocationJobsPaginator",
+    "ListProvisionedModelThroughputsPaginator",
+)
