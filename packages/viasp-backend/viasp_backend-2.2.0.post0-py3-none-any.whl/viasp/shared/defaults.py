3a3d71e98e29200915694b1ec510f18635a0be7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:08 +0000

    Add package viasp-backend/2.2.0.post0/viasp_backend-2.2.0.post0-py3-none-any.whl

---
+++ packages/viasp-backend/viasp_backend-2.2.0.post0-py3-none-any.whl/viasp/shared/defaults.py
@@ -0,0 +1,34 @@
+import pathlib
+import os
+import json
+
+DEFAULT_BACKEND_PROTOCOL = "http"
+DEFAULT_BACKEND_HOST = "127.0.0.1"
+DEFAULT_BACKEND_PORT = 5050
+DEFAULT_FRONTEND_PORT = 8050
+DEFAULT_BACKEND_URL = f"{DEFAULT_BACKEND_PROTOCOL}://{DEFAULT_BACKEND_HOST}:{DEFAULT_BACKEND_PORT}"
+DEFAULT_COLOR = "blue"
+DEFAULT_LOCALE = "en" + ".json"
+LOCALES_PATH = pathlib.Path(__file__).parent.parent.resolve() / "locales" / DEFAULT_LOCALE
+SHARED_PATH = pathlib.Path(__file__).parent.resolve()
+GRAPH_PATH = SHARED_PATH / "viasp_graph_storage.db"
+SERVER_PATH =  pathlib.Path(__file__).parent.parent.resolve() / "server/"
+STATIC_PATH =  os.path.join(SERVER_PATH, "static")
+CLINGRAPH_PATH = os.path.join(STATIC_PATH, "clingraph")
+PROGRAM_STORAGE_PATH = SHARED_PATH / "prg.lp"
+STDIN_TMP_STORAGE_PATH = SHARED_PATH / "viasp_stdin_tmp.lp"
+COLOR_PALETTE_PATH = SERVER_PATH / "colorPalette.json"
+SORTGENERATION_TIMEOUT_SECONDS = 10
+SORTGENERATION_BATCH_SIZE = 1000
+
+
+def load_messages(json_path):
+    with open(json_path, 'r') as file:
+        messages = json.load(file)
+    return messages
+
+# Load messages from the JSON file
+MESSAGES = load_messages(LOCALES_PATH)
+
+def _(message_key):
+    return MESSAGES.get(message_key, f"Message key '{message_key}' not found.")
