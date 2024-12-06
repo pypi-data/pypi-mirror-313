76c52f3af8c499be30752dc65f60299363504915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:46 +0000

    Add package mlquantify/0.0.11.8/mlquantify-0.0.11.8.tar.gz

---
+++ packages/mlquantify/mlquantify-0.0.11.8.tar.gz/mlquantify-0.0.11.8/mlquantify/__init__.py
@@ -0,0 +1,9 @@
+"mlquantify, a Python package for quantification"
+
+from . import base
+from . import model_selection
+from . import plots
+from . import classification
+from . import evaluation
+from . import methods
+from . import utils
