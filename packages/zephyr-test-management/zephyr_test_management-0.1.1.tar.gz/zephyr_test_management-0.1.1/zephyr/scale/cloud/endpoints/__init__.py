6e523586f8f2ab39755274c2ffb3071043dff4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:17 +0000

    Add package zephyr-test-management/0.1.1/zephyr_test_management-0.1.1.tar.gz

---
+++ packages/zephyr-test-management/zephyr_test_management-0.1.1.tar.gz/zephyr_test_management-0.1.1/zephyr/scale/cloud/endpoints/__init__.py
@@ -0,0 +1,13 @@
+from .endpoints import (TestCaseEndpoints,
+                        TestCycleEndpoints,
+                        TestPlanEndpoints,
+                        TestExecutionEndpoints,
+                        FolderEndpoints,
+                        StatusEndpoints,
+                        PriorityEndpoints,
+                        EnvironmentEndpoints,
+                        ProjectEndpoints,
+                        LinkEndpoints,
+                        IssueLinksEndpoints,
+                        AutomationEndpoints,
+                        HealthcheckEndpoints)
