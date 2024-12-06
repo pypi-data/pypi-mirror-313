72d51220a38b79f05ab1323b818d19a290e53e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:27 +0000

    Add package cs2pattern/0.1.0/cs2pattern-0.1.0-py3-none-any.whl

---
+++ packages/cs2pattern/cs2pattern-0.1.0-py3-none-any.whl/cs2pattern/__init__.py
@@ -0,0 +1,35 @@
+__author__ = "Lukas Mahler"
+__version__ = "0.0.0"
+__date__ = "04.12.2024"
+__email__ = "m@hler.eu"
+__status__ = "Development"
+
+
+from cs2pattern.check import check_rare, get_pattern_dict
+from cs2pattern.modular import *
+
+__all__ = [
+    'check_rare',
+    'get_pattern_dict',
+
+    'abyss',
+    'berries',
+    'blaze',
+    'fire_and_ice',
+    'gem_blue',
+    'gem_diamond',
+    'gem_gold',
+    'gem_green',
+    'gem_pink',
+    'gem_purple',
+    'grinder',
+    'hive',
+    'moonrise',
+    'paw',
+    'phoenix',
+    'pussy'
+]
+
+
+if __name__ == '__main__':
+    exit(1)
