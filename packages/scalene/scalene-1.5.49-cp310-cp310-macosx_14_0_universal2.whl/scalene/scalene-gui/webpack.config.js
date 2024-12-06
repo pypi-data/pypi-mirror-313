cc3a01aa676b247e8adb4d7be2e27f0a2b1b7e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:02 +0000

    Add package scalene/1.5.49/scalene-1.5.49-cp310-cp310-macosx_14_0_universal2.whl

---
+++ packages/scalene/scalene-1.5.49-cp310-cp310-macosx_14_0_universal2.whl/scalene/scalene-gui/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './scalene-gui.js',
+  mode: 'production',
+  output: {
+    filename: 'scalene-gui-bundle.js',
+    path: path.resolve(__dirname, ''),
+    libraryTarget: 'window',
+  },
+  resolve: {
+    fallback: {
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "buffer": require.resolve("buffer"),
+      "util": require.resolve("util"),
+      "assert": require.resolve("assert"),
+      "os": require.resolve("os-browserify/browser"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "url": require.resolve("url/"),
+      "zlib": require.resolve("browserify-zlib"),
+      "path": require.resolve("path-browserify"),
+      "fs": false,
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
+  optimization: {
+    minimize: true,            // Enable minimization
+    usedExports: false,         // Disable tree shaking
+    sideEffects: false,         // Include all files, assuming they have side effects
+    concatenateModules: false,  // Disable module concatenation (scope hoisting)
+    innerGraph: false,          // Disable inner graph analysis
+  },
+};
