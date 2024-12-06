fe425e5cefc3e7ec6f3483d2c122e24d27ec92b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:26 +0000

    Add package live2d-py/0.2.5/live2d_py-0.2.5-cp310-cp310-win_amd64.whl

---
+++ packages/live2d-py/live2d_py-0.2.5-cp310-cp310-win_amd64.whl/live2d/v3/__init__.py
@@ -0,0 +1,23 @@
+from .params import * # pyinstaller may not find it (hidden import)
+from .live2d import *
+
+from enum import Enum
+
+
+class MotionPriority(Enum):
+    NONE = 0
+    IDLE = 1
+    NORMAL = 2
+    FORCE = 3
+
+
+class MotionGroup(Enum):
+    IDLE = "Idle"
+    TAP_HEAD = "TapHead"
+
+
+class HitArea(Enum):
+    HEAD = MotionGroup.TAP_HEAD.value
+
+
+LIVE2D_VERSION = 3
