ee549558565727e0d626520fffa3b208b94dcdc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:08 +0000

    Add package robotframework-openapitools/0.4.0/robotframework_openapitools-0.4.0-py3-none-any.whl

---
+++ packages/robotframework-openapitools/robotframework_openapitools-0.4.0-py3-none-any.whl/roboswag/__init__.py
@@ -0,0 +1,9 @@
+from importlib.metadata import version
+
+from roboswag.cli import cli
+from roboswag.core import APIModel
+
+try:
+    __version__ = version("robotframework-openapitools")
+except Exception:  # pragma: no cover
+    pass
