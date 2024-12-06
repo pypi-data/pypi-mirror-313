012c7a512395d984708c30704c77ef799ced6cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:01 +0000

    Add package je-auto-control-dev/0.0.102/je_auto_control_dev-0.0.102-py3-none-any.whl

---
+++ packages/je-auto-control-dev/je_auto_control_dev-0.0.102-py3-none-any.whl/je_auto_control/utils/logging/loggin_instance.py
@@ -0,0 +1,25 @@
+import logging
+
+logging.root.setLevel(logging.DEBUG)
+autocontrol_logger = logging.getLogger("AutoControlGUI")
+formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+# File handler
+file_handler = logging.FileHandler(filename="AutoControlGUI.log", mode="w")
+file_handler.setFormatter(formatter)
+autocontrol_logger.addHandler(file_handler)
+
+class APITestkaLoggingHandler(logging.Handler):
+
+    # redirect logging stderr output to queue
+
+    def __init__(self):
+        super().__init__()
+        self.formatter = formatter
+        self.setLevel(logging.DEBUG)
+
+    def emit(self, record: logging.LogRecord) -> None:
+        print(self.format(record))
+
+
+# Stream handler
+autocontrol_logger.addHandler(APITestkaLoggingHandler())
