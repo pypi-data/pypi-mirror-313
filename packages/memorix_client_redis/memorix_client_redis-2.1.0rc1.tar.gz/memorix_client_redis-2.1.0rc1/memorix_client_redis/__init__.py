7aaa87e85322586a022814a7edc63b78dd59ef0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:11:30 +0000

    Add package memorix_client_redis/2.1.0rc1/memorix_client_redis-2.1.0rc1.tar.gz

---
+++ packages/memorix_client_redis/memorix_client_redis-2.1.0rc1.tar.gz/memorix_client_redis-2.1.0rc1/memorix_client_redis/__init__.py
@@ -0,0 +1,5 @@
+from .memorix_base import MemorixBase as MemorixBase
+from .cache import All as MemorixCacheAll
+from .pubsub import All as MemorixPubSubAll
+from .task import All as MemorixTaskAll
+from dataclasses import dataclass as dataclass
---
+++ packages/memorix_client_redis/memorix_client_redis-2.1.0rc1.tar.gz/memorix_client_redis-2.1.0rc1/memorix_client_redis/memorix_base.py
@@ -0,0 +1,34 @@
+import typing
+import asyncio
+import functools
+from .redis_connection import RedisConnection
+
+
+class MemorixBase(object):
+    def __init__(
+        self,
+        redis_url: typing.Optional[str] = None,
+        ref: typing.Optional["MemorixBase"] = None,
+    ) -> None:
+        if ref is None and redis_url is not None:
+            self._connection = RedisConnection(redis_url=redis_url)
+        elif ref is not None:
+            self._connection = ref._connection
+        else:
+            raise Exception("Didn't get redis_url or ref")
+
+        self._namespace_name_tree = typing.cast(typing.List[str], [])
+
+    def connect(self) -> None:
+        self._connection.redis.ping()
+        self._connection.pubsub.ping()
+
+    async def async_connect(self) -> None:
+        loop = asyncio.get_running_loop()
+        await loop.run_in_executor(
+            None,
+            functools.partial(
+                MemorixBase.connect,
+                self=self,
+            ),
+        )
