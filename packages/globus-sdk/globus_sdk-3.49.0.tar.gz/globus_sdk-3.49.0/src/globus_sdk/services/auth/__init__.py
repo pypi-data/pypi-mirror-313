89315eaf154981ba86f3ff6880b23d35dac68489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:14 +0000

    Add package globus-sdk/3.49.0/globus_sdk-3.49.0.tar.gz

---
+++ packages/globus-sdk/globus_sdk-3.49.0.tar.gz/globus_sdk-3.49.0/src/globus_sdk/services/auth/__init__.py
@@ -0,0 +1,46 @@
+from .client import (
+    AuthClient,
+    AuthLoginClient,
+    ConfidentialAppAuthClient,
+    NativeAppAuthClient,
+)
+from .data import DependentScopeSpec
+from .errors import AuthAPIError
+from .flow_managers import (
+    GlobusAuthorizationCodeFlowManager,
+    GlobusNativeAppFlowManager,
+)
+from .identity_map import IdentityMap
+from .response import (
+    GetConsentsResponse,
+    GetIdentitiesResponse,
+    OAuthAuthorizationCodeResponse,
+    OAuthClientCredentialsResponse,
+    OAuthDependentTokenResponse,
+    OAuthRefreshTokenResponse,
+    OAuthTokenResponse,
+)
+
+__all__ = (
+    # client classes
+    "AuthClient",
+    "AuthLoginClient",
+    "NativeAppAuthClient",
+    "ConfidentialAppAuthClient",
+    # errors
+    "AuthAPIError",
+    # high-level helpers
+    "DependentScopeSpec",
+    "IdentityMap",
+    # flow managers
+    "GlobusNativeAppFlowManager",
+    "GlobusAuthorizationCodeFlowManager",
+    # responses
+    "GetConsentsResponse",
+    "GetIdentitiesResponse",
+    "OAuthAuthorizationCodeResponse",
+    "OAuthClientCredentialsResponse",
+    "OAuthDependentTokenResponse",
+    "OAuthRefreshTokenResponse",
+    "OAuthTokenResponse",
+)
