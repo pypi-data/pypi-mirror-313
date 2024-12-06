72ccdc58d1abefdac723b64259659bde5693b42c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:41 +0000

    Add package CodeAss/0.13.1/codeass-0.13.1.tar.gz

---
+++ packages/CodeAss/codeass-0.13.1.tar.gz/codeass-0.13.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
+setup(
+    name="CodeAss",
+    version="0.13.1",
+    author="HullaBulla",
+    author_email="hullabulla666@gmail.com",
+    description="Code Assistant",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=[],
+    packages=find_packages(
+        include=[
+            "codeassistant",
+            "codeassistant.*",
+        ]
+    ),
+    install_requires=[requirements],
+    python_requires=">=3.10",
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.10",
+    ],
+    entry_points="""
+        [console_scripts]
+        ca=codeassistant.main:cli
+    """,
+)
