27fe62d02ca677b26ab9525778cfc7d919d47c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:33 +0000

    Add package tcrutils/13.0.9/tcrutils-13.0.9.tar.gz

---
+++ packages/tcrutils/tcrutils-13.0.9.tar.gz/tcrutils-13.0.9/tcrutils/src/tcr_constants.py
@@ -0,0 +1,21 @@
+# fmt: off
+BACKSLASH  = '\\'
+NEWLINE    = '\n'
+CARR_RET   = '\r'
+BACKSPACE  = '\b'
+BACKTICK   = '`'
+BACKTICKS  = 3*BACKTICK
+APOSTROPHE = "'"
+QUOTE      = '"'
+FAKE_PIPE  = '¦'
+
+# fmt: off
+__all__ = [
+  "BACKSLASH",
+  "NEWLINE",
+  "CARR_RET",
+  "BACKSPACE",
+  "BACKTICK", "BACKTICKS",
+  "APOSTROPHE", "QUOTE",
+  "FAKE_PIPE",
+]
