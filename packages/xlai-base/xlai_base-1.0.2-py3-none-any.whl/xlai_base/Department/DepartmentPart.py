a1d182e3951af57f3c4d28c98f417271a4b589cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:30 +0000

    Add package xlai-base/1.0.2/xlai_base-1.0.2-py3-none-any.whl

---
+++ packages/xlai-base/xlai_base-1.0.2-py3-none-any.whl/xlai_base/Department/DepartmentPart.py
@@ -0,0 +1,49 @@
+# coding=utf8
+
+class PP:
+    name = "PP"
+    host = 'pp.ai.home.tokgo.cn'
+    port = 15001
+    pass
+
+
+class SF:
+    name = "SF"
+    host = 'sf.ai.home.tokgo.cn'
+    port = 15002
+    pass
+
+
+class AL:
+    name = "AL"
+    host = 'al.ai.home.tokgo.cn'
+    port = 15003
+    pass
+
+
+class MB:
+    name = "MB"
+    host = 'mb.ai.home.tokgo.cn'
+    port = 15004
+    pass
+
+
+class EXT:
+    name = "EXT"
+    host = 'ext.ai.home.tokgo.cn'
+    port = 15005
+    pass
+
+
+class EV:
+    name = "EV"
+    host = 'ev.ai.home.tokgo.cn'
+    port = 15006
+    pass
+
+
+class MP:
+    name = "MP"
+    host = 'mp.ai.home.tokgo.cn'
+    port = 15007
+    pass
