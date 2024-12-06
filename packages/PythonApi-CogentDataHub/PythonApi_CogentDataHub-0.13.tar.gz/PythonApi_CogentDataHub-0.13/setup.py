ac4b768e3e918c27dc9fb8943aa4762555673bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:26 +0000

    Add package PythonApi-CogentDataHub/0.13/PythonApi_CogentDataHub-0.13.tar.gz

---
+++ packages/PythonApi-CogentDataHub/PythonApi_CogentDataHub-0.13.tar.gz/PythonApi_CogentDataHub-0.13/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(
+  name = 'PythonApi_CogentDataHub',         # How you named your package folder (MyLib) 
+  version = '0.13',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Python Api for Cogent DataHub',   # Give a short description about your library
+  author = 'Colin Yuen',                   # Type in your name
+  author_email = 'colin.yuen@skkynet.com',      # Type in your E-Mail
+  url = 'https://github.com/cyuenSkkynet/PythonApi_CogentDataHub',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/cyuenSkkynet/PythonApi_CogentDataHub/archive/refs/tags/v0.13.tar.gz',    # I explain this later on
+  keywords = ['Cogent', 'DataHub', 'Api', 'Python'],   # Keywords that define your package best
+  packages=find_packages(),
+  install_requires=[            # I get to this in a second
+        'matplotlib>=3.0.0',  # For plotting
+        'dataclasses; python_version < "3.7"'  # Conditional dependency for Python < 3.7 
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+  ],
+)
