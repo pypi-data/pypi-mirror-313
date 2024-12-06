167032130ee17bb86e49bb8649c4b810fa7a1613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:36 +0000

    Add package blueair-api/1.15.1/blueair_api-1.15.1.tar.gz

---
+++ packages/blueair-api/blueair_api-1.15.1.tar.gz/blueair_api-1.15.1/src/blueair_api/model_enum.py
@@ -0,0 +1,9 @@
+from enum import StrEnum
+
+
+class ModelEnum(StrEnum):
+    UNKNOWN = "Unknown"
+    HUMIDIFIER_H35I = "Blueair Humidifier H35i"
+    PROTECT_7470I = "Blueair Protect 7470i"
+    MAX_211I = "Blueair Blue Pure 211i Max"
+    MAX_311I = "Blueair Blue Pure 311i Max"
