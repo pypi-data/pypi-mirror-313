52da9151a49db4e3230c6174574b9565d0dcbf7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:36 +0000

    Add package wandb/0.19.0/wandb-0.19.0.tar.gz

---
+++ packages/wandb/wandb-0.19.0.tar.gz/wandb-0.19.0/core/internal/api/logging.go
@@ -0,0 +1,67 @@
+package api
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+
+	"github.com/hashicorp/go-retryablehttp"
+	"github.com/wandb/wandb/core/internal/wboperation"
+)
+
+// Logs the final response (after retries) if it's an error.
+func (backend *Backend) logFinalResponseOnError(
+	req *retryablehttp.Request,
+	resp *http.Response,
+) {
+	if resp.StatusCode < 400 || backend.logger == nil {
+		return
+	}
+
+	// We don't consume the response body so that the code making the request
+	// can read and close it.
+	backend.logger.Error(
+		"HTTP error",
+		"status", resp.StatusCode,
+		"method", req.Method,
+		"url", req.URL.String(),
+	)
+}
+
+// Wraps a RetryPolicy to log retries.
+func withRetryLogging(
+	policy retryablehttp.CheckRetry,
+	logger *slog.Logger,
+) retryablehttp.CheckRetry {
+	if logger == nil {
+		panic("api: withRetryLogging: nil logger")
+	}
+
+	return func(ctx context.Context, resp *http.Response, err error) (bool, error) {
+		willRetry, newErr := policy(ctx, resp, err)
+		if newErr != nil {
+			err = newErr
+		}
+
+		if willRetry {
+			switch {
+			case resp == nil && err == nil:
+				logger.Error("api: retrying HTTP request, no error or response")
+			case err != nil:
+				logger.Info("api: retrying error", "error", err)
+			case resp.StatusCode >= 400:
+				// TODO: Log the request body.
+				logger.Info(
+					"api: retrying HTTP error",
+					"status", resp.StatusCode,
+					"url", resp.Request.URL.String(),
+				)
+
+				// TODO: Report the attempt number & time to next retry.
+				wboperation.Get(ctx).MarkRetryingHTTPError(resp.Status)
+			}
+		}
+
+		return willRetry, err
+	}
+}
