6dc6f6cf85aa024039be7f792a6854e057109980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:16 +0000

    Add package aichatlite-web/0.0.5/aichatlite_web-0.0.5.tar.gz

---
+++ packages/aichatlite-web/aichatlite_web-0.0.5.tar.gz/aichatlite_web-0.0.5/aichatlite/core/features/feature_main.py
@@ -0,0 +1,24 @@
+from .handle_google_searchai import *
+
+
+from .handle_search_ai import handle_search_ai
+
+class FeatureHandlerMain:
+    def __init__(self, chatbot, agent_type, query, web_search):
+        self.chatbot = chatbot
+        self.agent_type = agent_type
+        self.query = query
+        self.web_search = web_search
+
+    def generate(self):
+        match self.agent_type:
+            case "QuestionAnswer":
+                return self.chatbot.generator(self.query)
+            case "FastSearchAI":
+                return handle_search_ai(chatbot=self.chatbot, query=self.query, k=2)
+            case "DeepSearchAI":
+                return handle_search_ai(chatbot=self.chatbot, query=self.query, k=5)
+            case "IntelligentSearchAI":
+                return handle_search_ai(chatbot=self.chatbot, query=self.query, k=10)
+            case _:
+                return "None"
