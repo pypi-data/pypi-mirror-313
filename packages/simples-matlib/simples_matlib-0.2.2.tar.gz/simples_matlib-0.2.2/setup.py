f56896dd9f04cecc49b4db2fd0f74d5717d1bf9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:27 +0000

    Add package simples-matlib/0.2.2/simples_matlib-0.2.2.tar.gz

---
+++ packages/simples-matlib/simples_matlib-0.2.2.tar.gz/simples_matlib-0.2.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simples_matlib",
+    version="0.2.2",
+    author="Antonio Anerão",
+    author_email="anerao.junior@gmail.com",
+    description="Uma simples biblioteca para criar gráficos usando matplotlib",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/antonioanerao/simples_matlib",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "matplotlib",
+        "numpy"
+    ],
+)
