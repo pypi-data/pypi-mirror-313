b46102f3732d763d838b474a40cf90b0974688ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:11:18 +0000

    Add package dasoundlib/0.6/dasoundlib-0.6-py3-none-any.whl

---
+++ packages/dasoundlib/dasoundlib-0.6-py3-none-any.whl/Soundlib/__init__.py
@@ -0,0 +1,11 @@
+'''
+The Soundlib module is developed and maintained by the 401.
+
+All rights to the code and its functionalities are reserved.
+
+Unauthorized distribution of this module is strictly prohibited.
+
+'''
+
+from ._soundlib import *
+from ._soundlib import __doc__
