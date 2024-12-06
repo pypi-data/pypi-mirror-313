25b9f1ebe5a103673e80ffcb8d989357203c6b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:23 +0000

    Add package pytesseraxcocr/0.1.2/pytesseraxcocr-0.1.2.tar.gz

---
+++ packages/pytesseraxcocr/pytesseraxcocr-0.1.2.tar.gz/pytesseraxcocr-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytesseraxcocr',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pyzbar'
+    ],
+    license='MIT',
+)
