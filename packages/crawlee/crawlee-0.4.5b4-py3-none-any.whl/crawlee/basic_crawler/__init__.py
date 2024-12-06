aa36af9ab2f3506c1839db316c611ecc6214a1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:11:21 +0000

    Add package crawlee/0.4.5b4/crawlee-0.4.5b4-py3-none-any.whl

---
+++ packages/crawlee/crawlee-0.4.5b4-py3-none-any.whl/crawlee/basic_crawler/__init__.py
@@ -0,0 +1,6 @@
+from crawlee._types import BasicCrawlingContext
+
+from ._basic_crawler import BasicCrawler, BasicCrawlerOptions
+from ._context_pipeline import ContextPipeline
+
+__all__ = ['BasicCrawler', 'BasicCrawlerOptions', 'BasicCrawlingContext', 'ContextPipeline']
