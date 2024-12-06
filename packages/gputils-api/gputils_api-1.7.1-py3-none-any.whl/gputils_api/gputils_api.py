6d722c81665d54b129ff214381fc43612408bb71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:59 +0000

    Add package gputils-api/1.7.1/gputils_api-1.7.1-py3-none-any.whl

---
+++ packages/gputils-api/gputils_api-1.7.1-py3-none-any.whl/gputils_api/gputils_api.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+def read_array_from_gputils_binary_file(path, dt=np.dtype('d')):
+    """
+    Reads an array from a bt file
+    :param path: path to file
+    :param dt: numpy-compatible data type
+    :raises ValueError: if the file name specified `path` does not have the .bt extension
+    """
+    if not path.endswith(".bt"):
+        raise ValueError("The file must have the .bt extension")
+    with open(path, 'rb') as f:
+        nr = int.from_bytes(f.read(8), byteorder='little', signed=False)  # read number of rows
+        nc = int.from_bytes(f.read(8), byteorder='little', signed=False)  # read number of columns
+        nm = int.from_bytes(f.read(8), byteorder='little', signed=False)  # read number of matrices
+        dat = np.fromfile(f, dtype=np.dtype(dt))  # read data
+
+        if nm >= 2:  # if we actually have a 3D tensor (not a matrix or a vector)
+            dat = dat.reshape((nm, nc, nr)).swapaxes(0, 2)  # I'll explain this to you when you grow up
+        else:
+            dat = dat.reshape((nr, nc, nm))  # reshape
+    return dat
+
+
+def write_array_to_gputils_binary_file(x, path):
+    """
+    Writes a numpy array into a bt file
+
+    :param x: numpy array to save to file
+    :param path: path to file
+    :raises ValueError: if `x` has more than 3 dimensions
+    :raises ValueError: if the file name specified `path` does not have the .bt extension
+    """
+
+    if not path.endswith(".bt"):
+        raise ValueError("The file must have the .bt extension")
+    x_shape = x.shape
+    x_dims = len(x_shape)
+    if x_dims >= 4:
+        raise ValueError("given array cannot have more than 3 dimensions")
+    nr = x_shape[0]
+    nc = x_shape[1] if x_dims >= 2 else 1
+    nm = x_shape[2] if x_dims == 3 else 1
+    if x_dims == 3:
+        x = x.swapaxes(0, 2).reshape(-1)  # column-major storage; axis 2 last
+    else:
+        x = x.T.reshape(-1)  # column-major storage
+    with open(path, 'wb') as f:
+        f.write(nr.to_bytes(8, 'little'))  # write number of rows
+        f.write(nc.to_bytes(8, 'little'))  # write number of columns
+        f.write(nm.to_bytes(8, 'little'))  # write number of matrices
+        x.tofile(f)  # write data
