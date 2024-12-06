374829b5309c5d1c9cdaa96f3bb86a20e82c94ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:06:13 +0000

    Add package globus-compute-endpoint/2.32.1/globus_compute_endpoint-2.32.1-py3-none-any.whl

---
+++ packages/globus-compute-endpoint/globus_compute_endpoint-2.32.1-py3-none-any.whl/globus_compute_endpoint/endpoint/config/__init__.py
@@ -0,0 +1,12 @@
+from .config import (  # noqa: F401
+    BaseConfig,
+    Config,
+    ManagerEndpointConfig,
+    UserEndpointConfig,
+)
+from .model import (  # noqa: F401
+    BaseConfigModel,
+    BaseEndpointConfigModel,
+    ManagerEndpointConfigModel,
+    UserEndpointConfigModel,
+)
