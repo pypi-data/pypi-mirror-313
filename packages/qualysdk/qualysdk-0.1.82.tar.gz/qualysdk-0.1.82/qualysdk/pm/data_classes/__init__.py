6621c75a34dbb4407969932ca4f1ac19cc486be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:35 +0000

    Add package qualysdk/0.1.82/qualysdk-0.1.82.tar.gz

---
+++ packages/qualysdk/qualysdk-0.1.82.tar.gz/qualysdk-0.1.82/qualysdk/pm/data_classes/__init__.py
@@ -0,0 +1,8 @@
+"""
+Contains the data classes used for Patch Management.
+"""
+
+from .Job import PMJob
+from .JobResultSummary import JobResultSummary
+from .PMAsset import PMAssetJobView
+from .PMRun import PMRun
