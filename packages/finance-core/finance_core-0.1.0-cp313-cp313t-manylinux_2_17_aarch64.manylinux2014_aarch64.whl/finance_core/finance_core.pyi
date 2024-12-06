84254c062f950c4608f82b0b475dc2987d0b8c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:29 +0000

    Add package finance-core/0.1.0/finance_core-0.1.0-cp313-cp313t-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/finance-core/finance_core-0.1.0-cp313-cp313t-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/finance_core/finance_core.pyi
@@ -0,0 +1,24 @@
+__all__ = ["sum_as_string", "calculate_return"]
+
+
+def sum_as_string(a: int, b: int) -> str:
+    """Calculates the sum of two numbers and return the result as string.
+
+    Args:
+        a: A number.
+        b: Another number.
+
+    Returns:
+        Sum of a and b.
+    """
+
+def calculate_return(initial_investment: float, final_value: float) -> float:
+    """Calcualte the return of an investment in percentage.
+
+    Args:
+        initial_investment: Initial investment.
+        final_value: Final value.
+
+    Returns:
+        Percentage return.
+    """
