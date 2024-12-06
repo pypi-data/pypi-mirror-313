b5ee716f40904457efcafbc34673db785a6e0b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:14 +0000

    Add package borco-scikit-build-core-example/0.0.3/borco_scikit_build_core_example-0.0.3-cp312-cp312-win_amd64.whl

---
+++ packages/borco-scikit-build-core-example/borco_scikit_build_core_example-0.0.3-cp312-cp312-win_amd64.whl/borco_scikit_build_core_example/__init__.py
@@ -0,0 +1,6 @@
+from borco_scikit_build_core_example._core import square  # pylint: disable=import-error,no-name-in-module
+
+
+__all__ = [
+    "square",
+]
---
+++ packages/borco-scikit-build-core-example/borco_scikit_build_core_example-0.0.3-cp312-cp312-win_amd64.whl/borco_scikit_build_core_example/_core.pyi
@@ -0,0 +1,6 @@
+# pylint: disable=unused-argument
+from __future__ import annotations
+
+
+def square(x: float) -> float:
+    """A function that return the square of a value."""
