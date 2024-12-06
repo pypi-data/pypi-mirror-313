04033ef122bd391a603835552dcf8d58001984ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:37 +0000

    Add package blueair-api/1.15.2/blueair_api-1.15.2.tar.gz

---
+++ packages/blueair-api/blueair_api-1.15.2.tar.gz/blueair_api-1.15.2/src/blueair_api/util_bootstrap.py
@@ -0,0 +1,70 @@
+import logging
+
+from aiohttp import ClientSession
+
+from .http_blueair import HttpBlueair
+from .http_aws_blueair import HttpAwsBlueair
+from .device import Device
+from .device_aws import DeviceAws
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def get_devices(
+    username: str,
+    password: str,
+    home_host: str = None,
+    auth_token: str = None,
+    client_session: ClientSession = None,
+) -> (HttpBlueair, list[Device]):
+    api = HttpBlueair(
+        client_session=client_session,
+        username=username,
+        password=password,
+        home_host=home_host,
+        auth_token=auth_token,
+    )
+    api_devices = await api.get_devices()
+
+    def create_device(device):
+        return Device(
+            api=api,
+            uuid=device["uuid"],
+            name=device["name"],
+            mac=device["mac"],
+        )
+
+    devices = map(create_device, api_devices)
+    return (
+        api,
+        list(devices),
+    )
+
+
+async def get_aws_devices(
+    username: str,
+    password: str,
+    region: str = "us",
+    client_session: ClientSession = None,
+) -> (HttpAwsBlueair, list[Device]):
+    api = HttpAwsBlueair(
+        username=username,
+        password=password,
+        region=region,
+        client_session=client_session,
+    )
+    api_devices = await api.devices()
+
+    def create_device(device):
+        return DeviceAws(
+            api=api,
+            uuid=device["uuid"],
+            name_api=device["name"],
+            mac=device["mac"],
+            type_name=device["type"],
+        )
+
+
+    devices = list(map(create_device, api_devices))
+
+    return (api, devices)
