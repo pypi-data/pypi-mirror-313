eaab290c36bdd307f545d7762c4ebfa57d6d89b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:40 +0000

    Add package transformation3d/2.2.0/transformation3d-2.2.0.tar.gz

---
+++ packages/transformation3d/transformation3d-2.2.0.tar.gz/transformation3d-2.2.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="transformation3d",
+    version="2.2.0",
+    description="An easy-to-use class representing transformations in 3D space.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TimSchneider42/python-transformation3d",
+    author="Tim Schneider",
+    author_email="schneider@ias.informatik.tu-darmstadt.de",
+    license="MIT",
+    packages=["transformation"],
+    install_requires=[
+        "scipy",
+        "numpy"
+    ],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
