7b1b21191a3e48eb2c063cfbbcbdc2ef822d6add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:04:31 +0000

    Add package fibonaci/0.0.3/fibonaci-0.0.3-py3-none-any.whl

---
+++ packages/fibonaci/fibonaci-0.0.3-py3-none-any.whl/fibonaci/cli.py
@@ -0,0 +1,18 @@
+def fib(n):
+	if n < 3:
+    	    return n-1
+	fib1 = 0
+	fib2 = 1
+	for _ in range(n-1):
+    	    fibn = fib1 + fib2
+    	    fib2, fib1 = fib1, fibn
+	return fibn
+
+def main():
+	import sys
+	n = int(sys.argv[1])
+	sys.set_int_max_str_digits(1000000)
+	print(f"fib({n}) = {str(fib(n))}")
+
+if __name__ == '__main__':
+    main()
