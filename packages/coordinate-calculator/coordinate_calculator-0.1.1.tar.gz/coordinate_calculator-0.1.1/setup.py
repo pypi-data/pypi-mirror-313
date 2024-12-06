3120d9cf517539fef954767adb4e5cf40c8d2ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:27 +0000

    Add package coordinate-calculator/0.1.1/coordinate_calculator-0.1.1.tar.gz

---
+++ packages/coordinate-calculator/coordinate_calculator-0.1.1.tar.gz/coordinate_calculator-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="coordinate-calculator",
+    version="0.1.1",
+    description="Drone YOLO client for object detection and server communication.",
+    author="ittipat jitrada",
+    author_email="ittipatjitrada@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "python-socketio",
+        "opencv-python",
+        "pillow",
+        "ultralytics",
+        "numpy"
+    ],
+    entry_points={
+        "console_scripts": [
+            "drone-yolo-client=drone_yolo_client.client:main",
+        ],
+    },
+)
