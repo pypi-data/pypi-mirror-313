d772fe6ae01ed6df9de1375479fde62068bad075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:23 +0000

    Add package ansible/11.1.0/ansible-11.1.0.tar.gz

---
+++ packages/ansible/ansible-11.1.0.tar.gz/ansible-11.1.0/ansible_collections/dellemc/powerflex/tests/unit/plugins/module_utils/mock_sdk_response.py
@@ -0,0 +1,15 @@
+# Copyright: (c) 2022, Dell Technologies
+
+# Apache License version 2.0 (see MODULE-LICENSE or http://www.apache.org/licenses/LICENSE-2.0.txt)
+
+"""Mock SDKResponse for Unit tests for Dell Technologies (Dell) PowerFlex modules"""
+
+from __future__ import (absolute_import, division, print_function)
+
+__metaclass__ = type
+
+
+class MockSDKResponse:
+    def __init__(self, data=None, status_code=200):
+        self.data = data
+        self.status_code = status_code
