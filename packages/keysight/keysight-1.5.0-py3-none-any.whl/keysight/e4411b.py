db02b55eeb1abd0d456540d3b8e0f27462a55451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:10:48 +0000

    Add package keysight/1.5.0/keysight-1.5.0-py3-none-any.whl

---
+++ packages/keysight/keysight-1.5.0-py3-none-any.whl/keysight/e4411b.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013-2024 The keysight developers. All rights reserved.
+# Project site: https://github.com/questrail/keysight
+# Use of this source code is governed by a MIT-style license that
+# can be found in the LICENSE.txt file for the project.
+"""Read a CSV file saved by an E4411B Spectrum Analyzer"""
+
+# Standard module imports
+import csv
+
+# Data analysis related imports
+import numpy as np
+
+
+def read_csv_file(filename):
+    """Read csv file into a numpy array"""
+    header_info = {}
+
+    infile = open(filename, "r", newline="", encoding="utf8")
+
+    with infile as csvfile:
+        data = csv.reader((line.replace("\0", "") for line in csvfile), delimiter=",")
+        mynext = data.__next__
+        temp_row = mynext()
+        header_info["timestamp"] = temp_row[0]
+        header_info["file"] = temp_row[1]
+        header_info["title"] = mynext()[1]
+        header_info["model"] = mynext()[1]
+        header_info["serial_number"] = mynext()[1]
+        temp_row = mynext()
+        header_info["center_freq"] = float(temp_row[1])
+        temp_row = mynext()
+        header_info["span_freq"] = float(temp_row[1])
+        temp_row = mynext()
+        header_info["resolution_bw"] = float(temp_row[1])
+        temp_row = mynext()
+        header_info["video_bw"] = float(temp_row[1])
+        temp_row = mynext()
+        header_info["ref_level"] = float(temp_row[1])
+        temp_row = mynext()
+        header_info["sweep_time"] = float(temp_row[1])
+        temp_row = mynext()
+        header_info["num_points"] = int(temp_row[1])
+        temp_row = mynext()  # Skip blank line 12
+        temp_row = mynext()  # Skip blank line 13
+        temp_row = mynext()
+        num_traces = len(temp_row) - 1
+        header_info["num_traces"] = num_traces
+        temp_row = mynext()
+        header_info["frequency"] = temp_row[0]
+
+        data_array = []
+
+        if num_traces == 1:
+            for row in data:
+                data_array.append((float(row[0]), float(row[1])))
+            data = np.array(
+                data_array,
+                dtype={
+                    "names": ("frequency", "amplitude"),
+                    "formats": ("f8", "f8"),
+                },
+            )
+        elif num_traces == 2:
+            for row in data:
+                data_array.append((float(row[0]), [float(row[1]), float(row[2])]))
+            data = np.array(
+                data_array,
+                dtype={
+                    "names": ("frequency", "amplitude"),
+                    "formats": ("f8", "2f8"),
+                },
+            )
+        elif num_traces == 3:
+            for row in data:
+                data_array.append(
+                    (
+                        float(row[0]),
+                        [float(row[1]), float(row[2]), float(row[3])],
+                    )
+                )
+            data = np.array(
+                data_array,
+                dtype={
+                    "names": ("frequency", "amplitude"),
+                    "formats": ("f8", "3f8"),
+                },
+            )
+
+    return (header_info, data)
