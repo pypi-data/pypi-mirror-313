ff4920973595beb39558a31a7f97d64818a5b984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:31 +0000

    Add package truss/0.9.54rc9/truss-0.9.54rc9-py3-none-any.whl

---
+++ packages/truss/truss-0.9.54rc9-py3-none-any.whl/truss_chains/model_skeleton.py
@@ -0,0 +1,53 @@
+import pathlib
+from typing import Optional
+
+from truss.templates.shared import secrets_resolver
+
+from truss_chains import definitions
+from truss_chains.utils import populate_chainlet_service_predict_urls
+
+
+class TrussChainletModel:
+    _context: definitions.DeploymentContext
+    _chainlet: definitions.ABCChainlet
+
+    def __init__(
+        self,
+        config: dict,
+        data_dir: pathlib.Path,
+        secrets: secrets_resolver.Secrets,
+        # TODO: Remove the default value once all truss versions are synced up.
+        environment: Optional[dict] = None,
+    ) -> None:
+        truss_metadata: definitions.TrussMetadata = (
+            definitions.TrussMetadata.model_validate(
+                config["model_metadata"][definitions.TRUSS_CONFIG_CHAINS_KEY]
+            )
+        )
+        deployment_environment: Optional[definitions.Environment] = (
+            definitions.Environment.model_validate(environment) if environment else None
+        )
+        chainlet_to_deployed_service = populate_chainlet_service_predict_urls(
+            truss_metadata.chainlet_to_service
+        )
+
+        self._context = definitions.DeploymentContext(
+            chainlet_to_service=chainlet_to_deployed_service,
+            secrets=secrets,
+            data_dir=data_dir,
+            environment=deployment_environment,
+        )
+
+    # Below illustrated code will be added by code generation.
+
+    # def load(self) -> None:
+    #     logging.info(f"Loading Chainlet `TextToNum`.")
+    #     self._chainlet = main.TextToNum(
+    #       mistral=stub.factory(MistralLLM, self._context))
+    #
+    # def predict(self, inputs: TextToNumInput) -> TextToNumOutput:
+    #     with utils.exception_to_http_error(
+    #         include_stack=True, chainlet_name="TextToNum"
+    #     ):
+    #         result = self._chainlet.run_remote(data=inputs.data)
+    #     return TextToNumOutput((result,))
---
+++ packages/truss/truss-0.9.54rc9-py3-none-any.whl/truss_chains/stub.py
@@ -0,0 +1,314 @@
+import abc
+import asyncio
+import contextlib
+import contextvars
+import logging
+import ssl
+import threading
+import time
+from typing import (
+    Any,
+    AsyncIterator,
+    ClassVar,
+    Iterator,
+    Mapping,
+    Optional,
+    Type,
+    TypeVar,
+    final,
+)
+
+import aiohttp
+import httpx
+import starlette.requests
+import tenacity
+
+from truss_chains import definitions, utils
+
+DEFAULT_MAX_CONNECTIONS = 1000
+DEFAULT_MAX_KEEPALIVE_CONNECTIONS = 400
+
+_trace_parent_context: contextvars.ContextVar[str] = contextvars.ContextVar(
+    "trace_parent"
+)
+
+
+@contextlib.contextmanager
+def trace_parent(request: starlette.requests.Request) -> Iterator[None]:
+    token = _trace_parent_context.set(
+        request.headers.get(definitions.OTEL_TRACE_PARENT_HEADER_KEY, "")
+    )
+    try:
+        yield
+    finally:
+        _trace_parent_context.reset(token)
+
+
+class BasetenSession:
+    """Helper to invoke predict method on Baseten deployments."""
+
+    _client_cycle_time_sec: ClassVar[int] = 3600 * 1  # 1 hour.
+    _client_limits: ClassVar[httpx.Limits] = httpx.Limits(
+        max_connections=DEFAULT_MAX_CONNECTIONS,
+        max_keepalive_connections=DEFAULT_MAX_KEEPALIVE_CONNECTIONS,
+    )
+    _auth_header: Mapping[str, str]
+    _service_descriptor: definitions.DeployedServiceDescriptor
+    _cached_sync_client: Optional[tuple[httpx.Client, int]]
+    _cached_async_client: Optional[tuple[aiohttp.ClientSession, int]]
+
+    def __init__(
+        self,
+        service_descriptor: definitions.DeployedServiceDescriptor,
+        api_key: str,
+    ) -> None:
+        logging.info(
+            f"Creating BasetenSession (HTTP) for `{service_descriptor.name}` "
+            f"({service_descriptor.options.retries} retries) with predict URL:\n"
+            f"    `{service_descriptor.predict_url}`"
+        )
+        self._auth_header = {"Authorization": f"Api-Key {api_key}"}
+        self._service_descriptor = service_descriptor
+        self._cached_sync_client = None
+        self._cached_async_client = None
+        self._sync_lock = threading.Lock()
+        self._async_lock = asyncio.Lock()
+        self._sync_num_requests = utils.ThreadSafeCounter()
+        self._async_num_requests = utils.AsyncSafeCounter()
+
+    @property
+    def name(self) -> str:
+        return self._service_descriptor.name
+
+    def _maybe_warn_for_overload(self, num_requests: int) -> None:
+        if self._client_limits.max_connections is None:
+            return
+        if num_requests > self._client_limits.max_connections * 0.8:
+            logging.warning(
+                f"High number of concurrently outgoing HTTP connections: "
+                f"`{num_requests}`. Close to or above connection limit of "
+                f"`{self._client_limits.max_connections}`. To avoid overload and "
+                f"timeouts, use more replicas/autoscaling for this chainlet."
+            )
+
+    def _client_cycle_needed(self, cached_client: Optional[tuple[Any, int]]) -> bool:
+        return (
+            not cached_client
+            or (int(time.time()) - cached_client[1]) > self._client_cycle_time_sec
+        )
+
+    def _client_sync(self) -> httpx.Client:
+        # Check `_client_cycle_needed` before and after locking to avoid
+        # needing a lock each time the client is accessed.
+        if self._client_cycle_needed(self._cached_sync_client):
+            with self._sync_lock:
+                if self._client_cycle_needed(self._cached_sync_client):
+                    self._cached_sync_client = (
+                        httpx.Client(
+                            headers=self._auth_header,
+                            timeout=self._service_descriptor.options.timeout_sec,
+                            limits=self._client_limits,
+                        ),
+                        int(time.time()),
+                    )
+        assert self._cached_sync_client is not None
+        return self._cached_sync_client[0]
+
+    async def _client_async(self) -> aiohttp.ClientSession:
+        # Check `_client_cycle_needed` before and after locking to avoid
+        # needing a lock each time the client is accessed.
+        if self._client_cycle_needed(self._cached_async_client):
+            async with self._async_lock:
+                if self._client_cycle_needed(self._cached_async_client):
+                    connector = aiohttp.TCPConnector(
+                        limit=DEFAULT_MAX_CONNECTIONS,
+                    )
+                    self._cached_async_client = (
+                        aiohttp.ClientSession(
+                            headers=self._auth_header,
+                            connector=connector,
+                            timeout=aiohttp.ClientTimeout(
+                                total=self._service_descriptor.options.timeout_sec
+                            ),
+                        ),
+                        int(time.time()),
+                    )
+        assert self._cached_async_client is not None
+        return self._cached_async_client[0]
+
+    def predict_sync(self, json_payload):
+        headers = {
+            definitions.OTEL_TRACE_PARENT_HEADER_KEY: _trace_parent_context.get()
+        }
+        retrying = tenacity.Retrying(
+            stop=tenacity.stop_after_attempt(self._service_descriptor.options.retries),
+            retry=tenacity.retry_if_exception_type(Exception),
+            reraise=True,
+        )
+        for attempt in retrying:
+            with attempt:
+                if (num := attempt.retry_state.attempt_number) > 1:
+                    logging.info(f"Retrying `{self.name}`, " f"attempt {num}")
+                try:
+                    with self._sync_num_requests as num_requests:
+                        self._maybe_warn_for_overload(num_requests)
+                        response = self._client_sync().post(
+                            self._service_descriptor.predict_url,
+                            json=json_payload,
+                            headers=headers,
+                        )
+                    utils.response_raise_errors(response, self.name)
+                    return response.json()
+
+                # As a special case we invalidate the client in case of certificate
+                # errors. This has happened in the past and is a defensive measure.
+                except ssl.SSLError:
+                    self._cached_sync_client = None
+                    raise
+
+    async def predict_async(self, json_payload):
+        headers = {
+            definitions.OTEL_TRACE_PARENT_HEADER_KEY: _trace_parent_context.get()
+        }
+        retrying = tenacity.AsyncRetrying(
+            stop=tenacity.stop_after_attempt(self._service_descriptor.options.retries),
+            retry=tenacity.retry_if_exception_type(Exception),
+            reraise=True,
+        )
+        async for attempt in retrying:
+            with attempt:
+                if (num := attempt.retry_state.attempt_number) > 1:
+                    logging.info(f"Retrying `{self.name}`, " f"attempt {num}")
+                try:
+                    client = await self._client_async()
+                    async with self._async_num_requests as num_requests:
+                        self._maybe_warn_for_overload(num_requests)
+                        async with client.post(
+                            self._service_descriptor.predict_url,
+                            json=json_payload,
+                            headers=headers,
+                        ) as response:
+                            await utils.async_response_raise_errors(response, self.name)
+                            return await response.json()
+                # As a special case we invalidate the client in case of certificate
+                # errors. This has happened in the past and is a defensive measure.
+                except ssl.SSLError:
+                    self._cached_async_client = None
+                    raise
+
+    async def predict_async_stream(self, json_payload) -> AsyncIterator[bytes]:  # type: ignore[return]  # Handled by retries.
+        headers = {
+            definitions.OTEL_TRACE_PARENT_HEADER_KEY: _trace_parent_context.get()
+        }
+        retrying = tenacity.AsyncRetrying(
+            stop=tenacity.stop_after_attempt(self._service_descriptor.options.retries),
+            retry=tenacity.retry_if_exception_type(Exception),
+            reraise=True,
+        )
+        async for attempt in retrying:
+            with attempt:
+                if (num := attempt.retry_state.attempt_number) > 1:
+                    logging.info(f"Retrying `{self.name}`, " f"attempt {num}")
+                try:
+                    client = await self._client_async()
+                    async with self._async_num_requests as num_requests:
+                        self._maybe_warn_for_overload(num_requests)
+                        response = await client.post(
+                            self._service_descriptor.predict_url,
+                            json=json_payload,
+                            headers=headers,
+                        )
+                        await utils.async_response_raise_errors(response, self.name)
+                        return response.content.iter_any()
+
+                # As a special case we invalidate the client in case of certificate
+                # errors. This has happened in the past and is a defensive measure.
+                except ssl.SSLError:
+                    self._cached_async_client = None
+                    raise
+
+
+class StubBase(abc.ABC):
+    """Base class for stubs that invoke remote chainlets.
+
+    It is used internally for RPCs to dependency chainlets, but it can also be used
+    in user-code for wrapping a deployed truss model into the chains framework, e.g.
+    like that::
+
+        import pydantic
+        import truss_chains as chains
+
+        class WhisperOutput(pydantic.BaseModel):
+            ...
+
+
+        class DeployedWhisper(chains.StubBase):
+
+            async def run_remote(self, audio_b64: str) -> WhisperOutput:
+                resp = await self._remote.predict_async(
+                    json_payload={"audio": audio_b64})
+                return WhisperOutput(text=resp["text"], language=resp["language"])
+
+
+        class MyChainlet(chains.ChainletBase):
+
+            def __init__(self, ..., context=chains.depends_context()):
+                ...
+                self._whisper = DeployedWhisper.from_url(
+                    WHISPER_URL,
+                    context,
+                    options=chains.RPCOptions(retries=3),
+                )
+
+    """
+
+    _remote: BasetenSession
+
+    @final
+    def __init__(
+        self,
+        service_descriptor: definitions.DeployedServiceDescriptor,
+        api_key: str,
+    ) -> None:
+        """
+        Args:
+            service_descriptor: Contains the URL and other configuration.
+            api_key: A baseten API key to authorize requests.
+        """
+        self._remote = BasetenSession(service_descriptor, api_key)
+
+    @classmethod
+    def from_url(
+        cls,
+        predict_url: str,
+        context: definitions.DeploymentContext,
+        options: Optional[definitions.RPCOptions] = None,
+    ):
+        """Factory method, convenient to be used in chainlet's ``__init__``-method.
+
+        Args:
+            predict_url: URL to predict endpoint of another chain / truss model.
+            context: Deployment context object, obtained in the chainlet's ``__init__``.
+            options: RPC options, e.g. retries.
+        """
+        options = options or definitions.RPCOptions()
+        return cls(
+            service_descriptor=definitions.DeployedServiceDescriptor(
+                name=cls.__name__,
+                display_name=cls.__name__,
+                predict_url=predict_url,
+                options=options,
+            ),
+            api_key=context.get_baseten_api_key(),
+        )
+
+
+StubT = TypeVar("StubT", bound=StubBase)
+
+
+def factory(stub_cls: Type[StubT], context: definitions.DeploymentContext) -> StubT:
+    # Assumes the stub_cls-name and the name of the service in ``context` match.
+    return stub_cls(
+        service_descriptor=context.get_service_descriptor(stub_cls.__name__),
+        api_key=context.get_baseten_api_key(),
+    )
