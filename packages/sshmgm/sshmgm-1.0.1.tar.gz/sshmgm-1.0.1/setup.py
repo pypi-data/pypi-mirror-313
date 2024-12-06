4fc782915a1565fea5b1451af64ae666184b07ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:13 +0000

    Add package sshmgm/1.0.1/sshmgm-1.0.1.tar.gz

---
+++ packages/sshmgm/sshmgm-1.0.1.tar.gz/sshmgm-1.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sshmgm",
+    version="1.0.1",
+    description="A tool for parsing and displaying SSH connection history",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Hadi Tayanloo",
+    author_email="htayanloo@example.com",
+    url="https://github.com/htayanloo/SSH-Manager",  # Replace with your GitHub repository link
+    packages=find_packages(),
+    install_requires=[
+        "rich"
+    ],
+    entry_points={
+        "console_scripts": [
+            "sshmgm-parser=ssh_manager.parser:main",
+            "sshmgm=ssh_manager.viewer:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    include_package_data=True,
+)
