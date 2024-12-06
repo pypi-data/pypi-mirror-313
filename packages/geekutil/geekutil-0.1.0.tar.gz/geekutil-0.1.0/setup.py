6cb2f5903b191ed5a5e0cbad092b4851d507bf7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:37 +0000

    Add package geekutil/0.1.0/geekutil-0.1.0.tar.gz

---
+++ packages/geekutil/geekutil-0.1.0.tar.gz/geekutil-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="geekutil",
+    version="0.1.0",
+    description="A simple demo package for mathematical operations",
+    author="hskyzhou",
+    author_email="xezw211@126.com",
+    packages=find_packages(),
+    install_requires=[],
+)
