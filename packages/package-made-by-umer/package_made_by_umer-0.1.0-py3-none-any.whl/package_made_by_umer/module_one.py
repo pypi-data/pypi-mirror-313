f92b8c7eeee124062d4f224a71fcf52f81f93d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:26:01 +0000

    Add package package-made-by-umer/0.1.0/package_made_by_umer-0.1.0-py3-none-any.whl

---
+++ packages/package-made-by-umer/package_made_by_umer-0.1.0-py3-none-any.whl/package_made_by_umer/module_one.py
@@ -0,0 +1,5 @@
+def function_created_by_umer_one(name:str, age:int) -> str:
+    return f"My name is {name} and my age is {age}"
+
+def function_created_by_umer_two(profession:str, experience:int) -> str:
+    return f"I a  {profession} and i have {experience} years of experience"
