cd5da97e4fe193a2dcf8104c989b7200b27ebac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:35 +0000

    Add package promptzl/0.9.2/promptzl-0.9.2-py3-none-any.whl

---
+++ packages/promptzl/promptzl-0.9.2-py3-none-any.whl/promptzl/__init__.py
@@ -0,0 +1,7 @@
+from .modules import (
+    CausalLM4Classification,
+    LLM4ClassificationBase,
+    MaskedLM4Classification,
+)
+from .prompt import FVP, Img, Key, Prompt, Txt, Vbz
+from .utils import calibrate
