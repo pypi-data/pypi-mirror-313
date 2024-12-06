d750fee0e0d3f5353c5b38771a89afc3aef6e930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:31 +0000

    Add package smallestai/1.3.2/smallestai-1.3.2.tar.gz

---
+++ packages/smallestai/smallestai-1.3.2.tar.gz/smallestai-1.3.2/smallest/models.py
@@ -0,0 +1,7 @@
+from typing import Literal
+
+TTSModels = Literal["lightning"]
+TTSLanguages = Literal["en", "hi"]
+TTSVoices = Literal["emily", "jasmine", "arman", "james", "mithali", "aravind", "raj", 
+                    "arjun", "sanya", "saina", "pooja", "saurabh", "nisha", "mansi", "radhika", "kajal", 
+                    "raghav", "deepika", "niharika", "monika", "raman", "diya", "ananya", "william"]
