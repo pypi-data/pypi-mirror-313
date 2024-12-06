ddbb79ae8b22804baff41496c66ff02f687320c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:10:16 +0000

    Add package obz/4/obz-4.tar.gz

---
+++ packages/obz/obz-4.tar.gz/obz-4/obz/modules/face.py
@@ -0,0 +1,28 @@
+# This file is placed in the Public Domain.
+# pylint: disable=W0611,E0402
+# ruff: noqa: F401
+
+
+"interface"
+
+
+from . import cmd, err, fnd, irc, log, mod, opm, rss, tdo, thr, upt
+
+
+def __dir__():
+    return (
+        'cmd',
+        'err',
+        'fnd',
+        'irc',
+        'log',
+        'mod',
+        'opm',
+        'rss',
+        'tdo',
+        'thr',
+        'upt'
+    )
+
+
+__all__ = __dir__()
