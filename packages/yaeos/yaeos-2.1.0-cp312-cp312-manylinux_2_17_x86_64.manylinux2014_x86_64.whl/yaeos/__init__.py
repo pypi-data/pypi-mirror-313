96a8ad101fda755735b7a283479f980f6e31426c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:16 +0000

    Add package yaeos/2.1.0/yaeos-2.1.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/yaeos/yaeos-2.1.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/yaeos/__init__.py
@@ -0,0 +1,38 @@
+"""Yet Another Equation-Of-State (library).
+
+Library to use EoS-based calculations. This main module imports all the
+relevant constants, procedures and objects to have better access to them.
+"""
+
+__version__ = "2.1.0"
+
+from yaeos.lib import yaeos_c
+from yaeos.models.excess_gibbs import NRTL, UNIFACVLE, UNIQUAC
+from yaeos.models.residual_helmholtz.cubic_eos import (
+    HV,
+    MHV,
+    PSRK,
+    PengRobinson76,
+    PengRobinson78,
+    QMR,
+    QMRTD,
+    RKPR,
+    SoaveRedlichKwong,
+)
+
+
+__all__ = [
+    "yaeos_c",
+    "SoaveRedlichKwong",
+    "PengRobinson76",
+    "PengRobinson78",
+    "RKPR",
+    "PSRK",
+    "QMR",
+    "QMRTD",
+    "NRTL",
+    "UNIFACVLE",
+    "UNIQUAC",
+    "MHV",
+    "HV",
+]
