39011132fcade47ebaf478ba909d12959d7bfc21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:34 +0000

    Add package apis-highlighter-ng/0.5.1/apis_highlighter_ng-0.5.1-py3-none-any.whl

---
+++ packages/apis-highlighter-ng/apis_highlighter_ng-0.5.1-py3-none-any.whl/apis_highlighter/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+
+from . import views
+
+app_name = "apis_highlighter"
+
+urlpatterns = [
+    path(
+        "annotations/<text_content_type>/<text_object_id>/<text_field_name>",
+        views.AnnotationsView.as_view(),
+        name="annotations",
+    ),
+    path(
+        "annotations/<text_content_type>/<text_object_id>/<text_field_name>/<project_id>",
+        views.AnnotationsView.as_view(),
+        name="annotations",
+    ),
+    path(
+        "annotation/<int:pk>/delete",
+        views.AnnotationDelete.as_view(),
+        name="annotationdelete",
+    ),
+    path(
+        "annotation/relation/<contenttype:contenttype>/form",
+        views.AnnotationRelationFormView.as_view(),
+        name="annotation_relation",
+    ),
+]
