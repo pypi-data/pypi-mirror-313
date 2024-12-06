7fa758227cf1e9ffcdab689dfdf660761fd26d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:06:16 +0000

    Add package GibbsTS/1.0.0/GibbsTS-1.0.0.tar.gz

---
+++ packages/GibbsTS/GibbsTS-1.0.0.tar.gz/GibbsTS-1.0.0/setup.py
@@ -0,0 +1,52 @@
+"""
+  GTS -- Gibbs Thermodynamic Surface: an automated toolkit to obtain high-pressure melting data
+
+  Copyright (C) 2024-2024 by Kun Yin and Xuan Zhao
+
+  This program is free software; you can redistribute it and/or modify it under the
+  terms of the GNU General Public License as published by the Free Software Foundation
+  version 3 of the License.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+  E-mail: yinkun@cdut.edu.cn
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="GibbsTS",
+    version="1.0.0",
+    description="GTS: An automated toolkit for building Gibbs thermodynamic surface with application to obtain "
+                "high-pressure melting data.",
+    author="Kun Yin",
+    author_email="yinkun@cdut.edu.cn",
+    # url="https://example.com/GTS",
+    license="GNU General Public License v3.0",
+    packages=find_packages(where='lib'),
+    package_dir={'': 'lib'},
+    py_modules=["gibbs_thermo_surface_01", "gibbs_thermo_surface_02", "gibbs_thermo_surface_03",
+                "gibbs_thermo_surface_04", "gibbs_thermo_surface_05", "gibbs_thermo_surface_06",
+                "gibbs_thermo_surface_07", "gibbs_thermo_surface_08", "gibbs_surf", "save_json"],
+    scripts=["lib/GTS"],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+    # project_urls={
+    #     'Bug Reports': 'https://example.com/GTS/issues',
+    #     'Source': 'https://example.com/GTS',
+    # },
+)
