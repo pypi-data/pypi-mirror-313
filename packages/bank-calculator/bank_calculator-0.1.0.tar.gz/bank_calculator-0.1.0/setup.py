c6207d38e5dbf081018cdaccc66773ff31ce55f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:39 +0000

    Add package bank-calculator/0.1.0/bank_calculator-0.1.0.tar.gz

---
+++ packages/bank-calculator/bank_calculator-0.1.0.tar.gz/bank_calculator-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='bank_calculator',
+version='0.1.0',
+author='Andrew Rawson',
+email='arawson@proton.me',
+description='A simple Python package that can calculate loan payments and dividends',
+packages=find_packages(),
+classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+],
+python_requires='>= 3.12'
+)
