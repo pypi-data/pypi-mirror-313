5c0968806043c8d1461440348a89e1c2f8e0f155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:32 +0000

    Add package rer.bandi/4.3.0/rer.bandi-4.3.0.tar.gz

---
+++ packages/rer.bandi/rer.bandi-4.3.0.tar.gz/rer.bandi-4.3.0/rer/bandi/interfaces/bandoSchema.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+from plone.app.textfield import RichText
+from plone.autoform import directives
+from plone.autoform import directives as form
+from plone.supermodel import model
+from rer.bandi import _
+from z3c.form.browser.checkbox import CheckBoxFieldWidget
+from z3c.form.interfaces import NOVALUE
+from zope import schema
+from z3c.form.browser.radio import RadioFieldWidget
+from zope.interface import Invalid
+from zope.schema._messageid import _ as zsm
+
+
+def checkRequiredField(value):
+    if value == NOVALUE or value == []:
+        raise Invalid(zsm("""Required input is missing."""))
+    return True
+
+
+class IBandoSchema(model.Schema):
+    """A Dexterity schema for Annoucements"""
+    finanziato = schema.Bool(
+        title=_("finanziatori_label", default="Financed by EU programmes"),
+        description=_("finanziatori_help", default=u"Seleziona questo campo se il bando è finanziato con fondi europei."),
+        required=False,
+    )
+    
+    directives.widget(tipologia_bando=RadioFieldWidget)
+    tipologia_bando = schema.Choice(
+        title=_("tipologia_bando_label", default="Announcement type"),
+        description=_("tipologia_bando_help", default=""),
+        vocabulary="rer.bandi.tipologie.vocabulary",
+        required=True,
+    )
+
+    directives.widget(destinatari=CheckBoxFieldWidget)
+    destinatari = schema.List(
+        title=_("destinatari_label", default="Who can apply"),
+        description=_(
+            "bandi_multiselect_help", default="Select one or more values."
+        ),
+        constraint=checkRequiredField,
+        required=True,
+        value_type=schema.Choice(
+            vocabulary="rer.bandi.destinatari.vocabulary"
+        ),
+    )
+
+    directives.widget(materie=CheckBoxFieldWidget)
+    materie = schema.List(
+        title=_("materie_label", default="Topic"),
+        description=_(
+            "bandi_multiselect_help", default="Select one or more values."
+        ),
+        required=False,
+        value_type=schema.Choice(vocabulary="rer.bandi.materie.vocabulary"),
+    )
+
+    scadenza_bando = schema.Datetime(
+        title=_("scadenza_bando_label", default="Expiration date and time"),
+        description=_(
+            "scadenza_bando_help",
+            default="Deadline to participate in the announcement",
+        ),
+        required=False,
+    )
+    chiusura_procedimento_bando = schema.Date(
+        title=_(
+            "chiusura_procedimento_bando_label",
+            default="Closing date procedure",
+        ),
+        description=_("chiusura_procedimento_bando_help", default=""),
+        required=False,
+    )
+
+    riferimenti_bando = RichText(
+        title=_("riferimenti_bando_label", default="References"),
+        description=_("riferimenti_bando_help", default=""),
+        required=False,
+    )
+
+    form.order_after(finanziato="IRichText.text")
+    form.order_after(tipologia_bando="finanziato")
+    form.order_after(destinatari="tipologia_bando")
+    form.order_after(materie="destinatari")
+    form.order_after(scadenza_bando="materie")
+    form.order_after(chiusura_procedimento_bando="scadenza_bando")
+    form.order_after(riferimenti_bando="chiusura_procedimento_bando")
---
+++ packages/rer.bandi/rer.bandi-4.3.0.tar.gz/rer.bandi-4.3.0/rer/bandi/services/search_parameters/get.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from plone.restapi.services import Service
+from rer.bandi import _
+from zope.component import getUtility
+from zope.globalrequest import getRequest
+from zope.i18n import translate
+from zope.interface import implementer
+from zope.publisher.interfaces import IPublishTraverse
+from zope.schema.interfaces import IVocabularyFactory
+
+
+def getVocabularyTermsForForm(vocab_name, context):
+    """
+    Return the values of vocabulary
+    """
+    utility = getUtility(IVocabularyFactory, vocab_name)
+
+    values = []
+
+    vocab = utility(context)
+
+    for entry in vocab:
+        if entry.title != "select_label":
+            values.append({"value": entry.value, "label": entry.title})
+    return values
+
+
+def getSearchFields():
+    request = getRequest()
+    portal = api.portal.get()
+    return [
+        {
+            "id": "SearchableText",
+            "label": translate(
+                _("bandi_search_text_label", default="Search text"),
+                context=request,
+            ),
+            "help": "",
+            "type": "text",
+        },
+        {
+            "id": "stato_bandi",
+            "label": translate(
+                _("bandi_search_state_label", default="State"),
+                context=request,
+            ),
+            "help": "",
+            "type": "select",
+            "multivalued": False,
+            "options": [
+                {
+                    "label": translate(
+                        _("bando_state_all_select_label", default="All"),
+                        context=request,
+                    ),
+                    "value": "",
+                },
+                {
+                    "label": translate(
+                        _("bando_state_open_select_label", default="Open"),
+                        context=request,
+                    ),
+                    "value": "open",
+                },
+                {
+                    "label": translate(
+                        _(
+                            "bando_state_inProgress_select_label",
+                            default="In progress",
+                        ),
+                        context=request,
+                    ),
+                    "value": "inProgress",
+                },
+                {
+                    "label": translate(
+                        _(
+                            "bando_state_closed_select_label",
+                            default="Closed",
+                        ),
+                        context=request,
+                    ),
+                    "value": "closed",
+                },
+            ],
+        },
+        {
+            "id": "tipologia_bando",
+            "label": translate(
+                _("bandi_search_type_label", default="Type"),
+                context=request,
+            ),
+            "help": "",
+            "type": "checkbox",
+            "options": getVocabularyTermsForForm(
+                context=portal, vocab_name="rer.bandi.tipologie.vocabulary"
+            ),
+        },
+        {
+            "id": "destinatari",
+            "label": translate(
+                _("destinatari_label", default="Who can apply"),
+                context=request,
+            ),
+            "help": "",
+            "type": "select",
+            "multivalued": True,
+            "options": getVocabularyTermsForForm(
+                context=portal, vocab_name="rer.bandi.destinatari.vocabulary"
+            ),
+        },
+        {
+            "id": "finanziato",
+            "label": translate(
+                _(
+                    "finanziatori_label",
+                    default="Financed by EU programmes",
+                ),
+                context=request,
+            ),
+            "help": "",
+            "type": "select",
+            "multivalued": False,
+            "options": [
+                    {
+                        "value": "true",
+                        "label": api.portal.translate(_("yes_label", default="Yes")),
+                    },
+                    {
+                        "value": "false",
+                        "label": api.portal.translate(_("no_label", default="No")),
+                    },
+                ],
+
+        },
+        {
+            "id": "materie",
+            "label": translate(
+                _("materie_label", default="Topic"), context=request
+            ),
+            "help": "",
+            "type": "select",
+            "multivalued": True,
+            "options": getVocabularyTermsForForm(
+                context=portal, vocab_name="rer.bandi.materie.vocabulary"
+            ),
+        },
+        {
+            "id": "Subject",
+            "label": translate(
+                _("subject_label", default="Subjects"),
+                context=request,
+            ),
+            "help": "",
+            "type": "select",
+            "multivalued": True,
+            "options": getVocabularyTermsForForm(
+                context=portal, vocab_name="plone.app.vocabularies.Keywords"
+            ),
+        },
+    ]
+
+
+@implementer(IPublishTraverse)
+class SearchParametersGet(Service):
+    def __init__(self, context, request):
+        super(SearchParametersGet, self).__init__(context, request)
+
+    def reply(self):
+        return getSearchFields()
