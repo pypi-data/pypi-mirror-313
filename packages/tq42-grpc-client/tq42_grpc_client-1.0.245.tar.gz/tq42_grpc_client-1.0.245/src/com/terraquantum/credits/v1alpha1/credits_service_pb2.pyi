672271c6a61434799ae00bac323e48707b7c4eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:32 +0000

    Add package tq42-grpc-client/1.0.245/tq42_grpc_client-1.0.245.tar.gz

---
+++ packages/tq42-grpc-client/tq42_grpc_client-1.0.245.tar.gz/tq42_grpc_client-1.0.245/src/com/terraquantum/credits/v1alpha1/credits_service_pb2.pyi
@@ -0,0 +1,5 @@
+from com.terraquantum.credits.v1alpha1 import get_balance_pb2 as _get_balance_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
