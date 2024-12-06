b1fb5d636200059524cfca6025e665202c7ac663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:04:42 +0000

    Add package torchkbnufft/1.5.2/torchkbnufft-1.5.2.tar.gz

---
+++ packages/torchkbnufft/torchkbnufft-1.5.2.tar.gz/torchkbnufft-1.5.2/torchkbnufft/__init__.py
@@ -0,0 +1,47 @@
+"""Package info"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("torchkbnufft")
+except PackageNotFoundError:
+    # package is not installed
+    import warnings
+
+    warnings.warn("Could not retrieve torchkbnufft version!")
+
+import torchkbnufft.functional
+import torchkbnufft.modules
+
+from ._math import (
+    absolute,
+    complex_mult,
+    complex_sign,
+    conj_complex_mult,
+    imag_exp,
+    inner_product,
+)
+from ._nufft import utils as nufft_utils
+from ._nufft.dcomp import calc_density_compensation_function
+from ._nufft.spmat import calc_tensor_spmatrix
+from ._nufft.toep import calc_toeplitz_kernel
+from .modules import KbInterp, KbInterpAdjoint, KbNufft, KbNufftAdjoint, ToepNufft
+
+__all__ = [
+    "KbInterp",
+    "KbInterpAdjoint",
+    "KbNufft",
+    "KbNufftAdjoint",
+    "ToepNufft",
+    "absolute",
+    "calc_density_compensation_function",
+    "calc_tensor_spmatrix",
+    "calc_toeplitz_kernel",
+    "complex_mult",
+    "complex_sign",
+    "conj_complex_mult",
+    "functional",
+    "imag_exp",
+    "inner_product",
+    "modules",
+]
