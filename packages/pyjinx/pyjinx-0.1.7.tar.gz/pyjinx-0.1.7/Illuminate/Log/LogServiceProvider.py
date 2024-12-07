2da793757f1ea67d6407e5951f014f52ac3493b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 15:05:44 +0000

    Add package pyjinx/0.1.7/pyjinx-0.1.7.tar.gz

---
+++ packages/pyjinx/pyjinx-0.1.7.tar.gz/pyjinx-0.1.7/Illuminate/Log/LogServiceProvider.py
@@ -0,0 +1,14 @@
+from Illuminate.Log.LogManager import LogManager
+from Illuminate.Support.ServiceProvider import ServiceProvider
+from Illuminate.Contracts.Foundation.Application import Application
+
+
+class LogServiceProvider(ServiceProvider):
+    def __init__(self, app: Application) -> None:
+        self.__app = app
+
+    def register(self):
+        self.__app.singleton("log", lambda app: LogManager(self.__app))
+
+    def boot(self):
+        pass
