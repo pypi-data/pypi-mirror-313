1212b9c7926f4fdeed6c7ca54b549c15dd2951e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 09:13:44 +0000

    Add package cnsir/0.1.11/cnsir-0.1.11-py3-none-any.whl

---
+++ packages/cnsir/cnsir-0.1.11-py3-none-any.whl/cnsir/__init__.py
@@ -0,0 +1,19 @@
+from .api import (
+    load_table,
+    load_knowledge,
+    add_attribute,
+    add_classification,
+    setup,
+    setup_config,
+    download_map,
+)
+
+__all__ = [
+    "load_table",
+    "load_knowledge",
+    "add_attribute",
+    "add_classification",
+    "setup",
+    "setup_config",
+    "download_map",
+]
