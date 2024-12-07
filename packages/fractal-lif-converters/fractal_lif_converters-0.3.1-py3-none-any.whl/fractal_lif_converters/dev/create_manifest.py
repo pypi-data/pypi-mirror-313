2fcb2aec8dd85b144636cf3adc8e3ddf7d03d8b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 07:05:16 +0000

    Add package fractal-lif-converters/0.3.1/fractal_lif_converters-0.3.1-py3-none-any.whl

---
+++ packages/fractal-lif-converters/fractal_lif_converters-0.3.1-py3-none-any.whl/fractal_lif_converters/dev/create_manifest.py
@@ -0,0 +1,10 @@
+"""
+Generate JSON schemas for task arguments afresh, and write them
+to the package manifest.
+"""
+from fractal_tasks_core.dev.create_manifest import create_manifest
+
+if __name__ == "__main__":
+    PACKAGE = "fractal_lif_converters"
+    AUTHORS = "Lorenzo Cerrone"
+    create_manifest(package=PACKAGE, authors=AUTHORS)
