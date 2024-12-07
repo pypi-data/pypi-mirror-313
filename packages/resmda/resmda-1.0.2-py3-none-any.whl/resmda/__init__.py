070f6642670e367906c08e7690f46967ae49d5bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 08:07:48 +0000

    Add package resmda/1.0.2/resmda-1.0.2-py3-none-any.whl

---
+++ packages/resmda/resmda-1.0.2-py3-none-any.whl/resmda/__init__.py
@@ -0,0 +1,33 @@
+# Copyright 2024 D. Werthmüller, G. Serrao Seabra, F.C. Vossepoel
+#
+# This file is part of resmda.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy
+# of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+from resmda import utils
+from resmda.utils import localization_matrix, Report
+from resmda.data_assimilation import esmda
+from resmda.reservoir_simulator import Simulator, RandomPermeability
+
+print("Warning: `resmda` will change its name to `dageo`.")
+print("         Install `dageo` to get the newest version.")
+
+# Initialize a random number generator.
+rng = utils.rng()
+
+
+__all__ = ['reservoir_simulator', 'data_assimilation', 'utils',
+           'esmda', 'Simulator', 'RandomPermeability',
+           'localization_matrix', 'rng', 'Report']
+
+__version__ = utils.__version__
