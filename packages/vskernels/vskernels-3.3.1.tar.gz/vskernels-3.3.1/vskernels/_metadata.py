36cc2116defc3dff9a938ff13c7e0742183056c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:39 +0000

    Add package vskernels/3.3.1/vskernels-3.3.1.tar.gz

---
+++ packages/vskernels/vskernels-3.3.1.tar.gz/vskernels-3.3.1/vskernels/_metadata.py
@@ -0,0 +1,12 @@
+"""Kernel objects for scaling and format conversion within VapourSynth"""
+
+__version__ = '3.3.1'
+
+__author_name__, __author_email__ = 'LightArrowsEXE', 'LightArrowsReboot@gmail.com'
+__maintainer_name__, __maintainer_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'
+
+__author__ = f'{__author_name__} <{__author_email__}>'
+__maintainer__ = __author__
+
+if __name__ == '__github__':
+    print(__version__)
