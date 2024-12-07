b51a96a890b4b9adc43dbca3600c128c976f6ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 07:05:42 +0000

    Add package quantfinlib/0.0.5/quantfinlib-0.0.5-py3-none-any.whl

---
+++ packages/quantfinlib/quantfinlib-0.0.5-py3-none-any.whl/quantfinlib/sim/__init__.py
@@ -0,0 +1,34 @@
+"""
+Simulation models.
+=================
+
+This module contains routines for generating random price scenarions.
+
+
+.. currentmodule:: quantfinlib.sim
+
+.. autosummary::
+   :toctree: _autosummary
+
+   BrownianMotion          Simple Brownian Model with Normal changes.
+   GeometricBrownianMotion Geometric Brownian Motion with Normal returns.
+   OrnsteinUhlenbeck       Mean reverting noise model.
+"""
+
+from ._bm import BrownianMotion
+from ._gbm import GeometricBrownianMotion
+from ._ou import OrnsteinUhlenbeck
+
+__all__ = [BrownianMotion, GeometricBrownianMotion, OrnsteinUhlenbeck]
+
+"""
+todo:
+HistoricalSampling(..., sync, replace)
+RandomizePhases
+AR
+Heston
+Garch
+DCCGarch
+FractalBrownianMotion
+MarkovChain
+"""
