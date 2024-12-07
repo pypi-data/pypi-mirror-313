5ab4a2a904667a29afb3c6a13ef3e365d253c641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 11:05:37 +0000

    Add package tile-mate/0.0.12/tile_mate-0.0.12.tar.gz

---
+++ packages/tile-mate/tile_mate-0.0.12.tar.gz/tile_mate-0.0.12/src/tile_mate/tile_model.py
@@ -0,0 +1,12 @@
+from pandera import Column, DataFrameSchema
+
+TILE_SCHEMA = DataFrameSchema(
+    {
+        'tile_id': Column(str, required=True),
+        'url': Column(str, required=True),
+        'year': Column(int, required=False),
+        'temporal_baseline_days': Column(int, required=False),
+        'season': Column(str, required=False),
+        'geometry': Column('geometry', required=True),
+    }
+)
