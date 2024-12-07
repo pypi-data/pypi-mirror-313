d73965d06c8840e51dd872b581d9c017413405cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 06:05:31 +0000

    Add package repository-service-tuf/0.16.0b1/repository_service_tuf-0.16.0b1-py3-none-any.whl

---
+++ packages/repository-service-tuf/repository_service_tuf-0.16.0b1-py3-none-any.whl/repository_service_tuf/cli/admin/__init__.py
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: 2023-2024 Repository Service for TUF Contributors
+#
+# SPDX-License-Identifier: MIT
+
+
+"""Alternative admin cli
+
+Provides alternative ceremony, metadata update, and sign admin cli commands.
+
+"""
+from typing import Optional
+
+from repository_service_tuf.cli import click, rstuf
+
+
+def _set_settings(context: click.Context, api_server: Optional[str]):
+    """Set context.obj['settings'] attributes."""
+    settings = context.obj["settings"]
+    if api_server:
+        settings.SERVER = api_server
+
+
+@rstuf.group()  # type: ignore
+@click.option(
+    "--api-server",
+    help="URL to an RSTUF API.",
+    required=False,
+)
+@click.pass_context
+def admin(context: click.Context, api_server: Optional[str]):
+    """Administrative Commands"""
+    # Because of tests it has to be in a separate function.
+    _set_settings(context, api_server)  # pragma: no cover
+
+
+@admin.group()
+@click.pass_context
+def metadata(context):
+    """
+    Metadata management.
+    """
