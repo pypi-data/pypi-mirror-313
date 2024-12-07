aa9c8798889dc54e62ce4a56efbe76f5948c49b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 08:05:49 +0000

    Add package colorich/0.6.0/colorich-0.6.0.tar.gz

---
+++ packages/colorich/colorich-0.6.0.tar.gz/colorich-0.6.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'colorich',
+    author='Ethan Amar',
+    author_email='EthanA120@Gmail.com',
+    version = '0.6.0',
+    packages = [],
+)
