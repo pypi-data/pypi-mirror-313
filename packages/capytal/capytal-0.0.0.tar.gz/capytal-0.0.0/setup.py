d850e623f674911d0eff6c7cbba00949cc64b73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 07:05:15 +0000

    Add package capytal/0.0.0/capytal-0.0.0.tar.gz

---
+++ packages/capytal/capytal-0.0.0.tar.gz/capytal-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="capytal",
+    version="0.0.0",
+    description="A package for building complex stochastic models",
+    author="Dan Gelderd",
+    url="https://danielgelderd@dev.azure.com/danielgelderd/capytal/_git/capytal",
+    packages=[],  # Empty list - no packages included
+)
