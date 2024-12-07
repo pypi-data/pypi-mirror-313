5009f3e7b16f81bcf0599955ce0d1d2468a3b1d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 06:05:07 +0000

    Add package test2ita3/0.1/test2ita3-0.1.tar.gz

---
+++ packages/test2ita3/test2ita3-0.1.tar.gz/test2ita3-0.1/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+from pathlib import  Path
+setuptools.setup(
+    name = "test2ita3",
+    version="0.1",
+    long_description=Path("README.md").read_text(),
+    packages= setuptools.find_packages(exclude=["data", "tests"])
+)
