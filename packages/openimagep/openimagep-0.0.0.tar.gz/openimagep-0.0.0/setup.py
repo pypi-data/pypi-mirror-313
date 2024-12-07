4faa509da35f514cfb59735ada2d5a9afce2a7e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 15:05:45 +0000

    Add package openimagep/0.0.0/openimagep-0.0.0.tar.gz

---
+++ packages/openimagep/openimagep-0.0.0.tar.gz/openimagep-0.0.0/setup.py
@@ -0,0 +1,24 @@
+from time import time
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="openimagep",
+    version="0.0.0",
+    author="Your Name Goes Here",
+    author_email="youremail@gmail.com",
+    description="A test package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
