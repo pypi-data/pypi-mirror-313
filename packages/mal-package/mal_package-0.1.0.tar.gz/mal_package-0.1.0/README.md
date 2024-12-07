5d37520a2af33088e90e14c919a671d05ba0d3b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 07:05:51 +0000

    Add package mal-package/0.1.0/mal_package-0.1.0.tar.gz

---
+++ packages/mal-package/mal_package-0.1.0.tar.gz/mal_package-0.1.0/README.md
@@ -0,0 +1,8 @@
+# My Package
+
+A simple example Python package.
+
+## Installation
+
+```bash
+pip install mal_package
---
+++ packages/mal-package/mal_package-0.1.0.tar.gz/mal_package-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mal_package",  # 패키지 이름
+    version="0.1.0",    # 버전
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A simple example package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/my_package",  # GitHub 등 저장소 URL
+    packages=find_packages(),  # 패키지 자동 탐지
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
