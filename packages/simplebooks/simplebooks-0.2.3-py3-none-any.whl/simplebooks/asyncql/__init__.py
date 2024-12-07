8816dffcc56e45d990d2e348de3e0aa2f7f99515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:08 +0000

    Add package simplebooks/0.2.3/simplebooks-0.2.3-py3-none-any.whl

---
+++ packages/simplebooks/simplebooks-0.2.3-py3-none-any.whl/simplebooks/asyncql/__init__.py
@@ -0,0 +1,61 @@
+from .Account import Account
+from .AccountCategory import AccountCategory
+from .AccountType import AccountType
+from .Currency import Currency
+from .Customer import Customer
+from .Entry import Entry
+from .EntryType import EntryType
+from .Identity import Identity
+from .Ledger import Ledger
+from .LedgerType import LedgerType
+from .Transaction import Transaction
+from .Vendor import Vendor
+from sqloquent.asyncql import (
+    async_contains, async_within, async_has_many, async_belongs_to,
+)
+
+
+Identity.ledgers = async_has_many(Identity, Ledger, 'identity_id')
+Ledger.owner = async_belongs_to(Ledger, Identity, 'identity_id')
+
+Ledger.currency = async_belongs_to(Ledger, Currency, 'currency_id')
+
+Ledger.accounts = async_has_many(Ledger, Account, 'ledger_id')
+Account.ledger = async_belongs_to(Account, Ledger, 'ledger_id')
+
+Account.children = async_has_many(Account, Account, 'parent_id')
+Account.parent = async_belongs_to(Account, Account, 'parent_id')
+
+Account.category = async_belongs_to(Account, AccountCategory, 'category_id')
+AccountCategory.accounts = async_has_many(AccountCategory, Account, 'category_id')
+
+Account.entries = async_has_many(Account, Entry, 'account_id')
+Entry.account = async_belongs_to(Entry, Account, 'account_id')
+
+Entry.transactions = async_within(Entry, Transaction, 'entry_ids')
+Transaction.entries = async_contains(Transaction, Entry, 'entry_ids')
+
+Transaction.ledgers = async_contains(Transaction, Ledger, 'ledger_ids')
+Ledger.transactions = async_within(Ledger, Transaction, 'ledger_ids')
+
+
+def set_connection_info(db_file_path: str):
+    """Set the connection info for all models to use the specified
+        sqlite3 database file path.
+    """
+    Account.connection_info = db_file_path
+    AccountCategory.connection_info = db_file_path
+    Currency.connection_info = db_file_path
+    Customer.connection_info = db_file_path
+    Entry.connection_info = db_file_path
+    Identity.connection_info = db_file_path
+    Ledger.connection_info = db_file_path
+    Transaction.connection_info = db_file_path
+    Vendor.connection_info = db_file_path
+
+
+# no longer needed
+del async_contains
+del async_belongs_to
+del async_within
+del async_has_many
