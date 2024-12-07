07ed23fa0dffc3113329f792f7fdc515e714f30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:37:42 +0000

    Add package licomp-hermione/0.3.1/licomp_hermione-0.3.1-py2.py3-none-any.whl

---
+++ packages/licomp-hermione/licomp_hermione-0.3.1-py2.py3-none-any.whl/licomp_hermione/config.py
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: 2024 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+version = '0.3.1'
+supported_api_version = '0.3'
+
+repo = 'https://github.com/hesa/licomp-hermione/'
+disclaimer = 'The data or the output of the tools in this repository come with guarantee'
+cli_name = 'licomp-hermione'
+module_name = 'licomp_hermione'
+
+description = """
+
+Simple command line tool to check compatibility between two licenses, given context
+
+"""
+
+epilog = """
+
+
+
+AUTHOR
+  Henrik Sandklef
+
+
+PROJECT SITE
+
+  https://github.com/hesa/licomp-recliense
+
+REPORTING BUGS
+
+  File a ticket at https://github.com/hesa/licomp-hermione/issues
+
+COPYRIGHT
+
+  Copyright (c) 2024 Henrik Sandklef<hesa@sandklef.com>.
+
+  License GPL-3.0-or-later
+
+ATTRIBUTION
+  licomp-hermione is using the license compatibility matrix from Hermine project.
+"""
