c97d57f729d6ddd3c63a93e1358f14a2b5858e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 15:06:10 +0000

    Add package keywordrich/0.0.2/keywordrich-0.0.2.tar.gz

---
+++ packages/keywordrich/keywordrich-0.0.2.tar.gz/keywordrich-0.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'keywordrich'
+
+setup(
+    name="keywordrich",
+    version=VERSION,
+    author="feng wang",
+    author_email="Sysiphus6@2925.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=open('README.md',encoding="UTF8").read(),
+    packages=find_packages(),
+    install_requires=['langchain', 'langchain_openai'],
+    keywords=['python', 'keyword rich', 'ai'],
+    entry_points={
+    'console_scripts': [
+        'keywordrich = keywordrich.main:main'
+    ]
+    },
+    license="MIT",
+    # url="https://github.com/",
+    scripts=['keywordrich/keywordrich.py'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
