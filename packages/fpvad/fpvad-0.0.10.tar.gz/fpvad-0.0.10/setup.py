48a8c50be9d7fd3895e5ff90378c68d27f093c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 15:06:25 +0000

    Add package fpvad/0.0.10/fpvad-0.0.10.tar.gz

---
+++ packages/fpvad/fpvad-0.0.10.tar.gz/fpvad-0.0.10/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup (
+    name="fpvad",
+    version="0.0.10",
+    packages=find_packages(),
+    install_requires=[
+        "asteroid-filterbanks==0.4.0",
+        # "librosa==0.10.2.post1",
+        # "numpy==1.26.4",
+        # "soundfile==0.12.1",
+        # "torch==2.3.1",
+        # "torchaudio==2.3.1",
+    ],
+    include_package_data=True,
+    package_data={
+        "fanvad": ["pyannote_v2.json"],
+    },
+    author="FanPars",
+    description="Detects voice activity in audio files",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown"
+)
