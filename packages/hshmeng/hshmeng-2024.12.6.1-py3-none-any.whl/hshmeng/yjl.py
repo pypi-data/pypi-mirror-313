8961198e70a5e92fe147f65d015f223475e78f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 07:05:45 +0000

    Add package hshmeng/2024.12.6.1/hshmeng-2024.12.6.1-py3-none-any.whl

---
+++ packages/hshmeng/hshmeng-2024.12.6.1-py3-none-any.whl/hshmeng/yjl.py
@@ -0,0 +1,13 @@
+if 'limeng' == 'hshmeng':
+    print("你看你妈呢")
+#此yjl不是指YangJiaLing，指代You Just Lost
+
+def love(test=None,k=None):
+    if test == 'qyzy':
+        print("离开她了就不能再爱她了吗")#钱杨遵义！！！
+        if k == 'yjl':
+            print("连女生都帮你了还追不到她，你在干什么！！！")
+        if k == 'lst':
+            print("人家暗示的那么明显了！你一句“好的”，“我知道了”就消失不见了，活该单身！！！")
+    if test == None:
+        pass
