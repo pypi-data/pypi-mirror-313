d1ba8df37a28f8c43b31c4128b51c197d9a8294b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:40 +0000

    Add package vsrgtools/1.8.1/vsrgtools-1.8.1-py3-none-any.whl

---
+++ packages/vsrgtools/vsrgtools-1.8.1-py3-none-any.whl/vsrgtools/_metadata.py
@@ -0,0 +1,12 @@
+"""Wrapper for RGVS, RGSF, and various other functions"""
+
+__version__ = '1.8.1'
+
+__author_name__, __author_email__ = 'Irrational Encoding Wizardry', 'wizards@encode.moe'
+__maintainer_name__, __maintainer_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'
+
+__author__ = f'{__author_name__} <{__author_email__}>'
+__maintainer__ = __author__
+
+if __name__ == '__github__':
+    print(__version__)
