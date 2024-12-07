2e083674aff3d6f7f445b769f05298386c300551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 09:14:07 +0000

    Add package pyTRADE-wrapper/1.1.0/pytrade_wrapper-1.1.0-py3-none-any.whl

---
+++ packages/pyTRADE-wrapper/pytrade_wrapper-1.1.0-py3-none-any.whl/pyTRADE/examples/minimal_example.py
@@ -0,0 +1,27 @@
+import time
+
+from pyTRADE.core.wrapper import TRADEWrapper
+from ai.thinkingrobots.trade import TRADE
+from jpype import JImplements, JOverride, JClass
+from edu.tufts.hrilab.interfaces import DockingInterface
+
+
+@JImplements(DockingInterface)
+class dummyWrapper:
+    @JOverride
+    def dock(self, dockId):
+        pass
+
+    @JOverride
+    def undock(self):
+        print("Undocking")
+        pass
+
+
+if __name__ == '__main__':
+    wrapper = TRADEWrapper()
+    dummyObject = dummyWrapper()
+    TRADE.registerAllServices(dummyObject, "")
+    time.sleep(1)
+    print(TRADE.getAvailableServices())
+    wrapper.call_trade("undock", )
