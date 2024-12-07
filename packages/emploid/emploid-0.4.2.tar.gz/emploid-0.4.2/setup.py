ad1958147cdb1c1f2b0e532f00323a40f5ea59fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 11:05:44 +0000

    Add package emploid/0.4.2/emploid-0.4.2.tar.gz

---
+++ packages/emploid/emploid-0.4.2.tar.gz/emploid-0.4.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='emploid',
+   version='0.4.2',
+   description='A simple to use automation tool for automating web, android and windows proccesses.',
+   long_description='A simple to use automation tool for automating web, android and windows proccesses.',
+   author='PixQuilly',
+   author_email='pixquilly@gmail.com',
+   packages=['emploid'],  #same as name
+   install_requires=['wheel', 'bar', 'greek', 'cv2', 'numpy', 'pyautogui'], #external packages as dependencies
+)
