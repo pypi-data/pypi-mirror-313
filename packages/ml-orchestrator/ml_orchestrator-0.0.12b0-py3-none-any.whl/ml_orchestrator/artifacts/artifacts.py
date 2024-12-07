eee26ae71bfe5b1de44f19ba6576a20fd63c58ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 14:10:13 +0000

    Add package ml-orchestrator/0.0.12b0/ml_orchestrator-0.0.12b0-py3-none-any.whl

---
+++ packages/ml-orchestrator/ml_orchestrator-0.0.12b0-py3-none-any.whl/ml_orchestrator/artifacts/artifacts.py
@@ -0,0 +1,34 @@
+from typing import Dict, Optional
+
+from ml_orchestrator.artifacts import Artifact
+
+
+class Model(Artifact):
+    schema_title: str = "system.Model"
+
+
+class Dataset(Artifact):
+    schema_title = "system.Dataset"
+
+    pass
+
+
+class HTML(Artifact):
+    schema_title = "system.HTML"
+
+    def __init__(self, name: Optional[str] = None, uri: Optional[str] = None, metadata: Optional[Dict] = None) -> None:
+        super().__init__(name=name, uri=uri, metadata=metadata)
+        self.uri = self.uri + ".html"
+
+
+class Markdown(Artifact):
+    schema_title = "system.Markdown"
+
+
+class Metrics(Artifact):
+    schema_title = "system.Metrics"
+
+    def log_metric(self, metric: str, value: float) -> None:
+        assert isinstance(value, (int, float, bool, str)), f"Type {type(value)} is not a valid metric value"
+        assert isinstance(metric, str), f"Type {type(metric)} is not a valid metric name"
+        self.metadata[metric] = value
