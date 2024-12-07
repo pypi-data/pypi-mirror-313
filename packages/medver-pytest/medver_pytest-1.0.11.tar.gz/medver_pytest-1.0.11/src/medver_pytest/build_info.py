0ac8c5162489a734fe82f4502fe3272b40fde1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:09:52 +0000

    Add package medver-pytest/1.0.11/medver_pytest-1.0.11.tar.gz

---
+++ packages/medver-pytest/medver_pytest-1.0.11.tar.gz/medver_pytest-1.0.11/src/medver_pytest/build_info.py
@@ -0,0 +1,23 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.10.15'
+    os_name = 'posix:ubuntu'
+    version = '1.0.11'
+    git_sha = '15706a98d8a202a06af05475a16ce5c440964938'
+    git_branch = 'master'
+    git_uncommitted = [
+        'M  do_post_ver',
+        'M  src/medver_pytest/.gitignore',
+        'D  src/medver_pytest/build_info.py',
+        'M  src/medver_pytest/constants_version.py',
+        'M  src/medver_pytest/log/logger.py',
+        'M  src/medver_pytest/storage/storage.py',
+        'M  src/medver_pytest/storage/storage_dev.py',
+        'M  src/medver_pytest/storage/storage_file_base.py',
+        'M  src/medver_pytest/storage/storage_shared_drive.py',
+        'M  tools/pyproject_template.toml',
+        'M  tools/xplat_utils',
+        'M  ut/mocks/mock_logger.py',
+        'M  xplat.cfg',
+    ]
+    git_unpushed = [
+    ]
