42008368f7185f40df244818a1ce472e7aec053f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 07:05:19 +0000

    Add package pyawscrondxcsesfrwk/1.0.0/pyawscrondxcsesfrwk-1.0.0.tar.gz

---
+++ packages/pyawscrondxcsesfrwk/pyawscrondxcsesfrwk-1.0.0.tar.gz/pyawscrondxcsesfrwk-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyawscrondxcsesfrwk",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    description="A customized version of pyawscron limited to DXC",
+    author="AWS Platform Engineering",
+    author_email="abc@example.com",
+    url="https://github.com/pitchblack408/pyawscron.git",
+    install_requires=[],
+)
