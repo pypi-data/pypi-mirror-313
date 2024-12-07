97616145b89e2c4570468f80f08122771e4ec656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:39:15 +0000

    Add package basilearn/0.1.221/basilearn-0.1.221.tar.gz

---
+++ packages/basilearn/basilearn-0.1.221.tar.gz/basilearn-0.1.221/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+import io
+
+# Read README with proper encoding
+with io.open("README.md", encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="basilearn",
+    version="0.1.221",
+    packages=find_packages(include=["basilearn", "basilearn.*"]),
+    install_requires=[
+        "setuptools>=56.0.0",
+    ],
+    author="Barbara Asiamah",
+    author_email="barbaraasiamah99@gmail.com",
+    description="A library for educational content and interactive learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Education",
+    ],
+    python_requires=">=3.7",
+    keywords="education, interactive-learning, python, library",
+    entry_points={
+    'console_scripts': [
+        'basilearn-run = basilearn.run:main',  
+    ]
+},
+
+    
+)
