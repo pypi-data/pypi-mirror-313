a7374c813e4f64051ad42ce98d698c4469ce43f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 08:05:34 +0000

    Add package finance-core/0.1.2/finance_core-0.1.2-cp39-cp39-win_amd64.whl

---
+++ packages/finance-core/finance_core-0.1.2-cp39-cp39-win_amd64.whl/finance_core/__init__.py
@@ -0,0 +1,13 @@
+from ._finance_core import (
+    sum_as_string,
+    Maximum,
+    Minimum,
+    SimpleMovingAverage
+)
+
+__all__ = [
+    "sum_as_string",
+    "Maximum",
+    "Minimum",
+    "SimpleMovingAverage"
+]
