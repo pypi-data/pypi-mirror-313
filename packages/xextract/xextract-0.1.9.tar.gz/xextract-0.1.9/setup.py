d52f6c359d1c1a8f1a721682f06f8d2fd272b1ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 07:05:07 +0000

    Add package xextract/0.1.9/xextract-0.1.9.tar.gz

---
+++ packages/xextract/xextract-0.1.9.tar.gz/xextract-0.1.9/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+
+setup(
+    name='xextract',
+    version='0.1.9',
+    description='Extract structured data from HTML and XML documents like a boss.',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    author='Michal "Mimino" Danilak',
+    author_email='michal.danilak@gmail.com',
+    url='https://github.com/Mimino666/python-xextract',
+    keywords='HTML parse parsing extraction extract crawl',
+    packages=['xextract',
+              'xextract.extractors'],
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    install_requires=['lxml', 'cssselect'],
+    test_suite='tests',
+    license='MIT',
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ),
+)
