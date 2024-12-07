00aed4b3be6f791e6110ce89a63ca8d580a36130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:22 +0000

    Add package fastapi-tryton-integration/0.1.2/fastapi_tryton_integration-0.1.2.tar.gz

---
+++ packages/fastapi-tryton-integration/fastapi_tryton_integration-0.1.2.tar.gz/fastapi_tryton_integration-0.1.2/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='fastapi_tryton_integration',
+    version='0.1.2',
+    author='Solutema SRL',
+    description='FastAPI connection module for Tryton ERP',
+    packages=['fastapi_tryton_integration', ],
+    )
