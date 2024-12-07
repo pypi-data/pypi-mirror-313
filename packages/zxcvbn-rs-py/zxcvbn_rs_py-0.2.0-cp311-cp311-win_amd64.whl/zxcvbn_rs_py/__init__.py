1636e99547eba15572225fcd2acc5ce514ace41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 07:05:45 +0000

    Add package zxcvbn-rs-py/0.2.0/zxcvbn_rs_py-0.2.0-cp311-cp311-win_amd64.whl

---
+++ packages/zxcvbn-rs-py/zxcvbn_rs_py-0.2.0-cp311-cp311-win_amd64.whl/zxcvbn_rs_py/__init__.py
@@ -0,0 +1,21 @@
+from .zxcvbn_rs_py import (
+    CrackTimesDisplay,
+    CrackTimesSeconds,
+    Entropy,
+    Feedback,
+    Suggestion,
+    Warning,
+    __version__,
+    zxcvbn,
+)
+
+__all__ = [
+    "CrackTimesDisplay",
+    "CrackTimesSeconds",
+    "Entropy",
+    "Feedback",
+    "Suggestion",
+    "Warning",
+    "__version__",
+    "zxcvbn",
+]
