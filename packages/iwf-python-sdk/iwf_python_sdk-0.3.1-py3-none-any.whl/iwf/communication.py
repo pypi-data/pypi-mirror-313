9d98c8c23e539502a33b7132f2c5eb8233469ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:37:55 +0000

    Add package iwf-python-sdk/0.3.1/iwf_python_sdk-0.3.1-py3-none-any.whl

---
+++ packages/iwf-python-sdk/iwf_python_sdk-0.3.1-py3-none-any.whl/iwf/communication.py
@@ -0,0 +1,68 @@
+from typing import Any, Optional, Union
+
+from iwf.errors import WorkflowDefinitionError
+from iwf.iwf_api.models import EncodedObject, InterStateChannelPublishing
+from iwf.object_encoder import ObjectEncoder
+from iwf.state_movement import StateMovement
+
+
+class Communication:
+    _type_store: dict[str, Optional[type]]
+    _object_encoder: ObjectEncoder
+    _to_publish_internal_channel: dict[str, list[EncodedObject]]
+    _state_movements: list[StateMovement]
+
+    def __init__(
+        self, type_store: dict[str, Optional[type]], object_encoder: ObjectEncoder
+    ):
+        self._object_encoder = object_encoder
+        self._type_store = type_store
+        self._to_publish_internal_channel = {}
+        self._state_movements = []
+
+    def trigger_state_execution(self, state: Union[str, type], state_input: Any = None):
+        """
+
+        Args:
+            state: the workflowState TODO the type hint should be type[WorkflowState]
+            state_input: the input of the state
+        """
+        movement = StateMovement.create(state, state_input)
+        self._state_movements.append(movement)
+
+    def publish_to_internal_channel(self, channel_name: str, value: Any = None):
+        registered_type = self._type_store.get(channel_name)
+
+        if registered_type is None:
+            for name, t in self._type_store.items():
+                if channel_name.startswith(name):
+                    registered_type = t
+
+        if registered_type is None:
+            raise WorkflowDefinitionError(
+                f"InternalChannel channel_name is not defined {channel_name}"
+            )
+
+        if (
+            value is not None
+            and registered_type is not None
+            and not isinstance(value, registered_type)
+        ):
+            raise WorkflowDefinitionError(
+                f"InternalChannel value is not of type {registered_type}"
+            )
+        vals = self._to_publish_internal_channel.get(channel_name)
+        if vals is None:
+            vals = []
+        vals.append(self._object_encoder.encode(value))
+        self._to_publish_internal_channel[channel_name] = vals
+
+    def get_to_publishing_internal_channel(self) -> list[InterStateChannelPublishing]:
+        pubs = []
+        for name, vals in self._to_publish_internal_channel.items():
+            for val in vals:
+                pubs.append(InterStateChannelPublishing(name, val))
+        return pubs
+
+    def get_to_trigger_state_movements(self) -> list[StateMovement]:
+        return self._state_movements
---
+++ packages/iwf-python-sdk/iwf_python_sdk-0.3.1-py3-none-any.whl/iwf/workflow_state_options.py
@@ -0,0 +1,108 @@
+from dataclasses import dataclass
+from typing import Any, Optional
+
+from iwf.iwf_api.models import (
+    ExecuteApiFailurePolicy,
+    PersistenceLoadingPolicy,
+    RetryPolicy,
+    WaitUntilApiFailurePolicy,
+    WorkflowStateOptions as IdlWorkflowStateOptions,
+)
+
+
+@dataclass
+class WorkflowStateOptions:
+    state_id: Optional[str] = None
+    # apply for both waitUntil and execute API
+    data_attributes_loading_policy: Optional[PersistenceLoadingPolicy] = None
+    search_attributes_loading_policy: Optional[PersistenceLoadingPolicy] = None
+    # below are wait_until API specific options:
+    wait_until_api_timeout_seconds: Optional[int] = None
+    wait_until_api_retry_policy: Optional[RetryPolicy] = None
+    wait_until_api_failure_policy: Optional[WaitUntilApiFailurePolicy] = None
+    wait_until_api_data_attributes_loading_policy: Optional[
+        PersistenceLoadingPolicy
+    ] = None
+    wait_until_api_search_attributes_loading_policy: Optional[
+        PersistenceLoadingPolicy
+    ] = None
+    # below are execute API specific options:
+    execute_api_timeout_seconds: Optional[int] = None
+    execute_api_retry_policy: Optional[RetryPolicy] = None
+    """
+        note that the failing handling state will take the same input as the failed state
+        the type is Optional[type[WorkflowState]] but there is an issue with type hint...
+        TODO fix this type hint
+    """
+    execute_failure_handling_state: Optional[type] = None
+    execute_api_data_attributes_loading_policy: Optional[PersistenceLoadingPolicy] = (
+        None
+    )
+    execute_api_search_attributes_loading_policy: Optional[PersistenceLoadingPolicy] = (
+        None
+    )
+
+
+def _to_idl_state_options(
+    skip_wait_until: bool,
+    options: Any,  # TODO this type was Optional[WorkflowStateOptions],
+    # however, type hint is not working with recursive call...
+    state_store: dict[str, Any],  # TODO this type should be dict[str, WorkflowState]
+) -> IdlWorkflowStateOptions:
+    res = IdlWorkflowStateOptions()
+    if skip_wait_until:
+        res.skip_wait_until = True
+
+    if options is None:
+        return res
+    assert isinstance(options, WorkflowStateOptions)
+
+    if options.wait_until_api_search_attributes_loading_policy is not None:
+        res.wait_until_api_search_attributes_loading_policy = (
+            options.wait_until_api_search_attributes_loading_policy
+        )
+    if options.execute_api_search_attributes_loading_policy is not None:
+        res.execute_api_search_attributes_loading_policy = (
+            options.execute_api_search_attributes_loading_policy
+        )
+    if options.search_attributes_loading_policy is not None:
+        res.search_attributes_loading_policy = options.search_attributes_loading_policy
+    if options.wait_until_api_data_attributes_loading_policy is not None:
+        res.wait_until_api_data_attributes_loading_policy = (
+            options.wait_until_api_data_attributes_loading_policy
+        )
+    if options.execute_api_data_attributes_loading_policy is not None:
+        res.execute_api_data_attributes_loading_policy = (
+            options.execute_api_data_attributes_loading_policy
+        )
+    if options.data_attributes_loading_policy is not None:
+        res.data_attributes_loading_policy = options.data_attributes_loading_policy
+    if options.wait_until_api_failure_policy is not None:
+        res.wait_until_api_failure_policy = options.wait_until_api_failure_policy
+    if options.wait_until_api_retry_policy is not None:
+        res.wait_until_api_retry_policy = options.wait_until_api_retry_policy
+    if options.wait_until_api_timeout_seconds is not None:
+        res.wait_until_api_timeout_seconds = options.wait_until_api_timeout_seconds
+    if options.execute_api_retry_policy is not None:
+        res.execute_api_retry_policy = options.execute_api_retry_policy
+    if options.execute_api_timeout_seconds is not None:
+        res.execute_api_timeout_seconds = options.execute_api_timeout_seconds
+    if options.execute_failure_handling_state is not None:
+        res.execute_api_failure_policy = (
+            ExecuteApiFailurePolicy.PROCEED_TO_CONFIGURED_STATE
+        )
+        from iwf.workflow_state import get_state_id_by_class
+
+        res.execute_api_failure_proceed_state_id = get_state_id_by_class(
+            options.execute_failure_handling_state
+        )
+        state = state_store[res.execute_api_failure_proceed_state_id]
+        proceed_state_options = state.get_state_options()
+
+        from iwf.workflow_state import should_skip_wait_until
+
+        proceed_state_idl_options = _to_idl_state_options(
+            should_skip_wait_until(state), proceed_state_options, state_store
+        )
+        res.execute_api_failure_proceed_state_options = proceed_state_idl_options
+    return res
