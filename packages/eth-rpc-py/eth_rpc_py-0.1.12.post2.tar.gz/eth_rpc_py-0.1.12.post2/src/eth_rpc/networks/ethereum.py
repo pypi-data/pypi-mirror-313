590e21a845f10a76d723fef8b6fadbf1526a00f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 15:05:51 +0000

    Add package eth-rpc-py/0.1.12.post2/eth_rpc_py-0.1.12.post2.tar.gz

---
+++ packages/eth-rpc-py/eth_rpc_py-0.1.12.post2.tar.gz/eth_rpc_py-0.1.12.post2/src/eth_rpc/networks/ethereum.py
@@ -0,0 +1,23 @@
+from typing import ClassVar
+
+from eth_rpc.types import BlockExplorer, Network, Rpcs, RpcUrl
+from pydantic.networks import AnyHttpUrl, AnyWebsocketUrl
+
+
+class Ethereum(Network):
+    chain_id: ClassVar[int] = 1
+    name: ClassVar[str] = "Ethereum"
+    native_currency: ClassVar[str] = "ETH"
+    rpc: ClassVar[Rpcs] = Rpcs(
+        default=RpcUrl(
+            http=AnyHttpUrl("https://cloudflare-eth.com"),
+            wss=AnyWebsocketUrl("wss://mainnet.gateway.tenderly.co"),
+        )
+    )
+    block_explorer: ClassVar[BlockExplorer] = BlockExplorer(
+        name="Etherscan",
+        url="https://etherscan.io",
+        api_url="https://api.etherscan.io/api",
+    )
+    alchemy_str: ClassVar[str | None] = "eth-mainnet"
+    apprx_block_time: ClassVar[float] = 12.05
