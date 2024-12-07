50cb61220ee6a01433ce004810844f3e788a34e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 09:14:27 +0000

    Add package llama-stack-client/0.0.58/llama_stack_client-0.0.58-py3-none-any.whl

---
+++ packages/llama-stack-client/llama_stack_client-0.0.58-py3-none-any.whl/llama_stack_client/__init__.py
@@ -0,0 +1,114 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from . import types
+from ._base_client import DefaultAsyncHttpxClient, DefaultHttpxClient
+from ._types import NoneType, NOT_GIVEN, NotGiven, Omit, ProxiesTypes, Transport
+from ._client import (
+    AsyncClient,
+    AsyncLlamaStackClient,
+    AsyncStream,
+    Client,
+    LlamaStackClient,
+    RequestOptions,
+    Stream,
+    Timeout,
+    Transport,
+)
+from ._constants import DEFAULT_CONNECTION_LIMITS, DEFAULT_MAX_RETRIES, DEFAULT_TIMEOUT
+from ._exceptions import (
+    APIConnectionError,
+    APIError,
+    APIResponseValidationError,
+    APIStatusError,
+    APITimeoutError,
+    AuthenticationError,
+    BadRequestError,
+    ConflictError,
+    InternalServerError,
+    LlamaStackClientError,
+    NotFoundError,
+    PermissionDeniedError,
+    RateLimitError,
+    UnprocessableEntityError,
+)
+from ._models import BaseModel
+from ._response import APIResponse as APIResponse, AsyncAPIResponse as AsyncAPIResponse
+from ._utils import file_from_path
+from ._utils._logs import setup_logging as _setup_logging
+from ._version import __title__, __version__
+
+__all__ = [
+    "types",
+    "__version__",
+    "__title__",
+    "NoneType",
+    "Transport",
+    "ProxiesTypes",
+    "NotGiven",
+    "NOT_GIVEN",
+    "Omit",
+    "LlamaStackClientError",
+    "APIError",
+    "APIStatusError",
+    "APITimeoutError",
+    "APIConnectionError",
+    "APIResponseValidationError",
+    "BadRequestError",
+    "AuthenticationError",
+    "PermissionDeniedError",
+    "NotFoundError",
+    "ConflictError",
+    "UnprocessableEntityError",
+    "RateLimitError",
+    "InternalServerError",
+    "Timeout",
+    "RequestOptions",
+    "Client",
+    "AsyncClient",
+    "Stream",
+    "AsyncStream",
+    "LlamaStackClient",
+    "AsyncLlamaStackClient",
+    "file_from_path",
+    "BaseModel",
+    "DEFAULT_TIMEOUT",
+    "DEFAULT_MAX_RETRIES",
+    "DEFAULT_CONNECTION_LIMITS",
+    "DefaultHttpxClient",
+    "DefaultAsyncHttpxClient",
+]
+
+
+# Only expose the direct client if its dependencies are installed
+class _MissingDependencyProxy:
+    def __init__(self, import_error):
+        self._error = f"LlamaStackDirectClient is not available because its dependencies are not installed. Original error: {import_error}"
+
+    def __getattr__(self, _):
+        raise ImportError(self._error)
+
+    def __call__(self, *args, **kwargs):
+        raise ImportError(self._error)
+
+
+try:
+    from .lib.direct.direct import LlamaStackDirectClient
+
+    __all__.append("LlamaStackDirectClient")
+except ImportError as e:
+    LlamaStackDirectClient = _MissingDependencyProxy(e)
+
+_setup_logging()
+
+# Update the __module__ attribute for exported symbols so that
+# error messages point to this module instead of the module
+# it was originally defined in, e.g.
+# llama_stack_client._exceptions.NotFoundError -> llama_stack_client.NotFoundError
+__locals = locals()
+for __name in __all__:
+    if not __name.startswith("__"):
+        try:
+            __locals[__name].__module__ = "llama_stack_client"
+        except (TypeError, AttributeError):
+            # Some of our exported symbols are builtins which we can't set attributes for.
+            pass
---
+++ packages/llama-stack-client/llama_stack_client-0.0.58-py3-none-any.whl/llama_stack_client/resources/telemetry.py
@@ -0,0 +1,265 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+import httpx
+
+from ..types import telemetry_get_trace_params, telemetry_log_event_params
+from .._types import NOT_GIVEN, Body, Query, Headers, NoneType, NotGiven
+from .._utils import (
+    maybe_transform,
+    strip_not_given,
+    async_maybe_transform,
+)
+from .._compat import cached_property
+from .._resource import SyncAPIResource, AsyncAPIResource
+from .._response import (
+    to_raw_response_wrapper,
+    to_streamed_response_wrapper,
+    async_to_raw_response_wrapper,
+    async_to_streamed_response_wrapper,
+)
+from ..types.trace import Trace
+from .._base_client import make_request_options
+
+__all__ = ["TelemetryResource", "AsyncTelemetryResource"]
+
+
+class TelemetryResource(SyncAPIResource):
+    @cached_property
+    def with_raw_response(self) -> TelemetryResourceWithRawResponse:
+        """
+        This property can be used as a prefix for any HTTP method call to return the
+        the raw response object instead of the parsed content.
+
+        For more information, see https://www.github.com/stainless-sdks/llama-stack-python#accessing-raw-response-data-eg-headers
+        """
+        return TelemetryResourceWithRawResponse(self)
+
+    @cached_property
+    def with_streaming_response(self) -> TelemetryResourceWithStreamingResponse:
+        """
+        An alternative to `.with_raw_response` that doesn't eagerly read the response body.
+
+        For more information, see https://www.github.com/stainless-sdks/llama-stack-python#with_streaming_response
+        """
+        return TelemetryResourceWithStreamingResponse(self)
+
+    def get_trace(
+        self,
+        *,
+        trace_id: str,
+        x_llama_stack_provider_data: str | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> Trace:
+        """
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        extra_headers = {
+            **strip_not_given({"X-LlamaStack-ProviderData": x_llama_stack_provider_data}),
+            **(extra_headers or {}),
+        }
+        return self._get(
+            "/alpha/telemetry/get-trace",
+            options=make_request_options(
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+                query=maybe_transform({"trace_id": trace_id}, telemetry_get_trace_params.TelemetryGetTraceParams),
+            ),
+            cast_to=Trace,
+        )
+
+    def log_event(
+        self,
+        *,
+        event: telemetry_log_event_params.Event,
+        x_llama_stack_provider_data: str | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> None:
+        """
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        extra_headers = {"Accept": "*/*", **(extra_headers or {})}
+        extra_headers = {
+            **strip_not_given({"X-LlamaStack-ProviderData": x_llama_stack_provider_data}),
+            **(extra_headers or {}),
+        }
+        return self._post(
+            "/alpha/telemetry/log-event",
+            body=maybe_transform({"event": event}, telemetry_log_event_params.TelemetryLogEventParams),
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=NoneType,
+        )
+
+
+class AsyncTelemetryResource(AsyncAPIResource):
+    @cached_property
+    def with_raw_response(self) -> AsyncTelemetryResourceWithRawResponse:
+        """
+        This property can be used as a prefix for any HTTP method call to return the
+        the raw response object instead of the parsed content.
+
+        For more information, see https://www.github.com/stainless-sdks/llama-stack-python#accessing-raw-response-data-eg-headers
+        """
+        return AsyncTelemetryResourceWithRawResponse(self)
+
+    @cached_property
+    def with_streaming_response(self) -> AsyncTelemetryResourceWithStreamingResponse:
+        """
+        An alternative to `.with_raw_response` that doesn't eagerly read the response body.
+
+        For more information, see https://www.github.com/stainless-sdks/llama-stack-python#with_streaming_response
+        """
+        return AsyncTelemetryResourceWithStreamingResponse(self)
+
+    async def get_trace(
+        self,
+        *,
+        trace_id: str,
+        x_llama_stack_provider_data: str | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> Trace:
+        """
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        extra_headers = {
+            **strip_not_given({"X-LlamaStack-ProviderData": x_llama_stack_provider_data}),
+            **(extra_headers or {}),
+        }
+        return await self._get(
+            "/alpha/telemetry/get-trace",
+            options=make_request_options(
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+                query=await async_maybe_transform(
+                    {"trace_id": trace_id}, telemetry_get_trace_params.TelemetryGetTraceParams
+                ),
+            ),
+            cast_to=Trace,
+        )
+
+    async def log_event(
+        self,
+        *,
+        event: telemetry_log_event_params.Event,
+        x_llama_stack_provider_data: str | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> None:
+        """
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        extra_headers = {"Accept": "*/*", **(extra_headers or {})}
+        extra_headers = {
+            **strip_not_given({"X-LlamaStack-ProviderData": x_llama_stack_provider_data}),
+            **(extra_headers or {}),
+        }
+        return await self._post(
+            "/alpha/telemetry/log-event",
+            body=await async_maybe_transform({"event": event}, telemetry_log_event_params.TelemetryLogEventParams),
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=NoneType,
+        )
+
+
+class TelemetryResourceWithRawResponse:
+    def __init__(self, telemetry: TelemetryResource) -> None:
+        self._telemetry = telemetry
+
+        self.get_trace = to_raw_response_wrapper(
+            telemetry.get_trace,
+        )
+        self.log_event = to_raw_response_wrapper(
+            telemetry.log_event,
+        )
+
+
+class AsyncTelemetryResourceWithRawResponse:
+    def __init__(self, telemetry: AsyncTelemetryResource) -> None:
+        self._telemetry = telemetry
+
+        self.get_trace = async_to_raw_response_wrapper(
+            telemetry.get_trace,
+        )
+        self.log_event = async_to_raw_response_wrapper(
+            telemetry.log_event,
+        )
+
+
+class TelemetryResourceWithStreamingResponse:
+    def __init__(self, telemetry: TelemetryResource) -> None:
+        self._telemetry = telemetry
+
+        self.get_trace = to_streamed_response_wrapper(
+            telemetry.get_trace,
+        )
+        self.log_event = to_streamed_response_wrapper(
+            telemetry.log_event,
+        )
+
+
+class AsyncTelemetryResourceWithStreamingResponse:
+    def __init__(self, telemetry: AsyncTelemetryResource) -> None:
+        self._telemetry = telemetry
+
+        self.get_trace = async_to_streamed_response_wrapper(
+            telemetry.get_trace,
+        )
+        self.log_event = async_to_streamed_response_wrapper(
+            telemetry.log_event,
+        )
