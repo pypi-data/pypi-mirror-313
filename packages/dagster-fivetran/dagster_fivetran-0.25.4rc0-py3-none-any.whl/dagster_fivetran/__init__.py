4f85dd224e47ea4ee46210cb91bf3ed397766cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 09:13:43 +0000

    Add package dagster-fivetran/0.25.4rc0/dagster_fivetran-0.25.4rc0-py3-none-any.whl

---
+++ packages/dagster-fivetran/dagster_fivetran-0.25.4rc0-py3-none-any.whl/dagster_fivetran/__init__.py
@@ -0,0 +1,32 @@
+from dagster._core.libraries import DagsterLibraryRegistry
+
+from dagster_fivetran.asset_defs import (
+    build_fivetran_assets as build_fivetran_assets,
+    load_assets_from_fivetran_instance as load_assets_from_fivetran_instance,
+)
+from dagster_fivetran.ops import (
+    fivetran_resync_op as fivetran_resync_op,
+    fivetran_sync_op as fivetran_sync_op,
+)
+from dagster_fivetran.resources import (
+    FivetranResource as FivetranResource,
+    FivetranWorkspace as FivetranWorkspace,
+    fivetran_resource as fivetran_resource,
+    load_fivetran_asset_specs as load_fivetran_asset_specs,
+)
+from dagster_fivetran.translator import DagsterFivetranTranslator as DagsterFivetranTranslator
+from dagster_fivetran.types import FivetranOutput as FivetranOutput
+from dagster_fivetran.version import __version__ as __version__
+
+try:
+    from dagster_fivetran.managed import (
+        FivetranConnector as FivetranConnector,
+        FivetranDestination as FivetranDestination,
+        FivetranManagedElementReconciler as FivetranManagedElementReconciler,
+    )
+
+except ImportError:
+    pass
+
+
+DagsterLibraryRegistry.register("dagster-fivetran", __version__)
