769530229312a522d8bd8c74644b2eff90fb94be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 07:05:30 +0000

    Add package bioimageio.spec/0.5.3.6/bioimageio.spec-0.5.3.6-py3-none-any.whl

---
+++ packages/bioimageio.spec/bioimageio.spec-0.5.3.6-py3-none-any.whl/bioimageio/spec/_internal/root_url.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+from typing import Any, ClassVar, Optional, Type
+from urllib.parse import urlsplit, urlunsplit
+
+import pydantic
+from pydantic import RootModel
+
+from .validated_string import ValidatedString
+
+
+class RootHttpUrl(ValidatedString):
+    """A 'URL folder', possibly an invalid HTTP URL"""
+
+    root_model: ClassVar[Type[RootModel[Any]]] = RootModel[pydantic.HttpUrl]
+    _validated: pydantic.HttpUrl
+
+    def absolute(self):
+        """analog to `absolute` method of pathlib."""
+        return self
+
+    @property
+    def scheme(self) -> str:
+        return self._validated.scheme
+
+    @property
+    def host(self) -> Optional[str]:
+        return self._validated.host
+
+    @property
+    def path(self) -> Optional[str]:
+        return self._validated.path
+
+    @property
+    def parent(self) -> RootHttpUrl:
+        parsed = urlsplit(str(self))
+        path = list(parsed.path.split("/"))
+        if (
+            parsed.netloc == "zenodo.org"
+            and parsed.path.startswith("/api/records/")
+            and parsed.path.endswith("/content")
+        ):
+            path[-2:-1] = []
+        else:
+            path = path[:-1]
+
+        return RootHttpUrl(
+            urlunsplit(
+                (
+                    parsed.scheme,
+                    parsed.netloc,
+                    "/".join(path),
+                    parsed.query,
+                    parsed.fragment,
+                )
+            )
+        )
