785ed235f80794f4a113792130c8099ec9fb6094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 09:13:59 +0000

    Add package epx/1.0.0/epx-1.0.0-py3-none-any.whl

---
+++ packages/epx/epx-1.0.0-py3-none-any.whl/epx/__init__.py
@@ -0,0 +1,13 @@
+# Don't touch! This is a keystring substituted in GitHub workflows!
+__version__ = "1.0.0"
+###################################################################
+
+from epx.job import Job, ModelConfig, ModelConfigSweep, JobResults
+from epx.run import Run, RunParameters
+from epx.synthpop import SynthPop
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+from logging import NullHandler
+
+logging.getLogger(__name__).addHandler(NullHandler())
---
+++ packages/epx/epx-1.0.0-py3-none-any.whl/epx/run/__init__.py
@@ -0,0 +1,2 @@
+from epx.run.exec import RunParameters
+from epx.run.run import Run
