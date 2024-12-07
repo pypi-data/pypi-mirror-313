d07a3fe7f9890d4a2539ad651be31db6088d5b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:13 +0000

    Add package PyTorchLabFlow/0.2/pytorchlabflow-0.2.tar.gz

---
+++ packages/PyTorchLabFlow/pytorchlabflow-0.2.tar.gz/pytorchlabflow-0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyTorchLabFlow',
+    version='0.2',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    include_package_data=True,
+    install_requires=[
+        'torch',
+        'matplotlib',
+        'pandas',
+        'tqdm'
+    ],
+    author='BBEK-Anand',
+    author_email='',
+    description='PyTorchLabFlow is a lightweight framework that simplifies PyTorch experiment management, reducing setup time with reusable components for training, logging, and checkpointing. It streamlines workflows, making it ideal for fast and efficient model development.',
+    long_description=open('./README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/BBEK-Anand/PyTorchLabFlow',
+    license="MIT",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
