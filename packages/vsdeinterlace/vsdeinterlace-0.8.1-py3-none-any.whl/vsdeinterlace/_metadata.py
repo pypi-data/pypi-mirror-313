24cb23573849dc7349024a49155cbd6e98ce1404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:38 +0000

    Add package vsdeinterlace/0.8.1/vsdeinterlace-0.8.1-py3-none-any.whl

---
+++ packages/vsdeinterlace/vsdeinterlace-0.8.1-py3-none-any.whl/vsdeinterlace/_metadata.py
@@ -0,0 +1,12 @@
+"""VapourSynth deinterlacing and interlaced/telecined content helper functions"""
+
+__version__ = '0.8.1'
+
+__author_name__, __author_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'
+__maintainer_name__, __maintainer_email__ = __author_name__, __author_email__
+
+__author__ = f'{__author_name__} <{__author_email__}>'
+__maintainer__ = __author__
+
+if __name__ == '__github__':
+    print(__version__)
