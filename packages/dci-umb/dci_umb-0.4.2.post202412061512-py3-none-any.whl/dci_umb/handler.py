b253ef242d28ed3922079066e67cab6cfbb01dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 08:06:07 +0000

    Add package dci-umb/0.4.2.post202412061512/dci_umb-0.4.2.post202412061512-py3-none-any.whl

---
+++ packages/dci-umb/dci_umb-0.4.2.post202412061512-py3-none-any.whl/dci_umb/handler.py
@@ -0,0 +1,44 @@
+import json
+import logging
+import requests
+
+
+logger = logging.getLogger(__name__)
+
+
+class Handler(object):
+    @staticmethod
+    def is_interested_in(event):
+        raise NotImplementedError
+
+    def handle_event(self, event):
+        raise NotImplementedError
+
+
+class HTTPBouncerMessageHandler(Handler):
+    def __init__(self, destination):
+        self.destination = destination
+
+    @staticmethod
+    def is_interested_in(event):
+        return True
+
+    def handle_event(self, event):
+        message_id = event.message.properties["message-id"]
+        try:
+            r = requests.post(
+                self.destination,
+                json={
+                    "headers": event.message.properties,
+                    "msg": json.loads(event.message.body),
+                },
+                timeout=(10, 50),
+            )
+            logger.info(
+                f"Sent {message_id} to {self.destination} - Result: {r.status_code}"
+            )
+        except (ValueError, TypeError):
+            logger.error(
+                "Can't json load event id %s - message body (%s): %s"
+                % (message_id, type(event.message.body), event.message.body)
+            )
