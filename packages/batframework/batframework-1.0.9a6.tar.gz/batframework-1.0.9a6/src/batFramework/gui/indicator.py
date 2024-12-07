3632cdd8bc42bbdc721170f68d6e971659947e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:40 +0000

    Add package batframework/1.0.9a6/batframework-1.0.9a6.tar.gz

---
+++ packages/batframework/batframework-1.0.9a6.tar.gz/batframework-1.0.9a6/src/batFramework/gui/indicator.py
@@ -0,0 +1,58 @@
+from .shape import Shape
+from typing import Any, Self, Callable
+import pygame
+from .widget import Widget
+from .interactiveWidget import InteractiveWidget
+from .draggableWidget import DraggableWidget
+import batFramework as bf
+
+
+class Indicator(Shape):
+    def __init__(self, size: tuple[int | float] = (10, 10)) -> None:
+        super().__init__(size)
+        self.debug_color = "magenta"
+        self.set_outline_width(1)
+        self.set_outline_color("black")
+
+    def to_string_id(self) -> str:
+        return "Indicator"
+
+    def set_value(self, value: Any) -> None:
+        pass
+
+    def get_value(self) -> Any:
+        pass
+
+    def top_at(self, x, y):
+        return None
+
+
+class ToggleIndicator(Indicator):
+    def __init__(self, default_value: bool) -> None:
+        super().__init__((20, 20))
+        self.value: bool = default_value
+        self.callback = lambda val: self.set_color("green" if val else "red")
+        self.set_value(default_value)
+        self.callback(default_value)
+        # TODO aspect ratio would be good right about here
+        # self.add_constraint(ConstraintAspectRatio(1))
+
+    def set_callback(self, callback : Callable[[bool],Any]) -> Self:
+        self.callback = callback
+        return self
+
+    def set_value(self, value: bool) -> None:
+        self.value = value
+        if self.callback:
+            self.callback(value)
+        self.dirty_surface = True
+
+    def get_value(self) -> bool:
+        return self.value
+
+    def top_at(self, x: float, y: float) -> "None|Widget":
+        r = super().top_at(x, y)
+        if r is self:
+            return None
+        return r
+
---
+++ packages/batframework/batframework-1.0.9a6.tar.gz/batframework-1.0.9a6/src/batFramework/gui/meter.py
@@ -0,0 +1,74 @@
+import batFramework as bf
+from .shape import Shape
+from typing import Self
+
+
+def custom_top_at(self, x, y):
+    if Shape.top_at(self, x, y) == self:
+        return self.parent
+    return None
+
+
+class Meter(Shape):
+    def __init__(self, min_value: float = 0, max_value: float = 1, step: float = 0.1):
+        super().__init__()
+        self.min_value, self.max_value = min_value, max_value
+        self.step = step
+        self.snap: bool = False
+        self.value = self.max_value
+        self.content = Shape((0, 0)).set_color(bf.color.BLUE)
+        self.content.set_debug_color("cyan")
+        self.content.top_at = lambda x, y: custom_top_at(self.content, x, y)
+        self.add(self.content)
+        self.set_padding(4)
+        self.set_color("gray20")
+        self.set_outline_width(1)
+        self.set_outline_color(bf.color.BLACK)
+        self.set_debug_color("pink")
+
+    def __str__(self) -> str:
+        return "Meter"
+
+    def set_step(self, step: float) -> Self:
+        self.step = step
+        self.set_value(self.value)
+        return self
+
+    def set_range(self, range_min: float, range_max: float) -> Self:
+        if range_min >= range_max:
+            print(
+                f"[Warning] : minimum value {range_min} is greater than or equal to maximum value {range_max}"
+            )
+            return self
+        self.min_value = range_min
+        self.max_value = range_max
+        self.dirty_shape = True
+
+    def get_debug_outlines(self):
+        yield from super().get_debug_outlines()
+        # yield from self.content.get_debug_outlines()
+
+    def set_value(self, value: float) -> Self:
+        value = max(self.min_value, min(self.max_value, value))
+        value = round(value / self.step) * self.step
+        self.value = round(value,10)
+        self.dirty_shape = True
+        return self
+
+    def get_value(self) -> float:
+        return self.value
+
+    def get_range(self) -> float:
+        return self.max_value - self.min_value
+
+    def get_ratio(self) -> float:
+        return (self.value-self.min_value) / (self.max_value - self.min_value)
+
+    def _build_content(self) -> None:
+        width = self.get_padded_width() * self.get_ratio()
+        self.content.set_size((width, self.get_padded_height()))
+        self.content.rect.topleft = self.get_padded_rect().topleft
+
+    def build(self) -> None:
+        super().build()
+        self._build_content()
