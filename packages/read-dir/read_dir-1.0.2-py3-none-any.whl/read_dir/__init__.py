9f20f2bd463f705eebafc73656e71fc90cfa9b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:08 +0000

    Add package read-dir/1.0.2/read_dir-1.0.2-py3-none-any.whl

---
+++ packages/read-dir/read_dir-1.0.2-py3-none-any.whl/read_dir/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/12/6 9:47 上午
+# @Author  : dubaichuan
+# @File    :__init__.py.py
+# @Software: PyCharm
+
+from . import read_dir
---
+++ packages/read-dir/read_dir-1.0.2-py3-none-any.whl/read_dir/read_dir.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/12/5 4:41 下午
+# @Author  : dubaichuan
+# @File    :read_dir.py
+# @Software: PyCharm
+
+import os
+import pandas as pd
+
+
+class read_dir(object):
+    def __int__(self, a):
+        self.a = a
+
+    def to_pd(self):
+        text = []
+        label = []
+        for j in os.listdir(f'text classification/{self.a}')[1:]:
+            for i in os.listdir(f'text classification/{self.a}/{j}'):
+                try:
+                    with open(f'text classification/{self.a}/{j}/{i}', 'r', encoding='gbk') as f:
+                        text.append(f.read())
+                        label.append(j)
+                except:
+                    continue
+        return pd.DataFrame({'text': text, 'label': label})
