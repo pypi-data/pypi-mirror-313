945586924cbfa4c0ec60185a893246f9a765c410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 06:05:26 +0000

    Add package agi-med-protos/1.1.0/agi_med_protos-1.1.0.tar.gz

---
+++ packages/agi-med-protos/agi_med_protos-1.1.0.tar.gz/agi_med_protos-1.1.0/src/agi_med_protos/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "1.1.0"
+
+from .abstract_client import AbstractClient
+from .basic_client import BasicClient
+from .header_client import HeaderClient
---
+++ packages/agi-med-protos/agi_med_protos-1.1.0.tar.gz/agi_med_protos-1.1.0/src/agi_med_protos/dto/__init__.py
@@ -0,0 +1,3 @@
+from .base_request import BaseRequest
+from .headers import Headers
+from .request_with_headers import RequestWithHeaders
