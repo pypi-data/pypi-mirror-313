be55edd5a2ca9ab54a878ff88b79a587f3c462ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 07:05:11 +0000

    Add package rigour/0.7.4/rigour-0.7.4-py2.py3-none-any.whl

---
+++ packages/rigour/rigour-0.7.4-py2.py3-none-any.whl/rigour/names/__init__.py
@@ -0,0 +1,11 @@
+"""
+Name handling utilities for person and organisation names.
+"""
+
+from rigour.names.pick import pick_name
+from rigour.names.part import name_parts
+
+__all__ = [
+    "pick_name",
+    "name_parts",
+]
