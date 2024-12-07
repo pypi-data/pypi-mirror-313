c715c11cd2a8d6e91ad10bd4572f783080f80fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 09:13:14 +0000

    Add package scipy-events/0.1.2/scipy_events-0.1.2-py3-none-any.whl

---
+++ packages/scipy-events/scipy_events-0.1.2-py3-none-any.whl/scipy_events/progress.py
@@ -0,0 +1,30 @@
+from typing import Protocol
+
+from .core import Event
+
+
+class ProgressBar(Protocol):
+    desc: str
+    n: int | float
+    total: int | float | None
+
+    def update(self, n: float = 1, /) -> bool | None: ...
+
+
+class Progress(Event):
+    def __init__(self, pbar: ProgressBar, /):
+        self.pbar = pbar
+
+    @classmethod
+    def from_tqdm(cls, *, total_time: float | None = None):
+        from tqdm.auto import tqdm
+
+        return cls(tqdm(total=total_time))
+
+    def __call__(self, t: float, y):
+        if self.pbar.total is None:
+            self.pbar.desc = f"{t:=.3f}"
+            self.pbar.update()
+        else:
+            self.pbar.update(t - self.pbar.n)
+        return 1
