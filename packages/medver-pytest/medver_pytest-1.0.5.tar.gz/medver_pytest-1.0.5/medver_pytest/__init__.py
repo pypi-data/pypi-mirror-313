9fa7051a724e450bb04639d83138f6975f30a6bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:39 +0000

    Add package medver-pytest/1.0.5/medver_pytest-1.0.5.tar.gz

---
+++ packages/medver-pytest/medver_pytest-1.0.5.tar.gz/medver_pytest-1.0.5/medver_pytest/__init__.py
@@ -0,0 +1,3 @@
+from .lib.harness import PytestHarness
+
+pth = PytestHarness()
---
+++ packages/medver-pytest/medver_pytest-1.0.5.tar.gz/medver_pytest-1.0.5/medver_pytest/lib/build_info.py
@@ -0,0 +1,10 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.10.15'
+    os_name = 'posix:ubuntu'
+    version = '1.0.5'
+    git_sha = '80fa8dcd6c2f7acc77c515f2b74affa646b26e03'
+    git_branch = 'master'
+    git_uncommitted = [
+    ]
+    git_unpushed = [
+    ]
