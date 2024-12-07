35da6c6407b34129a27f472b4ba977be08deb9b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:21 +0000

    Add package AnimeScraper/1.0.6/animescraper-1.0.6.tar.gz

---
+++ packages/AnimeScraper/animescraper-1.0.6.tar.gz/animescraper-1.0.6/AnimeScraper/__init__.py
@@ -0,0 +1,11 @@
+from .Scraper import KunYu
+from .SyncScraper import SyncKunYu
+
+__all__ = ["KunYu", "SyncKunYu"]
+
+# Package metadata
+__version__ = "1.0.6"
+__author__ = "Muhammad MuQiT"
+__email__ = "antasata678@gmail.com"
+__license__ = "GPL-V3.0"
+
