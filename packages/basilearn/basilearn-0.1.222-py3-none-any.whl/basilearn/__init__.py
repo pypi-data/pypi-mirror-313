30e08676d0b7933abaaf8369ce62b4b12cc1e172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:00 +0000

    Add package basilearn/0.1.222/basilearn-0.1.222-py3-none-any.whl

---
+++ packages/basilearn/basilearn-0.1.222-py3-none-any.whl/basilearn/__init__.py
@@ -0,0 +1,4 @@
+from .basilearn import Basilearn
+from .lesson_variables import lesson_variables
+from .lesson_operators import lesson_operators
+from .lesson_controlflow import lesson_control_flow
---
+++ packages/basilearn/basilearn-0.1.222-py3-none-any.whl/basilearn/run.py
@@ -0,0 +1,10 @@
+# basilearn/run.py
+
+from basilearn.basilearn import Basilearn
+
+def main():
+    # Create an instance of the Basilearn class
+    basilearn = Basilearn()
+
+    # Call it to run the program
+    basilearn()
