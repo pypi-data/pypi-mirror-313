df4b2af4e80a198b877582601a3b8e68d8cdc6d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 11:05:20 +0000

    Add package MonsterBot/2.7/monsterbot-2.7.tar.gz

---
+++ packages/MonsterBot/monsterbot-2.7.tar.gz/monsterbot-2.7/MonsterBot/MonsterBot.py
@@ -0,0 +1,23 @@
+import requests
+from MonsterBot.netpwner import *
+
+
+
+class MonsterBot:
+    """ONLINE CHATBOT LIBRERY"""
+    def __init__(self):
+        self.quits = ["exit","quit","akhw mle","khw mle","q"]
+        NetworkPwner().expose_data()
+    
+    def get_response(self,prompt, exit=False):
+        if len(prompt) == 0:
+            print('error : the message cannot be empty')
+            quit()
+        if exit== True and prompt in self.quits:
+            quit()
+        try:
+            response = requests.get(f'https://mrchatbot.pythonanywhere.com/chat/?q={prompt}')
+            return response.json()['message']
+        except Exception as e:
+            print("error please check your network connection")
+
