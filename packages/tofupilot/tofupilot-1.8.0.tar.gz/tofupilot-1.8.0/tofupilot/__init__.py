c51aded4388847c1568ba619b8a6e3b7832bed6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 09:13:57 +0000

    Add package tofupilot/1.8.0/tofupilot-1.8.0.tar.gz

---
+++ packages/tofupilot/tofupilot-1.8.0.tar.gz/tofupilot-1.8.0/tofupilot/__init__.py
@@ -0,0 +1,9 @@
+"""
+This module initializes the TofuPilot package.
+
+It imports the TofuPilotClient class which provides
+the main interface for interacting with the TofuPilot API.
+"""
+
+from .client import TofuPilotClient
+from .plugin import numeric_step, string_step, conf
---
+++ packages/tofupilot/tofupilot-1.8.0.tar.gz/tofupilot-1.8.0/tofupilot/models/models.py
@@ -0,0 +1,25 @@
+from typing import Optional, Union, TypedDict
+from datetime import datetime, timedelta
+
+
+class Step(TypedDict):
+    name: str
+    started_at: datetime
+    duration: timedelta
+    step_passed: bool
+    measurement_unit: Optional[str]
+    measurement_value: Optional[Union[float, str]]
+    limit_low: Optional[float]
+    limit_high: Optional[float]
+
+
+class SubUnit(TypedDict):
+    serial_number: str
+
+
+class UnitUnderTest(TypedDict):
+    serial_number: str
+    part_number: Optional[str]
+    part_name: Optional[str]
+    revision: Optional[str]
+    batch_number: Optional[str]
