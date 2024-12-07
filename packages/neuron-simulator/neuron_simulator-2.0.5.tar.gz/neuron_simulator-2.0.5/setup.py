94fba05682742425203c0c9c85992afac8ba3a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:37:23 +0000

    Add package neuron-simulator/2.0.5/neuron_simulator-2.0.5.tar.gz

---
+++ packages/neuron-simulator/neuron_simulator-2.0.5.tar.gz/neuron_simulator-2.0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='neuron_simulator',
+   version='2.0.5',
+   description='a visualization tool for neuron spiking graph',
+   author='Sophia Shan',
+   author_email='shanyufei2001@gmail.com',
+   python_requires= ">3.11.6",
+   packages= find_packages(['neuron', 'pygame-ce', 'pygame_gui', 'pygame_chart']), 
+   dependencies = [
+    "neuron == 8.2.6"
+  ]
+   
+     #same as name
+   # install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
+)
