ea429f59ede7b0209e5fcd2d7e890af24938deee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 08:06:44 +0000

    Add package pymolviz/1.3/pymolviz-1.3.tar.gz

---
+++ packages/pymolviz/pymolviz-1.3.tar.gz/pymolviz-1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+setup(name='pymolviz',
+        version='1.3',
+        description='Library to facilitate creation of PyMOL Vizualizations.',
+        url='https://github.com/Finnem/PyMolViz',
+        author='Finn Mier',
+        license='MIT',
+        packages=['pymolviz.meshes.derived', 'pymolviz.meshes', 'pymolviz.methods', 'pymolviz.PyMOLobjects', 'pymolviz.util', 'pymolviz.volumetric', 'pymolviz'],
+        install_requires=[
+                'numpy',
+                'matplotlib',
+                'scipy',
+                'pandas'
+        ],
+        long_description='See https://github.com/Finnem/PyMolViz for a detailed documentation.',
+        zip_safe=False)
+
