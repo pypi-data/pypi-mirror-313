f89a328f41475e3345ff92b1154a834748b7646b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:23 +0000

    Add package fasr/0.1.8/fasr-0.1.8-py3-none-any.whl

---
+++ packages/fasr/fasr-0.1.8-py3-none-any.whl/fasr/utils/prepare_model.py
@@ -0,0 +1,32 @@
+def prepare_offline_models(cache_dir: str = "checkpoints"):
+    """Prepare offline models for building pipeline"""
+    from modelscope import snapshot_download
+
+    models = [
+        "iic/punc_ct-transformer_zh-cn-common-vocab272727-pytorch",
+        "iic/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch",
+        "iic/SenseVoiceSmall",
+    ]
+    for model in models:
+        snapshot_download(model_id=model, cache_dir=cache_dir)
+
+
+def prepare_online_models(cache_dir: str = "checkpoints"):
+    """
+    Prepare online models for building pipeline
+    """
+    from modelscope import snapshot_download
+
+    models = [
+        "iic/SenseVoiceSmall",
+        "iic/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online",
+    ]
+    for model in models:
+        snapshot_download(model_id=model, cache_dir=cache_dir)
+
+
+def download(model: str, revision: str = None, cache_dir: str = "checkpoints"):
+    """Download model from modelscope"""
+    from modelscope import snapshot_download
+
+    model = snapshot_download(model, cache_dir=cache_dir, revision=revision)
