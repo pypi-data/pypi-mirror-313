d17a84fd1864a39d161087555ebe5612995e055c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 07:05:20 +0000

    Add package imxIcons/0.0.17/imxicons-0.0.17-py3-none-any.whl

---
+++ packages/imxIcons/imxicons-0.0.17-py3-none-any.whl/imxIcons/domain/signal/reflector_signal_v500.py
@@ -0,0 +1,28 @@
+from imxIcons.domain.supportedImxVersions import ImxVersionEnum
+from imxIcons.iconEntity import IconEntity, IconSvgGroup
+
+entities_path = "Signal.ReflectorPost"
+imx_version = ImxVersionEnum.v500
+
+
+reflector_posts = [
+    IconEntity(
+        imx_version=imx_version,
+        imx_path=entities_path,
+        icon_name="ReflectorPostStraight",
+        properties={"reflectorType": "straight"},
+        icon_groups=[
+            IconSvgGroup("signal-reflector-straight"),
+        ],
+    ),
+    IconEntity(
+        imx_version=imx_version,
+        imx_path=entities_path,
+        icon_name="IlluminatedSignNotIntegrated",
+        properties={"reflectorType": "diagonal"},
+        icon_groups=[
+            IconSvgGroup("signal-reflector-diagonal"),
+        ],
+    ),
+]
+reflector_post_icon_entities_v500 = reflector_posts
