953d9add0ce38b7eb8b2c468836a418166c5a4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 08:06:09 +0000

    Add package ovos-utils/0.6.0/ovos_utils-0.6.0-py3-none-any.whl

---
+++ packages/ovos-utils/ovos_utils-0.6.0-py3-none-any.whl/ovos_utils/messagebus.py
@@ -0,0 +1,6 @@
+from ovos_utils.fakebus import dig_for_message, Message, FakeMessage, FakeBus
+from ovos_utils.log import log_deprecation
+
+
+log_deprecation("ovos_utils.messagebus has been deprecated since version 0.1.0!! "
+                "please import from ovos_utils.fakebus or ovos_bus_client directly", "1.0.0")
