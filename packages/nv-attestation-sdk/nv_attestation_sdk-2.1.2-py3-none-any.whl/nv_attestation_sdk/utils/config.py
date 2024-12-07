b3a6e6cdfa06cf48e8c918da4524f9376b494562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:38:13 +0000

    Add package nv-attestation-sdk/2.1.2/nv_attestation_sdk-2.1.2-py3-none-any.whl

---
+++ packages/nv-attestation-sdk/nv_attestation_sdk-2.1.2-py3-none-any.whl/nv_attestation_sdk/utils/config.py
@@ -0,0 +1,16 @@
+#    Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+
+"""Config file for maintaining the dependant URLs for services and constants"""
+import os
+
+RIM_SERVICE_URL = os.getenv("NV_RIM_URL", "https://rim.attestation.nvidia.com/v1/rim/")
+ALLOW_HOLD_CERT = os.getenv("NV_ALLOW_HOLD_CERT") == "true"
+OCSP_SERVICE_URL = os.getenv("NV_OCSP_URL", "https://ocsp.ndis.nvidia.com/")
+REMOTE_GPU_VERIFIER_SERVICE_URL = os.getenv(
+    "NV_NRAS_GPU_URL", "https://nras.attestation.nvidia.com/v3/attest/gpu"
+)
+REMOTE_NVSWITCH_VERIFIER_SERVICE_URL = os.getenv(
+    "NV_NRAS_NVSWITCH_URL", "https://nras.attestation.nvidia.com/v3/attest/switch"
+)
+# Planned to move the below to a list of acceptable GPU architectures
+GPU_ARCH = "HOPPER"
