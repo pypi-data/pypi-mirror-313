d3a64539cc32eba4a35ccb88f6d5420f3a7bebc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 08:07:03 +0000

    Add package neo4j-viz/0.1.1/neo4j_viz-0.1.1.tar.gz

---
+++ packages/neo4j-viz/neo4j_viz-0.1.1.tar.gz/neo4j_viz-0.1.1/README.md
@@ -0,0 +1,32 @@
+# Graph Visualization for Python by Neo4j
+
+[![Latest version](https://img.shields.io/pypi/v/neo4j-viz)](https://pypi.org/project/neo4j-viz/)
+[![PyPI downloads month](https://img.shields.io/pypi/dm/neo4j-viz)](https://pypi.org/project/neo4j-viz/)
+![Python versions](https://img.shields.io/pypi/pyversions/neo4j-viz)
+[![License](https://img.shields.io/pypi/l/neo4j-viz)](LICENSE)
+
+`neo4j-viz` is a Python package for visualizing graphs from the Neo4j ecosystem, such as the [Neo4j Graph Data Science (GDS) library](https://github.com/neo4j/graph-data-science).
+The package wraps the [Neo4j Visualization library (NVL)](https://neo4j.com/docs/nvl/current/).
+
+
+## Supported features
+
+* Visualize graphs from Neo4j GDS and Pandas Dataframes
+* Nodes:
+    * Sizing
+    * Colors
+    * Caption
+* Relationships
+    * Colors
+    * Caption
+
+![Example Graph](examples/example_cora_graph.png)
+
+
+## Getting started
+
+```
+pip install neo4j-viz
+```
+
+For some Jupyter Notebook examples checkout the `/examples` directory
---
+++ packages/neo4j-viz/neo4j_viz-0.1.1.tar.gz/neo4j_viz-0.1.1/tests/test_render.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+from selenium import webdriver
+
+from neo4j_viz import Node, Relationship, VisualizationGraph
+
+
+def test_basic_render(tmp_path: Path) -> None:
+    nodes = [
+        Node(id="4:d09f48a4-5fca-421d-921d-a30a896c604d:0", caption="Person"),
+        Node(id="4:d09f48a4-5fca-421d-921d-a30a896c604d:6", caption="Product"),
+        Node(id="4:d09f48a4-5fca-421d-921d-a30a896c604d:11", caption="Product"),
+        Node(id="4:d09f48a4-5fca-421d-921d-a30a896c604d:12", caption="Product"),
+        Node(id="4:d09f48a4-5fca-421d-921d-a30a896c604d:1", caption="Person"),
+        Node(id="4:d09f48a4-5fca-421d-921d-a30a896c604d:7", caption="Product"),
+        Node(id="4:d09f48a4-5fca-421d-921d-a30a896c604d:8", caption="Product"),
+    ]
+    relationships = [
+        Relationship(
+            source="4:d09f48a4-5fca-421d-921d-a30a896c604d:0",
+            target="4:d09f48a4-5fca-421d-921d-a30a896c604d:6",
+            caption="BUYS",
+        ),
+        Relationship(
+            source="4:d09f48a4-5fca-421d-921d-a30a896c604d:0",
+            target="4:d09f48a4-5fca-421d-921d-a30a896c604d:11",
+            caption="BUYS",
+        ),
+    ]
+
+    VG = VisualizationGraph(nodes=nodes, relationships=relationships)
+
+    html = VG.render()
+
+    file_path = tmp_path / "basic_render.html"
+
+    with open(file_path, "w+") as the_file:
+        the_file.write(html.data)
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")  # avoid browser window popping up
+    driver = webdriver.Chrome(options=chrome_options)
+    # wait for page to render
+    driver.implicitly_wait(3)
+
+    driver.get(f"file://{file_path}")
+
+    logs = driver.get_log("browser")  # type: ignore[no-untyped-call]
+
+    severe_logs = [log for log in logs if log["level"] == "SEVERE"]
+
+    assert not severe_logs, f"Severe logs found: {severe_logs}"
