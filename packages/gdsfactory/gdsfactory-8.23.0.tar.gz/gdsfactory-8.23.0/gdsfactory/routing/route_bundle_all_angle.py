c63cdd032c456ea6ed463ef272d0d2e46c4b7477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:09:41 +0000

    Add package gdsfactory/8.23.0/gdsfactory-8.23.0.tar.gz

---
+++ packages/gdsfactory/gdsfactory-8.23.0.tar.gz/gdsfactory-8.23.0/gdsfactory/routing/route_bundle_all_angle.py
@@ -0,0 +1,108 @@
+from collections.abc import Sequence
+from functools import partial
+
+import kfactory as kf
+from kfactory.routing.aa.optical import (
+    BendFactory,
+    OpticalAllAngleRoute,
+    StraightFactory,
+    route_bundle,
+)
+
+from gdsfactory.components.bend_euler import bend_euler_all_angle
+from gdsfactory.components.straight import straight_all_angle
+from gdsfactory.port import Port
+from gdsfactory.typings import ComponentSpec, CrossSectionSpec
+
+
+def route_bundle_all_angle(
+    component: ComponentSpec,
+    ports1: list[Port],
+    ports2: list[Port],
+    backbone: Sequence[tuple[float, float]] | None = None,
+    separation: list[float] | float = 3.0,
+    straight: StraightFactory = straight_all_angle,
+    bend: BendFactory = partial(bend_euler_all_angle, radius=5),
+    bend_ports: tuple[str, str] = ("o1", "o2"),
+    straight_ports: tuple[str, str] = ("o1", "o2"),
+    cross_section: CrossSectionSpec | None = None,
+) -> list[OpticalAllAngleRoute]:
+    """Route a bundle of ports to another bundle of ports with all angles.
+
+    Args:
+        component: to add the routing.
+        ports1: list of start ports to connect.
+        ports2: list of end ports to connect.
+        backbone: list of points to connect the ports.
+        separation: list of spacings.
+        straight: function to create straights.
+        bend: function to create bends.
+        bend_ports: tuple of ports to connect the bends.
+        straight_ports: tuple of ports to connect the straights.
+        cross_section: cross_section to use. Overrides the  cross_section.
+    """
+    if cross_section:
+        straight = partial(straight, cross_section=cross_section)
+        bend = partial(bend, cross_section=cross_section)
+
+    backbone = backbone or []
+    if backbone:
+        backbone = [kf.kdb.DPoint(*p) for p in backbone]
+
+    return route_bundle(
+        c=component,
+        start_ports=ports1,
+        end_ports=ports2,
+        backbone=backbone,
+        separation=separation,
+        straight_factory=straight,
+        bend_factory=bend,
+        bend_ports=bend_ports,
+        straight_ports=straight_ports,
+    )
+
+
+if __name__ == "__main__":
+    import gdsfactory as gf
+
+    # c = gf.Component()
+    # rows = 3
+    # w1 = c << gf.c.array("straight", spacing=(0, 10), rows=rows, columns=1)
+    # w2 = c << gf.c.array("straight", spacing=(0, 10), rows=rows, columns=1)
+    # w2.drotate(-30)
+    # w2.dmovex(140)
+    # p1 = list(w1.ports.filter(orientation=0))
+    # p2 = list(w2.ports.filter(orientation=150))
+    # p1.reverse()
+    # p2.reverse()
+    # c1 = np.array(p2[0].dcenter)
+    # c2 = np.array(p1[0].dcenter)
+    # d = (np.array(p2[0].dcenter) + np.array(p1[0].dcenter)) / 2
+    # backbone = [
+    #     d - (10.0, 0),
+    #     d + (10.0, 0),
+    # ]
+    # route_bundle_all_angle(
+    #     c,
+    #     p1,
+    #     p2,
+    #     backbone=backbone,
+    #     separation=3,
+    # )
+
+    c = gf.Component()
+
+    mmi = gf.components.mmi2x2(width_mmi=10, gap_mmi=3)
+    mmi1 = c.create_vinst(mmi)  # create a virtual instance
+    mmi2 = c.create_vinst(mmi)  # create a virtual instance
+
+    mmi2.dmove((100, 10))
+    mmi2.drotate(30)
+
+    routes = gf.routing.route_bundle_all_angle(
+        c,
+        mmi1.ports.filter(orientation=0),
+        [mmi2.ports["o2"], mmi2.ports["o1"]],
+    )
+
+    c.show()
---
+++ packages/gdsfactory/gdsfactory-8.23.0.tar.gz/gdsfactory-8.23.0/gdsfactory/typings.py
@@ -0,0 +1,289 @@
+"""In programming, a factory is a function that returns an object.
+
+Functions are easy to understand because they have clear inputs and outputs.
+Most gdsfactory functions take some inputs and return a Component object.
+Some of these inputs parameters are also functions.
+
+- Component: Object with.
+    - name.
+    - references: to other components (x, y, rotation).
+    - polygons in different layers.
+    - ports dict.
+
+
+Specs:
+
+- ComponentSpec: Component, function, string or dict
+    (component=mzi, settings=dict(delta_length=20)).
+- LayerSpec: (3, 0), 3 (assumes 0 as datatype) or string.
+
+"""
+
+from __future__ import annotations
+
+import dataclasses
+import pathlib
+from collections.abc import Callable, Generator, Sequence
+from typing import Any, Literal, ParamSpec, TypeAlias, TypeVar
+
+import kfactory as kf
+import numpy as np
+import numpy.typing as npt
+from kfactory.cross_section import SymmetricalCrossSection
+from kfactory.kcell import LayerEnum
+
+from gdsfactory.component import (
+    Component,
+    ComponentAllAngle,
+    ComponentBase,
+    ComponentReference,
+)
+from gdsfactory.cross_section import (  # type: ignore[attr-defined]
+    CrossSection,
+    Transition,
+)
+from gdsfactory.technology import (
+    LayerLevel,
+    LayerMap,
+    LayerStack,
+    LayerViews,
+)
+
+STEP_DIRECTIVES = {
+    "x",
+    "y",
+    "dx",
+    "dy",
+}
+
+STEP_DIRECTIVES_ALL_ANGLE = {
+    "x",
+    "y",
+    "dx",
+    "dy",
+    "ds",
+    "exit_angle",
+    "cross_section",
+    "connector",
+    "separation",
+}
+
+
+@dataclasses.dataclass
+class Step:
+    """Manhattan Step.
+
+    Parameters:
+        x: absolute.
+        y: absolute.
+        dx: x-displacement.
+        dy: y-displacement.
+
+    """
+
+    x: float | None = None
+    y: float | None = None
+    dx: Delta | None = None
+    dy: Delta | None = None
+
+
+Anchor: TypeAlias = Literal[
+    "ce",
+    "cw",
+    "nc",
+    "ne",
+    "nw",
+    "sc",
+    "se",
+    "sw",
+    "center",
+    "cc",
+]
+Axis: TypeAlias = Literal["x", "y"]
+NSEW: TypeAlias = Literal["N", "S", "E", "W"]
+WidthTypes: TypeAlias = Literal["sine", "linear", "parabolic"]
+
+Float2: TypeAlias = tuple[float, float]
+Float3: TypeAlias = tuple[float, float, float]
+Floats: TypeAlias = Sequence[float]
+Strs: TypeAlias = Sequence[str]
+Int2: TypeAlias = tuple[int, int]
+Int3: TypeAlias = tuple[int, int, int]
+Ints: TypeAlias = tuple[int, ...] | list[int]
+
+BoundingBox: TypeAlias = tuple[float, float, float, float]
+BoundingBoxes: TypeAlias = Sequence[BoundingBox]
+Size: TypeAlias = tuple[float, float]
+Spacing: TypeAlias = tuple[float, float]
+Radius: TypeAlias = float
+
+Delta: TypeAlias = float
+AngleInDegrees: TypeAlias = float
+
+Layer: TypeAlias = tuple[int, int]
+Layers: TypeAlias = Sequence[Layer]
+LayerSpec: TypeAlias = LayerEnum | str | tuple[int, int] | int
+LayerSpecs: TypeAlias = Sequence[LayerSpec]
+
+AnyComponent: TypeAlias = Component | ComponentAllAngle
+AnyComponentT = TypeVar("AnyComponentT", bound=AnyComponent)
+AnyComponentFactory: TypeAlias = Callable[..., AnyComponent]
+AnyComponentPostProcess: TypeAlias = Callable[[AnyComponent], None]
+
+ComponentParams = ParamSpec("ComponentParams")
+ComponentFactory: TypeAlias = Callable[..., Component]
+ComponentAllAngleFactory: TypeAlias = Callable[..., ComponentAllAngle]
+ComponentBaseFactory: TypeAlias = Callable[..., ComponentBase]
+ComponentFactoryDict: TypeAlias = dict[str, ComponentFactory]
+ComponentFactories: TypeAlias = Sequence[ComponentFactory]
+
+ComponentSpec: TypeAlias = str | ComponentFactory | dict[str, Any] | kf.KCell
+ComponentSpecOrComponent: TypeAlias = ComponentSpec | Component
+ComponentSpecs: TypeAlias = Sequence[ComponentSpec]
+ComponentSpecsOrComponents: TypeAlias = Sequence[ComponentSpecOrComponent]
+
+PathType: TypeAlias = str | pathlib.Path
+PathTypes: TypeAlias = Sequence[PathType]
+Metadata: TypeAlias = dict[str, int | float | str]
+PostProcess: TypeAlias = Callable[[Component], None]
+PostProcesses: TypeAlias = Sequence[PostProcess]
+MaterialSpec: TypeAlias = (
+    str | float | tuple[float, float] | Callable[..., Any] | npt.NDArray[np.float64]
+)
+
+Instance: TypeAlias = ComponentReference
+ComponentOrPath: TypeAlias = PathType | Component
+ComponentOrReference: TypeAlias = Component | ComponentReference
+NameToFunctionDict: TypeAlias = dict[str, ComponentFactory]
+Number: TypeAlias = float | int
+Coordinate: TypeAlias = tuple[float, float]
+Coordinates: TypeAlias = Sequence[Coordinate]
+CrossSectionFactory: TypeAlias = Callable[..., CrossSection]
+CrossSectionOrFactory: TypeAlias = CrossSection | Callable[..., CrossSection]
+
+WidthFunction: TypeAlias = Callable[..., npt.NDArray[np.float64]]
+OffsetFunction: TypeAlias = Callable[..., npt.NDArray[np.float64]]
+
+Port: TypeAlias = kf.Port
+PortFactory: TypeAlias = Callable[..., Port]
+PortsFactory: TypeAlias = Callable[..., Sequence[Port]]
+PortSymmetries: TypeAlias = dict[str, Sequence[str]]
+PortsDict: TypeAlias = dict[str, Port]
+Ports: TypeAlias = kf.Ports | Sequence[Port]
+SelectPorts: TypeAlias = Callable[..., Sequence[Port]]
+
+PortType: TypeAlias = str
+PortName: TypeAlias = str
+
+PortTypes: TypeAlias = Sequence[PortType]
+PortNames: TypeAlias = Sequence[PortName]
+
+Sparameters: TypeAlias = dict[str, npt.NDArray[np.float64]]
+
+
+ComponentSpecOrList: TypeAlias = ComponentSpec | list[ComponentSpec]
+CellSpec: TypeAlias = (
+    str | ComponentFactory | dict[str, Any]  # PCell function, function name or dict
+)
+
+ComponentSpecDict: TypeAlias = dict[str, ComponentSpec]
+CrossSectionSpec: TypeAlias = (
+    CrossSectionFactory | CrossSection | SymmetricalCrossSection | dict[str, Any] | str
+)
+CrossSectionSpecs: TypeAlias = tuple[CrossSectionSpec, ...]
+
+MultiCrossSectionAngleSpec: TypeAlias = Sequence[
+    tuple[CrossSectionSpec, tuple[int, ...]]
+]
+
+
+ConductorConductorName: TypeAlias = tuple[str, str]
+ConductorViaConductorName: TypeAlias = tuple[str, str, str] | ConductorConductorName
+ConnectivitySpec: TypeAlias = ConductorConductorName | ConductorViaConductorName
+
+Route: TypeAlias = (
+    kf.routing.generic.ManhattanRoute | kf.routing.aa.optical.OpticalAllAngleRoute
+)
+RoutingStrategy: TypeAlias = Callable[..., Sequence[Route]]
+RoutingStrategies: TypeAlias = dict[str, RoutingStrategy]
+
+
+class TypedArray(np.ndarray[Any, np.dtype[Any]]):
+    """based on https://github.com/samuelcolvin/pydantic/issues/380."""
+
+    @classmethod
+    def __get_validators__(
+        cls,
+    ) -> Generator[Callable[[Any, Any], npt.NDArray[np.float64]], Any, None]:
+        yield cls.validate_type
+
+    @classmethod
+    def validate_type(cls, val: Any, _info: Any) -> npt.NDArray[np.float64]:
+        return np.array(val, dtype=cls.inner_type)  # type: ignore
+
+
+class ArrayMeta(type):
+    def __getitem__(cls, t: np.dtype[Any]) -> type[npt.NDArray[Any]]:
+        return type("Array", (TypedArray,), {"inner_type": t})
+
+
+class Array(np.ndarray[Any, np.dtype[Any]], metaclass=ArrayMeta):
+    pass
+
+
+__all__ = (
+    "AngleInDegrees",
+    "Any",
+    "AnyComponent",
+    "AnyComponentFactory",
+    "AnyComponentT",
+    "ComponentAllAngle",
+    "ComponentBase",
+    "ComponentFactory",
+    "ComponentFactoryDict",
+    "ComponentOrPath",
+    "ComponentOrReference",
+    "ComponentSpec",
+    "Coordinate",
+    "Coordinates",
+    "CrossSection",
+    "CrossSectionFactory",
+    "CrossSectionOrFactory",
+    "CrossSectionSpec",
+    "Delta",
+    "Float2",
+    "Float3",
+    "Floats",
+    "Instance",
+    "Int2",
+    "Int3",
+    "Ints",
+    "Layer",
+    "LayerLevel",
+    "LayerMap",
+    "LayerSpec",
+    "LayerSpecs",
+    "LayerStack",
+    "LayerViews",
+    "Layers",
+    "MultiCrossSectionAngleSpec",
+    "NameToFunctionDict",
+    "Number",
+    "PathType",
+    "PathTypes",
+    "Port",
+    "PortName",
+    "PortNames",
+    "PortType",
+    "PortTypes",
+    "Ports",
+    "PostProcesses",
+    "Radius",
+    "RoutingStrategies",
+    "SelectPorts",
+    "Size",
+    "Spacing",
+    "Strs",
+    "Transition",
+    "WidthTypes",
+)
