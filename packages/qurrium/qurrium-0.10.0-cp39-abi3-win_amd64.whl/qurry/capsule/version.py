923337be92afc71ce4479ad5e4aba92f1d561bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:06:00 +0000

    Add package qurrium/0.10.0/qurrium-0.10.0-cp39-abi3-win_amd64.whl

---
+++ packages/qurrium/qurrium-0.10.0-cp39-abi3-win_amd64.whl/qurry/capsule/version.py
@@ -0,0 +1,12 @@
+"""
+
+================================================================
+CapSule Version (:mod:`qurry.capsule.version`)
+================================================================
+"""
+
+version_main = (0, 9, 0)
+"""The main version of CapSule."""
+
+
+__version__ = version_main
