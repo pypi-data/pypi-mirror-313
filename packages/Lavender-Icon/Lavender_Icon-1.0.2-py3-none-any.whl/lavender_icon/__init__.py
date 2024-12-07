a60b9c7eec7980c968b5a9cb14f60dbdc73aae10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 11:05:24 +0000

    Add package Lavender-Icon/1.0.2/Lavender_Icon-1.0.2-py3-none-any.whl

---
+++ packages/Lavender-Icon/Lavender_Icon-1.0.2-py3-none-any.whl/lavender_icon/__init__.py
@@ -0,0 +1,20 @@
+from os import path
+import json
+
+
+def _load_icon_list():
+    icon_list_path = path.join(path.dirname(__file__), "src", "icons.json")
+    with open(icon_list_path, "r") as f:
+        icon_list = json.load(f)
+    return icon_list
+
+
+def lavenderIcon(ext):
+    icon_list = _load_icon_list()
+    ext = ext.upper()
+    if ext in icon_list:
+        return icon_list[ext]
+    else:
+        return icon_list["DEFAULT"]
+
+__all__ = ["lavenderIcon"]
