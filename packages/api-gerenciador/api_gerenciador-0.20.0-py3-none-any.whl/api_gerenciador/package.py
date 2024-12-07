cc9444258dee4a848784faa092b567de1c3e4b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:09:36 +0000

    Add package api-gerenciador/0.20.0/api_gerenciador-0.20.0-py3-none-any.whl

---
+++ packages/api-gerenciador/api_gerenciador-0.20.0-py3-none-any.whl/api_gerenciador/package.py
@@ -0,0 +1,135 @@
+from typing import Optional
+
+import requests
+from utils import PROJECT, TOKEN_SECRET, TOKEN_PUBLIC
+
+
+class GerenciadorApi:
+
+    def __init__(self) -> None:
+        self.__DOMAIN = self.__get_domain()
+
+    def __get_authorization(self) -> dict:
+        return {
+            'apikey': TOKEN_PUBLIC,
+            'Authorization': f'Bearer {TOKEN_SECRET}'
+        }
+
+    def __get_domain(self) -> Optional[str]:
+        URL_PROJECT = f"https://{PROJECT}.supabase.co/rest/v1/rpc/get_config?config_id=1"
+        response = requests.get(URL_PROJECT, headers=self.__get_authorization(), data={'config_id ': 1})
+        if response.status_code == 200 and len(response.json()) > 0:
+            return response.json()[0].get('domain', None)
+
+    def __get_headers(self) -> dict:
+        try:
+
+            response = requests.get(f"https://{PROJECT}.supabase.co/rest/v1/rpc/get_last_token",
+                                    headers=self.__get_authorization())
+
+            if response.status_code == 200 and len(response.json()) > 0:
+
+                token = response.json()[0].get('token', None)
+
+                if token is not None:
+                    return {'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'}
+
+        except Exception as e:
+            return {}
+
+    def __response(self, url: str, method: str, data: str | None = None) -> dict:
+        try:
+            if data is None:
+                data = {}
+
+            if method == 'GET':
+                response = requests.get(url, headers=self.__get_headers())
+
+            elif method == 'POST':
+                response = requests.post(url, headers=self.__get_headers(), data=data)
+
+            elif method == 'PUT':
+                response = requests.put(url, headers=self.__get_headers(), data=data)
+
+            else:
+                return {}
+
+            if response.status_code == 200 or response.status_code == 201:
+
+                data = response.json()
+
+                if data.get('content', None) is not None:
+                    return data.get('content')
+
+                return response.json()['data']
+            else:
+                return {}
+
+        except requests.exceptions.RequestException as e:
+            return {}
+        except Exception as e:
+            return {}
+
+    # Busca todas as filas vinculadas ao server rodando, para ativar.
+    def find_filas_by_application(self, site: str, server_process: Optional[str], container_id: Optional[str]) -> dict:
+
+        __url = f'{self.__DOMAIN}/fila?site={site}'
+
+        if server_process is not None:
+            __url += f'&server_process={server_process}'
+
+        if container_id is not None:
+            __url += f'&container_id={container_id}'
+
+        print(f'URL BUSCAR FILAS: {__url}')
+
+        return self.__response(__url, 'GET')
+
+    # Atualizar a fila, informando o status de processamento.
+    def update_fila(self, payload: str) -> dict:
+        __url = f'{self.__DOMAIN}/fila'
+        return self.__response(__url, 'PUT', payload)
+
+    # Busca todos os dados do site para processar algum dado.
+    def find_all_applications_by_application_name(self, name: str) -> dict:
+        __url = f'{self.__DOMAIN}/site?name={name}'
+        return self.__response(__url, 'GET')
+
+    # Atualiza a senha do site.
+    def update_password_site(self, id: int, password: str) -> dict:
+        __url = f'{self.__DOMAIN}/site?id={id}&password={password}'
+        return self.__response(__url, 'PUT')
+
+    # Busca a ultima sessão gerado no site.
+    def find_last_session_site(self, siteName: str) -> dict:
+        __url = f'{self.__DOMAIN}/sessao?siteName={siteName}'
+        return self.__response(__url, 'GET')
+
+    # Permite cadastrar a sessão.
+    def create_session_by_user(self, payload: str) -> dict:
+        __url = f'{self.__DOMAIN}/sessao'
+        return self.__response(__url, 'POST', payload)
+
+    # Busca todas as filas vinculadas ao server rodando.
+    def find_all_fila_playlist_by_application(self, application: str) -> dict:
+        __url = f'{self.__DOMAIN}/fila/playlist?application={application}'
+        return self.__response(__url, 'GET')
+
+    # Atualiza a fila, informando o status de processamento.
+    def update_fila_playlist(self, payload: str) -> dict:
+        __url = f'{self.__DOMAIN}/fila/playlist'
+        return self.__response(__url, 'PUT', payload)
+
+    # Atualiza o site e tipo do container.
+    def update_container(self, container_identifier: str, configuration_type: str, site_name: str,
+                         server_ip: str) -> dict:
+        __url = f'{self.__DOMAIN}/container/sync-website?container_identifier={container_identifier}&configuration_type={configuration_type}&site_name={site_name}&server_ip={server_ip}'
+        return self.__response(__url, 'POST')
+
+    # Filtra as filas para transferir macs
+    def filter_queue_transfer_mac(self, site: str) -> dict:
+        __url = f'{self.__DOMAIN}/transfer-mac/filter?site={site}&processed=false'
+        return self.__response(__url, 'GET')
+
+
+
---
+++ packages/api-gerenciador/api_gerenciador-0.20.0-py3-none-any.whl/api_gerenciador/utils.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+
+# Obtém o diretório onde o script está sendo executado
+BASE_DIR = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+# Caminho completo para o arquivo .env
+ENV_PATH = os.path.join(BASE_DIR, '.env')
+
+# Carrega as variáveis de ambiente do arquivo .env no caminho do executável
+load_dotenv(dotenv_path=ENV_PATH)
+
+PROJECT = os.getenv('PROJECT')
+TOKEN_PUBLIC = os.getenv('TOKEN_PUBLIC')
+TOKEN_SECRET = os.getenv('TOKEN_SECRET')
