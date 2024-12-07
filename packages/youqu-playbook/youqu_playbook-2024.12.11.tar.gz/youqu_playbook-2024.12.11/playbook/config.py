46ac3cf927213de91e6315a3d0d8081d8b5915d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 14:10:52 +0000

    Add package youqu-playbook/2024.12.11/youqu_playbook-2024.12.11.tar.gz

---
+++ packages/youqu-playbook/youqu_playbook-2024.12.11.tar.gz/youqu_playbook-2024.12.11/playbook/config.py
@@ -0,0 +1,14 @@
+import os
+
+
+class _Config:
+    PASSWORD = os.getenv("YOUQU_PASSWORD") or "1"
+    PYPI_MIRROR = "https://pypi.tuna.tsinghua.edu.cn/simple"
+    WORKDIR = os.path.abspath(".")
+    SPECIAL_CLIENTS = {
+        "autotest_deepin_installer": "uos@10.8.15.30",
+        "autotest_udcp": "uos@10.8.15.22",
+    }
+
+
+config = _Config()
