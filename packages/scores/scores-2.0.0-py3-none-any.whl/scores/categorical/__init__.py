17fbaad7fd6363c2bec0a6f4f0cbd9e52784782f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 15:05:51 +0000

    Add package scores/2.0.0/scores-2.0.0-py3-none-any.whl

---
+++ packages/scores/scores-2.0.0-py3-none-any.whl/scores/categorical/__init__.py
@@ -0,0 +1,25 @@
+"""
+Import the functions from the implementations into the public API
+"""
+
+from scores.categorical.binary_impl import (
+    probability_of_detection,
+    probability_of_false_detection,
+)
+from scores.categorical.contingency_impl import (
+    BasicContingencyManager,
+    BinaryContingencyManager,
+    EventOperator,
+    ThresholdEventOperator,
+)
+from scores.categorical.multicategorical_impl import firm
+
+__all__ = [
+    "probability_of_detection",
+    "probability_of_false_detection",
+    "firm",
+    "BasicContingencyManager",
+    "BinaryContingencyManager",
+    "ThresholdEventOperator",
+    "EventOperator",
+]
---
+++ packages/scores/scores-2.0.0-py3-none-any.whl/scores/typing.py
@@ -0,0 +1,24 @@
+"""
+This module contains various compound or union types which can be used across the codebase to ensure
+a consistent approach to typing is handled.
+"""
+
+from collections.abc import Hashable, Iterable
+from typing import Union
+
+import pandas as pd
+import xarray
+
+# Flexible Dimension Types should be used for preserve_dims and reduce_dims in all
+# cases across the repository
+FlexibleDimensionTypes = Iterable[Hashable]
+
+# Xarraylike data types should be used for all forecast, observed and weights
+# However currently some are specified as DataArray only
+XarrayLike = Union[xarray.DataArray, xarray.Dataset]
+
+# These type hint values *may* be used for various arguments across the
+# scores repository but are not establishing a standard or expectation beyond
+# the function they are used in
+
+FlexibleArrayType = Union[XarrayLike, pd.Series]
