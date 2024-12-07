6448e86b6069d5a1e1de2733556a392264f1ea52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 11:05:23 +0000

    Add package pydantic-graphql/1.1.0/pydantic_graphql-1.1.0.tar.gz

---
+++ packages/pydantic-graphql/pydantic_graphql-1.1.0.tar.gz/pydantic_graphql-1.1.0/README.md
@@ -0,0 +1,20 @@
+# ql
+Graphql client library, wrapped around pydantic classes for type validation,
+provide simple, safe and pythonic way to query data from a graphql api.
+
+using pydantic for creating python objects from rest api is common, it is easy and 
+it has type validation, so why not make it easy also for graphql apis?
+
+features:
+  * python objects to valid graphql string
+  * http send and recv information
+  * scalar query responses
+
+## install
+```console
+pip3 install pydantic-graphql
+```
+
+## documentation 
+[https://dsal3389.github.io/ql/](https://dsal3389.github.io/ql/)
+
---
+++ packages/pydantic-graphql/pydantic_graphql-1.1.0.tar.gz/pydantic_graphql-1.1.0/src/ql/__init__.py
@@ -0,0 +1,54 @@
+__all__ = [
+    "model",
+    "all_models",
+    "model_queryable_fields",
+    "model_mutable_fields",
+    "implements",
+    "typename",
+    "query",
+    "query_response",
+    "query_response_scalar",
+    "scalar_query_response",
+    "raw_query_response",
+    "raw_query_response_scalar",
+    "fragment_ref",
+    "arguments",
+    "on",
+    "http",
+    "metadata",
+    "QueryResponseDict",
+    "QLErrorResponseException",
+    "QLErrorDetails",
+    "_",
+]
+
+from ._http import http
+from ._model import (
+    model,
+    all_models,
+    implements,
+    model_queryable_fields,
+    model_mutable_fields,
+    typename,
+)
+from ._query import (
+    query,
+    query_response,
+    query_response_scalar,
+    scalar_query_response,
+    raw_query_response,
+    raw_query_response_scalar,
+    arguments,
+    on,
+    fragment_ref,
+)
+from ._typing import metadata, QueryResponseDict
+from ._exceptions import QLErrorResponseException, QLErrorDetails
+
+from functools import wraps
+
+
+@wraps(model_queryable_fields)
+def _(*args, **kwargs):
+    """thin wrapper around the `query_fields_nt`"""
+    return model_queryable_fields(*args, **kwargs)
