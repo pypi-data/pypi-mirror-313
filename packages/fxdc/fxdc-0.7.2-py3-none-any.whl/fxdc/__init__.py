f3c1cfd70917af63b3ac8a57bfa6e125156e7dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 09:14:15 +0000

    Add package fxdc/0.7.2/fxdc-0.7.2-py3-none-any.whl

---
+++ packages/fxdc/fxdc-0.7.2-py3-none-any.whl/fxdc/__init__.py
@@ -0,0 +1,17 @@
+from .read import load, loads
+from .parsedata import *
+from .config import Config
+from .writedata import ParseObject
+from .write import dumps, dump
+
+__all__ = [
+    'load',
+    'loads',
+    'FxDCObject',
+    'Config',
+    'Parser',
+    'ParseObject',
+    'dumps',
+    'dump'
+]
+
