2081990158a03672b79e6044eb0794cd9660d1c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 07:05:46 +0000

    Add package bookio-sourcesquirrel/0.2.0/bookio_sourcesquirrel-0.2.0-py3-none-any.whl

---
+++ packages/bookio-sourcesquirrel/bookio_sourcesquirrel-0.2.0-py3-none-any.whl/sourcesquirrel/constants/blockchains.py
@@ -0,0 +1,14 @@
+ALGORAND = "algorand"
+CARDANO = "cardano"
+COINBASE = "base"
+ETHEREUM = "ethereum"
+POLYGON = "polygon"
+
+
+BLOCKCHAINS = [
+    ALGORAND,
+    CARDANO,
+    COINBASE,
+    ETHEREUM,
+    POLYGON,
+]
---
+++ packages/bookio-sourcesquirrel/bookio_sourcesquirrel-0.2.0-py3-none-any.whl/sourcesquirrel/constants/environments.py
@@ -0,0 +1,14 @@
+DEV = "dev"
+LOCAL = "local"
+PROD = "prod"
+QA = "qa"
+TEST = "test"
+
+
+ENVIRONMENTS = [
+    DEV,
+    LOCAL,
+    PROD,
+    QA,
+    TEST,
+]
