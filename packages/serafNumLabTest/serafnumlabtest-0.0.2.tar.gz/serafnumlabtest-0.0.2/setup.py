996166bea63295658763feefe5202c6e9b2d9e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 07:05:14 +0000

    Add package serafNumLabTest/0.0.2/serafnumlabtest-0.0.2.tar.gz

---
+++ packages/serafNumLabTest/serafnumlabtest-0.0.2.tar.gz/serafnumlabtest-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='serafNumLabTest',
+    version='0.0.2',
+    description='Перевод систем счисления',
+    license='MIT',
+    packages=find_packages(),
+    author='Serafim H',
+    author_email='i@drseraf.ru',
+    keywords=['sis'],
+    url='https://github.com'
+)
