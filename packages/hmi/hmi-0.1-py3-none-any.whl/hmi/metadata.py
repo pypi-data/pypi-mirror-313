dec226f6825b679aa39bf1cb84a446fb3646d020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 14:10:26 +0000

    Add package hmi/0.1/hmi-0.1-py3-none-any.whl

---
+++ packages/hmi/hmi-0.1-py3-none-any.whl/hmi/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'hmi'
+project = "Human Mobility Index"
+project_no_spaces = project.replace(' ', '')
+version = '0.8.2'
+description = "The HMI package is a python module that provides an interface to compute distances between locations based on Özak's (2010, 2012, 2015) Human Mobility Index."
+authors = ['Ömer Özak']
+authors_string = ', '.join(authors)
+emails = ['ozak@smu.edu']
+license = 'GNU'
+copyright = '(c)' + authors_string + '(2015)'
+url = 'http://omerozak.com/'
