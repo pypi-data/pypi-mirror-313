d2b71ea5e2acf8f0c3c2d9ad42fc94d9ec69ec00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 15:05:53 +0000

    Add package bigquery-advanced-utils/0.0.1.dev3/bigquery_advanced_utils-0.0.1.dev3-py3-none-any.whl

---
+++ packages/bigquery-advanced-utils/bigquery_advanced_utils-0.0.1.dev3-py3-none-any.whl/bigquery_advanced_utils/utils/__init__.py
@@ -0,0 +1,10 @@
+""" Definition of the main classes and functions for the package utils"""
+
+from .string import String
+from .custom_exceptions import (
+    InvalidArgumentToFunction,
+    ScheduledQueryIdWrongFormat,
+)
+from .numeric import Numeric
+
+from .custom_data_checks import CustomDataChecks
