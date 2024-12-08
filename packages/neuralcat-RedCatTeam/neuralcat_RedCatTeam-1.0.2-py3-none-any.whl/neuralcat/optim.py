84c067daaace46087f9254114bef1dbdecb9eb6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 04:06:04 +0000

    Add package neuralcat-RedCatTeam/1.0.2/neuralcat_RedCatTeam-1.0.2-py3-none-any.whl

---
+++ packages/neuralcat-RedCatTeam/neuralcat_RedCatTeam-1.0.2-py3-none-any.whl/neuralcat/optim.py
@@ -0,0 +1,103 @@
+import catnet.layers as layers
+import tqdm
+#tqdm is a progress bar, so we can see how far into the epoch we are
+from catnet.nn import Layer
+
+class SGD:
+    def __init__(self, lr):
+        self.lr = lr
+
+    def delta(self, param):
+        return param.gradient * self.lr
+
+    def __call__(self, model, loss):
+        loss.get_gradients()
+
+        for layer in model.layers:
+            if isinstance(layer, Layer):
+                layer.update(self)
+
+class Momentum:
+    def __init__(self, lr = 0.01, beta=0.9):
+        self.lr = lr
+        self.beta = beta
+        self.averages = {}
+
+    def momentum_average(self, prev, grad):
+        return (self.beta * prev) + (self.lr * grad)
+
+    def delta(self, param):
+        param_id = param.id
+
+        if param_id not in self.averages:
+            self.averages[param_id] = 0
+
+        self.averages[param_id] = self.momentum_average(self.averages[param_id], param.gradient)
+        return self.averages[param_id]
+
+    def __call__(self, model, loss):
+        loss.get_gradients()
+        for layer in model.layers:
+            if isinstance(layer, Layer):
+                layer.update(self)
+class RMSProp:
+    def __init__(self, lr = 0.01, beta=0.9, epsilon=10**-10):
+        self.lr = lr
+        self.beta = beta
+        self.epsilon = epsilon
+        self.averages = {}
+
+    def rms_average(self, prev, grad):
+        return self.beta * prev + (1 - self.beta) * (grad ** 2)
+
+    def delta(self, param):
+        param_id = param.id
+
+        if param_id not in self.averages:
+            self.averages[param_id] = 0
+
+        self.averages[param_id] = self.rms_average(self.averages[param_id], param.gradient)
+        return (self.lr / (self.averages[param_id] + self.epsilon) ** 0.5) * param.gradient
+
+    def __call__(self, model, loss):
+        loss.get_gradients()
+        for layer in model.layers:
+            if isinstance(layer, Layer):
+                layer.update(self)
+
+class Adam:
+    def __init__(self, lr = 0.01, beta1=0.9, beta2=0.999, epsilon=10**-8):
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.averages = {}
+        self.averages2 = {}
+
+    def rms_average(self, prev, grad):
+        return (self.beta2 * prev) + (1 - self.beta2) * (grad ** 2)
+
+    def momentum_average(self, prev, grad):
+        return (self.beta1 * prev) + ((1 - self.beta1) * grad)
+
+    def delta(self, param):
+        param_id = param.id
+
+        if param_id not in self.averages:
+            self.averages[param_id] = 0
+            self.averages2[param_id] = 0
+
+        self.averages[param_id] = self.momentum_average(self.averages[param_id], param.gradient)
+        self.averages2[param_id] = self.rms_average(self.averages2[param_id], param.gradient)
+
+        adjust1 = self.averages[param_id] / (1 - self.beta1)
+        adjust2 = self.averages2[param_id] / (1 - self.beta2)
+
+
+        return self.lr * (adjust1 / (adjust2 ** 0.5 + self.epsilon))
+
+    def __call__(self, model, loss):
+        loss.get_gradients()
+        for layer in model.layers:            
+            if isinstance(layer, Layer):
+                layer.update(self)
