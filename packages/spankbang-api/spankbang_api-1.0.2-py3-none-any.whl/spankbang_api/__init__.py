689911ad643837b92638958320434e66ce99b6b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 05:05:33 +0000

    Add package spankbang-api/1.0.2/spankbang_api-1.0.2-py3-none-any.whl

---
+++ packages/spankbang-api/spankbang_api-1.0.2-py3-none-any.whl/spankbang_api/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['Client', "Search", "Quality", "Callback", "Video", "consts", "default", "threaded", "FFMPEG",
+           "legacy_download"]
+
+from spankbang_api.spankbang_api import Client, Search, Quality, Callback, Video, default, threaded, FFMPEG, legacy_download
+from spankbang_api.modules import consts
