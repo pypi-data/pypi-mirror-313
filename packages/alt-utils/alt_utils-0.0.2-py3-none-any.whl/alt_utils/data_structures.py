a74836c86cc7d54da556982a9e80bade60566401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:21:21 +0000

    Add package alt-utils/0.0.2/alt_utils-0.0.2-py3-none-any.whl

---
+++ packages/alt-utils/alt_utils-0.0.2-py3-none-any.whl/alt_utils/data_structures.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass, fields, is_dataclass
+from typing import Self
+
+
+@dataclass
+class NestedDeserializableDataclass:
+    """
+    A dataclass that can be generated from a dict. Fields that are dataclasses
+    themselves are properly initialized as well.
+    """
+
+    @classmethod
+    def from_dict(cls, d: dict) -> Self:
+        d = d.copy()
+        for field in fields(cls):
+            if type(field.type) is not str:
+                if issubclass(field.type, NestedDeserializableDataclass):
+                    d[field.name] = field.type.from_dict(d[field.name])
+                elif is_dataclass(field.type):
+                    d[field.name] = field.type(**d[field.name])
+        return cls(**d)
