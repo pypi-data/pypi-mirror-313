bf1f95658334ac0e355ecb1bdc54bd206bf77e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:21:41 +0000

    Add package julep/1.45.0/julep-1.45.0.tar.gz

---
+++ packages/julep/julep-1.45.0.tar.gz/julep-1.45.0/src/julep/types/session_create_params.py
@@ -0,0 +1,44 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Optional
+from typing_extensions import Literal, TypedDict
+
+__all__ = ["SessionCreateParams", "RecallOptions"]
+
+
+class SessionCreateParams(TypedDict, total=False):
+    agent: Optional[str]
+
+    agents: Optional[List[str]]
+
+    auto_run_tools: bool
+
+    context_overflow: Optional[Literal["truncate", "adaptive"]]
+
+    metadata: Optional[object]
+
+    recall_options: Optional[RecallOptions]
+
+    render_templates: bool
+
+    situation: str
+
+    token_budget: Optional[int]
+
+    user: Optional[str]
+
+    users: Optional[List[str]]
+
+
+class RecallOptions(TypedDict, total=False):
+    confidence: float
+
+    hybrid_alpha: float
+
+    max_query_length: int
+
+    mode: Literal["hybrid", "vector", "text"]
+
+    num_search_messages: int
