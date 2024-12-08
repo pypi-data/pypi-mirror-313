4c40dc2a47b8772b221c3802a6cfd7c39431ff53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:20:54 +0000

    Add package alt-utils/0.0.2/alt_utils-0.0.2.tar.gz

---
+++ packages/alt-utils/alt_utils-0.0.2.tar.gz/alt_utils-0.0.2/tests/test_data_structures.py
@@ -0,0 +1,33 @@
+import unittest
+from dataclasses import dataclass
+
+from src.alt_utils import NestedDeserializableDataclass
+
+
+@dataclass
+class DatabaseConfig:
+    db: str
+    user: str
+    password: str
+    host: str
+    port: int = 5432
+
+
+@dataclass
+class Config(NestedDeserializableDataclass):
+    database: DatabaseConfig
+    misc: str
+
+
+class TestDataStructures(unittest.TestCase):
+    def test_nested_serializable_dataclass(self):
+        config_dict = {
+            "database": {"db": "dbname", "user": "username", "host": "hostname", "password": "rosebud"},
+            "misc": "something",
+        }
+        config_gen = Config.from_dict(config_dict)
+        config_manual = Config(
+            database=DatabaseConfig(db="dbname", user="username", host="hostname", password="rosebud", port=5432),
+            misc="something",
+        )
+        self.assertEqual(config_gen, config_manual)
