d65e24250814f9988f0d0448ca9a8f025012f83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 05:06:06 +0000

    Add package DevilFree/1.0/devilfree-1.0.tar.gz

---
+++ packages/DevilFree/devilfree-1.0.tar.gz/devilfree-1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+
+
+setup(
+	version='1.0',
+	name='DevilFree',
+	author='DevilFree',
+	description='A powerful encryption libraries in Python created by the @NasrPy',
+	packages=find_packages(),
+	classifiers=[
+	'Programming Language :: Python :: 3'
+
+	]
+
+)
+
+
+
+
+
