e1adf3adfd21e858a47b9b75be5107633f80295e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:21:01 +0000

    Add package qarray/1.4.1/qarray-1.4.1.tar.gz

---
+++ packages/qarray/qarray-1.4.1.tar.gz/qarray-1.4.1/examples/gui_sensor.py
@@ -0,0 +1,25 @@
+"""
+Author: b-vanstraaten
+Date: 07/12/2024
+"""
+
+from qarray import ChargeSensedDotArray
+from qarray.noise_models import WhiteNoise, TelegraphNoise, NoNoise
+
+# defining the capacitance matrices
+Cdd = [[0., 0.1], [0.1, 0.]]  # an (n_dot, n_dot) array of the capacitive coupling between dots
+Cgd = [[1., 0.1, 0.05], [0.1, 1., 0.05]]  # an (n_dot, n_gate) array of the capacitive coupling between gates and dots
+Cds = [[0.05, 0.00]]  # an (n_sensor, n_dot) array of the capacitive coupling between dots and sensors
+Cgs = [[0.06, 0.05, 1]]  # an (n_sensor, n_gate) array of the capacitive coupling between gates and sensor dots
+
+noise_model = WhiteNoise(amplitude=1e-2)
+
+# creating the model
+model = ChargeSensedDotArray(
+    Cdd=Cdd, Cgd=Cgd, Cds=Cds, Cgs=Cgs,
+    coulomb_peak_width=0.1, T=0,
+    implementation='rust', algorithm='default',
+    noise_model=noise_model
+)
+initial_dac_values = [0, 0, 0.5]
+model.gui(initial_dac_values = initial_dac_values)
