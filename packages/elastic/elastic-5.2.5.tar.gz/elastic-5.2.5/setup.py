a0705fad502d1a1f99007beee6b6cd6aa0826eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:21:45 +0000

    Add package elastic/5.2.5/elastic-5.2.5.tar.gz

---
+++ packages/elastic/elastic-5.2.5.tar.gz/elastic-5.2.5/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+from setuptools import setup
+from setuptools_scm import ScmVersion
+from setuptools_scm.version import get_no_local_node
+
+def current_version(version: ScmVersion) -> str:
+    from setuptools_scm.version import guess_next_version
+    
+    delim = '.'
+    if version.branch in ("master", "main"):
+        delim = '.'
+    if version.branch in ("dev", "devel"):
+        delim = '-dev'
+    if version.branch in ("fix", "hotfix"):
+        delim = '-post'
+
+    return version.format_next_version(guess_next_version,
+                                        "{tag}"+ delim +"{distance}")
+
+
+setup(use_scm_version={"version_scheme": current_version, 
+                        "local_scheme": get_no_local_node})
+
