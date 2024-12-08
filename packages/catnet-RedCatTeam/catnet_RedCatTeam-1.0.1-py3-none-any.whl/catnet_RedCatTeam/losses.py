a013dd8a207c0983750cc6a56c3178778695b37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:21:20 +0000

    Add package catnet-RedCatTeam/1.0.1/catnet_RedCatTeam-1.0.1-py3-none-any.whl

---
+++ packages/catnet-RedCatTeam/catnet_RedCatTeam-1.0.1-py3-none-any.whl/catnet_RedCatTeam/losses.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class MSE:
+    def __call__(self, pred, y):
+        self.error = pred - y
+        return np.mean(self.error ** 2)
+
+    def backward(self):
+        return 2 * (1 / self.error.shape[-1]) * self.error 
