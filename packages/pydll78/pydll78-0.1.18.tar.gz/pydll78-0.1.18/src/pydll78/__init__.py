ca87f05ea5eb4680e81ad15e8a6c553d7c5cf39d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:20:56 +0000

    Add package pydll78/0.1.18/pydll78-0.1.18.tar.gz

---
+++ packages/pydll78/pydll78-0.1.18.tar.gz/pydll78-0.1.18/src/pydll78/__init__.py
@@ -0,0 +1,17 @@
+
+# 导出包级别的模块
+from .module_loader import ModuleLoader
+from .kafka78 import Kafka78
+from .file_updater import FileUpdater
+from .playwright78 import Playwright78
+from .config78 import Config78
+
+# 如果有常用的变量或者函数，也可以在这里导出
+
+__all__ = [
+    'ModuleLoader',
+    'Kafka78',
+    'FileUpdater',
+    'Playwright78',
+    'Config78'
+]
