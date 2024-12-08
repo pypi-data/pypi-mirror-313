248e222899ac94de14e432b0264d1d643182a716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 04:06:11 +0000

    Add package dspsim/0.1.16/dspsim-0.1.16-cp312-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/dspsim/dspsim-0.1.16-cp312-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/dspsim/include/dspsim/dspsim.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <dspsim/context.h>
+#include <dspsim/model.h>
+#include <dspsim/signal.h>
+#include <dspsim/clock.h>
+#include <dspsim/port.h>
+
+#include <string>
+
+namespace dspsim
+{
+    // std::string hello_from_bin() { return "DSPSim!"; }
+    std::string hello_from_bin();
+} // namespace dspsim
