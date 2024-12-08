b71cb56e47a99ef745c6ebfafed02a822ed322de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:21:00 +0000

    Add package nicetrack/0.1.0/nicetrack-0.1.0-py3-none-any.whl

---
+++ packages/nicetrack/nicetrack-0.1.0-py3-none-any.whl/nicetrack/geo.py
@@ -0,0 +1,213 @@
+"""
+Created on 16.08.2023
+
+@author: wf
+"""
+
+import logging
+import math
+import os
+from dataclasses import dataclass
+from datetime import datetime
+from pathlib import Path
+from typing import List, Tuple
+
+import gpxpy
+from OSMPythonTools.cachingStrategy import JSON, CachingStrategy
+from OSMPythonTools.nominatim import Nominatim
+
+
+@dataclass
+class Trackpoint:
+    """
+    a 4D geographic trackpoint
+    """
+
+    lat: float
+    lon: float
+    elevation: float = None
+    timestamp: datetime = None
+
+    def decimal_to_dms(self, decimal_degree: float):
+        degrees = int(decimal_degree)
+        minutes = int((decimal_degree - degrees) * 60)
+        seconds = (decimal_degree - degrees - minutes / 60) * 3600.0
+        return degrees, minutes, seconds
+
+    def lat_lon_to_dms_string(self, latitude: float, longitude: float) -> str:
+        lat_degree, lat_minute, lat_second = self.decimal_to_dms(latitude)
+        lon_degree, lon_minute, lon_second = self.decimal_to_dms(longitude)
+        lat_direction = "N" if latitude >= 0 else "S"
+        lon_direction = "E" if longitude >= 0 else "W"
+        lat_str = f"{abs(lat_degree)}° {abs(lat_minute)}' {abs(lat_second):.4f}'' {lat_direction}"
+        lon_str = f"{abs(lon_degree)}° {abs(lon_minute)}' {abs(lon_second):.4f}'' {lon_direction}"
+        return lat_str, lon_str
+
+    def as_dms(self) -> str:
+        dms = self.lat_lon_to_dms_string(self.lat, self.lon)
+        return dms
+
+    def as_google_maps_link(self) -> str:
+        link = f"https://maps.google.com/?q={self.lat},{self.lon}"
+        return link
+
+    def as_google_maps_anchor(self) -> str:
+        google_maps_link = self.as_google_maps_link()
+        lat_str, lon_str = self.as_dms()
+        a = f"<a href='{google_maps_link}' title='google maps' target='_blank'>{lat_str}{lon_str}</a>"
+        return a
+
+    def get_details(self, nominatim):
+        """
+        get the location details via nominatim
+        """
+        result = nominatim.query(f"{self.lat}, {self.lon}").toJSON()
+        location = result[0]
+        details = location.get("display_name", "Location not found")
+        return details
+
+    def get_info(self, nominatim, with_details: bool = True) -> str:
+        if with_details:
+            details = self.get_details(nominatim)
+        else:
+            details = ""
+        geo_date_html = "" if self.timestamp is None else f"{self.timestamp}<br>\n"
+        google_maps_a = self.as_google_maps_anchor()
+        info = f"{geo_date_html}{details}{google_maps_a}"
+        return info
+
+
+class GeoPath:
+    """
+    a 3D Path of geographic coordinates in lat/lon notation
+    """
+
+    def __init__(self, name: str = None, cacheDir: str = None):
+        self.name = name
+        self.path: List[Trackpoint] = []
+        if cacheDir is None:
+            home = str(Path.home())
+            cacheDir = f"{home}/.nominatim"
+        self.cacheDir = cacheDir
+        if not os.path.exists(self.cacheDir):
+            os.makedirs(self.cacheDir)
+        logging.getLogger("OSMPythonTools").setLevel(logging.ERROR)
+        CachingStrategy.use(JSON, cacheDir=self.cacheDir)
+        self.nominatim = Nominatim()
+
+    @classmethod
+    def from_points(cls, *points) -> "GeoPath":
+        """
+        get a geopath for the given points
+        """
+        geo_path = cls()
+        for point in points:
+            lat, lon = point
+            geo_path.add_point(lat, lon)
+        return geo_path
+
+    @classmethod
+    def from_gpx(cls, gpx_data: str) -> "GeoPath":
+        geo_path = cls()
+        gpx = gpxpy.parse(gpx_data)
+        for track in gpx.tracks:
+            for segment in track.segments:
+                for point in segment.points:
+                    geo_path.add_point(
+                        point.latitude, point.longitude, point.elevation, point.time
+                    )
+        return geo_path
+
+    def add_point(
+        self,
+        lat: float,
+        lon: float,
+        elevation: float = None,
+        timestamp: datetime = None,
+    ) -> None:
+        self.path.append(Trackpoint(lat, lon, elevation, timestamp))
+
+    def get_path(self) -> List[Trackpoint]:
+        return self.path
+
+    def as_tuple_list(self) -> List[Tuple[float, float]]:
+        return [(point.lat, point.lon) for point in self.path]
+
+    def haversine_distance(self, point1: Trackpoint, point2: Trackpoint) -> float:
+        R = 6371.0
+        lat1 = math.radians(point1.lat)
+        lon1 = math.radians(point1.lon)
+        lat2 = math.radians(point2.lat)
+        lon2 = math.radians(point2.lon)
+        dlat = lat2 - lat1
+        dlon = lon2 - lon1
+        a = (
+            math.sin(dlat / 2) ** 2
+            + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
+        )
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+        distance = R * c
+        return distance
+
+    def validate_index(self, index: int):
+        max_index = len(self.path) - 1
+        if index < 0 or index > max_index:
+            raise ValueError(f"Invalid index {index}: valid range is 0-{max_index}")
+
+    def distance(self, index1: int, index2: int) -> float:
+        self.validate_index(index1)
+        self.validate_index(index2)
+        index1, index2 = min(index1, index2), max(index1, index2)
+        total_distance = 0
+        for i in range(index1, index2):
+            total_distance += self.haversine_distance(self.path[i], self.path[i + 1])
+        return total_distance
+
+    def total_distance(self) -> float:
+        total_distance = 0
+        if len(self.path) > 1:
+            total_distance = self.distance(0, len(self.path) - 1)
+        return total_distance
+
+    def as_dms(self, index: int) -> Tuple[str, str]:
+        self.validate_index(index)
+        tp = self.path[index]
+        dms = tp.as_dms()
+        return dms
+
+    def as_google_maps_link(self, index: int) -> str:
+        self.validate_index(index)
+        tp = self.path[index]
+        link = tp.as_google_maps_link()
+        return link
+
+    def get_start_location_details(self) -> str:
+        if len(self.path) < 1:
+            return "No points in path"
+        tp = self.path[0]
+        details = tp.get_details(self.nominatim)
+        return details
+
+    def get_video_frame_index(self, index: int, fps: int) -> int:
+        """
+        Return a video frame index based on a geo_path index and fps parameter using the timestamps.
+
+        Args:
+        - geo_index (int): The specific index on the GeoPath.
+        - fps (int): Frames per second of the video.
+
+        Returns:
+        - int: Corresponding frame index.
+        """
+        self.validate_index(index)
+
+        start_time = self.path[0].timestamp
+        target_time = self.path[index].timestamp
+
+        # Calculate time difference in seconds
+        delta_seconds = (target_time - start_time).total_seconds()
+
+        # Calculate frame index based on the time difference and fps
+        frame_index = int(delta_seconds * fps)
+
+        return frame_index
---
+++ packages/nicetrack/nicetrack-0.1.0-py3-none-any.whl/nicetrack/video_stream.py
@@ -0,0 +1,101 @@
+"""
+Created on 27.08.2023
+
+@author: wf
+"""
+
+import os
+
+import cv2
+from fastapi import Header, HTTPException
+from starlette.responses import FileResponse, StreamingResponse
+
+
+class VideoStream:
+    """
+    wrapper to stream a local file as a video
+    """
+
+    def __init__(self, video_path):
+        self.video_path = video_path
+        self.video_size = os.path.getsize(video_path)
+
+    def generate_frames(self, start_time: float = 0.0, fps: float = 30.0):
+        cap = cv2.VideoCapture(self.video_path)
+
+        # Calculate the starting frame based on start_time and fps
+        start_frame = int(start_time * fps)
+        cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                cap.release()
+                break
+            _, buffer = cv2.imencode(".jpg", frame)
+            frame = buffer.tobytes()
+            yield (
+                b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n"
+            )
+
+    async def video_feed(self, start_time: float = 0.0, fps: float = 30.0):
+        frame_gen = self.generate_frames(start_time=start_time, fps=fps)
+        return StreamingResponse(
+            frame_gen, media_type="multipart/x-mixed-replace; boundary=frame"
+        )
+
+    def parse_range_header(self, range_header: str) -> tuple:
+        """
+        Parse the Range header to determine the part of the file to stream.
+        Returns a tuple of (start_byte, end_byte).
+        """
+        start_byte, end_byte = None, None
+
+        if range_header:
+            try:
+                byte1, byte2 = range_header.replace("bytes=", "").split("-")
+                start_byte = int(byte1)
+                end_byte = int(byte2) if byte2 else None
+            except ValueError:
+                raise HTTPException(status_code=400, detail="Malformed range header")
+
+        return start_byte, end_byte
+
+    def stream_partial_content(self, start: int, end: int) -> StreamingResponse:
+        """
+        Stream the content between the start and end bytes.
+        """
+
+        def content_generator():
+            with open(self.video_path, "rb") as video:
+                video.seek(start)
+                while start <= end:
+                    chunk_size = min(8192, end - start + 1)
+                    buffer = video.read(chunk_size)
+                    if not buffer:
+                        break
+                    start += chunk_size
+                    yield buffer
+
+        content_length = end - start + 1
+        headers = {
+            "Content-Range": f"bytes {start}-{end}/{self.video_size}",
+            "Accept-Ranges": "bytes",
+            "Content-Length": str(content_length),
+            "Content-Type": "video/mp4",
+        }
+
+        return StreamingResponse(content_generator(), status_code=206, headers=headers)
+
+    def get_video(self, range_header: str = Header(default=None)):
+        start, end = self.parse_range_header(range_header)
+
+        if not start and not end:
+            # Just serve the whole file if no range
+            return FileResponse(self.video_path, media_type="video/mp4")
+
+        if start >= self.video_size:
+            raise HTTPException(status_code=416, detail="Range not satisfiable")
+
+        end = end or self.video_size - 1
+        return self.stream_partial_content(start, end)
