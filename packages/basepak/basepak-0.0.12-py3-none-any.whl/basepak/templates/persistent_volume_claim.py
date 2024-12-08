edcd53d3b3c7dc387f97fb2c3b4d3f008e8a0dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:21:02 +0000

    Add package basepak/0.0.12/basepak-0.0.12-py3-none-any.whl

---
+++ packages/basepak/basepak-0.0.12-py3-none-any.whl/basepak/templates/persistent_volume_claim.py
@@ -0,0 +1,28 @@
+from collections.abc import Mapping
+
+
+def generate_template(params: Mapping) -> str:
+    from .. import configer, consts
+    user_labels = params.get('METADATA', {}).get('labels', {}) | params.get('metadata', {}).get('labels', {})
+    user_labels.setdefault(consts.IS_PURGEABLE_KEY, 'false')
+    template_persistent_volume_claim = {
+        'apiVersion': 'v1',
+        'kind': 'PersistentVolumeClaim',
+        'metadata': {
+            'name': params['PERSISTENT_VOLUME_CLAIM_NAME'],
+            'namespace': params['NAMESPACE'],
+            'labels': consts.DEFAULT_LABELS | user_labels,
+        },
+        'spec': {
+            'storageClassName': params.get('STORAGE_CLASS', ''),
+            'accessModes': params.get('ACCESS_MODES', ['ReadWriteMany']),
+            'resources': {
+                'requests': {
+                    'storage': params.get('DISK_REQUIRED', '1Gi')
+                }
+            }
+        }
+    }
+
+    configer.generate(template_persistent_volume_claim, params['GENERATED_MANIFESTS_FOLDER'])
+    return template_persistent_volume_claim['metadata']['name']
