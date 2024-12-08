622ef704e6384453d034426caf3eb1ad62a27b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:21:45 +0000

    Add package dappier/0.1.3/dappier-0.1.3-py3-none-any.whl

---
+++ packages/dappier/dappier-0.1.3-py3-none-any.whl/dappier/types/ai_model_request.py
@@ -0,0 +1,5 @@
+class AIModelRequest:
+    def __init__(self, query: str):
+        self.query = query
+    
+    
---
+++ packages/dappier/dappier-0.1.3-py3-none-any.whl/dappier/types/ai_model_response.py
@@ -0,0 +1,3 @@
+class AIModelResponse:
+    def __init__(self, message: str):
+        self.message = message
