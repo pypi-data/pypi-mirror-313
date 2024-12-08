500c36e30114c66e067f7b365c01dee590471992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 04:06:02 +0000

    Add package euclidean-algorithm/0.1.1/euclidean_algorithm-0.1.1-py3-none-any.whl

---
+++ packages/euclidean-algorithm/euclidean_algorithm-0.1.1-py3-none-any.whl/euclidean_algorithm/euclidean_algorithm/__init__.py
@@ -0,0 +1,21 @@
+"""
+:author: Hspu1
+:license: MIT
+:copyright: (c) 2024 Hspu1
+"""
+
+__all__ = (
+    "euclidean_algorithm",
+    "EuclideanAlgorithmValueError",
+    "EuclideanAlgorithmLengthError"
+)
+
+
+from .euclidean_algorithm import euclidean_algorithm
+from .exceptions import (
+    EuclideanAlgorithmValueError, EuclideanAlgorithmLengthError
+)
+
+__author__ = "Hspu1"
+__version__ = "0.1.1"
+__email__ = "rumyantsev1m0s1@gmail.com"
