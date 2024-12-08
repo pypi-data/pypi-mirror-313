630aec22c1a643c13896c8b40a5429f2b531546e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:20:55 +0000

    Add package ellipsis/3.1.50/ellipsis-3.1.50-py3-none-any.whl

---
+++ packages/ellipsis/ellipsis-3.1.50-py3-none-any.whl/ellipsis/path/vector/__init__.py
@@ -0,0 +1,9 @@
+from ellipsis.path.vector.root  import editFilter
+from ellipsis.path.vector.root  import add
+
+
+from ellipsis.path.vector import timestamp
+from ellipsis.path.vector import style
+from ellipsis.path.vector import featureProperty
+
+
