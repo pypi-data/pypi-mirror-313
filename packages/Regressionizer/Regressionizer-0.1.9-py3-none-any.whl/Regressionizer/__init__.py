79fd6a9632989dbf6a9d8123853a9587e286f287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:21:40 +0000

    Add package Regressionizer/0.1.9/Regressionizer-0.1.9-py3-none-any.whl

---
+++ packages/Regressionizer/Regressionizer-0.1.9-py3-none-any.whl/Regressionizer/__init__.py
@@ -0,0 +1,3 @@
+"""Regressonizer package for linear and quantile regression workflows."""
+from Regressionizer.Regressionizer import Regressionizer
+from Regressionizer.Regressionizer import to_datetime_index, dates_to_seconds
