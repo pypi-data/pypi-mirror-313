6b4639320502d9b7efbc0b1036397d2c98993821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:21:09 +0000

    Add package dj-multidomain/0.4.0/dj_multidomain-0.4.0.tar.gz

---
+++ packages/dj-multidomain/dj_multidomain-0.4.0.tar.gz/dj_multidomain-0.4.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dj-multidomain',
+    version='0.4.0',
+    packages=find_packages(),
+    install_requires=[
+        'django>=4.0',
+        'publicsuffix2>=2.0.2',
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Framework :: Django',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
+        'Framework :: Django :: 5.1',
+    ],
+    author='Ferhat Mousavi',
+    author_email='ferhat.mousavi@gmail.com',
+    description='A Django middleware module to handle multiple domains.',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/ferhat-mousavi/dj-multidomain',
+)
